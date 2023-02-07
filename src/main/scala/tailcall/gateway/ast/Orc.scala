687b5a15e971598f5692dc3755dedadeb4dad4d5
Tushar Mathur <tusharmath@gmail.com>
2023-02-07 18:14:49 +0530

    style: whitespace changes

--- src/main/scala/tailcall/gateway/ast/Orc.scala
+++ src/main/scala/tailcall/gateway/ast/Orc.scala
@@ -16,22 +16,22 @@ sealed trait Orc {
 }
 
 object Orc {
-  final case class OrchValue(dynamic: DynamicValue)          extends Orc
+  final case class OrchValue(dynamic: DynamicValue)         extends Orc
   final case class OrchList(list: List[Orc])                extends Orc
   final case class OrchObject(fields: Map[String, Orc])     extends Orc
-  final case class OrchEndpoint(endpoint: Endpoint)          extends Orc
-  final case class OrchPipe(left: Orc, right: Orc)         extends Orc
-  final case class OrchPath(path: List[String])              extends Orc
+  final case class OrchEndpoint(endpoint: Endpoint)         extends Orc
+  final case class OrchPipe(left: Orc, right: Orc)          extends Orc
+  final case class OrchPath(path: List[String])             extends Orc
   final case class OrchApplySpec(path: List[(String, Orc)]) extends Orc
-  final case class OrchConcat(left: Orc, right: Orc)       extends Orc
-  final case class OrchBatch(endpoint: Orc, groupBy: Orc)  extends Orc
+  final case class OrchConcat(left: Orc, right: Orc)        extends Orc
+  final case class OrchBatch(endpoint: Orc, groupBy: Orc)   extends Orc
 
-  def obj(fields: (String, Orc)*): Orc         = OrchObject(fields.toMap)
-  def endpoint(endpoint: Endpoint): Orc         = OrchEndpoint(endpoint)
-  def spec(path: (String, Orc)*): Orc          = OrchApplySpec(path.toList)
+  def obj(fields: (String, Orc)*): Orc        = OrchObject(fields.toMap)
+  def endpoint(endpoint: Endpoint): Orc       = OrchEndpoint(endpoint)
+  def spec(path: (String, Orc)*): Orc         = OrchApplySpec(path.toList)
   def concat(left: Orc, right: Orc): Orc      = OrchConcat(left, right)
-  def path(path: String*): Orc                  = OrchPath(path.toList)
-  def context(path: String*): Orc               = OrchPath("context" :: path.toList)
+  def path(path: String*): Orc                = OrchPath(path.toList)
+  def context(path: String*): Orc             = OrchPath("context" :: path.toList)
   def batch(endpoint: Orc, groupBy: Orc): Orc = OrchBatch(endpoint, groupBy)
 
   implicit val schema: Schema[Orc] = DeriveSchema.gen[Orc]
