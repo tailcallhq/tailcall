7e5ed9e07df217485e565f844721ee31411494a5
amitksingh1490 <amitksingh1490@gmail.com>
2023-02-20 22:32:24 +0530

    refactor:  move mat and logic in a object

--- src/main/scala/tailcall/gateway/remote/operations/MathOps.scala
+++ src/main/scala/tailcall/gateway/remote/operations/MathOps.scala
@@ -5,19 +5,24 @@ import tailcall.gateway.remote.Remote
 
 trait MathOps {
   implicit final class RemoteMathOps[A](val self: Remote[A]) {
-    def +(other: Remote[A])(implicit ev: Numeric[A]): Remote[A] = Remote(Lambda.add(self.toLambda, other.toLambda))
+    def +(other: Remote[A])(implicit ev: Numeric[A]): Remote[A] = Remote(Lambda.math.add(self.toLambda, other.toLambda))
 
-    def -(other: Remote[A])(implicit ev: Numeric[A]): Remote[A] = Remote(Lambda.subtract(self.toLambda, other.toLambda))
+    def -(other: Remote[A])(implicit ev: Numeric[A]): Remote[A] =
+      Remote(Lambda.math.subtract(self.toLambda, other.toLambda))
 
-    def *(other: Remote[A])(implicit ev: Numeric[A]): Remote[A] = Remote(Lambda.multiply(self.toLambda, other.toLambda))
+    def *(other: Remote[A])(implicit ev: Numeric[A]): Remote[A] =
+      Remote(Lambda.math.multiply(self.toLambda, other.toLambda))
 
-    def /(other: Remote[A])(implicit ev: Numeric[A]): Remote[A] = Remote(Lambda.divide(self.toLambda, other.toLambda))
+    def /(other: Remote[A])(implicit ev: Numeric[A]): Remote[A] =
+      Remote(Lambda.math.divide(self.toLambda, other.toLambda))
 
-    def %(other: Remote[A])(implicit ev: Numeric[A]): Remote[A] = Remote(Lambda.modulo(self.toLambda, other.toLambda))
+    def %(other: Remote[A])(implicit ev: Numeric[A]): Remote[A] =
+      Remote(Lambda.math.modulo(self.toLambda, other.toLambda))
 
-    def >(other: Remote[A])(implicit ev: Numeric[A]): Remote[Boolean] = Remote(Lambda.gt(self.toLambda, other.toLambda))
+    def >(other: Remote[A])(implicit ev: Numeric[A]): Remote[Boolean] =
+      Remote(Lambda.math.gt(self.toLambda, other.toLambda))
 
-    def unary_-(implicit ev: Numeric[A]): Remote[A] = Remote(Lambda.negate(self.toLambda))
+    def unary_-(implicit ev: Numeric[A]): Remote[A] = Remote(Lambda.math.negate(self.toLambda))
   }
 
 }
