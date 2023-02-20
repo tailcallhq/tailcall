0ce1558c849f3a36861114169c556d090ca0a4a9
amitksingh1490 <amitksingh1490@gmail.com>
2023-02-20 22:32:24 +0530

    refactor:  move mat and logic in a object

--- src/main/scala/tailcall/gateway/lambda/Lambda.scala
+++ src/main/scala/tailcall/gateway/lambda/Lambda.scala
@@ -17,52 +17,56 @@ sealed trait Lambda[-A, +B] {
 
 object Lambda {
 
-  def and[A](left: A ~> Boolean, right: A ~> Boolean): A ~> Boolean =
-    Lambda.unsafe.attempt[A, Boolean] { ctx =>
-      DynamicEval.Logical(Logical.Binary(left.compile(ctx), right.compile(ctx), Logical.Binary.And))
-    }
+  object logic {
+    def and[A](left: A ~> Boolean, right: A ~> Boolean): A ~> Boolean =
+      Lambda.unsafe.attempt[A, Boolean] { ctx =>
+        DynamicEval.Logical(Logical.Binary(left.compile(ctx), right.compile(ctx), Logical.Binary.And))
+      }
+
+    def diverge[A, B](cond: A ~> Boolean, isTrue: A ~> B, isFalse: A ~> B): A ~> B =
+      Lambda.unsafe.attempt[A, B] { ctx =>
+        DynamicEval
+          .Logical(Logical.Unary(cond.compile(ctx), Logical.Unary.Diverge(isTrue.compile(ctx), isFalse.compile(ctx))))
+      }
+
+    def not[A](a: A ~> Boolean): A ~> Boolean =
+      Lambda.unsafe.attempt[A, Boolean](ctx => DynamicEval.Logical(Logical.Unary(a.compile(ctx), Logical.Unary.Not)))
+
+    def or[A](left: A ~> Boolean, right: A ~> Boolean): A ~> Boolean =
+      Lambda.unsafe.attempt[A, Boolean] { ctx =>
+        DynamicEval.Logical(Logical.Binary(left.compile(ctx), right.compile(ctx), Logical.Binary.Or))
+      }
+
+  }
 
   def apply[A, B](b: B)(implicit ctor: Constructor[B]): A ~> B =
     Lambda.unsafe.attempt(_ => Literal(ctor.schema.toDynamic(b), ctor.asInstanceOf[Constructor[Any]]))
 
-  def diverge[A, B](cond: A ~> Boolean, isTrue: A ~> B, isFalse: A ~> B): A ~> B =
-    Lambda.unsafe.attempt[A, B] { ctx =>
-      DynamicEval
-        .Logical(Logical.Unary(cond.compile(ctx), Logical.Unary.Diverge(isTrue.compile(ctx), isFalse.compile(ctx))))
-    }
+  object math {
+    def subtract[A, B](a: A ~> B, b: A ~> B)(implicit ev: Numeric[B]): A ~> B = add(a, negate(b))
 
-  def divide[A, B](a: A ~> B, b: A ~> B)(implicit ev: Numeric[B]): A ~> B =
-    Lambda.unsafe.attempt(ctx => Math(Math.Binary(a.compile(ctx), b.compile(ctx), Math.Binary.Divide), ev.any))
+    def add[A, B](a: A ~> B, b: A ~> B)(implicit ev: Numeric[B]): A ~> B    =
+      Lambda.unsafe.attempt(ctx => Math(Math.Binary(a.compile(ctx), b.compile(ctx), Math.Binary.Add), ev.any))
+    def divide[A, B](a: A ~> B, b: A ~> B)(implicit ev: Numeric[B]): A ~> B =
+      Lambda.unsafe.attempt(ctx => Math(Math.Binary(a.compile(ctx), b.compile(ctx), Math.Binary.Divide), ev.any))
+
+    def modulo[A, B](a: A ~> B, b: A ~> B)(implicit ev: Numeric[B]): A ~> B =
+      Lambda.unsafe.attempt(ctx => Math(Math.Binary(a.compile(ctx), b.compile(ctx), Math.Binary.Modulo), ev.any))
+
+    def multiply[A, B](a: A ~> B, b: A ~> B)(implicit ev: Numeric[B]): A ~> B =
+      Lambda.unsafe.attempt(ctx => Math(Math.Binary(a.compile(ctx), b.compile(ctx), Math.Binary.Multiply), ev.any))
+
+    def gt[A, B](a: A ~> B, b: A ~> B)(implicit ev: Numeric[B]): A ~> Boolean =
+      Lambda.unsafe.attempt(ctx => Math(Math.Binary(a.compile(ctx), b.compile(ctx), Math.Binary.GreaterThan), ev.any))
+
+    def negate[A, B](ab: A ~> B)(implicit ev: Numeric[B]): A ~> B =
+      Lambda.unsafe.attempt(ctx => Math(Math.Unary(ab.compile(ctx), Math.Unary.Negate), ev.any))
+  }
 
   def equalTo[A, B](a: A ~> B, b: A ~> B)(implicit ev: Equatable[B]): A ~> Boolean =
     Lambda.unsafe.attempt(ctx => EqualTo(a.compile(ctx), b.compile(ctx), ev.any))
 
-  def gt[A, B](a: A ~> B, b: A ~> B)(implicit ev: Numeric[B]): A ~> Boolean =
-    Lambda.unsafe.attempt(ctx => Math(Math.Binary(a.compile(ctx), b.compile(ctx), Math.Binary.GreaterThan), ev.any))
-
   def identity[A]: A ~> A = Lambda.unsafe.attempt[A, A](_ => DynamicEval.Identity)
-
-  def modulo[A, B](a: A ~> B, b: A ~> B)(implicit ev: Numeric[B]): A ~> B =
-    Lambda.unsafe.attempt(ctx => Math(Math.Binary(a.compile(ctx), b.compile(ctx), Math.Binary.Modulo), ev.any))
-
-  def multiply[A, B](a: A ~> B, b: A ~> B)(implicit ev: Numeric[B]): A ~> B =
-    Lambda.unsafe.attempt(ctx => Math(Math.Binary(a.compile(ctx), b.compile(ctx), Math.Binary.Multiply), ev.any))
-
-  def not[A](a: A ~> Boolean): A ~> Boolean =
-    Lambda.unsafe.attempt[A, Boolean](ctx => DynamicEval.Logical(Logical.Unary(a.compile(ctx), Logical.Unary.Not)))
-
-  def or[A](left: A ~> Boolean, right: A ~> Boolean): A ~> Boolean =
-    Lambda.unsafe.attempt[A, Boolean] { ctx =>
-      DynamicEval.Logical(Logical.Binary(left.compile(ctx), right.compile(ctx), Logical.Binary.Or))
-    }
-
-  def subtract[A, B](a: A ~> B, b: A ~> B)(implicit ev: Numeric[B]): A ~> B = add(a, negate(b))
-
-  def add[A, B](a: A ~> B, b: A ~> B)(implicit ev: Numeric[B]): A ~> B =
-    Lambda.unsafe.attempt(ctx => Math(Math.Binary(a.compile(ctx), b.compile(ctx), Math.Binary.Add), ev.any))
-
-  def negate[A, B](ab: A ~> B)(implicit ev: Numeric[B]): A ~> B =
-    Lambda.unsafe.attempt(ctx => Math(Math.Unary(ab.compile(ctx), Math.Unary.Negate), ev.any))
 
   def fromFunction[A, B](f: Remote[A] => Remote[B]): A ~> B = ???
 
--- src/main/scala/tailcall/gateway/remote/operations/BooleanOps.scala
+++ src/main/scala/tailcall/gateway/remote/operations/BooleanOps.scala
@@ -5,13 +5,13 @@ import tailcall.gateway.remote.Remote
 
 trait BooleanOps {
   implicit final class RemoteBooleanOps(val self: Remote[Boolean]) {
-    def &&(other: Remote[Boolean]): Remote[Boolean] = Remote(Lambda.and(self.toLambda, other.toLambda))
+    def &&(other: Remote[Boolean]): Remote[Boolean] = Remote(Lambda.logic.and(self.toLambda, other.toLambda))
 
-    def ||(other: Remote[Boolean]): Remote[Boolean] = Remote(Lambda.or(self.toLambda, other.toLambda))
+    def ||(other: Remote[Boolean]): Remote[Boolean] = Remote(Lambda.logic.or(self.toLambda, other.toLambda))
 
-    def unary_! : Remote[Boolean] = Remote(Lambda.not(self.toLambda))
+    def unary_! : Remote[Boolean] = Remote(Lambda.logic.not(self.toLambda))
 
     def diverge[A](isTrue: Remote[A], isFalse: Remote[A]): Remote[A] =
-      Remote(Lambda.diverge(self.toLambda, isTrue.toLambda, isFalse.toLambda))
+      Remote(Lambda.logic.diverge(self.toLambda, isTrue.toLambda, isFalse.toLambda))
   }
 }
