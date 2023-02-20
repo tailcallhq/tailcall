4a4a69b912a8b70c9be3f1ae1532a693b8e1a85d
Tushar Mathur <tusharmath@gmail.com>
2023-02-20 19:21:34 +0530

    refactor: add lambda package
    --wip-- [skip ci]
    
    --wip-- [skip ci]
    
    --wip-- [skip ci]
    
    --wip-- [skip ci]

---
+++ src/main/scala/tailcall/gateway/lambda/DynamicEval.scala
@@ -0,0 +1,51 @@
+package tailcall.gateway.lambda
+
+import zio.schema.{DeriveSchema, DynamicValue, Schema}
+
+sealed trait DynamicEval
+
+object DynamicEval {
+  // scalafmt: { maxColumn = 240 }
+  final case class Literal(value: DynamicValue, ctor: Constructor[Any])                extends DynamicEval
+  final case class EqualTo(left: DynamicEval, right: DynamicEval, tag: Equatable[Any]) extends DynamicEval
+  final case class Math(operation: Math.Operation, tag: Numeric[Any])                  extends DynamicEval
+  object Math {
+    sealed trait Operation
+
+    final case class Binary(left: DynamicEval, right: DynamicEval, operation: Binary.Operation) extends Operation
+    object Binary {
+      sealed trait Operation
+      case object Add         extends Operation
+      case object Multiply    extends Operation
+      case object Divide      extends Operation
+      case object Modulo      extends Operation
+      case object GreaterThan extends Operation
+    }
+
+    final case class Unary(value: DynamicEval, operation: Unary.Operation) extends Operation
+    object Unary {
+      sealed trait Operation
+      case object Negate extends Operation
+    }
+  }
+
+  final case class Logical(operation: Logical.Operation) extends DynamicEval
+  object Logical {
+    sealed trait Operation
+    final case class Binary(left: DynamicEval, right: DynamicEval, operation: Binary.Operation) extends Operation
+    object Binary {
+      sealed trait Operation
+      case object And extends Operation
+      case object Or  extends Operation
+    }
+
+    final case class Unary(value: DynamicEval, operation: Unary.Operation) extends Operation
+    object Unary {
+      sealed trait Operation
+      case object Not                                                     extends Operation
+      final case class Diverge(isTrue: DynamicEval, isFalse: DynamicEval) extends Operation
+    }
+  }
+
+  implicit val schema: Schema[DynamicEval] = DeriveSchema.gen[DynamicEval]
+}
---
+++ src/main/scala/tailcall/gateway/lambda/Equatable.scala
@@ -0,0 +1,12 @@
+package tailcall.gateway.lambda
+
+sealed trait Equatable[A] {
+  def equal(a: A, b: A): Boolean
+  def any: Equatable[Any] = this.asInstanceOf[Equatable[Any]]
+}
+
+object Equatable {
+  implicit case object IntEquatable extends Equatable[Int] {
+    override def equal(a: Int, b: Int): Boolean = a == b
+  }
+}
