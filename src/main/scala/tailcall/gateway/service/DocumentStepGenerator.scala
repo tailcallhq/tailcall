8eab74a332c3102f818146b4e69b9744a8d72a01
Tushar Mathur <tusharmath@gmail.com>
2023-02-25 12:56:51 +0530

    refactor: implement StepGenerator

--- src/main/scala/tailcall/gateway/service/DocumentStepGenerator.scala
+++ src/main/scala/tailcall/gateway/service/DocumentStepGenerator.scala
@@ -1,7 +1,12 @@
 package tailcall.gateway.service
 
 import caliban.schema.Step
-import tailcall.gateway.ast.Document
+import tailcall.gateway.ast.Document.Definition.FieldDefinition
+import tailcall.gateway.ast.{Context, Document}
+import tailcall.gateway.internal.DynamicValueUtil
+import tailcall.gateway.remote.Remote
+import zio.query.ZQuery
+import zio.schema.DynamicValue
 import zio.{ZIO, ZLayer}
 
 trait DocumentStepGenerator {
@@ -10,7 +15,20 @@ trait DocumentStepGenerator {
 
 object DocumentStepGenerator {
   final case class Live(rtm: EvaluationRuntime) extends DocumentStepGenerator {
-    override def resolve(document: Document): Step[Any] = Step.NullStep
+    def resolve(field: FieldDefinition): Step[Any] = {
+      Step.QueryStep(ZQuery.fromZIO(
+        field.resolver.run(Remote(Context(DynamicValue(())))).evaluate.map(DynamicValueUtil.toValue)
+          .map(Step.PureStep(_)).provide(ZLayer.succeed(rtm))
+      ))
+    }
+
+    override def resolve(document: Document): Step[Any] = {
+      document.query match {
+        case Some(Document.Definition.ObjectTypeDefinition(name, fields)) => Step
+            .ObjectStep(name, fields.map(field => field.name -> resolve(field)).toMap)
+        case None                                                         => Step.NullStep
+      }
+    }
   }
 
   def live: ZLayer[EvaluationRuntime, Nothing, DocumentStepGenerator] = {
