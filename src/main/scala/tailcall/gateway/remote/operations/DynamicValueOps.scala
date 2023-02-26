f5167f649b4d2d04a5b97eeca2a1ec7fae19f2e8
amitksingh1490 <amitksingh1490@gmail.com>
2023-02-26 20:14:00 +0530

    refactor: add toTyped to remote

--- src/main/scala/tailcall/gateway/remote/operations/DynamicValueOps.scala
+++ src/main/scala/tailcall/gateway/remote/operations/DynamicValueOps.scala
@@ -1,16 +1,12 @@
 package tailcall.gateway.remote.operations
 
-import tailcall.gateway.lambda.Lambda
+import tailcall.gateway.lambda.{Constructor, Lambda}
 import tailcall.gateway.remote.Remote
 import zio.schema.DynamicValue
 
 trait DynamicValueOps {
   implicit final class RemoteDynamicValueOps(private val self: Remote[DynamicValue]) {
-    def asSeq: Remote[Option[Seq[DynamicValue]]] = Remote(self.toLambda >>> Lambda.dynamic.toTyped[Seq[DynamicValue]])
-    def asMap: Remote[Option[Map[DynamicValue, DynamicValue]]] =
-      Remote(self.toLambda >>> Lambda.dynamic.toTyped[Map[DynamicValue, DynamicValue]])
-    def asString: Remote[Option[String]]   = Remote(self.toLambda >>> Lambda.dynamic.toTyped[String])
-    def asInt: Remote[Option[Int]]         = Remote(self.toLambda >>> Lambda.dynamic.toTyped[Int])
-    def asBoolean: Remote[Option[Boolean]] = Remote(self.toLambda >>> Lambda.dynamic.toTyped[Boolean])
+    def toTyped[A](implicit ctor: Constructor[A]): Remote[Option[A]] =
+      Remote(self.toLambda >>> Lambda.dynamic.toTyped[A])
   }
 }
