83e9eba15914315e7550dde9082b8f90436642c5
Tushar Mathur <tusharmath@gmail.com>
2023-02-27 21:39:04 +0530

    test: StepGenerator with Arg support

--- src/main/scala/tailcall/gateway/remote/package.scala
+++ src/main/scala/tailcall/gateway/remote/package.scala
@@ -2,4 +2,4 @@ package tailcall.gateway
 
 import tailcall.gateway.remote.operations._
 
-package object remote extends MathOps with DynamicValueOps with BooleanOps with MapOps {}
+package object remote extends MathOps with DynamicValueOps with BooleanOps with MapOps with OptionOps {}
