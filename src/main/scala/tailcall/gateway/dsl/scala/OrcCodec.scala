3213f1b56595c2cb39a365c1f9c2ec80bddf4d55
Tushar Mathur <tusharmath@gmail.com>
2023-03-04 23:42:13 +0530

    refactor: rename Document to Blueprint

--- src/main/scala/tailcall/gateway/dsl/scala/OrcCodec.scala
+++ src/main/scala/tailcall/gateway/dsl/scala/OrcCodec.scala
@@ -1,25 +1,25 @@
 package tailcall.gateway.dsl.scala
 
-import tailcall.gateway.ast.Document
+import tailcall.gateway.ast.Blueprint
 import tailcall.gateway.dsl.scala.Orc._
 import tailcall.gateway.remote.Remote
 import zio.schema.DynamicValue
 import zio.{IO, ZIO}
 
 object OrcCodec {
-  def toType(t: Type, isNull: Boolean = true): Document.Type = {
+  def toType(t: Type, isNull: Boolean = true): Blueprint.Type = {
     val nonNull = !isNull
     t match {
       case Type.NonNull(ofType)  => toType(ofType, nonNull)
-      case Type.NamedType(name)  => Document.NamedType(name, nonNull)
-      case Type.ListType(ofType) => Document.ListType(toType(ofType, nonNull), nonNull)
+      case Type.NamedType(name)  => Blueprint.NamedType(name, nonNull)
+      case Type.ListType(ofType) => Blueprint.ListType(toType(ofType, nonNull), nonNull)
     }
   }
 
-  def toInputValueDefinition(lField: LabelledField[Input]): IO[String, Document.InputValueDefinition] =
+  def toInputValueDefinition(lField: LabelledField[Input]): IO[String, Blueprint.InputValueDefinition] =
     for {
       ofType <- ZIO.fromOption(lField.field.ofType) <> ZIO.fail("Input type must be named")
-    } yield Document.InputValueDefinition(lField.name, toType(ofType), lField.field.definition.defaultValue)
+    } yield Blueprint.InputValueDefinition(lField.name, toType(ofType), lField.field.definition.defaultValue)
 
   def toResolver(lfield: LabelledField[Output]): Option[Remote[DynamicValue] => Remote[DynamicValue]] =
     lfield.field.definition.resolve match {
@@ -27,16 +27,16 @@ object OrcCodec {
       case Resolver.FromFunction(f) => Some(f)
       case Resolver.FromParent      => Some(_.path("value", lfield.name).toDynamic)
     }
-  def toFieldDefinition(lField: LabelledField[Output]): IO[String, Document.FieldDefinition]          = {
+  def toFieldDefinition(lField: LabelledField[Output]): IO[String, Blueprint.FieldDefinition]         = {
     for {
       ofType <- ZIO.fromOption(lField.field.ofType) <> ZIO.fail("Output type must be named")
       args   <- ZIO.foreach(lField.field.definition.arguments)(toInputValueDefinition)
-    } yield Document
+    } yield Blueprint
       .FieldDefinition(name = lField.name, ofType = toType(ofType), args = args, resolver = toResolver(lField))
   }
 
-  def toDocument(o: Orc): IO[String, Document] = {
-    val schemaDefinition = Document
+  def toDocument(o: Orc): IO[String, Blueprint] = {
+    val schemaDefinition = Blueprint
       .SchemaDefinition(query = o.query, mutation = o.mutation, subscription = o.subscription)
 
     for {
@@ -44,14 +44,14 @@ object OrcCodec {
         case Orc.Obj(name, FieldSet.InputSet(fields))  => toInputObjectTypeDefinition(name, fields)
         case Orc.Obj(name, FieldSet.OutputSet(fields)) => toObjectTypeDefinition(name, fields)
       })(identity)
-    } yield Document(schemaDefinition :: objectDefinitions)
+    } yield Blueprint(schemaDefinition, objectDefinitions)
   }
 
   private def toObjectTypeDefinition(name: String, fields: List[LabelledField[Output]]) = {
-    ZIO.foreach(fields)(toFieldDefinition).map(Document.ObjectTypeDefinition(name, _))
+    ZIO.foreach(fields)(toFieldDefinition).map(Blueprint.ObjectTypeDefinition(name, _))
   }
 
   private def toInputObjectTypeDefinition(name: String, fields: List[LabelledField[Input]]) = {
-    ZIO.foreach(fields)(toInputValueDefinition).map(Document.InputObjectTypeDefinition(name, _))
+    ZIO.foreach(fields)(toInputValueDefinition).map(Blueprint.InputObjectTypeDefinition(name, _))
   }
 }
