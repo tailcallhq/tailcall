c99387fcd13930f5ce1540ab04bcbd7b1e9e92d7
amitksingh1490 <amitksingh1490@gmail.com>
2023-03-04 23:35:29 +0530

    feat: add fromParentValue

--- src/test/scala/tailcall/gateway/StepGeneratorSpec.scala
+++ src/test/scala/tailcall/gateway/StepGeneratorSpec.scala
@@ -1,11 +1,14 @@
 package tailcall.gateway
 
+import caliban.InputValue
 import tailcall.gateway.ast.Document
 import tailcall.gateway.dsl.scala.Orc
 import tailcall.gateway.dsl.scala.Orc.Field
+import tailcall.gateway.internal.DynamicValueUtil.toInputValue
 import tailcall.gateway.remote._
 import tailcall.gateway.service._
 import zio.ZIO
+import zio.schema.DynamicValue
 import zio.test.Assertion.equalTo
 import zio.test.{ZIOSpecDefault, assertZIO}
 
@@ -109,7 +112,7 @@ object StepGeneratorSpec extends ZIOSpecDefault {
           "Foo"   -> List("bar" -> Field.output.to("Bar").resolveWith(100)),
           "Bar"   -> List("baz" -> Field.output.to("Baz").resolveWith(200)),
           "Baz"   -> List("value" -> Field.output.to("Int").resolveWithFunction {
-            _.path("parent", "value").debug("here").map(_.toTyped[Int]).toDynamic
+            _.path("parent", "value").map(_.toTyped[Int]).toDynamic
           })
         )
         val program = execute(orc)("query {foo { bar { baz {value} }}}")
@@ -130,19 +133,52 @@ object StepGeneratorSpec extends ZIOSpecDefault {
         val program = execute(orc)("query {foo { a b c }}")
         assertZIO(program)(equalTo("""{"foo":{"a":1,"b":2,"c":3}}"""))
 
+      },
+      test("graphql variables") {
+        // type Query {foo: Foo}
+        // type Foo {a: Int, b: Int}
+        val orc = Orc(
+          "Query" -> List(
+            "foo" -> Field.output.to("Foo").withArgument("a" -> Field.input.to("Int"), "b" -> Field.input.to("Int"))
+              .resolveWithFunction(ctx =>
+                (for {
+                  a <- ctx.toTypedPath[Int]("args", "a")
+                  b <- ctx.toTypedPath[Int]("args", "b")
+
+                } yield a + b).toDynamic
+              )
+          ),
+          "Foo"   -> List("sum" -> Field.output.to("Int").resolveWithParentValue)
+        )
+
+        val program = execute(orc)(
+          "query ($a: Int, $b: Int) {foo (a: $a, b: $b) { sum }}",
+          Map("a" -> DynamicValue(10), "b" -> DynamicValue(20))
+        )
+        assertZIO(program)(equalTo("""{"foo":{"sum":30}}"""))
       }
     ).provide(GraphQLGenerator.live, TypeGenerator.live, StepGenerator.live, EvaluationRuntime.live)
   }
 
-  def execute(orc: Orc)(query: String): ZIO[GraphQLGenerator, Throwable, String] =
-    orc.toDocument.flatMap(execute(_)(query))
+  def execute(
+    orc: Orc
+  )(query: String, variables: Map[String, DynamicValue] = Map.empty): ZIO[GraphQLGenerator, Throwable, String] =
+    orc.toDocument.flatMap(execute(_)(query, variables.map { case (k, v) => k -> toInputValue(v) }))
 
-  def execute(doc: Document)(query: String): ZIO[GraphQLGenerator, Throwable, String] =
+  def execute(
+    doc: Document
+  )(query: String, variables: Map[String, InputValue]): ZIO[GraphQLGenerator, Throwable, String] =
     for {
       graphQL     <- doc.toGraphQL
       interpreter <- graphQL.interpreter
-      result <- interpreter.execute(query, skipValidation = true) // TODO: enable validation after __type is available
-      _      <- result.errors.headOption match {
+      result      <-
+        interpreter
+          .execute(
+            query,
+            variables = variables,
+            skipValidation = true
+          ) // TODO: enable validation after __type is available
+      _           <- result.errors.headOption match {
         case Some(error) => ZIO.fail(error)
         case None        => ZIO.unit
       }
