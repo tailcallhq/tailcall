71766a233f5c72a7faedc1ab6df51a5fde9a80c1
Tushar Mathur <tusharmath@gmail.com>
2023-03-05 15:55:12 +0530

    refactor: represent Remote as a Case Class

--- src/main/scala/tailcall/gateway/remote/Remote.scala
+++ src/main/scala/tailcall/gateway/remote/Remote.scala
@@ -12,23 +12,16 @@ import zio.schema.{DynamicValue, Schema}
  * extremely powerful. We use this inside the compiler to
  * convert the composition logic into some form of a Remote.
  */
-sealed trait Remote[+A] {
+final case class Remote[+A](toLambda: Any ~> A) {
   self =>
-  final def toLambda: Any ~> A                       = Remote.toLambda(self)
   def evaluate: ZIO[EvaluationRuntime, Throwable, A] = toLambda.evaluate {}
 
   def toDynamic[A1 >: A](implicit ev: Schema[A1]): Remote[DynamicValue] =
     Remote(self.toLambda >>> Lambda.dynamic.toDynamic)
-
-  def debug(message: String): Remote[A] = Remote(self.toLambda >>> Lambda.unsafe.debug(message))
 }
 
 object Remote {
-  final case class FromLambda[A](lambda: Any ~> A) extends Remote[A]
-
-  def apply[A](a: => A)(implicit c: Schema[A]): Remote[A] = FromLambda(Lambda(a))
-
-  def apply[A](a: Any ~> A): Remote[A] = FromLambda(a)
+  def apply[A](a: => A)(implicit c: Schema[A]): Remote[A] = Remote(Lambda(a))
 
   def bind[A, B](ab: Remote[A] => Remote[B]): Remote[A] => Remote[B] = a => Remote(a.toLambda >>> Remote.toLambda(ab))
 
@@ -39,8 +32,6 @@ object Remote {
   def fromOption[A](a: Option[Remote[A]]): Remote[Option[A]] = Remote(Lambda.option(a.map(_.toLambda)))
 
   def toLambda[A, B](ab: Remote[A] => Remote[B]): A ~> B = Lambda.fromLambdaFunction[A, B](a => ab(Remote(a)).toLambda)
-
-  def toLambda[A](remote: Remote[A]): Any ~> A = remote match { case FromLambda(lambda) => lambda }
 
   implicit def schema[A]: Schema[Remote[A]] = Schema[Any ~> A].transform(Remote(_), _.toLambda)
 
