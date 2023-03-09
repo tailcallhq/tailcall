4cda521840c0c60c97e4ebfa2657cd625187d0c6
Tushar Mathur <tusharmath@gmail.com>
2023-03-09 16:22:00 +0530

    refactor: update Main with all service endpoints

--- server/src/main/scala/tailcall/server/Main.scala
+++ server/src/main/scala/tailcall/server/Main.scala
@@ -1,27 +1,48 @@
 package tailcall.server
 
 import tailcall.runtime.ast.Blueprint
+import tailcall.server.service.BinaryDigest.Digest
+import tailcall.server.service.{BinaryDigest, SchemaRegistry}
 import zio._
 import zio.http._
 import zio.http.model.{HttpError, Method}
+import zio.json.EncoderOps
 
 object Main extends ZIOAppDefault {
 
   // TODO: add all registry routes
-  val app = Http.collectZIO[Request] { case req @ Method.PUT -> !! / "registry" / "create" =>
-    for {
-      body      <- req.body.asCharSeq
-      blueprint <- Blueprint.decode(body) match {
-        case Left(value)  => ZIO.fail(HttpError.BadRequest(value))
-        case Right(value) => ZIO.succeed(value)
-      }
-    } yield Response.ok
+  val registery = Http.collectZIO[Request] {
+    case req @ Method.PUT -> !! / "schema" => for {
+        body      <- req.body.asCharSeq
+        blueprint <- Blueprint.decode(body) match {
+          case Left(value)  => ZIO.fail(HttpError.BadRequest(value))
+          case Right(value) => ZIO.succeed(value)
+        }
+        digest    <- SchemaRegistry.add(blueprint)
+      } yield Response.json(digest.toJson)
+
+    case Method.GET -> !! / "schemas" => for {
+        list <- SchemaRegistry.list(0, Int.MaxValue)
+      } yield Response.json(list.toJson)
+
+    case Method.DELETE -> !! / "schemas" / digest => for {
+        deleted <- SchemaRegistry.drop(Digest.fromHex(digest))
+      } yield Response.json(deleted.toJson)
+
+    case Method.GET -> !! / "schemas" / digest => for {
+        schema    <- SchemaRegistry.get(Digest.fromHex(digest))
+        blueprint <- schema match {
+          case Some(blueprint) => ZIO.succeed(blueprint)
+          case None            => ZIO.fail(HttpError.NotFound("Schema not found"))
+        }
+      } yield Response.json(blueprint.toJson)
   }
 
-  val sanitized = app.mapError {
+  val sanitized = registery.mapError {
     case error: HttpError => Response.fromHttpError(error)
     case error            => Response.fromHttpError(HttpError.InternalServerError(cause = Option(error)))
   }
 
-  override val run = Server.serve(sanitized).provide(Server.default)
+  override val run = Server.serve(sanitized).exitCode
+    .provide(Server.default, SchemaRegistry.memory, BinaryDigest.algorithm("SHA-256"))
 }
--- server/src/main/scala/tailcall/server/service/BinaryDigest.scala
+++ server/src/main/scala/tailcall/server/service/BinaryDigest.scala
@@ -1,6 +1,6 @@
 package tailcall.server.service
 
-import zio.json.JsonEncoder
+import zio.json.{DeriveJsonDecoder, DeriveJsonEncoder, JsonDecoder, JsonEncoder}
 import zio.schema.Schema
 import zio.{ULayer, ZLayer}
 
@@ -16,6 +16,13 @@ trait BinaryDigest {
 object BinaryDigest {
   final case class Digest(value: Array[Byte]) {
     def toHex: String = value.map("%02x".format(_)).mkString
+  }
+
+  object Digest {
+    implicit val encoder: JsonEncoder[Digest] = DeriveJsonEncoder.gen[Digest]
+    implicit val decoder: JsonDecoder[Digest] = DeriveJsonDecoder.gen[Digest]
+
+    def fromHex(hex: String): Digest = Digest(hex.grouped(2).map(Integer.parseInt(_, 16).toByte).toArray)
   }
 
   def algorithm(name: String): ULayer[BinaryDigest] =
