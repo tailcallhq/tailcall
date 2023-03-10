0a7d88b0e193b82e2aba06e59fd6c0f6355f32f3
Tushar Mathur <tusharmath@gmail.com>
2023-03-10 12:50:32 +0530

    refactor: update registry

--- server/src/main/scala/tailcall/server/service/BinaryDigest.scala
+++ server/src/main/scala/tailcall/server/service/BinaryDigest.scala
@@ -1,7 +1,7 @@
 package tailcall.server.service
 
 import zio.json.{DeriveJsonDecoder, DeriveJsonEncoder, JsonDecoder, JsonEncoder}
-import zio.{ULayer, ZLayer}
+import zio.{ULayer, ZIO, ZLayer}
 
 import java.security.MessageDigest
 
@@ -11,23 +11,25 @@ trait BinaryDigest {
 }
 
 object BinaryDigest {
-  final case class Digest(value: Array[Byte]) {
-    def toHex: String            = value.map("%02x".format(_)).mkString
-    override def hashCode(): Int = toHex.hashCode
+  final case class Digest(hex: String) {
+    def getBytes: Array[Byte] = hex.getBytes
   }
 
   object Digest {
     implicit val encoder: JsonEncoder[Digest] = DeriveJsonEncoder.gen[Digest]
     implicit val decoder: JsonDecoder[Digest] = DeriveJsonDecoder.gen[Digest]
 
-    def fromHex(hex: String): Digest = Digest(hex.grouped(2).map(Integer.parseInt(_, 16).toByte).toArray)
+    def fromHex(hex: String): Digest = Digest(hex)
   }
 
   def algorithm(name: String): ULayer[BinaryDigest] =
     ZLayer.succeed(new BinaryDigest {
       override def digestWith[A](a: A, encoder: JsonEncoder[A]): Digest = {
         val encoded = String.valueOf(encoder.encodeJson(a)).getBytes()
-        Digest(MessageDigest.getInstance(name).digest(encoded))
+        Digest(MessageDigest.getInstance(name).digest(encoded).map("%02x".format(_)).mkString)
       }
     })
+
+  def digest[A](a: A)(implicit encoder: JsonEncoder[A]): ZIO[BinaryDigest, Nothing, Digest] =
+    ZIO.serviceWith[BinaryDigest](_.digest(a))
 }
