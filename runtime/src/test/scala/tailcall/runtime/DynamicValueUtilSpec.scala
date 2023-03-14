aab025c85fcb40482faa81487a217edf6296aae1
Aadit M Shah <aaditmshah@aadit.codes>
2023-03-14 15:20:34 +0530

    :white_check_mark: Test whether `toResponseValue compose fromResponseValue == identity`

--- runtime/src/test/scala/tailcall/runtime/DynamicValueUtilSpec.scala
+++ runtime/src/test/scala/tailcall/runtime/DynamicValueUtilSpec.scala
@@ -1,6 +1,5 @@
 package tailcall.runtime
 
-import caliban.{ResponseValue, Value}
 import tailcall.runtime.internal.DynamicValueUtil._
 import tailcall.runtime.internal.{Caliban, Primitive}
 import zio.json.ast.Json
@@ -8,12 +7,8 @@ import zio.schema.{DeriveSchema, DynamicValue, Schema, TypeId}
 import zio.test._
 
 import scala.collection.immutable.ListMap
-import scala.util.Try
 
 object DynamicValueUtilSpec extends ZIOSpecDefault {
-  val helloWorld    = "Hello World!"
-  val meaningOfLife = 42
-
   val genJson: Gen[Any, Json] = Gen.suspend(Gen.oneOf(
     Gen.chunkOfBounded(0, 5)(for {
       key   <- Gen.string1(Gen.alphaChar)
@@ -26,16 +21,7 @@ object DynamicValueUtilSpec extends ZIOSpecDefault {
     Gen.const(Json.Null)
   ))
 
-  sealed trait Foo
-
   final case class Foobar(foo: List[Int], bar: DynamicValue)
-
-  object Foo {
-    final case class Bar(bar: List[Option[Int]])        extends Foo
-    final case class Baz(baz: Set[Either[Int, String]]) extends Foo
-
-    implicit val schema: Schema[Foo] = DeriveSchema.gen[Foo]
-  }
 
   object Foobar {
     implicit val schema: Schema[Foobar] = DeriveSchema.gen[Foobar]
@@ -47,34 +33,24 @@ object DynamicValueUtilSpec extends ZIOSpecDefault {
         check(Primitive.gen) { primitive =>
           assertTrue(asString(primitive.toDynamicValue) == Some(primitive.value.toString))
         } &&
-        assertTrue(asString(DynamicValue(List(meaningOfLife))) == None)
-      },
-      test("toResponseValue") {
-        assertTrue(
-          toResponseValue(DynamicValue(Foobar(List(meaningOfLife), DynamicValue(())))) == ResponseValue
-            .ObjectValue(List("foo" -> ResponseValue.ListValue(List(Value.IntValue(42))), "bar" -> Value.NullValue))
-        ) &&
-        assertTrue(
-          toResponseValue(
-            DynamicValue(Map("foo" -> DynamicValue(List(meaningOfLife)), "bar" -> DynamicValue(())))
-          ) == ResponseValue
-            .ObjectValue(List("foo" -> ResponseValue.ListValue(List(Value.IntValue(42))), "bar" -> Value.NullValue))
-        ) &&
-        assert(Try(toResponseValue(DynamicValue(Map(meaningOfLife -> helloWorld)))))(Assertion.isFailure(
-          Assertion.hasMessage(Assertion.equalTo("could not transform"))
-        ))
+        assertTrue(asString(DynamicValue(List(42))) == None)
       },
       test("toTyped") {
-        assertTrue(toTyped[String](DynamicValue(helloWorld)) == Some("Hello World!")) &&
-        assertTrue(toTyped[String](DynamicValue(meaningOfLife)) == None)
+        assertTrue(toTyped[String](DynamicValue("Hello World!")) == Some("Hello World!")) &&
+        assertTrue(toTyped[String](DynamicValue(42)) == None)
       },
       test("getPath") {
-        val d = DynamicValue(Foobar(List(meaningOfLife), DynamicValue(Option(Map("baz" -> helloWorld)))))
+        val d = DynamicValue(Foobar(List(42), DynamicValue(Option(Map("baz" -> "Hello World!")))))
         assertTrue(getPath(d, List("foo", "0")) == Some(DynamicValue(42))) &&
         assertTrue(getPath(d, List("bar", "baz")) == Some(DynamicValue("Hello World!"))) &&
         assertTrue(getPath(d, List("foo", "1")) == None) &&
         assertTrue(getPath(d, List("bar", "qux")) == None) &&
         assertTrue(getPath(d, List("quux")) == None)
+      },
+      test("toResponseValue compose fromResponseValue == identity") {
+        check(Caliban.genResponseValue) { responseValue =>
+          assertTrue(toResponseValue(fromResponseValue(responseValue)) == responseValue)
+        }
       },
       test("toInputValue compose fromInputValue == identity") {
         check(Caliban.genInputValue) { inputValue =>
@@ -83,7 +59,7 @@ object DynamicValueUtilSpec extends ZIOSpecDefault {
       },
       test("record") {
         assertTrue(
-          record("foo" -> DynamicValue(List(meaningOfLife)), "bar" -> DynamicValue(helloWorld)) == DynamicValue
+          record("foo" -> DynamicValue(List(42)), "bar" -> DynamicValue("Hello World!")) == DynamicValue
             .Record(TypeId.Structural, ListMap("foo" -> DynamicValue(List(42)), "bar" -> DynamicValue("Hello World!")))
         )
       },
