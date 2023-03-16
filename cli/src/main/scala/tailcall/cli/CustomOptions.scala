43e698506fbecd0b5aad70cb55a62ae9dc91a0ac
Tushar Mathur <tusharmath@gmail.com>
2023-03-16 21:18:40 +0530

    refactor: add Digest option

--- cli/src/main/scala/tailcall/cli/CustomOptions.scala
+++ cli/src/main/scala/tailcall/cli/CustomOptions.scala
@@ -1,11 +1,13 @@
 package tailcall.cli
 
+import tailcall.runtime.ast.Digest
+import tailcall.runtime.ast.Digest.Algorithm
 import zio.cli._
 
 object CustomOptions {
-  def digest(name: String): Options[String] =
+  def digest(name: String): Options[Digest] =
     Options.text(name).mapOrFail { digest =>
-      if ("^[a-fA-F0-9]{64}$".r.matches(digest)) Right(digest)
+      if ("^[a-fA-F0-9]{64}$".r.matches(digest)) Right(Digest.fromHex(Algorithm.SHA_256, digest))
       else Left(ValidationError(ValidationErrorType.InvalidArgument, HelpDoc.p("Digest must be a SHA-256 hash.")))
     }
 }
