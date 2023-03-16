a438afcde1e32125b5769d14b459efa0f2ff0bd8
Tushar Mathur <tusharmath@gmail.com>
2023-03-16 14:16:54 +0530

    refactor: rename file

--- cli/src/main/scala/tailcall/cli/Main.scala
+++ cli/src/main/scala/tailcall/cli/Main.scala
@@ -2,7 +2,7 @@ package tailcall.cli
 
 import zio.{Scope, ZIO, ZIOAppArgs, ZIOAppDefault}
 
-object MainCLI extends ZIOAppDefault {
+object Main extends ZIOAppDefault {
   override def run: ZIO[Any with ZIOAppArgs with Scope, Any, Any] =
     ZIOAppArgs.getArgs.flatMap(args => CommandSpec.app.run(args.toList).provide(CommandExecutor.live))
 }
