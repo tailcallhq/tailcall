ea4a6c1cc0547d435c994fef009c35a7510ced30
Tushar Mathur <tusharmath@gmail.com>
2023-03-16 18:06:10 +0530

    refactor: add Logger

--- cli/src/main/scala/tailcall/cli/Main.scala
+++ cli/src/main/scala/tailcall/cli/Main.scala
@@ -1,11 +1,12 @@
 package tailcall.cli
 
-import tailcall.cli.service.CommandExecutor
+import tailcall.cli.service.{CommandExecutor, Logger}
 import zio.{ExitCode, Scope, ZIO, ZIOAppArgs, ZIOAppDefault}
 
 object Main extends ZIOAppDefault {
   self =>
   override def run: ZIO[Any with ZIOAppArgs with Scope, Any, Any] =
-    ZIOAppArgs.getArgs
-      .flatMap(args => (CommandSpec.app.run(args.toList) <> self.exit(ExitCode.failure)).provide(CommandExecutor.live))
+    ZIOAppArgs.getArgs.flatMap(args =>
+      (CommandSpec.app.run(args.toList) <> self.exit(ExitCode.failure)).provide(CommandExecutor.live, Logger.live)
+    )
 }
