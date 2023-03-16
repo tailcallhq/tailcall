23d5e348002b0124bf9c0838e529720de64149e7
Tushar Mathur <tusharmath@gmail.com>
2023-03-16 21:20:04 +0530

    refactor: Update Logger

--- cli/src/main/scala/tailcall/cli/service/Logger.scala
+++ cli/src/main/scala/tailcall/cli/service/Logger.scala
@@ -2,20 +2,22 @@ package tailcall.cli.service
 
 import zio.cli.HelpDoc
 import zio.cli.HelpDoc.Span
-import zio.{UIO, ZIO, ZLayer}
+import zio.{Cause, UIO, ZIO, ZLayer}
 
 trait Logger {
-  def log(doc: Span): UIO[Unit]
-  def apply(doc: Span): UIO[Unit]        = log(doc)
-  def error(error: Throwable): UIO[Unit] = log(Span.Error(Span.Text(error.getMessage)))
+  def log(doc: => Span): UIO[Unit]
+  def apply(doc: => Span): UIO[Unit]        = log(doc)
+  def error(error: => Throwable): UIO[Unit] = {
+    ZIO.logErrorCause(Cause.fail(error)) *> log(Span.Error(Span.Text(error.getMessage)))
+  }
 }
 
 object Logger {
   final class Live extends Logger {
-    override def log(doc: Span): UIO[Unit] =
+    override def log(doc: => Span): UIO[Unit] =
       ZIO.succeed(println(HelpDoc.p(doc).toPlaintext(color = true, columnWidth = Int.MaxValue)))
   }
 
-  def log(doc: Span): ZIO[Logger, Nothing, Unit] = ZIO.serviceWithZIO[Logger](_.log(doc))
-  def live: ZLayer[Any, Nothing, Logger]         = ZLayer.succeed(new Live)
+  def log(doc: => Span): ZIO[Logger, Nothing, Unit] = ZIO.serviceWithZIO[Logger](_.log(doc))
+  def live: ZLayer[Any, Nothing, Logger]            = ZLayer.succeed(new Live)
 }
