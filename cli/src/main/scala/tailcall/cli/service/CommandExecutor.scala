f0191c383b901cd0353780c5e302e9f045599a35
Tushar Mathur <tusharmath@gmail.com>
2023-03-16 18:06:10 +0530

    refactor: add Logger

--- cli/src/main/scala/tailcall/cli/service/CommandExecutor.scala
+++ cli/src/main/scala/tailcall/cli/service/CommandExecutor.scala
@@ -2,50 +2,42 @@ package tailcall.cli.service
 
 import tailcall.cli.CommandADT
 import tailcall.runtime.service.ConfigReader
-import zio.cli.HelpDoc
-import zio.cli.HelpDoc.Span.{strong, text, uri}
-import zio.{Duration, UIO, ZIO, ZLayer}
+import zio.cli.HelpDoc.Span.{spans, strong, text, uri}
+import zio.{Duration, ZIO, ZLayer}
 
 trait CommandExecutor {
   def execute(command: CommandADT): ZIO[Any, Throwable, Unit]
 }
 
 object CommandExecutor {
-  import HelpDoc._
 
-  def errorMessage(error: Throwable): UIO[Unit] = show(HelpDoc.p(Span.Error(Span.Text(error.getMessage))))
-  def show(doc: HelpDoc): UIO[Unit] = ZIO.succeed(println(doc.toPlaintext(color = true, columnWidth = Int.MaxValue)))
-  def show(span: Span): UIO[Unit]   = show(HelpDoc.p(span))
-  def span(texts: Span*): Span      = Span.spans(texts.toList)
-  def span(text: String): Span      = Span.Text(text)
+  final class Live(log: Logger) extends CommandExecutor {
+    def timed[R, E, A](program: ZIO[R, E, A]): ZIO[R, E, A] =
+      for {
+        start <- zio.Clock.nanoTime
+        a     <- program
+        end   <- zio.Clock.nanoTime
+        _     <- log {
+          val duration = Duration.fromNanos(end - start)
+          text(s"Completed in ${duration.toMillis} ms.")
+        }
+      } yield a
 
-  def timed[R, E, A](program: ZIO[R, E, A]): ZIO[R, E, A] =
-    for {
-      start <- zio.Clock.nanoTime
-      a     <- program
-      end   <- zio.Clock.nanoTime
-      _     <- show {
-        val duration = Duration.fromNanos(end - start)
-        text(s"Completed in ${duration.toMillis} ms.")
-      }
-    } yield a
-
-  final class Live extends CommandExecutor {
     override def execute(command: CommandADT): ZIO[Any, Throwable, Unit] =
-      command match {
-        case CommandADT.Deploy(digest, endpoint) => ZIO.attempt(println(s"Deploying $digest to $endpoint"))
-        case CommandADT.Validate(file)           => timed {
-            for {
-              _ <- show(span(text("Running file validation on "), uri(file.toAbsolutePath.toUri)))
-              _ <- ConfigReader.config.readFile(file.toFile).tapError(errorMessage(_))
-              _ <- show(strong("File is valid and is ready to be deployed."))
+      timed {
+        command match {
+          case CommandADT.Deploy(digest, endpoint) => ZIO.attempt(println(s"Deploying $digest to $endpoint"))
+          case CommandADT.Validate(file)           => for {
+              _ <- log(spans(text("Running file validation on: "), uri(file.toAbsolutePath.toUri)))
+              _ <- ConfigReader.config.readFile(file.toFile).tapError(log.error(_))
+              _ <- log(strong("\uD83D\uDC4D File is valid and is ready to be deployed."))
             } yield ()
-          }
+        }
       }
   }
 
   def execute(command: CommandADT): ZIO[CommandExecutor, Throwable, Unit] =
     ZIO.serviceWithZIO[CommandExecutor](_.execute(command))
 
-  def live: ZLayer[Any, Nothing, CommandExecutor] = ZLayer.succeed(new Live)
+  def live: ZLayer[Logger, Nothing, CommandExecutor] = ZLayer.fromFunction(new Live(_))
 }
---
+++ cli/src/main/scala/tailcall/cli/service/Logger.scala
@@ -0,0 +1,21 @@
+package tailcall.cli.service
+
+import zio.cli.HelpDoc
+import zio.cli.HelpDoc.Span
+import zio.{UIO, ZIO, ZLayer}
+
+trait Logger {
+  def log(doc: Span): UIO[Unit]
+  def apply(doc: Span): UIO[Unit]        = log(doc)
+  def error(error: Throwable): UIO[Unit] = log(Span.Error(Span.Text(error.getMessage)))
+}
+
+object Logger {
+  final class Live extends Logger {
+    override def log(doc: Span): UIO[Unit] =
+      ZIO.succeed(println(HelpDoc.p(doc).toPlaintext(color = true, columnWidth = Int.MaxValue)))
+  }
+
+  def log(doc: Span): ZIO[Logger, Nothing, Unit] = ZIO.serviceWithZIO[Logger](_.log(doc))
+  def live: ZLayer[Any, Nothing, Logger]         = ZLayer.succeed(new Live)
+}
