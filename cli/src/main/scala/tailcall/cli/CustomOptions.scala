9a35e32a14c3a05a51840435024c6b1cb85c2cc7
Tushar Mathur <tusharmath@gmail.com>
2023-03-16 18:53:05 +0530

    refactor: update sha check

--- cli/src/main/scala/tailcall/cli/CustomOptions.scala
+++ cli/src/main/scala/tailcall/cli/CustomOptions.scala
@@ -5,7 +5,7 @@ import zio.cli._
 object CustomOptions {
   def digest(name: String): Options[String] =
     Options.text(name).mapOrFail { digest =>
-      if (digest.length == 64) Right(digest)
-      else Left(ValidationError(ValidationErrorType.InvalidArgument, HelpDoc.p("Digest must be 64 characters long")))
+      if ("^[a-fA-F0-9]{64}$".r.matches(digest)) Right(digest)
+      else Left(ValidationError(ValidationErrorType.InvalidArgument, HelpDoc.p("Digest must be a SHA-256 hash.")))
     }
 }
