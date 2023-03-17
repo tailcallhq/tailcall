cf82eb9206e546d17aee891e6cd432670cdf3387
Tushar Mathur <tusharmath@gmail.com>
2023-03-17 12:43:21 +0530

    refactor: add support to send writeFlags to FileIO

--- runtime/src/main/scala/tailcall/runtime/service/FileIO.scala
+++ runtime/src/main/scala/tailcall/runtime/service/FileIO.scala
@@ -1,23 +1,40 @@
 package tailcall.runtime.service
 
+import tailcall.runtime.service.FileIO.Flags
 import zio.{Task, ZIO, ZLayer}
 
 import java.io.File
-import java.nio.file.Files
+import java.nio.file.{Files, StandardOpenOption}
 
 trait FileIO {
   def read(file: File): Task[String]
-  def write(file: File, content: String): Task[Unit]
+  def write(file: File, content: String, flags: Flags = FileIO.defaultFlag): Task[Unit]
 }
 
 object FileIO {
-  def read(file: File): ZIO[FileIO, Throwable, String]                 = ZIO.serviceWithZIO(_.read(file))
+  def defaultFlag: Flags = Flags(Nil)
+
+  def read(file: File): ZIO[FileIO, Throwable, String] = ZIO.serviceWithZIO(_.read(file))
+
   def write(file: File, content: String): ZIO[FileIO, Throwable, Unit] = ZIO.serviceWithZIO(_.write(file, content))
 
   def live: ZLayer[Any, Nothing, FileIO] =
     ZLayer.succeed(new FileIO {
-      override def read(file: File): Task[String]                 = ZIO.attemptBlocking(Files.readString(file.toPath))
-      override def write(file: File, content: String): Task[Unit] =
-        ZIO.attemptBlocking(Files.write(file.toPath, content.getBytes)).unit
+      override def read(file: File): Task[String] = ZIO.attemptBlocking(Files.readString(file.toPath))
+      override def write(file: File, content: String, flags: Flags): Task[Unit] =
+        ZIO.attemptBlocking(Files.write(file.toPath, content.getBytes, flags.options: _*)).unit
     })
+
+  final case class Flags(options: List[StandardOpenOption]) {
+    def append(option: StandardOpenOption): Flags = copy(options = option :: options)
+    def withRead: Flags                           = append(StandardOpenOption.READ)
+    def withAppend: Flags                         = append(StandardOpenOption.APPEND)
+    def withTruncateExisting: Flags               = append(StandardOpenOption.TRUNCATE_EXISTING)
+    def withCreate: Flags                         = append(StandardOpenOption.CREATE)
+    def withCreateNew: Flags                      = append(StandardOpenOption.CREATE_NEW)
+    def withDeleteOnClose: Flags                  = append(StandardOpenOption.DELETE_ON_CLOSE)
+    def withSparse: Flags                         = append(StandardOpenOption.SPARSE)
+    def withSync: Flags                           = append(StandardOpenOption.SYNC)
+    def withDsync: Flags                          = append(StandardOpenOption.DSYNC)
+  }
 }
