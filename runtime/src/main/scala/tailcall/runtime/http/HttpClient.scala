9f1c47a4ccdea1bee8491d43438a54875cc2f2b9
Tushar Mathur <tusharmath@gmail.com>
2023-03-21 11:26:02 +0530

    refactor: add default for HttpClient

--- runtime/src/main/scala/tailcall/runtime/http/HttpClient.scala
+++ runtime/src/main/scala/tailcall/runtime/http/HttpClient.scala
@@ -12,5 +12,7 @@ object HttpClient {
   final case class Live(client: Client) extends HttpClient {
     def request(req: Request): ZIO[Any, Throwable, Response] = { client.request(req.toZHttpRequest) }
   }
-  def live: ZLayer[Client, Throwable, HttpClient] = ZLayer.fromFunction(client => Live(client))
+  def live: ZLayer[Client, Nothing, HttpClient] = ZLayer.fromFunction(client => Live(client))
+
+  def default: ZLayer[Any, Throwable, HttpClient] = Client.default >>> live
 }
