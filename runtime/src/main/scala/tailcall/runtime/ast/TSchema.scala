133ada3920032e43988a58f1c1647658d454a070
Tushar Mathur <tusharmath@gmail.com>
2023-03-26 16:16:18 +0530

    refactor: add unify operator to TSchema

--- runtime/src/main/scala/tailcall/runtime/ast/TSchema.scala
+++ runtime/src/main/scala/tailcall/runtime/ast/TSchema.scala
@@ -13,75 +13,32 @@ import zio.schema.{Schema, StandardType, TypeId}
 @jsonDiscriminator("type")
 sealed trait TSchema {
   self =>
-  def &(other: TSchema): TSchema = TSchema.Intersection(self, other)
-  def |(other: TSchema): TSchema = TSchema.Union(self, other)
+  final def &(other: TSchema): TSchema   = TSchema.Intersection(self, other)
+  final def |(other: TSchema): TSchema   = TSchema.Union(self, other)
+  final def =:=(other: TSchema): Boolean = self <:< other && other <:< self
+  final def <:<(other: TSchema): Boolean = TSchema.isSubType(self, other)
+  final def arr: TSchema                 = TSchema.arr(self)
+  final def isNullable: Boolean          = TSchema.NULL <:< self
 
-  def <:<(other: TSchema): Boolean = TSchema.isSubType(self, other)
-  def =:=(other: TSchema): Boolean = self <:< other && other <:< self
-  def arr: TSchema                 = TSchema.arr(self)
+  /**
+   * Unifies two schemas into a single schema that is a
+   * supertype of both. The unify function is different from
+   * the union function because it is not just combining two
+   * types into a single Union type. Instead, it is creating
+   * a new schema that includes all the properties of both
+   * input schemas. This is done to reduce unnecessary
+   * unions.
+   */
+  final def unify(other: TSchema): TSchema = TSchema.unify(self, other)
 }
 
 object TSchema {
 
-  @jsonHint("String")
-  case object String extends TSchema
-
-  @jsonHint("Integer")
-  case object Int extends TSchema
-
-  @jsonHint("null")
-  case object Null extends TSchema
-
-  @jsonHint("Unit")
-  case object Unit extends TSchema
-
-  @jsonHint("Boolean")
-  case object Boolean extends TSchema
-
-  @jsonHint("object")
-  final case class Obj(fields: List[Field]) extends TSchema
-
-  @jsonHint("array")
-  final case class Arr(item: TSchema) extends TSchema
-
-  final case class Field(name: String, schema: TSchema)
-
-  @jsonHint("union")
-  final case class Union(self: TSchema, other: TSchema) extends TSchema
-
-  @jsonHint("intersect")
-  final case class Intersection(self: TSchema, other: TSchema) extends TSchema
-
-  // TODO: add unit tests
-  private def isSubType(s1: TSchema, s2: TSchema): Boolean = {
-    def checkFields(fields1: List[Field], fields2: List[Field]): Boolean = {
-      fields2.forall { f2 =>
-        fields1.exists { f1 =>
-          f1.name == f2.name &&
-          isSubType(f1.schema, f2.schema)
-        }
-      }
-    }
-
-    (s1, s2) match {
-      case (_, TSchema.Null)                  => true
-      case (TSchema.Null, _)                  => false
-      case (TSchema.String, TSchema.String)   => true
-      case (TSchema.Int, TSchema.Int)         => true
-      case (TSchema.Unit, TSchema.Unit)       => true
-      case (TSchema.Boolean, TSchema.Boolean) => true
-      case (Obj(fields1), Obj(fields2))       => checkFields(fields1, fields2)
-      case (Arr(item1), Arr(item2))           => isSubType(item1, item2)
-      case (Union(s1a, s1b), _)               => isSubType(s1a, s2) || isSubType(s1b, s2)
-      case (Intersection(s1a, s1b), _)        => isSubType(s1a, s2) && isSubType(s1b, s2)
-      case _                                  => false
-    }
-  }
-
   def string: TSchema = TSchema.String
-  def int: TSchema    = TSchema.Int
-  def `null`: TSchema = TSchema.Null
-  def unit: TSchema   = TSchema.Unit
+
+  def int: TSchema = TSchema.Int
+
+  def unit: TSchema = TSchema.Unit
 
   def bool: TSchema = TSchema.Boolean
 
@@ -144,7 +101,7 @@ object TSchema {
     schema match {
       case TSchema.String  => Schema[String]
       case TSchema.Int     => Schema[Int]
-      case TSchema.Null    => ???
+      case TSchema.NULL    => ???
       case TSchema.Unit    => Schema[Unit]
       case TSchema.Boolean => Schema[Boolean]
 
@@ -156,6 +113,89 @@ object TSchema {
       case Union(_, _)        => ???
       case Intersection(_, _) => ???
     }
+
+  private def unify(a: TSchema, b: TSchema): TSchema =
+    (a, b) match {
+      case (TSchema.Int, TSchema.Int)                   => TSchema.Int
+      case (TSchema.String, TSchema.String)             => TSchema.String
+      case (TSchema.Boolean, TSchema.Boolean)           => TSchema.Boolean
+      case (TSchema.Unit, TSchema.Unit)                 => TSchema.Unit
+      case (TSchema.NULL, TSchema.NULL)                 => TSchema.NULL
+      case (TSchema.Obj(fields1), TSchema.Obj(fields2)) =>
+        val field1Map: Map[String, TSchema] = fields1.map(f => f.name -> f.schema).toMap
+        val field2Map: Map[String, TSchema] = fields2.map(f => f.name -> f.schema).toMap
+        TSchema.Obj((field1Map.keys ++ field2Map.keys).toList.map { key =>
+          TSchema.Field(
+            key,
+            (field1Map.get(key), field2Map.get(key)) match {
+              case (Some(s1), Some(s2)) => unify(s1, s2)
+              case (Some(s1), None)     => s1 | TSchema.NULL
+              case (None, Some(s2))     => s2 | TSchema.NULL
+              case (None, None) => throw new IllegalStateException(s"Key ${key} should be present in one of the maps")
+            },
+          )
+        })
+
+      case (TSchema.Arr(item1), TSchema.Arr(item2)) => TSchema.Arr(unify(item1, item2))
+      case (TSchema.Union(a, b), c)                 => a unify b unify c
+      case (a, TSchema.Union(b, c))                 => a unify b unify c
+      case (a, b)                                   => a | b
+    }
+
+  // TODO: add unit tests
+  private def isSubType(s1: TSchema, s2: TSchema): Boolean = {
+    def checkFields(fields1: List[Field], fields2: List[Field]): Boolean = {
+      fields2.forall { f2 =>
+        fields1.exists { f1 =>
+          f1.name == f2.name &&
+          isSubType(f1.schema, f2.schema)
+        }
+      }
+    }
+
+    (s1, s2) match {
+      case (_, TSchema.NULL)                  => true
+      case (TSchema.NULL, _)                  => false
+      case (TSchema.String, TSchema.String)   => true
+      case (TSchema.Int, TSchema.Int)         => true
+      case (TSchema.Unit, TSchema.Unit)       => true
+      case (TSchema.Boolean, TSchema.Boolean) => true
+      case (Obj(fields1), Obj(fields2))       => checkFields(fields1, fields2)
+      case (Arr(item1), Arr(item2))           => isSubType(item1, item2)
+      case (Union(s1a, s1b), _)               => isSubType(s1a, s2) || isSubType(s1b, s2)
+      case (Intersection(s1a, s1b), _)        => isSubType(s1a, s2) && isSubType(s1b, s2)
+      case _                                  => false
+    }
+  }
+
+  @jsonHint("object")
+  final case class Obj(fields: List[Field]) extends TSchema
+
+  @jsonHint("array")
+  final case class Arr(item: TSchema) extends TSchema
+
+  final case class Field(name: String, schema: TSchema)
+
+  @jsonHint("union")
+  final case class Union(self: TSchema, other: TSchema) extends TSchema
+
+  @jsonHint("intersect")
+  final case class Intersection(self: TSchema, other: TSchema) extends TSchema
+
+  @jsonHint("String")
+  case object String extends TSchema
+
+  @jsonHint("Integer")
+  case object Int extends TSchema
+
+  @jsonHint("null")
+  case object NULL extends TSchema
+
+  @jsonHint("Unit")
+  case object Unit extends TSchema
+
+  @jsonHint("Boolean")
+  case object Boolean extends TSchema
 
   implicit lazy val fieldSchema: JsonCodec[TSchema.Field]    = DeriveJsonCodec.gen[TSchema.Field]
   implicit lazy val schemaCodec: zio.json.JsonCodec[TSchema] = zio.json.DeriveJsonCodec.gen[TSchema]
--- runtime/src/test/scala/tailcall/runtime/internal/TestGen.scala
+++ runtime/src/test/scala/tailcall/runtime/internal/TestGen.scala
@@ -15,7 +15,7 @@ object TestGen {
 
   def genVersion: Gen[Any, Int] = Gen.int(0, 10)
 
-  def genScalar: Gen[Any, TSchema] = Gen.fromIterable(List(TSchema.string, TSchema.int, TSchema.`null`))
+  def genScalar: Gen[Any, TSchema] = Gen.fromIterable(List(TSchema.string, TSchema.int, TSchema.NULL))
 
   def genField: Gen[Any, TSchema.Field] =
     for {
