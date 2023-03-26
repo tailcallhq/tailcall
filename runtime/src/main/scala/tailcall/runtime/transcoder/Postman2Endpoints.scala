3291e64416e08170367ba69576b501d846fcfffb
Tushar Mathur <tusharmath@gmail.com>
2023-03-26 19:57:58 +0530

    chore: add Postman2Endpoints

---
+++ runtime/src/main/scala/tailcall/runtime/transcoder/Postman2Endpoints.scala
@@ -0,0 +1,13 @@
+package tailcall.runtime.transcoder
+
+import tailcall.runtime.ast.Endpoint
+import tailcall.runtime.dsl.Postman
+import tailcall.runtime.internal.TValid
+
+trait Postman2Endpoints {
+  def toEndpoints(postman: Postman, config: Postman2Endpoints.Config): TValid[String, List[Endpoint]] = ???
+}
+
+object Postman2Endpoints {
+  final case class Config(allowHttpCalls: Boolean = false)
+}
