16d5428501d45d6f2710871e452c640440dcb56f
Tushar Mathur <tusharmath@gmail.com>
2023-03-29 20:08:07 +0530

    refactor: reorder methods on TValid

--- runtime/src/main/scala/tailcall/runtime/internal/TValid.scala
+++ runtime/src/main/scala/tailcall/runtime/internal/TValid.scala
@@ -4,22 +4,24 @@ import zio.Chunk
 
 sealed trait TValid[+E, +A] {
   self =>
+  final def <>[E1, A1 >: A](other: TValid[E1, A1]): TValid[E1, A1] = self orElse other
+
+  final def orElse[E1, A1 >: A](other: TValid[E1, A1]): TValid[E1, A1] =
+    self.fold[TValid[E1, A1]](_ => other, TValid.succeed(_))
+
   final def get(implicit ev: E <:< Nothing): A =
     self match {
       case TValid.Failure(_)     => throw new NoSuchElementException("Failure does not exist")
       case TValid.Succeed(value) => value
     }
 
+  final def getOrElse[A1 >: A](default: => A1): A1 = self.fold[A1](_ => default, identity)
+
   final def some: TValid[E, Option[A]] = self.map(Some(_))
 
   final def map[B](ab: A => B): TValid[E, B] = self.flatMap(a => TValid.succeed(ab(a)))
 
   final def flatMap[E1 >: E, B](ab: A => TValid[E1, B]): TValid[E1, B] = self.fold(TValid.fail(_), ab)
-
-  final def <>[E1, A1 >: A](other: TValid[E1, A1]): TValid[E1, A1] = self orElse other
-
-  final def orElse[E1, A1 >: A](other: TValid[E1, A1]): TValid[E1, A1] =
-    self.fold[TValid[E1, A1]](_ => other, TValid.succeed(_))
 
   final def toEither: Either[E, A] = self.fold[Either[E, A]](Left(_), Right(_))
 
@@ -33,23 +35,12 @@ sealed trait TValid[+E, +A] {
 
   final def toZIO: zio.ZIO[Any, E, A] = self.fold(zio.ZIO.fail(_), zio.ZIO.succeed(_))
 
-  final def getOrElse[A1 >: A](default: => A1): A1 = self.fold[A1](_ => default, identity)
+  final def zip[E1 >: E, B, C](other: TValid[E1, B])(f: (A, B) => C): TValid[E1, C] =
+    self.flatMap(a => other.map(b => f(a, b)))
 }
 
 object TValid {
-  def unsupported(from: String, to: String): TValid[String, Nothing] =
-    fail(s"Conversion from ${from} to ${to} is not yet supported")
-
-  def none: TValid[Nothing, Option[Nothing]] = succeed(None)
-
-  def fromOption[A](option: Option[A]): TValid[Unit, A] = option.fold[TValid[Unit, A]](TValid.fail(()))(Succeed(_))
-
-  def fail[E](message: E): TValid[E, Nothing] = Failure(message)
-
   def foreach[A, E, B](list: List[A])(f: A => TValid[E, B]): TValid[E, List[B]] = foreachIterable(list)(f).map(_.toList)
-
-  def foreachChunk[A, E, B](chunk: Chunk[A])(f: A => TValid[E, B]): TValid[E, Chunk[B]] =
-    foreachIterable(chunk)(f).map(Chunk.fromIterable(_))
 
   def foreachIterable[A, E, B](iter: Iterable[A])(f: A => TValid[E, B]): TValid[E, Iterable[B]] = {
     val builder = Iterable.newBuilder[B]
@@ -59,7 +50,19 @@ object TValid {
 
   def succeed[A](value: A): TValid[Nothing, A] = Succeed(value)
 
+  def foreachChunk[A, E, B](chunk: Chunk[A])(f: A => TValid[E, B]): TValid[E, Chunk[B]] =
+    foreachIterable(chunk)(f).map(Chunk.fromIterable(_))
+
   def fromEither[E, A](either: Either[E, A]): TValid[E, A] = either.fold[TValid[E, A]](fail(_), succeed(_))
+
+  def fromOption[A](option: Option[A]): TValid[Unit, A] = option.fold[TValid[Unit, A]](TValid.fail(()))(Succeed(_))
+
+  def none: TValid[Nothing, Option[Nothing]] = succeed(None)
+
+  def unsupported(from: String, to: String): TValid[String, Nothing] =
+    fail(s"Conversion from ${from} to ${to} is not yet supported")
+
+  def fail[E](message: E): TValid[E, Nothing] = Failure(message)
 
   // TODO: can fail with a chunk of errors
   final case class Failure[E](message: E) extends TValid[E, Nothing]
