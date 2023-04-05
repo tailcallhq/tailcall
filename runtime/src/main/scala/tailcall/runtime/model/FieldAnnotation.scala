c641c4e7b638a8a411b1b25d79f310f1a85d4da5
Tushar Mathur <tusharmath@gmail.com>
2023-04-05 13:27:09 +0530

    refactor: use directive codec for Rename field annotation

--- runtime/src/main/scala/tailcall/runtime/model/FieldAnnotation.scala
+++ runtime/src/main/scala/tailcall/runtime/model/FieldAnnotation.scala
@@ -1,28 +1,24 @@
 package tailcall.runtime.model
 
-import caliban.Value
 import caliban.parsing.adt.Directive
 import tailcall.runtime.DirectiveCodec
-import zio.schema.{DeriveSchema, Schema}
+import tailcall.runtime.DirectiveCodec.DecoderSyntax
+import zio.schema.DeriveSchema
+import zio.schema.annotation.caseName
 
 sealed trait FieldAnnotation
 
 object FieldAnnotation {
   def from(directives: List[Directive]): List[FieldAnnotation] = directives.flatMap(from(_))
 
-  def from(directive: Directive): Option[FieldAnnotation] =
-    directive.name match {
-      case "rename" => directive.arguments.get("name") match {
-          case Some(Value.StringValue(value)) => Some(Rename(value))
-          case _                              => None
-        }
-      case _        => None
-    }
+  def from(directive: Directive): Option[FieldAnnotation] = directive.fromDirective[Rename].toOption
 
   def rename(name: String): FieldAnnotation = Rename(name)
 
+  @caseName("rename")
   final case class Rename(name: String) extends FieldAnnotation
 
-  implicit val schema: Schema[FieldAnnotation]                 = DeriveSchema.gen[FieldAnnotation]
-  implicit val directiveCodec: DirectiveCodec[FieldAnnotation] = DirectiveCodec.fromSchema(schema)
+  object Rename {
+    implicit val codec: DirectiveCodec[Rename] = DirectiveCodec.fromSchema(DeriveSchema.gen[Rename])
+  }
 }
