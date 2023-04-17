6d369556a0dde2962d514e2d826b2329b2e6eec9
Tushar Mathur <tusharmath@gmail.com>
2023-04-17 19:55:54 +0530

    fix: handle modify on input types

--- runtime/src/main/scala/tailcall/runtime/model/Config.scala
+++ runtime/src/main/scala/tailcall/runtime/model/Config.scala
@@ -44,9 +44,9 @@ final case class Config(version: Int = 0, server: Server = Server(), graphQL: Gr
     }
   }
 
-  def renderJson: IO[String, String]    = DSLFormat.JSON.encode(self)
-  def renderYaml: IO[String, String]    = DSLFormat.YML.encode(self)
-  def renderGraphQL: IO[String, String] = DSLFormat.GRAPHQL.encode(self)
+  def asJSONConfig: IO[String, String]    = DSLFormat.JSON.encode(self)
+  def asYAMLConfig: IO[String, String]    = DSLFormat.YML.encode(self)
+  def asGraphQLConfig: IO[String, String] = DSLFormat.GRAPHQL.encode(self)
 }
 
 object Config {
