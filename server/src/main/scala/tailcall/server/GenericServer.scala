3083ed5db37e6c47c248ba42a01dc58399ffb2dd
Tushar Mathur <tusharmath@gmail.com>
2023-04-18 17:31:26 +0530

    feature: add 0 route on server

--- server/src/main/scala/tailcall/server/GenericServer.scala
+++ server/src/main/scala/tailcall/server/GenericServer.scala
@@ -13,12 +13,12 @@ object GenericServer {
   def graphQL =
     Http.collectZIO[Request] { case req @ Method.POST -> !! / "graphql" / id =>
       for {
-        schema      <- SchemaRegistry.get(Digest.fromHex(id))
-        result      <- schema match {
+        schema <- if (id == "0") SchemaRegistry.list(0, 1).map(_.headOption) else SchemaRegistry.get(Digest.fromHex(id))
+        result <- schema match {
           case Some(value) => value.toGraphQL
           case None        => ZIO.fail(HttpError.BadRequest(s"Blueprint ${id} has not been published yet."))
         }
-        query       <- GraphQLUtils.decodeQuery(req.body)
+        query  <- GraphQLUtils.decodeQuery(req.body)
         interpreter <- result.interpreter
         res         <- interpreter.execute(query).provideLayer(DataLoader.http(Option(req)))
         _ <- ZIO.foreachDiscard(res.errors)(error => ZIO.logWarningCause("GraphQLExecutionError", Cause.fail(error)))
