e7cc5403909642c44ba15d5e20ae898b6f9c805d
Tushar Mathur <tusharmath@gmail.com>
2023-04-22 12:22:14 +0530

    Revert "chore(deps): update calibanversion to v2.1.0" (#107)

--- server/src/main/scala/tailcall/server/AdminGraphQL.scala
+++ server/src/main/scala/tailcall/server/AdminGraphQL.scala
@@ -3,7 +3,7 @@ package tailcall.server
 import caliban.introspection.adt.{__Type, __TypeKind}
 import caliban.schema.Annotations.GQLName
 import caliban.schema.{GenericSchema, Schema, Step}
-import caliban.{ResponseValue, RootResolver, Value}
+import caliban.{GraphQL, ResponseValue, RootResolver, Value}
 import tailcall.registry.SchemaRegistry
 import tailcall.runtime.lambda.~>
 import tailcall.runtime.model.{Blueprint, Digest}
@@ -16,7 +16,7 @@ import zio.schema.DynamicValue
 object AdminGraphQL {
   type AdminGraphQLEnv = SchemaRegistry
   object schema extends GenericSchema[AdminGraphQLEnv]
-  import schema.auto._
+  import schema._
 
   final case class BlueprintSpec(digest: Digest, source: Blueprint, url: String)
   object BlueprintSpec {
@@ -25,10 +25,14 @@ object AdminGraphQL {
   }
 
   @GQLName("Query")
-  final case class Query[R, E](blueprints: ZIO[R, E, List[BlueprintSpec]], digests: ZIO[R, E, List[Digest]])
+  final case class Query[R, E](
+    blueprint: Digest => ZIO[R, E, Option[BlueprintSpec]],
+    blueprints: ZIO[R, E, List[BlueprintSpec]],
+    digests: ZIO[R, E, List[Digest]],
+  )
 
   implicit val lambdaSchema: Schema[Any, DynamicValue ~> DynamicValue] = new Schema[Any, DynamicValue ~> DynamicValue] {
-    override def toType(isInput: Boolean, isSubscription: Boolean): __Type =
+    override protected[this] def toType(isInput: Boolean, isSubscription: Boolean): __Type =
       __Type(kind = __TypeKind.SCALAR, name = Some("Lambda"))
 
     override def resolve(value: DynamicValue ~> DynamicValue): Step[Any] = {
@@ -46,13 +50,18 @@ object AdminGraphQL {
   }
 
   implicit val dynamicValueSchema: Schema[Any, DynamicValue] = new Schema[Any, DynamicValue] {
-    override def toType(isInput: Boolean, isSubscription: Boolean): __Type =
+    override protected[this] def toType(isInput: Boolean, isSubscription: Boolean): __Type =
       __Type(kind = __TypeKind.SCALAR, name = Some("DynamicValue"))
-    override def resolve(value: DynamicValue): Step[Any]                   =
+    override def resolve(value: DynamicValue): Step[Any]                                   =
       Step.PureStep(Transcoder.toResponseValue(value).getOrElse(_ => Value.NullValue))
   }
 
-  val graphQL = caliban.graphQL[AdminGraphQLEnv, Query[AdminGraphQLEnv, Throwable], Unit, Unit](RootResolver(Query(
+  val graphQL = GraphQL.graphQL[AdminGraphQLEnv, Query[AdminGraphQLEnv, Throwable], Unit, Unit](RootResolver(Query(
+    digest =>
+      SchemaRegistry.get(digest).map {
+        case Some(blueprint) => Option(BlueprintSpec(digest, blueprint))
+        case None            => None
+      },
     SchemaRegistry.list(0, Int.MaxValue).map(_.map(blueprint => BlueprintSpec(blueprint.digest, blueprint))),
     SchemaRegistry.digests(0, Int.MaxValue),
   )))
