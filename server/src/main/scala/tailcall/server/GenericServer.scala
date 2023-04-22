ef7d7559c08e32b39110f0d903ce7323674ba9da
amitksingh1490 <amitksingh1490@gmail.com>
2023-04-22 16:14:16 +0530

    chore: minor cleanup

--- server/src/main/scala/tailcall/server/GenericServer.scala
+++ server/src/main/scala/tailcall/server/GenericServer.scala
@@ -14,17 +14,12 @@ object GenericServer {
     Http.collectZIO[Request] { case req @ Method.POST -> !! / "graphql" / id =>
       for {
         schema <- if (id == "0") SchemaRegistry.list(0, 1).map(_.headOption) else SchemaRegistry.get(Digest.fromHex(id))
-        timeout     <- ZIO.succeed(
-          (for {
-            blueprint <- schema
-            timeout   <- blueprint.server.globalResponseTimeout
-          } yield timeout).getOrElse(10000)
-        )
-        result      <- schema match {
+        result <- schema match {
           case Some(value) => value.toGraphQL
           case None        => ZIO.fail(HttpError.BadRequest(s"Blueprint ${id} has not been published yet."))
         }
-        query       <- GraphQLUtils.decodeQuery(req.body)
+        timeout = schema.flatMap(blueprint => blueprint.server.globalResponseTimeout).getOrElse(10000)
+        query  <- GraphQLUtils.decodeQuery(req.body)
         interpreter <- result.interpreter
         res         <- interpreter.execute(query).provideLayer(DataLoader.http(Option(req)))
           .timeoutFail(HttpError.RequestTimeout(s"Request timed out after ${timeout}ms"))(timeout.millis)
