d280e2c79731324dea68aff369e00bd164eb6c7c
Tushar Mathur <tusharmath@gmail.com>
2023-04-23 17:19:10 +0530

    test: update test for Config2Blueprint

--- runtime/src/test/scala/tailcall/runtime/Config2BlueprintSpec.scala
+++ runtime/src/test/scala/tailcall/runtime/Config2BlueprintSpec.scala
@@ -8,8 +8,9 @@ import java.net.URL
 object Config2BlueprintSpec extends ZIOSpecDefault {
   def spec =
     suite("Config2BlueprintSpec")(test("timeout") {
-      val blueprint = Config(server = Server(baseURL = Some(new URL("http://localhost:8080")), timeout = Some(1000)))
-        .toBlueprint
-      assertTrue(blueprint.server.globalResponseTimeout == Some(1000))
+      val timeout = Config(server = Server(baseURL = Some(new URL("http://localhost:8080")), timeout = Some(1000)))
+        .toBlueprint.toOption.flatMap(_.server.globalResponseTimeout)
+
+      assertTrue(timeout == Option(1000))
     })
 }
