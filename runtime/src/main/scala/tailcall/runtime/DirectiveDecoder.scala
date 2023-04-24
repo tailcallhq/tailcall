bc0d40ec5bc04e2868235cd2d0210a27f347463f
Tushar Mathur <tusharmath@gmail.com>
2023-04-24 12:58:51 +0530

    refactor: convert DirectiveDecoder to a case class

--- runtime/src/main/scala/tailcall/runtime/DirectiveDecoder.scala
+++ runtime/src/main/scala/tailcall/runtime/DirectiveDecoder.scala
@@ -6,52 +6,54 @@ import zio.json.{DecoderOps, EncoderOps, JsonDecoder}
 import zio.schema.Schema
 import zio.schema.annotation.caseName
 
-trait DirectiveDecoder[A] {
-  def decode(directive: Directive): TValid[String, A]
-  final def map[B](ab: A => B): DirectiveDecoder[B] = { (directive: Directive) => decode(directive).map(ab) }
+final case class DirectiveDecoder[A](decode: Directive => TValid[String, A], name: String) {
+  def map[B](ab: A => B): DirectiveDecoder[B] = copy(decode = decode.andThen(_.map(ab)))
 }
 
 object DirectiveDecoder {
-  def collect[A](f: Directive => TValid[String, A]): DirectiveDecoder[A] = { (directive: Directive) => f(directive) }
 
   def gen[A: Schema]: DirectiveDecoder[A] = fromSchema(Schema[A])
 
   def fromSchema[A](schema: Schema[A]): DirectiveDecoder[A] = {
-    val decoder  = zio.schema.codec.JsonCodec.jsonDecoder(schema)
-    val nameHint = schema.annotations.collectFirst { case caseName(name) => name }
-    DirectiveDecoder { directive =>
-      for {
-        name <- schema match {
-          case schema: Schema.Enum[_]   => TValid.succeed(schema.id.name)
-          case schema: Schema.Record[_] => TValid.succeed(schema.id.name)
-          case _                        => TValid.fail("Can only decode sealed traits and case classes as directives")
-        }
-        a    <- fromJsonDecoder(nameHint.getOrElse(name), decoder).decode(directive)
-      } yield a
+    val jsonDecoder = zio.schema.codec.JsonCodec.jsonDecoder(schema)
+    val nameHint    = schema.annotations.collectFirst { case caseName(name) => name }
+    val schemaName  = schema match {
+      case schema: Schema.Enum[_]   => schema.id.name
+      case schema: Schema.Record[_] => schema.id.name
+      case _ => throw new RuntimeException("Can only decode sealed traits and case classes as directives")
     }
+
+    val name    = nameHint.getOrElse(schemaName)
+    val decoder = fromJsonDecoder(nameHint.getOrElse(name), jsonDecoder)
+    DirectiveDecoder(decoder.decode(_), name)
   }
 
   def fromJsonDecoder[A](name: String, decoder: JsonDecoder[A]): DirectiveDecoder[A] =
-    DirectiveDecoder { directive =>
-      for {
-        _    <-
-          if (name != directive.name) TValid.fail(s"Expected directive name to be $name but was ${directive.name}")
-          else TValid.succeed(())
-        args <- TValid.fromEither(directive.arguments.toJsonAST)
-        a    <- TValid.fromEither(args.toJson.fromJson[A](decoder))
-      } yield a
-    }
+    DirectiveDecoder(
+      directive =>
+        for {
+          _    <-
+            if (name != directive.name) TValid.fail(s"Expected directive name to be $name but was ${directive.name}")
+            else TValid.succeed(())
+          args <- TValid.fromEither(directive.arguments.toJsonAST)
+          a    <- TValid.fromEither(args.toJson.fromJson[A](decoder))
+        } yield a,
+      name,
+    )
 
+  // FIXME: Drop this decoder
   def fromJsonListDecoder[A](decoder: JsonDecoder[A]): DirectiveDecoder[List[A]] =
-    DirectiveDecoder { directive =>
-      for {
-        inputValue <- directive.arguments.get("value") match {
-          case Some(inputValue) => TValid.succeed(inputValue)
-          case None             => TValid.fail(s"key `value` was not found in directive ${directive.name}")
-        }
-        a          <- TValid.fromEither(inputValue.toJson.fromJson[List[A]](JsonDecoder.list(decoder)))
-      } yield a
-    }
+    DirectiveDecoder(
+      directive =>
+        for {
+          inputValue <- directive.arguments.get("value") match {
+            case Some(inputValue) => TValid.succeed(inputValue)
+            case None             => TValid.fail(s"key `value` was not found in directive ${directive.name}")
+          }
+          a          <- TValid.fromEither(inputValue.toJson.fromJson[List[A]](JsonDecoder.list(decoder)))
+        } yield a,
+      "NO_NAME_DIRECTIVE",
+    )
 
   def apply[A](implicit decoder: DirectiveDecoder[A]): DirectiveDecoder[A] = decoder
 
