7bea06ee7ebc11b8ed478e33abcee543b5691c39
Tushar Mathur <tusharmath@gmail.com>
2023-04-24 12:48:34 +0530

    refactor: making DirectiveEncoder a case class

--- runtime/src/main/scala/tailcall/runtime/DirectiveEncoder.scala
+++ runtime/src/main/scala/tailcall/runtime/DirectiveEncoder.scala
@@ -7,68 +7,45 @@ import zio.json.{DecoderOps, JsonEncoder}
 import zio.schema.Schema
 import zio.schema.annotation.caseName
 
-trait DirectiveEncoder[A] {
-  self =>
-  final def contramap[B](ab: B => A): DirectiveEncoder[B] =
-    new DirectiveEncoder[B] {
-      override def encode(a: B): TValid[String, Directive] = self.encode(ab(a))
-      override def name: String                            = self.name
-    }
-
-  def encode(a: A): TValid[String, Directive]
-  def name: String
-
-  final def withName(newName: String): DirectiveEncoder[A] =
-    new DirectiveEncoder[A] {
-      override def encode(a: A): TValid[String, Directive] = self.encode(a)
-      override def name: String                            = newName
-    }
+final case class DirectiveEncoder[A](encode: A => TValid[String, Directive], name: String) {
+  def contramap[B](ab: B => A): DirectiveEncoder[B] = DirectiveEncoder(ab andThen encode, name)
+  def withName(name: String): DirectiveEncoder[A]   = copy(name = name)
 }
 
 object DirectiveEncoder {
-  def gen[A: Schema]: DirectiveEncoder[A] = fromSchema(Schema[A])
-
-  def fromSchema[A](schema: Schema[A]): DirectiveEncoder[A] = {
-    val encoder = zio.schema.codec.JsonCodec.jsonEncoder(schema)
-
-    new DirectiveEncoder[A] {
-      override def encode(a: A): TValid[String, Directive] = fromJsonEncoder(name, encoder).encode(a)
-
-      override def name: String = {
-        val nameHint   = schema.annotations.collectFirst { case caseName(name) => name }
-        val schemaName = schema match {
-          case schema: Schema.Enum[_]   => schema.id.name
-          case schema: Schema.Record[_] => schema.id.name
-          case _ => throw new RuntimeException("Can only encode sealed traits and case classes as directives")
-        }
-        nameHint.getOrElse(schemaName)
-      }
-    }
-  }
+  implicit def encoder[A](implicit codec: DirectiveCodec[A]): DirectiveEncoder[A] = codec.encoder
 
   def fromJsonEncoder[A](directiveName: String, encoder: JsonEncoder[A]): DirectiveEncoder[A] =
-    new DirectiveEncoder[A] {
-      override def encode(a: A): TValid[String, Directive] = {
-        for {
-          args <- TValid.fromEither(encoder.encodeJson(a).fromJson[Map[String, InputValue]])
-        } yield Directive(name, args)
-      }
+    DirectiveEncoder(
+      a =>
+        TValid.fromEither(encoder.encodeJson(a).fromJson[Map[String, InputValue]])
+          .map(args => Directive(directiveName, args)),
+      directiveName,
+    )
 
-      override def name: String = directiveName
+  // FIXME: This function can be deprecated
+  def fromJsonListEncoder[A](directiveName: String, encoder: JsonEncoder[A]): DirectiveEncoder[List[A]] = {
+    val jsonEncoder = JsonEncoder.list(encoder)
+    DirectiveEncoder(
+      a =>
+        TValid.fromEither(jsonEncoder.encodeJson(a).fromJson[InputValue])
+          .map(args => Directive(directiveName, Map("value" -> args))),
+      directiveName,
+    )
+  }
+
+  def fromSchema[A](schema: Schema[A]): DirectiveEncoder[A] = {
+    val jsonEncoder = zio.schema.codec.JsonCodec.jsonEncoder(schema)
+    val nameHint    = schema.annotations.collectFirst { case caseName(name) => name }
+    val schemaName  = schema match {
+      case schema: Schema.Enum[_]   => schema.id.name
+      case schema: Schema.Record[_] => schema.id.name
+      case _ => throw new RuntimeException("Can only encode sealed traits and case classes as directives")
     }
+    val name        = nameHint.getOrElse(schemaName)
+    val encoder     = fromJsonEncoder(name, jsonEncoder)
+    DirectiveEncoder(a => encoder.encode(a), name)
+  }
 
-  // FIXME: this should be removed
-  def fromJsonListEncoder[A](directiveName: String, encoder: JsonEncoder[A]): DirectiveEncoder[List[A]] =
-    new DirectiveEncoder[List[A]] {
-      override def encode(a: List[A]): TValid[String, Directive] =
-        for {
-          args <- TValid.fromEither(JsonEncoder.list(encoder).encodeJson(a).fromJson[InputValue])
-        } yield Directive(name, Map("value" -> args))
-
-      override def name: String = directiveName
-    }
-
-  def apply[A](implicit encoder: DirectiveEncoder[A]): DirectiveEncoder[A] = encoder
-
-  implicit def encoder[A](implicit codec: DirectiveCodec[A]): DirectiveEncoder[A] = codec.encoder
+  def gen[A: Schema]: DirectiveEncoder[A] = fromSchema(Schema[A])
 }
