b66c0d6427bb7332cd111f47f62b622109d42add
Tushar Mathur <tusharmath@gmail.com>
2023-04-24 14:41:46 +0530

    refactor: drop fromJsonListCodec

--- runtime/src/main/scala/tailcall/runtime/DirectiveEncoder.scala
+++ runtime/src/main/scala/tailcall/runtime/DirectiveEncoder.scala
@@ -23,24 +23,13 @@ object DirectiveEncoder {
           .map(args => Directive(directiveName, args)),
     )
 
-  // FIXME: This function can be deprecated
-  def fromJsonListEncoder[A](directiveName: String, encoder: JsonEncoder[A]): DirectiveEncoder[List[A]] = {
-    val jsonEncoder = JsonEncoder.list(encoder)
-    DirectiveEncoder(
-      directiveName,
-      a =>
-        TValid.fromEither(jsonEncoder.encodeJson(a).fromJson[InputValue])
-          .map(args => Directive(directiveName, Map("value" -> args))),
-    )
-  }
-
   def fromSchema[A](schema: Schema[A]): DirectiveEncoder[A] = {
     val jsonEncoder = zio.schema.codec.JsonCodec.jsonEncoder(schema)
     val nameHint    = schema.annotations.collectFirst { case caseName(name) => name }
     val schemaName  = schema match {
       case schema: Schema.Enum[_]   => schema.id.name
       case schema: Schema.Record[_] => schema.id.name
-      case _ => throw new RuntimeException("Can only encode sealed traits and case classes as directives")
+      case _                        => throw new RuntimeException("Can only encode case classes as directives")
     }
     val name        = nameHint.getOrElse(schemaName)
     val encoder     = fromJsonEncoder(name, jsonEncoder)
