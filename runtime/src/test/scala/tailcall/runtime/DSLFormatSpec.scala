137f115c404bd31758549b938d4155d2f9b7dfb2
Tushar Mathur <tusharmath@gmail.com>
2023-04-24 13:51:32 +0530

    feature: add Http in Config.Field

--- runtime/src/test/scala/tailcall/runtime/DSLFormatSpec.scala
+++ runtime/src/test/scala/tailcall/runtime/DSLFormatSpec.scala
@@ -1,6 +1,7 @@
 package tailcall.runtime
 
 import tailcall.runtime.model.Config.{Field, Type}
+import tailcall.runtime.model.Step.Http
 import tailcall.runtime.model.{Config, Path, Step}
 import tailcall.runtime.service.DSLFormat
 import zio.ZIO
@@ -14,25 +15,53 @@ object DSLFormatSpec extends ZIOSpecDefault {
       expected <- config.asJSONConfig
     } yield assertTrue(actual == expected)
 
+  private def assertGraphQL(config: Config, expected: String): ZIO[Any, String, TestResult] =
+    for { actual <- config.asGraphQLConfig } yield assertTrue(actual == expected)
+
   def spec =
-    suite("GraphQL")(test("http directive") {
-      val doc = """
-                  |type User {
-                  | id: Int
-                  | name: String
-                  |}
-                  |
-                  |type Query {
-                  |  foo: [User] @http(path: "/users")
-                  |}
-                  |""".stripMargin
+    suite("GraphQL")(
+      test("http directive") {
+        val doc = """
+                    |type User {
+                    | id: Int
+                    | name: String
+                    |}
+                    |
+                    |type Query {
+                    |  foo: [User] @http(path: "/users")
+                    |}
+                    |""".stripMargin
 
-      val expected = Config.empty.withTypes(
-        "User"  -> Type("id" -> Field.ofType("Int"), "name" -> Field.ofType("String")),
-        "Query" -> Type("foo" -> Field.ofType("User").asList.withSteps(Step.Http(Path.unsafe.fromString("/users")))),
-      )
+        val expected = Config.empty.withTypes(
+          "User"  -> Type("id" -> Field.ofType("Int"), "name" -> Field.ofType("String")),
+          "Query" -> Type("foo" -> Field.ofType("User").asList.withSteps(Step.Http(Path.unsafe.fromString("/users")))),
+        )
 
-      assertGraphQL(doc, expected)
-    })
+        assertGraphQL(doc, expected)
+      },
+      suite("asGraphQLConfig")(test("encodes Http") {
+        val config = Config.default.withTypes(
+          "User"  -> Type("id" -> Field.ofType("Int"), "name" -> Field.ofType("String")),
+          "Query" -> Type("foo" -> Field.ofType("User").asList.withHttp(Http(Path.unsafe.fromString("/users")))),
+        )
+
+        val expected = """
+                         |schema {
+                         |  query: Query
+                         |}
+                         |
+                         |type Query {
+                         |  foo: [User] @http(path: "/users")
+                         |}
+                         |
+                         |type User {
+                         |  id: Int
+                         |  name: String
+                         |}
+                         |""".stripMargin.trim
+
+        assertGraphQL(config, expected)
+      }),
+    )
 
 }
