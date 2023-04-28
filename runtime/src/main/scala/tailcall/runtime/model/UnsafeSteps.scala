46fd1467aec85699ca1fb33fefec198667042634
Tushar Mathur <tusharmath@gmail.com>
2023-04-28 13:31:04 +0530

    refactor: rearrange code

--- runtime/src/main/scala/tailcall/runtime/model/UnsafeSteps.scala
+++ runtime/src/main/scala/tailcall/runtime/model/UnsafeSteps.scala
@@ -19,6 +19,8 @@ object UnsafeSteps {
 
   implicit val directiveCodec: DirectiveCodec[UnsafeSteps] = DirectiveCodec.fromJsonCodec("unsafe", jsonCodec)
 
+  def apply(steps: Operation*): UnsafeSteps = UnsafeSteps(steps.toList)
+
   sealed trait Operation {
     self =>
     def compress: Operation =
@@ -42,11 +44,6 @@ object UnsafeSteps {
     @jsonHint("lambda")
     final case class LambdaFunction(f: DynamicValue ~>> DynamicValue) extends Operation
 
-    object LambdaFunction {
-      implicit lazy val jsonCodec: JsonCodec[LambdaFunction] = zio.schema.codec.JsonCodec
-        .jsonCodec(Schema[DynamicValue ~>> DynamicValue]).transform(LambdaFunction(_), _.f)
-    }
-
     @jsonHint("http")
     final case class Http(
       path: Path,
@@ -55,24 +52,30 @@ object UnsafeSteps {
       output: Option[TSchema] = None,
       body: Option[String] = None,
     ) extends Operation {
+      def withBody(body: Option[String]): Http = copy(body = body)
+
       def withInput(input: Option[TSchema]): Http = copy(input = input)
 
       def withMethod(method: Method): Http = copy(method = Option(method))
 
       def withOutput(output: Option[TSchema]): Http = copy(output = output)
-
-      def withBody(body: Option[String]): Http = copy(body = body)
     }
 
     @jsonHint("transform")
     final case class Transform(transformation: JsonT) extends Operation
+
+    object LambdaFunction {
+      implicit lazy val jsonCodec: JsonCodec[LambdaFunction] = zio.schema.codec.JsonCodec
+        .jsonCodec(Schema[DynamicValue ~>> DynamicValue]).transform(LambdaFunction(_), _.f)
+    }
 
     object Transform {
       implicit val jsonCodec: JsonCodec[Transform] = JsonCodec[JsonT].transform(Transform(_), _.transformation)
     }
 
     object Http {
-      implicit val jsonCodec: JsonCodec[Http] = DeriveJsonCodec.gen[Http]
+      implicit val jsonCodec: JsonCodec[Http]      = DeriveJsonCodec.gen[Http]
+      implicit val directive: DirectiveCodec[Http] = DirectiveCodec.fromJsonCodec("http", jsonCodec)
 
       def fromEndpoint(endpoint: Endpoint): Http =
         Http(
@@ -82,10 +85,6 @@ object UnsafeSteps {
           output = endpoint.output,
           body = endpoint.body.flatMap(Mustache.syntax.printString(_).toOption),
         )
-
-      implicit val directive: DirectiveCodec[Http] = DirectiveCodec.fromJsonCodec("http", jsonCodec)
     }
   }
-
-  def apply(steps: Operation*): UnsafeSteps = UnsafeSteps(steps.toList)
 }
