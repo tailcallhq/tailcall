ffb3a59be5ed9e6aeafc31def4c56c92ef6e9e96
amitksingh1490 <amitksingh1490@gmail.com>
2023-05-01 12:31:48 +0530

    refactor: cleanup tests

--- runtime/src/test/scala/tailcall/runtime/HttpCacheSpec.scala
+++ runtime/src/test/scala/tailcall/runtime/HttpCacheSpec.scala
@@ -15,31 +15,26 @@ object HttpCacheSpec extends ZIOSpecDefault {
   def spec =
     suite("HttpCacheSpec Cache-Control")(
       test("ttl") {
-        //
         val ttl      = HttpCache.ttl(Response.ok.addHeaders(headers = Header("Cache-Control", "max-age=1000")))
         val expected = Some(Duration.fromSeconds(1000))
         assert(ttl)(equalTo(expected))
       },
       test("ttl cache-control") {
-        //
         val ttl      = HttpCache.ttl(Response.ok.addHeaders(headers = Header("cache-control", "max-age=1000")))
         val expected = Some(Duration.fromSeconds(1000))
         assert(ttl)(equalTo(expected))
       },
       test("ttl cache-control private") {
-        //
         val ttl      = HttpCache.ttl(Response.ok.addHeaders(headers = Header("cache-control", "max-age=1000, private")))
         val expected = None
         assert(ttl)(equalTo(expected))
       },
       test("expires -1") {
-        //
         val ttl      = HttpCache.ttl(Response.ok.addHeaders(headers = Header("expires", "-1")))
         val expected = None
         assert(ttl)(equalTo(expected))
       },
       test("cache-control and expires") {
-        //
         lazy val expiry = Instant.now().plusSeconds(1000).toString
         val ttl         = HttpCache.ttl(
           Response.ok.addHeaders(headers = Headers(Header("expires", expiry), Header("cache-control", "max-age=2000")))
@@ -48,15 +43,11 @@ object HttpCacheSpec extends ZIOSpecDefault {
         assert(ttl)(equalTo(expected))
       },
       test("expires after 1000 second") {
-        //
-        val p        = for {
-          now        <- ZIO.succeed(Instant.parse("2021-01-01T00:00:00Z"))
-          headerTime <- ZIO
-            .succeed(new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss z").format(now.toEpochMilli + 1000000L))
-          _          <- TestClock.setTime(now)
-        } yield HttpCache.ttl(Response.ok.addHeaders(headers = Header("expires", headerTime)), now)
-        val expected = Some(Duration.fromSeconds(1000))
-        assertZIO(p)(equalTo(expected))
+        val now        = Instant.parse("2021-01-01T00:00:00Z")
+        val headerTime = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss z").format(now.toEpochMilli + 1000000L)
+        val p          = HttpCache.ttl(Response.ok.addHeaders(headers = Header("expires", headerTime)), now)
+        val expected   = Some(Duration.fromSeconds(1000))
+        assert(p)(equalTo(expected))
       },
     )
 
