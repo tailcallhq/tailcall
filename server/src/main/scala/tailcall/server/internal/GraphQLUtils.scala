35f211ca5cb30906d90d09fc4b877982d41d2624
amit <amitksingh1490@gmail.com>
2023-06-26 15:59:13 +0530

    refactor: cleanup decoding in case of persisted queries

--- server/src/main/scala/tailcall/server/internal/GraphQLUtils.scala
+++ server/src/main/scala/tailcall/server/internal/GraphQLUtils.scala
@@ -1,11 +1,10 @@
 package tailcall.server.internal
 
-import caliban.GraphQLRequest
+import caliban.{GraphQLRequest, InputValue}
 import zio._
 import zio.http._
 import zio.http.model.HttpError
 import zio.json._
-import zio.json.ast.Json
 
 import java.nio.charset.StandardCharsets
 
@@ -20,27 +19,13 @@ object GraphQLUtils {
     } yield req
 
   def decodeRequest(queryParams: QueryParams): ZIO[Any, Throwable, GraphQLRequest] = {
-    def updateJson(either: Either[String, Json]): Json = {
-      either match {
-        case Left(_)     => Json.Null
-        case Right(json) => json match {
-            case obj: Json.Obj =>
-              val updatedFields = obj.fields.map { case (key, value) => key -> updateJson(Right(value)) }
-              Json.Obj(updatedFields: _*)
-            case str: Json.Str => str.asString.fold(json)(parsedString => parsedString.fromJson[Json].getOrElse(json))
-            case _             => json
-          }
-      }
-    }
-
-    val updatedJson = updateJson(queryParams.map(x => (x._1, x._2.mkString(","))).toMap.toJsonAST).toJson
-
-    for {
-      req <- updatedJson.fromJson[GraphQLRequest] match {
-        case Left(value)  => ZIO.fail(HttpError.BadRequest(value))
-        case Right(value) => ZIO.succeed(value)
-      }
-    } yield req
+    val query         = queryParams.get("query").flatMap(_.headOption)
+    val extensions    = queryParams.get("extensions").flatMap(_.headOption)
+      .flatMap(_.fromJson[Map[String, InputValue]].toOption)
+    val variables     = queryParams.get("variables").flatMap(_.headOption)
+      .flatMap(_.fromJson[Map[String, InputValue]].toOption)
+    val operationName = queryParams.get("operationName").flatMap(_.headOption)
+    ZIO.succeed(GraphQLRequest(query, operationName, variables, extensions))
   }
 
 }
