59596ae15dc60ff3365cb86394b8371c2f50f237
Tushar Mathur <tusharmath@gmail.com>
2023-06-26 15:51:24 +0530

    refactor: add update method in HttpContext

--- runtime/src/main/scala/tailcall/runtime/service/HttpContext.scala
+++ runtime/src/main/scala/tailcall/runtime/service/HttpContext.scala
@@ -4,20 +4,19 @@ import tailcall.runtime.http.{HttpClient, Request}
 import tailcall.runtime.service.HttpContext.State
 import zio._
 import zio.http.model.Headers
-import zio.http.{Request => ZRequest, Response}
+import zio.http.{Response, Request => ZRequest}
 
 trait HttpContext {
   def dataLoader: DataLoader[Any, Throwable, Request, Response]
   def requestHeaders: Headers
-  final def getState: UIO[State] = updateState(identity)
-
-  def updateState(state: State => State): UIO[State]
+  final def getState: UIO[State] = update(identity)
+  def update(state: State => State): UIO[State]
 }
 
 object HttpContext {
-  def default: ZLayer[Any, Throwable, HttpContext] = HttpClient.default >>> live(None)
-
-  def getState: ZIO[HttpContext, Nothing, State] = ZIO.serviceWithZIO(_.getState)
+  def default: ZLayer[Any, Throwable, HttpContext]                = HttpClient.default >>> live(None)
+  def update(f: State => State): ZIO[HttpContext, Nothing, State] = ZIO.serviceWithZIO(_.update(f))
+  def getState: ZIO[HttpContext, Nothing, State]                  = ZIO.serviceWithZIO(_.getState)
 
   def live(req: Option[ZRequest]): ZLayer[HttpClient, Nothing, HttpContext] =
     DataLoader.http(req) >>> ZLayer {
@@ -27,12 +26,13 @@ object HttpContext {
       } yield Live(dataLoader, req.map(_.headers).getOrElse(Headers.empty), ref)
     }
 
-  final case class State(value: Option[Duration])
+  final case class State(cacheMaxAge: Option[Duration])
+
   final case class Live(
     dataLoader: DataLoader[Any, Throwable, Request, Response],
     requestHeaders: Headers,
     ref: Ref[State],
   ) extends HttpContext {
-    override def updateState(state: State => State): UIO[State] = ref.updateAndGet(state)
+    override def update(state: State => State): UIO[State] = ref.updateAndGet(state)
   }
 }
