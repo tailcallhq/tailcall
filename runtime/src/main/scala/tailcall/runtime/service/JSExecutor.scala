262cc5fa7f35ed0fe87f4a363998178ba5923f54
Tushar Mathur <tusharmath@gmail.com>
2023-06-27 15:36:00 +0530

    test: add cancellation support

--- runtime/src/main/scala/tailcall/runtime/service/JSExecutor.scala
+++ runtime/src/main/scala/tailcall/runtime/service/JSExecutor.scala
@@ -2,7 +2,7 @@ package tailcall.runtime.service
 
 import org.graalvm.polyglot._
 import zio.json.{DecoderOps, EncoderOps, JsonCodec}
-import zio.{Scope, Task, ZIO, ZLayer}
+import zio.{Duration, Task, ZIO, ZLayer}
 
 trait JSExecutor {
   def execute(script: String, input: String): Task[String]
@@ -15,16 +15,29 @@ object JSExecutor {
   def execute[A: JsonCodec](script: String, input: A): ZIO[JSExecutor, Throwable, A] =
     ZIO.serviceWithZIO[JSExecutor](_.execute(script, input))
 
-  def live: ZLayer[Scope, Nothing, JSExecutor] =
-    ZLayer {
-      for {
-        ctx <- ZIO.succeed(Context.create())
-        _   <- ZIO.addFinalizer(ZIO.attemptBlocking(ctx.close(true)).orDie)
-      } yield Live(ctx)
+  def live(timeout: Duration): ZLayer[Any, Nothing, JSExecutor] = ZLayer.succeed(new Live(timeout))
+
+  final class Live(timeout: Duration) extends JSExecutor {
+
+    override def execute(script: String, input: String): Task[String] = {
+      ZIO.scoped {
+        for {
+          ctx    <- ZIO.succeed(Context.create())
+          f      <- runScript(script, input, ctx).fork
+          _      <- interrupt(ctx).fork
+          result <- f.join
+        } yield result
+      }
     }
 
-  final case class Live(ctx: Context) extends JSExecutor {
-    override def execute(script: String, input: String): Task[String] = {
+    private def interrupt(ctx: Context): Task[Unit] = {
+      ZIO.attemptBlocking {
+        Thread.sleep(timeout.toMillis)
+        ctx.interrupt(timeout)
+      }
+    }
+
+    private def runScript(script: String, input: String, ctx: Context): Task[String] = {
       ZIO.attemptBlocking {
         val source = s"""(function (input) {return JSON.stringify(($script)(JSON.parse(input))); })"""
         ctx.eval("js", source).execute(input).asString()
--- runtime/src/test/scala/tailcall/runtime/JSExecutorSpec.scala
+++ runtime/src/test/scala/tailcall/runtime/JSExecutorSpec.scala
@@ -2,9 +2,9 @@ package tailcall.runtime
 
 import tailcall.runtime.service.JSExecutor
 import tailcall.test.TailcallSpec
-import zio.Scope
-import zio.test.Assertion.equalTo
+import zio.test.Assertion.{equalTo, isLeft}
 import zio.test.{Spec, TestEnvironment, assertZIO}
+import zio.{Scope, durationInt}
 
 object JSExecutorSpec extends TailcallSpec {
   override def spec: Spec[TestEnvironment with Scope, Any] =
@@ -14,11 +14,9 @@ object JSExecutorSpec extends TailcallSpec {
         assertZIO(program)(equalTo(101))
       },
       test("long running") {
-        val program = for {
-          fib    <- JSExecutor.execute("function (a) { while(true) {a = a + 1}  return a}", 100).fork
-          result <- fib.join
-        } yield result
-        assertZIO(program)(equalTo(101))
+        val program = JSExecutor.execute("function (a) { while (true) {a = a + 1}  return a}", 100)
+          .mapError(_.getMessage).either
+        assertZIO(program)(isLeft(equalTo("Execution got interrupted")))
       },
-    ).provide(JSExecutor.live)
+    ).provide(JSExecutor.live(5 millis))
 }
