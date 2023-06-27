a752bf2e18da3daf27658db81facbcf0dcfdd5fc
Tushar Mathur <tusharmath@gmail.com>
2023-06-27 13:19:38 +0530

    test: add a long running test

--- runtime/src/test/scala/tailcall/runtime/JSExecutorSpec.scala
+++ runtime/src/test/scala/tailcall/runtime/JSExecutorSpec.scala
@@ -8,8 +8,17 @@ import zio.test.{Spec, TestEnvironment, assertZIO}
 
 object JSExecutorSpec extends TailcallSpec {
   override def spec: Spec[TestEnvironment with Scope, Any] =
-    suite("JSExecutor")(test("basic evaluation") {
-      val program = JSExecutor.execute("function (a) { return a + 1}", 100)
-      assertZIO(program)(equalTo(101))
-    }).provide(JSExecutor.live, Scope.default)
+    suite("JSExecutor")(
+      test("basic evaluation") {
+        val program = JSExecutor.execute("function (a) { return a + 1}", 100)
+        assertZIO(program)(equalTo(101))
+      },
+      test("long running") {
+        val program = for {
+          fib    <- JSExecutor.execute("function (a) { while(true) {a = a + 1}  return a}", 100).fork
+          result <- fib.join
+        } yield result
+        assertZIO(program)(equalTo(101))
+      },
+    ).provide(JSExecutor.live)
 }
