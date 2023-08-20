1b28077643180b30a1b9b4c1a100df275b78f6b6
Amit Singh <amitksingh1490@gmail.com>
2023-08-20 11:09:28 +0530

    refactor: cleanup routes (#276)

--- tailcall/src/main/scala/tailcall/server/GenericServer.scala
+++ tailcall/src/main/scala/tailcall/server/GenericServer.scala
@@ -14,26 +14,31 @@ object GenericServer {
   def graphQL(
     timeout: Duration
   ): HttpApp[HttpClient with GraphQLGenerator with ApolloPersistence with InterpreterRegistry, Throwable] =
-    Http.collectZIO[Request] { case req @ method -> !! / "graphql" / hex =>
-      for {
-        option      <- InterpreterRegistry.get(hex)
-        int         <- ZIO.fromOption(option).orElseFail(HttpError.BadRequest(s"Blueprint ${hex} was not found."))
-        gReq        <-
-          if (req.url.queryParams != QueryParams.empty) GraphQLUtils.decodeRequest(req.url.queryParams)
-          else GraphQLUtils.decodeRequest(req.body)
-        persistence <- ZIO.service[ApolloPersistence]
-        res         <- (for {
-          res <- int.executeRequest(gReq).map(res => res.copy(errors = res.errors.map(toBetterError)))
-            .timeoutFail(HttpError.RequestTimeout(s"Request timed out after ${timeout.toMillis()}ms"))(timeout)
-          _ <- ZIO.foreachDiscard(res.errors)(error => ZIO.logWarningCause("GraphQLExecutionError", Cause.fail(error)))
-          maxAge <- HttpContext.getState.map(_.cacheMaxAge)
-          jsonResponse = Response.json(res.toJson)
-        } yield
-          if (method == Method.POST || res.errors.nonEmpty) jsonResponse
-          else jsonResponse.withCacheControlMaxAge(maxAge.getOrElse(0 seconds)))
-          .provideLayer(HttpContext.live(Option(req)) ++ ZLayer.succeed(persistence))
-      } yield res
+    Http.collectZIO[Request] {
+      case req @ method -> !! / "graphql"       => genericServer(timeout, req, method, "0")
+      case req @ method -> !! / "graphql" / hex => genericServer(timeout, req, method, hex)
     }
+
+  private def genericServer(timeout: zio.Duration, req: Request, method: Method, hex: String) = {
+    for {
+      option      <- InterpreterRegistry.get(hex)
+      int         <- ZIO.fromOption(option).orElseFail(HttpError.BadRequest(s"Blueprint ${hex} was not found."))
+      gReq        <-
+        if (req.url.queryParams != QueryParams.empty) GraphQLUtils.decodeRequest(req.url.queryParams)
+        else GraphQLUtils.decodeRequest(req.body)
+      persistence <- ZIO.service[ApolloPersistence]
+      res         <- (for {
+        res <- int.executeRequest(gReq).map(res => res.copy(errors = res.errors.map(toBetterError)))
+          .timeoutFail(HttpError.RequestTimeout(s"Request timed out after ${timeout.toMillis()}ms"))(timeout)
+        _   <- ZIO.foreachDiscard(res.errors)(error => ZIO.logWarningCause("GraphQLExecutionError", Cause.fail(error)))
+        maxAge <- HttpContext.getState.map(_.cacheMaxAge)
+        jsonResponse = Response.json(res.toJson)
+      } yield
+        if (method == Method.POST || res.errors.nonEmpty) jsonResponse
+        else jsonResponse.withCacheControlMaxAge(maxAge.getOrElse(0 seconds)))
+        .provideLayer(HttpContext.live(Option(req)) ++ ZLayer.succeed(persistence))
+    } yield res
+  }
 
   private def toBetterError(error: CalibanError): CalibanError = {
     error match {
