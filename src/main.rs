cd18b31c212bed8a3a6e6f0b17a8487e0034c25b
Tushar Mathur <tusharmath@gmail.com>
2023-09-09 17:40:31 +0530

    handle CLI Error from Main by downcasting

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,17 @@ async fn main() -> Result<()> {
         Ok(_) => {}
         Err(error) => {
             // Ensure all errors are converted to CLIErrors before being printed.
-            let cli_error = error.downcast::<CLIError>()?;
+            let cli_error = match error.downcast::<CLIError>() {
+                Ok(cli_error) => cli_error,
+                Err(error) => {
+                    let sources = error
+                        .source()
+                        .map(|error| vec![CLIError::new(error.to_string().as_str())])
+                        .unwrap_or_default();
+
+                    CLIError::new(&error.to_string()).caused_by(sources)
+                }
+            };
             eprintln!("{}", cli_error.color(true));
             std::process::exit(1);
         }
