1eb29cba2bf1233c9a789200a584eaaad64592c4
Sujeet Sreenivasan <sujeetsr@gmail.com>
2023-09-11 10:38:34 -0700

    wip: run benchmark, comment with result

---
+++ .github/benchmark/wrk-output-to-md.js
@@ -0,0 +1,51 @@
+const fs = require('fs');
+
+const separator = '|';
+let markdown = '';
+
+function isTableRow(line) {
+  return line.trim().startsWith('Latency') || line.trim().startsWith('Req/Sec');
+}
+
+function isTableHeader(line) { 
+  return line.trim().startsWith('Thread Stats');
+}
+
+function isTotalRequestLine(line) {
+  return line.includes('requests in');
+}
+
+function getHeader() {
+  return '|Thread Stats|Avg|Stdev|Max|+/- Stdev|\n|---|---|---|---|---|\n';
+}
+
+function getRow(line) {
+  const words = line.trim().split(/[\s]+/);
+  let row = separator;
+  words.forEach((word) => {
+    const trimmedWord = word.trim();
+    if (trimmedWord.length > 0) {
+      row = row + trimmedWord + separator;
+    }
+  });
+  return row + '\n';
+
+}
+
+const lines = fs.readFileSync('./wrk-output.txt', 'utf8');
+lines.split('\n').forEach((line) => {
+  if (isTableHeader(line)) {
+    markdown = markdown + getHeader();
+  } else if (isTableRow(line)) {
+    markdown = markdown + getRow(line);
+  } else {
+    if (isTotalRequestLine(line)) {
+      markdown = markdown + '\n';
+    }
+    markdown = markdown + line.trim() + '\n\n';
+  }
+
+});
+
+console.log(markdown);
+fs.writeFileSync('./wrk-output.md', markdown);
