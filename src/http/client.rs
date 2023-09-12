b13c06e5570551421c663b1e738920c9c00734ba
Tushar Mathur <tusharmath@gmail.com>
2023-09-12 15:57:24 +0530

    clean up request and client

--- src/http/client.rs
+++ src/http/client.rs
@@ -31,7 +31,7 @@ impl HttpClient {
             .tcp_keepalive(Some(Duration::from_secs(5)))
             .timeout(Duration::from_secs(60))
             .connect_timeout(Duration::from_secs(60))
-            .user_agent("Tailcall/1.0");
+            .user_agent("tailcall/1.0");
 
         if let Some(proxy) = proxy {
             builder = builder.proxy(reqwest::Proxy::http(proxy.url).unwrap());
@@ -49,46 +49,7 @@ impl HttpClient {
 
         HttpClient { client: client.build(), enable_cache_control }
     }
-    pub async fn execute(&self, request: reqwest::Request) -> reqwest_middleware::Result<Response> {
-        let cached_req: Request = Request::from(&request);
-        let response = self.client.execute(request).await?;
-        let mut cached_response = Response::from(&response);
-        cached_response.body = response.json().await?;
-        if self.enable_cache_control {
-            let cache_ttl = CachePolicy::new(&cached_req, &cached_response)
-                .time_to_live(SystemTime::now())
-                .as_secs();
-            Ok(cached_response.ttl(Option::from(cache_ttl)))
-        } else {
-            Ok(cached_response)
-        }
-    }
-
-    pub async fn get<T>(
-        &self,
-        url: T,
-        forwarded_headers: BTreeMap<String, String>,
-    ) -> reqwest_middleware::Result<Response>
-    where
-        T: IntoUrl,
-    {
-        let mut headers = reqwest::header::HeaderMap::new();
-        for (key, value) in forwarded_headers.iter() {
-            headers.insert(key.parse::<HeaderName>().unwrap(), value.parse().unwrap());
-        }
-
-        let request = self.client.get(url).headers(headers).build()?;
-        let cached_req = Request::from(&request);
-        let response = self.client.execute(request).await?;
-        let mut cached_response = Response::from(&response);
-        cached_response.body = response.json().await?;
-        if self.enable_cache_control {
-            let cache_ttl = CachePolicy::new(&cached_req, &cached_response)
-                .time_to_live(SystemTime::now())
-                .as_secs();
-            Ok(cached_response.ttl(Option::from(cache_ttl)))
-        } else {
-            Ok(cached_response)
-        }
+    pub async fn execute(&self, request: reqwest::Request) -> reqwest_middleware::Result<reqwest::Response> {
+        Ok(self.client.execute(request).await?)
     }
 }
--- src/http/request.rs
+++ src/http/request.rs
@@ -1,41 +1,46 @@
-use derive_setters::Setters;
-
 use http_cache_semantics::RequestLike;
 use hyper::Uri;
 use reqwest::header::HeaderMap;
 
-#[derive(Clone, Debug, Default, Setters)]
-pub struct Request {
-    pub url: String,
-    pub method: reqwest::Method,
-    pub headers: reqwest::header::HeaderMap,
-    pub body: String,
-}
+// TODO: why do we need clone?
+#[derive(Debug)]
+pub struct Request<'a>(&'a reqwest::Request);
 
-impl From<&reqwest::Request> for Request {
-    fn from(req: &reqwest::Request) -> Self {
-        let url = req.url().to_string();
-        let method = req.method().clone();
-        let headers = req.headers().clone();
-        let body = String::new();
-        Request { url, method, headers, body }
+impl <'a> Request<'a> {
+    pub fn to_reqwest(self) -> &'a reqwest::Request {
+        self.0
+    }
+    pub fn url(&self) -> &reqwest::Url {
+        self.0.url()
+    }
+    pub fn method(&self) -> &reqwest::Method {
+        self.0.method()
+    }
+    pub fn headers(&self) -> &HeaderMap {
+        self.0.headers()
     }
 }
 
-impl RequestLike for Request {
+impl<'a> From<&'a reqwest::Request> for Request<'a> {
+    fn from(req: &'a reqwest::Request) -> Self {
+        Request(req)
+    }
+}
+
+impl <'a>RequestLike for Request<'a> {
     fn uri(&self) -> Uri {
-        self.url.parse().unwrap()
+        self.url().as_str().parse().unwrap()
     }
 
     fn is_same_uri(&self, other: &Uri) -> bool {
-        self.url == (*other).to_string()
+        self.url().to_string() == (*other).to_string()
     }
 
     fn method(&self) -> &reqwest::Method {
-        &self.method
+        self.method()
     }
 
     fn headers(&self) -> &HeaderMap {
-        &self.headers
+        self.headers()
     }
 }
