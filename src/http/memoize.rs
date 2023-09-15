6a0f3a9e00503532fe91774737738f1db331a542
Tushar Mathur <tusharmath@gmail.com>
2023-09-15 16:56:53 +0530

    implement `execute` in Memoize

--- src/http/memoize.rs
+++ src/http/memoize.rs
@@ -1,14 +1,34 @@
 use super::HttpClient;
+use anyhow::Result;
+use http_cache_semantics::RequestLike;
+use hyper::Uri;
+use reqwest::Method;
 
 #[allow(dead_code)]
 pub struct Memoize {
     client: HttpClient,
-    cache: moka::sync::Cache<String, String>,
+    cache: moka::sync::Cache<Uri, super::Response>,
 }
 
 impl Memoize {
     #[allow(dead_code)]
     pub fn new(client: HttpClient) -> Self {
         Self { client, cache: moka::sync::Cache::new(u64::MAX) }
+    }
+
+    pub async fn execute(&self, req: reqwest::Request) -> Result<super::Response> {
+        if req.method() == Method::GET {
+            let key = req.uri();
+            let cached = self.cache.get(&key);
+            if let Some(cached) = cached {
+                Ok(cached)
+            } else {
+                let response = self.client.execute(req).await?;
+                self.cache.insert(key, response.clone());
+                Ok(response)
+            }
+        } else {
+            Ok(self.client.execute(req).await?)
+        }
     }
 }
