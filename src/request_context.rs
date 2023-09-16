321a3cf15a37817bd3229e772295e2765449cbcb
Tushar Mathur <tusharmath@gmail.com>
2023-09-16 11:45:45 +0530

    refactor(server): add RequestContext

---
+++ src/request_context.rs
@@ -0,0 +1,24 @@
+use async_graphql::dynamic;
+use derive_setters::Setters;
+
+use crate::{blueprint::Blueprint, config::Server, http::HttpClient};
+
+#[derive(Clone, Setters)]
+pub struct RequestContext {
+    pub schema: dynamic::Schema,
+    pub client: HttpClient,
+    pub server: Server,
+}
+
+impl RequestContext {
+    pub fn new(blueprint: Blueprint, server: Server) -> Self {
+        let enable_http_cache = server.enable_http_cache();
+        let enable_cache_control = server.enable_cache_control();
+        let proxy = server.proxy.clone();
+        RequestContext {
+            schema: blueprint.to_schema(&server),
+            client: HttpClient::new(enable_http_cache, proxy, enable_cache_control),
+            server,
+        }
+    }
+}
