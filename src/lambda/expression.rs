8b6c619dd919f6be004df77f8917bc9e20f044f9
Ayush Mishra <ayusham001@gmail.com>
2023-09-27 19:32:52 +0530

    Removed formatting

--- src/lambda/expression.rs
+++ src/lambda/expression.rs
@@ -15,85 +15,85 @@ use crate::request_template::RequestTemplate;
 
 #[derive(Debug, Error, Serialize)]
 pub enum EvaluationError {
-    #[error("IOException: {0}")]
-    IOException(String),
+  #[error("IOException: {0}")]
+  IOException(String),
 
-    #[error("JSException: {0}")]
-    JSException(String),
+  #[error("JSException: {0}")]
+  JSException(String),
 
-    #[error("APIValidationError: {0:?}")]
-    APIValidationError(Vec<String>),
+  #[error("APIValidationError: {0:?}")]
+  APIValidationError(Vec<String>),
 }
 
 #[derive(Clone, Debug)]
 pub enum Operation {
-    Endpoint(RequestTemplate),
-    JS(Box<Expression>, String),
+  Endpoint(RequestTemplate),
+  JS(Box<Expression>, String),
 }
 
 impl<'a> Expression {
-    pub fn eval<'a>(
-        &'a self,
-        ctx: &'a EvaluationContext<'a>,
-    ) -> Pin<Box<dyn Future<Output = Result<async_graphql::Value>> + 'a + Send>> {
-        Box::pin(async move {
-            match self {
-                Expression::Unsafe(operation) => {
-                    match operation {
-                        Operation::Endpoint(req_template) => {
-                            let req = req_template.to_request(ctx)?;
-                            let url = req.uri().clone();
-                            let is_get = req.method() == reqwest::Method::GET;
-                            // Attempt to short circuit GET request
-                            if is_get {
-                                if let Some(cached) = ctx.req_ctx.cache.get(&url) {
-                                    return Ok(cached.body);
-                                }
-                            }
-
-                            // Prepare for HTTP calls
-                            let res = ctx
-                                .req_ctx
-                                .execute(req)
-                                .await
-                                .map_err(|e| EvaluationError::IOException(e.to_string()))?;
-                            if ctx.req_ctx.server.enable_http_validation() {
-                                req_template
-                                    .endpoint
-                                    .output
-                                    .validate(&res.body)
-                                    .map_err(EvaluationError::from)?;
-                            }
-
-                            // Insert into cache for future requests
-                            if is_get {
-                                ctx.req_ctx.cache.insert(url, res.clone());
-                            }
-
-                            Ok(res.body)
-                        }
-                        Operation::JS(input, script) => {
-                            let result;
-                            #[cfg(not(feature = "unsafe-js"))]
-                            {
-                                let _ = script;
-                                let _ = input;
-                                result = Err(EvaluationError::JSException("JS execution is disabled".to_string()).into());
-                            }
-
-                            #[cfg(feature = "unsafe-js")]
-                            {
-                                let input = input.eval(ctx).await?;
-                                result = javascript::execute_js(script, input, Some(ctx.timeout))
-                                    .map_err(|e| EvaluationError::JSException(e.to_string()).into());
-                            }
-                            result
-                        }
-                    }
+  pub fn eval<'a>(
+    &'a self,
+    ctx: &'a EvaluationContext<'a>,
+  ) -> Pin<Box<dyn Future<Output = Result<async_graphql::Value>> + 'a + Send>> {
+    Box::pin(async move {
+      match self {
+        Expression::Unsafe(operation) => {
+          match operation {
+            Operation::Endpoint(req_template) => {
+              let req = req_template.to_request(ctx)?;
+              let url = req.uri().clone();
+              let is_get = req.method() == reqwest::Method::GET;
+              // Attempt to short circuit GET request
+              if is_get {
+                if let Some(cached) = ctx.req_ctx.cache.get(&url) {
+                  return Ok(cached.body);
                 }
+              }
+
+              // Prepare for HTTP calls
+              let res = ctx
+                .req_ctx
+                .execute(req)
+                .await
+                .map_err(|e| EvaluationError::IOException(e.to_string()))?;
+              if ctx.req_ctx.server.enable_http_validation() {
+                req_template
+                  .endpoint
+                  .output
+                  .validate(&res.body)
+                  .map_err(EvaluationError::from)?;
+              }
+
+              // Insert into cache for future requests
+              if is_get {
+                ctx.req_ctx.cache.insert(url, res.clone());
+              }
+
+              Ok(res.body)
             }
-        })
-    }
+            Operation::JS(input, script) => {
+              let result;
+              #[cfg(not(feature = "unsafe-js"))]
+              {
+                let _ = script;
+                let _ = input;
+                result = Err(EvaluationError::JSException("JS execution is disabled".to_string()).into());
+              }
+
+              #[cfg(feature = "unsafe-js")]
+              {
+                let input = input.eval(ctx).await?;
+                result = javascript::execute_js(script, input, Some(ctx.timeout))
+                  .map_err(|e| EvaluationError::JSException(e.to_string()).into());
+              }
+              result
+            }
+          }
+        }
+      }
+    })
+  }
 }
 
 #[derive(Clone, Debug)]
