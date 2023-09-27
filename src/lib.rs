bef29927dbfbecc392546249e6f1f5f6ed23de86
amit <amitksingh1490@gmail.com>
2023-09-27 18:01:59 +0530

    Fix: remove unused files

--- src/lib.rs
+++ src/lib.rs
@@ -9,13 +9,11 @@ pub mod directive;
 pub mod document;
 pub mod endpoint;
 pub mod http;
-pub mod inet_address;
 #[cfg(feature = "unsafe-js")]
 pub mod javascript;
 pub mod json;
 pub mod lambda;
 pub mod mustache;
-pub mod path;
 pub mod path_string;
 pub mod print_schema;
 pub mod request_template;
