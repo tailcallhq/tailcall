073b18e6b7ae1a4069a9a4d5b9f66a03bf7d1f9c
Tushar Mathur <tusharmath@gmail.com>
2023-09-29 17:27:35 +0530

    test: add more tests for transform

--- src/blueprint/transform.rs
+++ src/blueprint/transform.rs
@@ -41,16 +41,43 @@ where
 
 #[cfg(test)]
 mod tests {
-  use crate::blueprint::transform::Transform;
+  use crate::{
+    blueprint::transform::Transform,
+    valid::{Valid, ValidExtensions, ValidationError},
+  };
 
   #[test]
-  fn test_combine() {
-    let t1 = Transform::<i32, i32>::new(|a, b| Ok(a + b));
-    let t2 = Transform::<i32, i32>::new(|a, b| Ok(a * b));
+  fn test_combine_ok() {
+    let t1 = Transform::<i32, i32>::new(|a, b| Valid::succeed(a + b));
+    let t2 = Transform::<i32, i32>::new(|a, b| Valid::succeed(a * b));
     let t = t1 + t2;
 
     let actual = t.transform(&2, 3).unwrap();
     let expected = 10;
+
+    assert_eq!(actual, expected)
+  }
+
+  #[test]
+  fn test_combine_fail() {
+    let t1 = Transform::<i32, i32, String>::new(|a, b| Valid::succeed(a + b));
+    let t2 = Transform::<i32, i32, String>::new(|a, b| Valid::fail(format!("{} * {} is not valid", a, b)));
+    let t = t1 + t2;
+
+    let actual = t.transform(&2, 3).unwrap_err();
+    let expected = ValidationError::new("2 * 5 is not valid".into());
+
+    assert_eq!(actual, expected)
+  }
+
+  #[test]
+  fn test_combine_fail_both() {
+    let t1 = Transform::<i32, i32, i32>::new(|a, b| Valid::fail(a + b));
+    let t2 = Transform::<i32, i32, i32>::new(|a, b| Valid::fail(a * b));
+    let t = t1 + t2;
+
+    let actual = t.transform(&2, 3).unwrap_err();
+    let expected = ValidationError::new(5).combine(ValidationError::new(6));
 
     assert_eq!(actual, expected)
   }
