2780df94450870f93d0083cad39c5f39067d7426
Tushar Mathur <tusharmath@gmail.com>
2023-09-29 16:48:09 +0530

    refactor: update transform

--- src/blueprint/transform.rs
+++ src/blueprint/transform.rs
@@ -1,24 +1,57 @@
-use super::Blueprint;
-use crate::{
-  config::Config,
-  valid::{Valid, ValidExtensions},
-};
+use crate::valid::{Valid, ValidExtensions, ValidationError};
 
-pub struct Transform {
-  pub transform: Box<dyn Fn(&Config, Blueprint) -> Valid<Blueprint, &'static str>>,
+type Transformation<S, T, E> = dyn FnOnce(&S, T) -> Valid<T, E>;
+pub struct Transform<S, T, E = &'static str> {
+  pub transform: Box<Transformation<S, T, E>>,
 }
 
-impl Transform {
-  fn new(transform: impl Fn(&Config, Blueprint) -> Valid<Blueprint, &'static str>) -> Self {
+impl<S, T, E> Transform<S, T, E> {
+  pub fn new(transform: impl FnOnce(&S, T) -> Valid<T, E> + 'static) -> Self {
     Self { transform: Box::new(transform) }
   }
 
-  fn and_then(self, other: Self) -> Self {
-    Self::new(move |config, blueprint| {
-      (self.transform)(config, blueprint).fold(
-        |blueprint| (other.transform)(config, blueprint),
-        other.transform(config, blueprint),
-      )
+  pub fn combine(self, other: Self) -> Self
+  where
+    T: Clone + 'static,
+    S: 'static,
+    E: 'static,
+  {
+    Self::new(move |s, t| match (self.transform)(s, t.clone()) {
+      Ok(blueprint) => (other.transform)(s, blueprint),
+      Err(e) => Err::<T, ValidationError<E>>(e).validate_or((other.transform)(s, t)),
     })
+  }
+
+  pub fn transform(self, s: &S, t: T) -> Valid<T, E> {
+    (self.transform)(s, t)
+  }
+}
+
+impl<S, T: Clone, E> std::ops::Add for Transform<S, T, E>
+where
+  T: Clone + 'static,
+  S: 'static,
+  E: 'static,
+{
+  type Output = Self;
+  fn add(self, rhs: Self) -> Self::Output {
+    self.combine(rhs)
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use crate::blueprint::transform::Transform;
+
+  #[test]
+  fn test_combine() {
+    let t1 = Transform::<i32, i32>::new(|a, b| Ok(a + b));
+    let t2 = Transform::<i32, i32>::new(|a, b| Ok(a * b));
+    let t = t1 + t2;
+
+    let actual = t.transform(&2, 3).unwrap();
+    let expected = 10;
+
+    assert_eq!(actual, expected)
   }
 }
