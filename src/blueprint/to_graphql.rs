eed9cbfc244491c2e3c7425553b79cdfad5416c3
ologbonowiwi <ologbonowiwi520@gmail.com>
2023-09-30 15:47:34 -0300

    feat: create structs loader

--- src/blueprint/to_graphql.rs
+++ src/blueprint/to_graphql.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::Read;
 
@@ -9,7 +10,38 @@ fn read_file(path: &str) -> Result<String, std::io::Error> {
   Ok(src)
 }
 
-// Create the test structure
+fn load_pub_structs(src: &str) -> HashMap<String, String> {
+  let mut structs = HashMap::new();
+  let mut in_struct = false;
+
+  let mut struct_name = String::new();
+
+  for line in src.lines() {
+    if line.starts_with("pub struct")  {
+      in_struct = true;
+
+      let mut parts = line.split_whitespace();
+      struct_name = parts.nth(2).unwrap().to_string();
+
+      structs.insert(struct_name.clone(), line.to_string());
+    } else if in_struct {
+      let mut r#struct = structs.get(&struct_name).unwrap().to_string();
+
+      // it is possible to add a checker starts_with("pub") if needed to add only public fields
+      r#struct.push_str("\n");
+      r#struct.push_str(line);
+
+      if line.ends_with("}") {
+        in_struct = false;
+      }
+
+      structs.insert(struct_name.clone(), r#struct);
+    }
+  }
+
+  structs
+}
+
 #[cfg(test)]
 mod tests {
   use super::*;
@@ -22,9 +54,25 @@ mod tests {
   }
 
   #[test]
-  fn it_loads_a_file() {
+  fn loads_a_file() {
     let source_file = "src/blueprint/mod.rs";
     let result = read_file(source_file);
     assert!(result.is_ok());
+  }
+
+  #[test]
+  fn finds_http_struct() {
+    let source_file = "src/config/config.rs";
+    let result = read_file(source_file);
+
+    assert!(result.is_ok());
+
+    let result = result.unwrap();
+
+    let structs = load_pub_structs(result.as_str());
+
+    assert!(structs.len() > 0);
+    assert!(structs.contains_key("Http"));
+    println!("{}", structs.get("Http").unwrap());
   }
 }
