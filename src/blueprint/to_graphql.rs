c64b87294d1c7ec95b0592a75e8a62e16a2679d5
ologbonowiwi <ologbonowiwi520@gmail.com>
2023-09-30 14:45:47 -0300

    feat: read a file given a path

---
+++ src/blueprint/to_graphql.rs
@@ -0,0 +1,32 @@
+use std::fs::File;
+use std::io::Read;
+
+fn read_file(path: &str) -> Result<String, std::io::Error> {
+    let mut file = File::open(path)?;
+    let mut src = String::new();
+    file.read_to_string(&mut src)?;
+
+    println!("{src}");
+
+    Ok(src)
+}
+
+// Create the test structure
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn fails_when_file_is_invalid() {
+        let source_file = "some-unknown-location.rs";
+        let result = read_file(source_file);
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn it_loads_a_file() {
+        let source_file = "src/blueprint/mod.rs";
+        let result = read_file(source_file);
+        assert!(result.is_ok());
+    }
+}
