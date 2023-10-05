18fbb7012b032f59207b6b7901c1e05af80e9abb
amit <amitksingh1490@gmail.com>
2023-10-05 12:31:29 +0530

    feat: errors in config.rs

--- src/directive.rs
+++ src/directive.rs
@@ -1,23 +1,29 @@
-use anyhow::Result;
 use async_graphql::parser::types::ConstDirective;
 use async_graphql::{Name, Pos, Positioned};
 use serde::{Deserialize, Serialize};
 use serde_json::{Map, Value};
 use serde_path_to_error::deserialize;
 
-use crate::valid::ValidationError;
+use crate::valid::{Valid, ValidationError, VectorExtension};
 
 fn pos<A>(a: A) -> Positioned<A> {
   Positioned::new(a, Pos::default())
 }
 
-fn from_directive<'a, A: Deserialize<'a>>(directive: &'a ConstDirective) -> Result<A> {
+fn from_directive<'a, A: Deserialize<'a>>(directive: &'a ConstDirective) -> Valid<A, String> {
   let mut map = Map::new();
-  for (k, v) in directive.arguments.clone() {
-    map.insert(k.node.as_str().to_string(), serde_json::to_value(&v.node)?);
-  }
+  let p = directive.arguments.iter().validate_all(|(k, v)| {
+    Valid::Ok((
+      k.node.as_str().to_string(),
+      serde_json::to_value(&v.node)
+        .map_err(|e| ValidationError::new(e.to_string()).trace(directive.name.node.as_str()))?,
+    ))
+  })?;
+  p.into_iter().for_each(|(k, v)| {
+    map.insert(k, v);
+  });
 
-  Ok(deserialize(Value::Object(map)).map_err(|e| ValidationError::from(e).trace(directive.name.node.as_str()))?)
+  deserialize(Value::Object(map)).map_err(|e| ValidationError::from(e).trace(directive.name.node.as_str()))
 }
 
 fn to_directive<A: Serialize>(a: &A, name: String) -> ConstDirective {
@@ -37,12 +43,12 @@ fn to_directive<A: Serialize>(a: &A, name: String) -> ConstDirective {
 }
 
 pub trait DirectiveCodec<'a, A> {
-  fn from_directive(directive: &'a ConstDirective) -> Result<A>;
+  fn from_directive(directive: &'a ConstDirective) -> Valid<A, String>;
   fn to_directive(&'a self, name: String) -> ConstDirective;
 }
 
 impl<'a, A: Deserialize<'a> + Serialize> DirectiveCodec<'a, A> for A {
-  fn from_directive(directive: &'a ConstDirective) -> Result<A> {
+  fn from_directive(directive: &'a ConstDirective) -> Valid<A, String> {
     from_directive(directive)
   }
 
