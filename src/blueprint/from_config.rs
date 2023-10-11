c030d9a27fb58b7a1d3f9629bb69192e333a834b
wrath-of-god <wrath.of.god1607@gmail.com>
2023-10-11 22:02:47 +0530

    multi validate

--- src/blueprint/from_config.rs
+++ src/blueprint/from_config.rs
@@ -65,17 +65,15 @@ fn validate_server(config: &Config) -> Valid<()> {
       .trace("schema");
     }
   }
-  for (k, v) in &config.server.response_headers.0 {
+  let a = config.server.response_headers.0.clone().validate_all(|(k, v)| {
     HeaderName::from_bytes(k.as_bytes())
       .map_err(|e| ValidationError::new(format!("Parsing failed because of {}", e)))
       .validate_or(
         HeaderValue::from_str(v.as_str()).map_err(|e| ValidationError::new(format!("Parsing failed because of {}", e))),
       )
-      .trace("responseHeaders")
-      .trace("@server")
-      .trace("schema")?;
-  }
+  });
 
+  a.trace("responseHeaders").trace("@server").trace("schema")?;
   Valid::Ok(())
 }
 
