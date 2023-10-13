f9ef015fb4088392a07913dca95eac31b31c20da
neo773 <62795688+neo773@users.noreply.github.com>
2023-10-14 00:34:12 +0530

    delete redundant file

--- npm/publish.js
+++
@@ -1,6 +0,0 @@
-import { spawnSync } from 'child_process';
-
-const APP_VERSION = process.env.APP_VERSION
-
-spawnSync('npm', ['version', APP_VERSION.replace('v', '')]);
-spawnSync('npm', ['publish']);
