ebbdd4d532096440f38844eb7939f82653a85168
Tushar Mathur <tusharmath@gmail.com>
2023-10-13 19:36:17 +0530

    drop key from group by

--- src/config/group_by.rs
+++ src/config/group_by.rs
@@ -3,19 +3,16 @@ use serde::{Deserialize, Serialize};
 use crate::config::is_default;
 #[derive(Clone, Debug, Serialize, Deserialize, PartialEq, Default)]
 pub struct GroupBy {
-  key: String,
   #[serde(default, skip_serializing_if = "is_default")]
-  path: Option<Vec<String>>,
+  path: Vec<String>,
 }
-
-const EMPTY_VEC: &Vec<String> = &vec![];
 
 impl GroupBy {
   pub fn path(&self) -> &Vec<String> {
-    self.path.as_ref().unwrap_or(EMPTY_VEC)
+    &self.path
   }
 
   pub fn key(&self) -> &String {
-    &self.key
+    self.path.last().unwrap()
   }
 }
