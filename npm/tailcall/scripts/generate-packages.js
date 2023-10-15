3e2809f71686e96abcca06bbab5a650461e3315d
Mahesh <maheshmahi19@outlook.com>
2023-10-15 17:32:45 +0530

    fix: imporve ci pipeline

--- npm/tailcall/scripts/generate-packages.js
+++ npm/tailcall/scripts/generate-packages.js
@@ -1,64 +1,59 @@
-import * as fs from "node:fs"
+import * as fs from "node:fs/promises"
 import {dirname, resolve} from "node:path"
 import {fileURLToPath} from "node:url"
 
 const APP_VERSION = process.env.APP_VERSION
 
-const MANIFEST_PATH = resolve(fileURLToPath(import.meta.url), "../..", "package.json")
+const MANIFEST_PATH = resolve(fileURLToPath(import.meta.url), "../package.json")
 
 const rootManifest = JSON.parse(fs.readFileSync(MANIFEST_PATH).toString("utf-8"))
 
-function generatePackages() {
+// Skip Running if no APP_VERSION is specified.
+if (!APP_VERSION) {
+  console.log("[Publish] No Version specified to publish CLI")
+  process.exit(1)
+}
+
+async function generatePackages() {
   const __filename = fileURLToPath(import.meta.url)
   const __dirname = dirname(__filename)
 
-  let target
-
-  try {
-    target = resolve(__dirname, "../../../dist")
-  } catch {
-    console.log("[Publish] Error, no build is available")
-    process.exit(1)
-  }
-
+  const target = resolve(__dirname, "../../../dist")
   const optionalPackages = {}
 
   try {
     console.log("[Publish] Read Built Files")
-    const files = fs.readdirSync(target)
+    const files = await fs.readdir(target)
 
     for (const _file of files) {
       const packageName = _file.replace("tailcall-", "")
       const packageRoot = resolve(__dirname, "../..", packageName)
 
-      console.log(`[Publish] Copy ${_file} to ${packageRoot} `)
-      fs.copyFileSync(`${target}/${_file}`, `${packageRoot}/tailcall`)
-
       const packageFileName = `${packageRoot}/package.json`
 
-      const packagefile = fs.readFileSync(packageFileName, {encoding: "utf-8"})
+      const packagefile = await fs.readFile(packageFileName, {encoding: "utf-8"})
 
       const packageJSON = JSON.parse(packagefile)
 
-      packageJSON["version"] = APP_VERSION || "latest"
+      packageJSON["version"] = APP_VERSION
 
       const updatedPackageJSON = JSON.stringify(packageJSON, null, 2)
-      fs.writeFileSync(packageFileName, updatedPackageJSON)
+      await fs.writeFile(packageFileName, updatedPackageJSON)
 
-      optionalPackages[packageJSON.name] = APP_VERSION || "latest"
+      optionalPackages[packageJSON.name] = APP_VERSION
 
       console.log("[Publish] Update package.json for sub-package")
     }
 
-    rootManifest["version"] = APP_VERSION || "latest"
+    rootManifest["version"] = APP_VERSION
 
     rootManifest["optionalDependencies"] = optionalPackages
 
-    fs.writeFileSync(MANIFEST_PATH, JSON.stringify(rootManifest, null, 2))
+    await fs.writeFile(MANIFEST_PATH, JSON.stringify(rootManifest, null, 2))
 
     console.log("[Publish] Updated root package.json")
   } catch (error) {
-    console.log("[Publish] [Error] with reading Target files")
+    console.error(error)
     process.exit(1)
   }
 }
