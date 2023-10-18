3d6a8111e29ea5c4266a4a4f0e2a6ee172830572
Wesley Matos <wm4tos.777@gmail.com>
2023-10-18 15:53:54 -0300

    feat: call tailcall binary properly

--- npm/tailcall/src/cli.js
+++ npm/tailcall/src/cli.js
@@ -1,11 +1,36 @@
+const { spawnSync } = require('child_process');
+
 /**
  * @returns {String}
  */
 function getExecutionPath() {
     const arch = process.arch;
-    const os = process.platform;
+    let os = process.platform;
+
+    if (os === 'darwin') {
+        // our builds use apple instead of darwin
+        os = 'apple'
+    }
+
+    try {
+        const absolutePath = require.resolve(`tailcall-${os}-${arch}/bin/tailcall`)
+        return absolutePath
+    } catch (e) {
+        throw new Error(`Couldn't find tailcall binary inside node_modules for ${os}-${arch}`)
+    }
 }
 
 function run(argv) {
+    // the two first arguments are the node bin and the package bing
+    const args = argv.slice(2)
 
+    const { status } = spawnSync(
+        getExecutionPath(),
+        args,
+        { stdio: 'inherit' }
+    );
+
+    process.exit(status ?? 0)
 }
+
+module.exports = { run }
