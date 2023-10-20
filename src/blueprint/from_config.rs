4178c588d8b40a883c59212ee41c70baa09d165f
git-bruh <e817509a-8ee9-4332-b0ad-3a6bdf9ab63f@aleeas.com>
2023-10-20 19:31:01 +0530

    blueprint/from_config: don't error on header/env  usage

--- src/blueprint/from_config.rs
+++ src/blueprint/from_config.rs
@@ -256,10 +256,6 @@ where
       // "vars" refer to the server's configuration variables, set up in
       // `config/server.rs` and they all have fallbacks to default values, so
       // we don't need any checks here
-
-      // neither of these values make sense for usage in a URL template
-      // TODO should we error out here or is this valid usage?
-      return Valid::fail("the 'header' and 'vars' directives can't be used in a template".to_string());
     }
     _ => {
       return Valid::fail(format!("unknown template directive '{head}'"));
