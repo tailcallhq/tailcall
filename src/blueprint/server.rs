f8e974d41ceaa0b4862063042429a383445b1beb
Tushar Mathur <tusharmath@gmail.com>
2023-10-23 17:34:04 +0530

    migrate server to NeoValid

--- src/blueprint/server.rs
+++ src/blueprint/server.rs
@@ -6,7 +6,7 @@ use hyper::header::{HeaderName, HeaderValue};
 use hyper::HeaderMap;
 
 use crate::config;
-use crate::valid::{Valid, ValidExtensions, ValidationError, VectorExtension};
+use crate::valid::{NeoValid, ValidationError};
 
 #[derive(Clone, Debug, Setters)]
 pub struct Server {
@@ -43,35 +43,34 @@ impl Server {
 impl TryFrom<crate::config::Server> for Server {
   type Error = ValidationError<String>;
 
-  fn try_from(config_server: config::Server) -> Valid<Self, String> {
-    // Configure other server settings
-    let server = configure_server(&config_server)?;
-    Valid::Ok(server.clone())
+  fn try_from(config_server: config::Server) -> Result<Self, Self::Error> {
+    configure_server(&config_server).to_result()
   }
 }
 
-fn validate_hostname(hostname: String) -> Valid<IpAddr, String> {
-  let host = if hostname == "localhost" {
-    IpAddr::from([127, 0, 0, 1])
+fn validate_hostname(hostname: String) -> NeoValid<IpAddr, String> {
+  if hostname == "localhost" {
+    NeoValid::succeed(IpAddr::from([127, 0, 0, 1]))
   } else {
-    hostname
-      .parse()
-      .map_err(|e: AddrParseError| ValidationError::new(format!("Parsing failed because of {}", e)))
-      .trace("hostname")
-      .trace("@server")
-      .trace("schema")?
-  };
-  Ok(host)
+    NeoValid::from(
+      hostname
+        .parse()
+        .map_err(|e: AddrParseError| ValidationError::new(format!("Parsing failed because of {}", e))),
+    )
+    .trace("hostname")
+    .trace("@server")
+    .trace("schema")
+  }
 }
 
 const RESTRICTED_ROUTES: &[&str] = &["/", "/graphql"];
 
-fn handle_graphiql(graphiql: Option<String>) -> Valid<Option<String>, String> {
+fn handle_graphiql(graphiql: Option<String>) -> NeoValid<Option<String>, String> {
   let mut graph = None;
   if let Some(enable_graphiql) = graphiql.clone() {
     let lowered_route = enable_graphiql.to_lowercase();
     if RESTRICTED_ROUTES.contains(&lowered_route.as_str()) {
-      return Err(
+      return NeoValid::from_validation_err(
         ValidationError::new(format!(
           "Cannot use restricted routes '{}' for enabling graphiql",
           enable_graphiql
@@ -84,39 +83,41 @@ fn handle_graphiql(graphiql: Option<String>) -> Valid<Option<String>, String> {
       graph = Some(enable_graphiql);
     }
   };
-  Ok(graph)
+  NeoValid::succeed(graph)
 }
 
-fn handle_response_headers(resp_headers: BTreeMap<String, String>) -> Valid<HeaderMap, String> {
-  let headers = resp_headers
-    .validate_all(|(k, v)| {
-      let name = HeaderName::from_bytes(k.as_bytes())
-        .map_err(|e| ValidationError::new(format!("Parsing failed because of {}", e)));
-      let value =
-        HeaderValue::from_str(v.as_str()).map_err(|e| ValidationError::new(format!("Parsing failed because of {}", e)));
-      name.validate_both(value)
-    })
-    .trace("responseHeaders")
-    .trace("@server")
-    .trace("schema")?;
-
-  let mut response_headers = HeaderMap::new();
-  response_headers.extend(headers);
-  Ok(response_headers)
-}
-
-fn configure_server(config_config: &config::Server) -> Valid<Server, String> {
-  Ok(Server {
-    enable_apollo_tracing: config_config.enable_apollo_tracing(),
-    enable_cache_control_header: config_config.enable_cache_control(),
-    enable_graphiql: handle_graphiql(config_config.enable_graphiql())?,
-    enable_introspection: config_config.enable_introspection(),
-    enable_query_validation: config_config.enable_query_validation(),
-    enable_response_validation: config_config.enable_http_validation(),
-    global_response_timeout: config_config.get_global_response_timeout(),
-    port: config_config.get_port(),
-    hostname: validate_hostname(config_config.get_hostname().to_lowercase())?,
-    vars: config_config.get_vars(),
-    response_headers: handle_response_headers(config_config.get_response_headers().0)?,
+fn handle_response_headers(resp_headers: BTreeMap<String, String>) -> NeoValid<HeaderMap, String> {
+  NeoValid::from_iter(resp_headers.iter(), |(k, v)| {
+    let name = NeoValid::from(
+      HeaderName::from_bytes(k.as_bytes())
+        .map_err(|e| ValidationError::new(format!("Parsing failed because of {}", e))),
+    );
+    let value = NeoValid::from(
+      HeaderValue::from_str(v.as_str()).map_err(|e| ValidationError::new(format!("Parsing failed because of {}", e))),
+    );
+    name.zip(value)
   })
+  .map(|headers| headers.into_iter().collect::<HeaderMap>())
+  .trace("responseHeaders")
+  .trace("@server")
+  .trace("schema")
+}
+
+fn configure_server(config_config: &config::Server) -> NeoValid<Server, String> {
+  handle_graphiql(config_config.enable_graphiql())
+    .zip(validate_hostname(config_config.get_hostname().to_lowercase()))
+    .zip(handle_response_headers(config_config.get_response_headers().0))
+    .map(|((enable_graphiql, hostname), response_headers)| Server {
+      enable_apollo_tracing: config_config.enable_apollo_tracing(),
+      enable_cache_control_header: config_config.enable_cache_control(),
+      enable_graphiql,
+      enable_introspection: config_config.enable_introspection(),
+      enable_query_validation: config_config.enable_query_validation(),
+      enable_response_validation: config_config.enable_http_validation(),
+      global_response_timeout: config_config.get_global_response_timeout(),
+      port: config_config.get_port(),
+      hostname,
+      vars: config_config.get_vars(),
+      response_headers,
+    })
 }
