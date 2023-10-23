80f3124ec613be5e2ecff28c0fedae7b7ab16a9f
Wesley Matos <wm4tos.777@gmail.com>
2023-10-23 17:06:57 -0300

    refactor: remove method

--- npm/prepare-core-package.js
+++ npm/prepare-core-package.js
@@ -4,19 +4,15 @@ import { copyBin, writePackageJson } from './lib/index.js';
 
 const { binary_source, node_os, node_arch, node_package, version } = process.env;
 
-const prepare = async () => {
-  const destination = join('packages', node_package);
+const destination = join('packages', node_package);
 
-  await mkdir(destination, { recursive: true });
+await mkdir(destination, { recursive: true });
 
-  await copyBin(destination, binary_source)
+await copyBin(destination, binary_source)
 
-  await writePackageJson(destination, {
-    name: node_package,
-    version,
-    os: [node_os],
-    cpu: [node_arch],
-  });
-}
-
-prepare();
+await writePackageJson(destination, {
+  name: node_package,
+  version,
+  os: [node_os],
+  cpu: [node_arch],
+});
