a2d3d5c63e7e1557fd8ddf52e5d7fd9e1d500661
keremecon <134702704+kremedev@users.noreply.github.com>
2023-10-25 20:25:56 +0300

    conversation fixes

--- src/config/server.rs
+++ src/config/server.rs
@@ -49,7 +49,7 @@ impl Server {
     self.global_response_timeout.unwrap_or(0)
   }
   pub fn get_worker(&self) -> usize {
-    self.worker.unwrap_or(4)
+    self.worker.unwrap_or(0)
   }
   pub fn get_port(&self) -> u16 {
     self.port.unwrap_or(8000)
