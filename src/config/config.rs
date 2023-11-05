50afbd1be515d788aa5317d9390aa9b569ba803c
Tushar Mathur <tusharmath@gmail.com>
2023-11-05 22:40:29 +0530

    refactor: update DirectiveCodec to infer name

--- src/config/config.rs
+++ src/config/config.rs
@@ -215,12 +215,12 @@ pub struct Field {
   #[serde(default)]
   pub args: BTreeMap<String, Arg>,
   pub doc: Option<String>,
-  pub modify: Option<ModifyField>,
-  pub inline: Option<InlineType>,
+  pub modify: Option<Modify>,
+  pub inline: Option<Inline>,
   pub http: Option<Http>,
   #[serde(rename = "unsafe")]
   pub unsafe_operation: Option<Unsafe>,
-  pub const_field: Option<ConstField>,
+  pub const_field: Option<Const>,
 }
 
 impl Field {
@@ -255,7 +255,7 @@ pub struct Unsafe {
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
-pub struct ModifyField {
+pub struct Modify {
   pub name: Option<String>,
   #[serde(default)]
   #[serde(skip_serializing_if = "is_default")]
@@ -263,7 +263,7 @@ pub struct ModifyField {
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
-pub struct InlineType {
+pub struct Inline {
   pub path: Vec<String>,
 }
 
@@ -275,7 +275,7 @@ pub struct Arg {
   #[serde(default)]
   pub required: bool,
   pub doc: Option<String>,
-  pub modify: Option<ModifyField>,
+  pub modify: Option<Modify>,
   pub default_value: Option<Value>,
 }
 
@@ -315,7 +315,7 @@ pub struct Http {
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
-pub struct ConstField {
+pub struct Const {
   pub data: Value,
 }
 
