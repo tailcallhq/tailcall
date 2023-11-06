669d1773e4d9b3238d3b6ed52dfbbe240a827230
Mayant Mukul <mayantmukul@outlook.in>
2023-11-05 23:49:07 -0800

    chore: undo unnecessary change

--- src/config/from_document.rs
+++ src/config/from_document.rs
@@ -73,34 +73,30 @@ fn to_root_schema(schema_definition: &SchemaDefinition) -> RootSchema {
 fn pos_name_to_string(pos: &Positioned<Name>) -> String {
   pos.node.to_string()
 }
-
-fn to_type(type_definition: &Positioned<TypeDefinition>) -> Valid<Option<config::Type>, String> {
-  match type_definition.node.kind.clone() {
-    TypeKind::Object(object_type) => to_object_type(
-      &object_type.fields,
-      &type_definition.node.description,
-      false,
-      &object_type.implements,
-    )
-    .some(),
-    TypeKind::Interface(interface_type) => to_object_type(
-      &interface_type.fields,
-      &type_definition.node.description,
-      true,
-      &interface_type.implements,
-    )
-    .some(),
-    TypeKind::Enum(enum_type) => Valid::succeed(Some(to_enum(enum_type))),
-    TypeKind::InputObject(input_object_type) => to_input_object(input_object_type).some(),
-    TypeKind::Union(_) => Valid::none(),
-    TypeKind::Scalar => Valid::succeed(Some(to_scalar_type())),
-  }
-}
-
 fn to_types(type_definitions: &Vec<&Positioned<TypeDefinition>>) -> Valid<BTreeMap<String, config::Type>, String> {
   Valid::from_iter(type_definitions, |type_definition| {
     let type_name = pos_name_to_string(&type_definition.node.name);
-    to_type(type_definition).map(|option| (type_name, option))
+    match type_definition.node.kind.clone() {
+      TypeKind::Object(object_type) => to_object_type(
+        &object_type.fields,
+        &type_definition.node.description,
+        false,
+        &object_type.implements,
+      )
+      .some(),
+      TypeKind::Interface(interface_type) => to_object_type(
+        &interface_type.fields,
+        &type_definition.node.description,
+        true,
+        &interface_type.implements,
+      )
+      .some(),
+      TypeKind::Enum(enum_type) => Valid::succeed(Some(to_enum(enum_type))),
+      TypeKind::InputObject(input_object_type) => to_input_object(input_object_type).some(),
+      TypeKind::Union(_) => Valid::none(),
+      TypeKind::Scalar => Valid::succeed(Some(to_scalar_type())),
+    }
+    .map(|option| (type_name, option))
   })
   .map(|vec| {
     BTreeMap::from_iter(
