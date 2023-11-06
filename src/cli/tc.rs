24a0bd59e33cdabf84a2c1e50c364b04a4208ffa
Mayant Mukul <mayantmukul@outlook.in>
2023-11-05 23:23:31 -0800

    chore: cleanup tc.rs

--- src/cli/tc.rs
+++ src/cli/tc.rs
@@ -28,7 +28,17 @@ pub async fn run() -> Result<()> {
     }
     Command::Check { file_path, n_plus_one_queries, schema } => {
       let config = Config::from_file_paths(file_path.iter()).await?;
-      display_details(&config, n_plus_one_queries, schema)
+      let blueprint = Blueprint::try_from(&config);
+      match blueprint {
+        Ok(blueprint) => {
+          display_config(&config, n_plus_one_queries);
+          if schema {
+            display_schema(&blueprint);
+          }
+          Ok(())
+        }
+        Err(e) => Err(e.into()),
+      }
     }
     Command::Init { file_path } => Ok(init(&file_path).await?),
   }
@@ -83,22 +93,14 @@ pub async fn init(file_path: &str) -> Result<()> {
   Ok(())
 }
 
-pub fn display_details(config: &Config, n_plus_one_queries: bool, schema: bool) -> Result<()> {
-  let blueprint = Blueprint::try_from(config);
-  match blueprint {
-    Ok(blueprint) => {
-      Fmt::display(Fmt::success(&"No errors found".to_string()));
-      let seq = vec![Fmt::n_plus_one_data(n_plus_one_queries, config)];
-      Fmt::display(Fmt::table(seq));
+pub fn display_schema(blueprint: &Blueprint) {
+  Fmt::display(Fmt::heading(&"GraphQL Schema:\n".to_string()));
+  let sdl = blueprint.to_schema();
+  Fmt::display(print_schema::print_schema(sdl));
+}
 
-      if schema {
-        Fmt::display(Fmt::heading(&"GraphQL Schema:\n".to_string()));
-        let sdl = blueprint.to_schema();
-        Fmt::display(print_schema::print_schema(sdl));
-      }
-
-      Ok(())
-    }
-    Err(e) => Err(e.into()),
-  }
+fn display_config(config: &Config, n_plus_one_queries: bool) {
+  Fmt::display(Fmt::success(&"No errors found".to_string()));
+  let seq = vec![Fmt::n_plus_one_data(n_plus_one_queries, config)];
+  Fmt::display(Fmt::table(seq));
 }
