ffebb0552badc89135b343f40740a77f4c30d4f6
Sandipsinh Rathod <zotbysandip@gmail.com>
2023-11-10 14:33:19 +0530

    added test for polling server

--- tests/custom_resp_header_spec.rs
+++ tests/custom_resp_header_spec.rs
@@ -1,44 +1,37 @@
 // Integration tests for the API server.
 mod integration_tests {
+  use tailcall::http::{start_server, start_server_with_polling};
 
   // Helper function to start the test server.
-  async fn initiate_test_server(mock_schema_path: String) -> &'static str {
-    let config = tailcall::config::Config::from_file_paths([mock_schema_path].iter())
-      .await
-      .unwrap();
-    tailcall::http::start_server(config)
-      .await
-      .expect("Server failed to start");
-    "Success"
-  }
-
-  async fn initiate_test_server_over_http(mock_schema_path: String, poll_interval: u64) -> &'static str {
-    let config = tailcall::config::Config::from_file_paths([mock_schema_path.clone()].iter())
-      .await
-      .unwrap();
-    tailcall::http::start_server_with_polling(config, mock_schema_path, poll_interval)
-      .await
-      .expect("Server failed to start");
+  async fn initiate_test_server(mock_schema_path: String, poll_interval: Option<u64>) -> &'static str {
+    let config = tailcall::config::Config::from_file_or_url([mock_schema_path].iter())
+        .await
+        .unwrap();
+    match (poll_interval, config.1) {
+      (Some(poll_interval), Some(file_path)) => {
+        start_server_with_polling(config.0, file_path, poll_interval).await.unwrap();
+      }
+      _ => {
+        start_server(config.0).await.unwrap();
+      }
+    }
     "Success"
   }
 
   async fn verify_response_headers(schema_path: &str, poll_interval: Option<u64>) {
     // Start the background server with the provided schema.
-    match poll_interval {
-      Some(interval) => tokio::spawn(initiate_test_server_over_http(schema_path.into(), interval)),
-      None => tokio::spawn(initiate_test_server(schema_path.into())),
-    };
+    tokio::spawn(initiate_test_server(schema_path.into(), poll_interval));
 
     // Provide a small delay to ensure the server has started.
-    tokio::time::sleep(tokio::time::Duration::from_millis(300)).await;
+    tokio::time::sleep(tokio::time::Duration::from_millis(1000)).await; // need to provide a bit of delay depending upon internet speed and response time
 
     let http_client = reqwest::Client::new();
     let query_data = "{\"query\":\"query { item { id name } }\"}";
 
     let api_request = http_client
-      .post("http://localhost:8000/graphql")
-      .header("Content-Type", "application/json")
-      .body(query_data);
+        .post("http://localhost:8000/graphql")
+        .header("Content-Type", "application/json")
+        .body(query_data);
 
     let response = api_request.send().await.expect("Failed to send request");
 
@@ -55,7 +48,7 @@ mod integration_tests {
 
   #[tokio::test]
   async fn test_verify_response_headers_over_http() {
-    let schema_path = "https://raw.githubusercontent.com/tailcallhq/tailcall/main/examples/jsonplaceholder.json";
+    let schema_path = "https://raw.githubusercontent.com/tailcallhq/tailcall/main/tests/graphql_mock/test-custom-headers.graphql";
     let poll_interval = 10;
     verify_response_headers(schema_path, Some(poll_interval)).await;
   }
