4a3bcaadb1bad2858d830cb702dad42201945f1f
Wesley Matos <wm4tos.777@gmail.com>
2023-11-16 06:31:13 -0300

    refactor: create graphql types modify module

--- src/blueprint/from_config/from_config.rs
+++ src/blueprint/from_config/from_config.rs
@@ -250,30 +250,6 @@ fn validate_field_type_exist(config: &Config, field: &Field) -> Valid<(), String
   }
 }
 
-fn update_modify<'a>() -> TryFold<'a, (&'a Config, &'a Field, &'a config::Type, &'a str), FieldDefinition, String> {
-  TryFold::<(&Config, &Field, &config::Type, &'a str), FieldDefinition, String>::new(
-    |(config, field, type_of, _), mut b_field| {
-      if let Some(modify) = field.modify.as_ref() {
-        if let Some(new_name) = &modify.name {
-          for name in type_of.implements.iter() {
-            let interface = config.find_type(name);
-            if let Some(interface) = interface {
-              if interface.fields.iter().any(|(name, _)| name == new_name) {
-                return Valid::fail("Field is already implemented from interface".to_string());
-              }
-            }
-          }
-
-          let lambda = Lambda::context_field(b_field.name.clone());
-          b_field = b_field.resolver_or_default(lambda, |r| r);
-          b_field = b_field.name(new_name.clone());
-        }
-      }
-      Valid::succeed(b_field)
-    },
-  )
-}
-
 pub fn is_scalar(type_name: &str) -> bool {
   ["String", "Int", "Float", "Boolean", "ID", "JSON"].contains(&type_name)
 }
--- src/blueprint/from_config/graphql/types/mod.rs
+++ src/blueprint/from_config/graphql/types/mod.rs
@@ -1,7 +1,9 @@
 mod http;
 mod const_field;
 mod unsafe_field;
+mod modify;
 
 pub use http::*;
 pub use const_field::*;
 pub use unsafe_field::*;
+pub use modify::*;
