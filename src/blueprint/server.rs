2af531c5b874643e6641105962077f2c94a958c7
neo773 <62795688+neo773@users.noreply.github.com>
2023-11-17 04:18:23 +0530

    fix lint

--- src/blueprint/server.rs
+++ src/blueprint/server.rs
@@ -9,7 +9,7 @@ use serde::{Deserialize, Serialize};
 use crate::config;
 use crate::valid::{Valid, ValidationError};
 
-#[derive(Deserialize, Serialize, Clone, Debug, Setters)]
+#[derive(Deserialize, Serialize, Clone, Debug)]
 #[serde(rename_all = "camelCase", default)]
 pub struct Http {
   pub version: HttpVersion,
