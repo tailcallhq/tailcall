300634deb1b33998a4a7cced5150faba3273662b
amit <amitksingh1490@gmail.com>
2023-11-17 13:56:43 +0530

    feat: use neos code from https://github.com/tailcallhq/tailcall/pull/634

---
+++ npm/post-install.js
@@ -0,0 +1,27 @@
+// @ts-check
+// @ts-ignore
+import { familySync, GLIBC, MUSL } from "detect-libc"
+import { exec } from 'child_process'
+import util from 'util'
+
+const execa = util.promisify(exec)
+const platform = process.platform
+const arch = process.arch
+
+const libcFamily = familySync()
+let libc
+if (platform === "win32") {
+  libc = "-msvc"
+} else {
+  libc = libcFamily === GLIBC ? "-gnu" : libcFamily === MUSL ? "-musl" : ""
+}
+
+const pkg = `@tailcallhq/core-${platform}-${arch}${libc}`
+
+try {
+  // @ts-ignore
+  const { stdout, stderr } = await execa(`npm install ${pkg}@${version} --no-save`)
+  stderr ? console.log(stderr) : console.log(`Successfully installed optional dependency: ${pkg}`, stdout)
+} catch (error) {
+  console.error(`Failed to install optional dependency: ${pkg}`, error.stderr)
+}
--- npm/pre-install.js
+++ npm/pre-install.js
@@ -1,8 +1,7 @@
 const os = process.platform
 const arch = process.arch
-const libc = process.libc
 
-const dependency = libc ? Object.keys(optionalDependencies).find((name) => name.includes(`${os}-${arch}-${libc}`)) : Object.keys(optionalDependencies).find((name) => name.includes(`${os}-${arch}`))
+const dependency = Object.keys(optionalDependencies).find((name) => name.includes(`${os}-${arch}`))
 if (!dependency) {
   const redColor = "\x1b[31m"
   const resetColor = "\x1b[0m"
