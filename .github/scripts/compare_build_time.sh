a2b0a0e985423a91108ed239ef0dc85c7341121a
alankritdabral_2 <149957793+AlooDon@users.noreply.github.com>
2023-11-19 21:59:48 +0530

    Update compare_build_time.sh

--- .github/scripts/compare_build_time.sh
+++ .github/scripts/compare_build_time.sh
@@ -1,16 +1,35 @@
 #!/bin/bash
 
-file1="benches/iai-callgrind/benchmark.txt"
-file2="benches/iai-callgrind/benchmarks.txt"
+# Switch to main branch
+git checkout main
 
+# Run benchmarks and save output to a file
+echo -n > benches/iai-callgrind/old_benchmark.txt
+cargo bench --bench json_like_bench_iai-callgrind -- --save-baseline main >> benches/iai-callgrind/old_benchmark.txt
+cargo bench --bench data_loader_bench_iai-callgrind -- --save-baseline main >> benches/iai-callgrind/old_benchmark.txt
+cargo bench --bench impl_path_string_for_evaluation_context_iai-callgrind -- --save-baseline main >> benches/iai-callgrind/old_benchmark.txt
+cargo bench --bench request_template_bench_iai-callgrind -- --save-baseline main >> benches/iai-callgrind/old_benchmark.txt
+sed -i 's/ \{1,\}\([0-9]\)/\1/g' benches/iai-callgrind/old_benchmark.txt
 
-benchmarks=(
-    "json_like_bench_iai_callgrind::batched_body::benchmark_batched_body"
-    "data_loader_bench_iai_callgrind::data_loader::benchmark_data_loader"
-    "impl_path_string_for_evaluation_context_iai_callgrind::bench::bench_main"
-    "request_template_bench_iai_callgrind::bench_to_request::benchmark_to_request"
-    # Add more benchmarks here as needed
-)
+file1="benches/iai-callgrind/old_benchmark.txt"
+
+# Switch to current branch
+git checkout -
+
+# Run benchmarks and save output to another file
+echo -n > benches/iai-callgrind/new_benchmarks.txt
+cargo bench --bench json_like_bench_iai-callgrind -- --save-baseline change >> benches/iai-callgrind/new_benchmarks.txt
+cargo bench --bench data_loader_bench_iai-callgrind -- --save-baseline change >> benches/iai-callgrind/new_benchmarks.txt
+cargo bench --bench impl_path_string_for_evaluation_context_iai-callgrind -- --save-baseline change >> benches/iai-callgrind/new_benchmarks.txt
+cargo bench --bench request_template_bench_iai-callgrind -- --save-baseline change >> benches/iai-callgrind/new_benchmarks.txt
+sed -i 's/ \{1,\}\([0-9]\)/\1/g' benches/iai-callgrind/new_benchmarks.txt
+
+file2="benches/iai-callgrind/new_benchmarks.txt"
+
+config_file="benches/iai-callgrind/benchmarks.cfg" # to add more benchmarks add in this file
+
+# Read benchmarks from the configuration file
+readarray -t benchmarks < "$config_file"
 
 attributes=("Instructions" "L1 Hits" "L2 Hits" "RAM Hits")
 
@@ -22,12 +41,17 @@ for bench in "${benchmarks[@]}"; do
 
     # Check attribute changes
     for attribute in "${attributes[@]}"; do
-        value1=$(grep -A5 "$bench" "file1.txt" | grep -Po "${attribute}:\K\d+")
+        value1=$(grep -A5 "$bench" "$file1" | grep -Po "${attribute}:\K\d+")
         value2=$(grep -A5 "$bench" "$file2" | grep -Po "${attribute}:\K\d+")
 
         if [ -n "$value1" ] && [ -n "$value2" ]; then
-            percent_change=$(awk -v v1="$value1" -v v2="$value2" 'BEGIN { if(v1 != 0) pc = (v2 - v1) / v1 * 100; else pc = "nan"; printf "%.2f", pc }')
-            if (( $(awk -v pc="$percent_change" 'BEGIN { print (pc > 10) }') )); then
+            if [ "$value1" -ne 0 ]; then
+                percent_change=$(( ((value2 - value1) * 100) / value1 ))
+            else
+                percent_change="nan"
+            fi
+
+            if ((percent_change > 10)); then
                 echo "$bench $attribute has a change of $percent_change%, failing CI."
                 fail_ci=1
             else
@@ -40,25 +64,25 @@ for bench in "${benchmarks[@]}"; do
 
     # Check performance metric changes 
     # Total read+write = L1 Hits + L2 Hits + RAM Hits.
-    # Estimated Cycles = L1 Hits + 5 × (L2 Hits) + 35 × (RAM Hits)
-    for file in "file1.txt" "$file2"; do
+    # Estimated Cycles = L1 Hits + 5 × (L2 Hits) + 35 × (RAM Hits).
+    for file in "$file1" "$file2"; do
         l1_hits=$(grep -A5 "$bench" "$file" | grep -Po "L1 Hits:\K\d+")
         l2_hits=$(grep -A5 "$bench" "$file" | grep -Po "L2 Hits:\K\d+")
         ram_hits=$(grep -A5 "$bench" "$file" | grep -Po "RAM Hits:\K\d+")
 
-        if [ $x -ne 0 ]; then
-            p1=$(( ( (l1_hits + l2_hits + ram_hits) - x ) * 100 / x ))
+        if [ "$x" -ne 0 ]; then
+            p1=$(( ((l1_hits + l2_hits + ram_hits - x) * 100) / x ))
             echo "$bench Total read+write has a change of $p1%"
 
-            if (( p1 > 10 )); then
+            if ((p1 > 10)); then
                 echo "$bench Total read+write has a change greater than 10%, failing CI."
                 fail_ci=1
             fi
 
-            p2=$(( ( (l1_hits + 5 * l2_hits + 35 * ram_hits) - y ) * 100 / y ))
+            p2=$(( ((l1_hits + 5 * l2_hits + 35 * ram_hits - y) * 100) / y ))
             echo "$bench Estimated Cycles has a change of $p2%"
 
-            if (( p2 > 10 )); then
+            if ((p2 > 10)); then
                 echo "$bench Estimated Cycles has a change greater than 10%, failing CI."
                 fail_ci=1
             fi
@@ -74,4 +98,4 @@ for bench in "${benchmarks[@]}"; do
     echo "----------------------------------"
 done
 
-exit $fail_ci
+exit "$fail_ci"
