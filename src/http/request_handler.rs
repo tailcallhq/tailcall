c141677ccb714d2a3c99c17c2ba8f8658e849cf0
Sandipsinh Rathod <zotbysandip@gmail.com>
2023-11-21 17:47:18 +0530

    merged main branch

--- src/http/request_handler.rs
+++ src/http/request_handler.rs
@@ -10,6 +10,7 @@ use serde::de::DeserializeOwned;
 use super::request_context::RequestContext;
 use super::ServerContext;
 use crate::async_graphql_hyper::{GraphQLBatchRequest, GraphQLRequest, GraphQLRequestLike, GraphQLResponse};
+use crate::parser::de::Parser;
 
 fn graphiql() -> Result<Response<Body>> {
   Ok(Response::new(Body::from(
@@ -80,7 +81,46 @@ pub async fn graphql_request<T: DeserializeOwned + GraphQLRequestLike>(
     }
   }
 }
+async fn url_req<T: DeserializeOwned + GraphQLRequestLike>(
+  req: Request<Body>,
+  server_ctx: &ServerContext,
+) -> Result<Response<Body>> {
+  let req_ctx = Arc::new(create_request_context(&req, server_ctx));
+  if let Some(q) = req.uri().path_and_query() {
+    let q = q.as_str();
+    let mut parser = Parser::from_path(q)?;
+    let request = parser.parse::<T>();
+    match request {
+      Ok(request) => {
+        let mut response = request.data(req_ctx.clone()).execute(&server_ctx.schema).await;
+        response = update_cache_control_header(response, server_ctx, req_ctx);
+        let mut resp = response.to_response()?;
+        update_response_headers(&mut resp, server_ctx);
+        Ok(resp)
+      }
+      Err(err) => {
+        log::error!("Failed to parse request: {q}",);
 
+        let mut response = async_graphql::Response::default();
+        let server_error = ServerError::new(format!("Unexpected GraphQL Request: {}", err), None);
+        response.errors = vec![server_error];
+
+        Ok(GraphQLResponse::from(response).to_response()?)
+      }
+    }
+  } else {
+    log::error!("Failed to parse request, invalid url",);
+    let mut response = async_graphql::Response::default();
+    let server_error = ServerError::new("Unexpected GraphQL Request.".to_string(), None);
+    response.errors = vec![server_error];
+
+    Ok(GraphQLResponse::from(response).to_response()?)
+  }
+}
+
+async fn custom_url_req(req: Request<Body>, server_ctx: &ServerContext) -> Result<Response<Body>> {
+  url_req::<GraphQLRequest>(req, server_ctx).await
+}
 async fn graphql_single_request(req: Request<Body>, server_ctx: &ServerContext) -> Result<Response<Body>> {
   graphql_request::<GraphQLRequest>(req, server_ctx).await
 }
@@ -103,6 +143,7 @@ fn create_allowed_headers(headers: &HeaderMap, allowed: &BTreeSet<String>) -> He
 pub async fn handle_single_request(req: Request<Body>, state: Arc<ServerContext>) -> Result<Response<Body>> {
   match *req.method() {
     hyper::Method::POST if req.uri().path() == "/graphql" => graphql_single_request(req, state.as_ref()).await,
+    hyper::Method::GET if req.uri().path().starts_with("/api") => custom_url_req(req, state.as_ref()).await,
     hyper::Method::GET if state.blueprint.server.enable_graphiql => graphiql(),
     _ => not_found(),
   }
@@ -111,6 +152,7 @@ pub async fn handle_single_request(req: Request<Body>, state: Arc<ServerContext>
 pub async fn handle_batch_request(req: Request<Body>, state: Arc<ServerContext>) -> Result<Response<Body>> {
   match *req.method() {
     hyper::Method::POST if req.uri().path() == "/graphql" => graphql_batch_request(req, state.as_ref()).await,
+    hyper::Method::GET if req.uri().path().starts_with("/api") => custom_url_req(req, state.as_ref()).await,
     hyper::Method::GET if state.blueprint.server.enable_graphiql => graphiql(),
     _ => not_found(),
   }
