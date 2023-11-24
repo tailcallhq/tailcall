152b6e19cd8ce73a6c2be7b54a82c1cbd25c219d
Anuraag Marapally <47111459+A-N-uraag@users.noreply.github.com>
2023-11-24 17:37:39 +0530

    Fix: Cache-Control header cacheability handling (#683)
    Co-authored-by: Wesley <100464352+ologbonowiwi@users.noreply.github.com>

--- src/async_graphql_hyper.rs
+++ src/async_graphql_hyper.rs
@@ -126,27 +126,30 @@ impl GraphQLResponse {
     Ok(response)
   }
 
-  /// Sets the `cache_control` max_age for a given `GraphQLResponse`.
+  /// Sets the `cache_control` for a given `GraphQLResponse`.
   ///
   /// The function modifies the `GraphQLResponse` to set the `cache_control` `max_age`
-  /// to the specified `min_cache` value.
+  /// to the specified `min_cache` value and `public` flag to `cache_public`
   ///
   /// # Arguments
   ///
   /// * `res` - The GraphQL response whose `cache_control` is to be set.
   /// * `min_cache` - The `max_age` value to be set for `cache_control`.
+  /// * `cache_public` - The negation of `public` flag to be set for `cache_control`.
   ///
   /// # Returns
   ///
-  /// * A modified `GraphQLResponse` with updated `cache_control` `max_age`.
-  pub fn set_cache_control(mut self, min_cache: i32) -> GraphQLResponse {
+  /// * A modified `GraphQLResponse` with updated `cache_control` `max_age` and `public` flag.
+  pub fn set_cache_control(mut self, min_cache: i32, cache_public: bool) -> GraphQLResponse {
     match self.0 {
       BatchResponse::Single(ref mut res) => {
         res.cache_control.max_age = min_cache;
+        res.cache_control.public = cache_public;
       }
       BatchResponse::Batch(ref mut list) => {
         for res in list {
           res.cache_control.max_age = min_cache;
+          res.cache_control.public = cache_public;
         }
       }
     };
