8ee45df35b9b7b83f03e6887e9f9f6723fb952d9
Shylock Hg <tcath2s@gmail.com>
2023-11-26 11:18:05 +0800

    Check resovler in recursive.

--- src/blueprint/from_config/schema.rs
+++ src/blueprint/from_config/schema.rs
@@ -1,9 +1,9 @@
-use std::collections::HashMap;
+use std::collections::{BTreeMap, HashMap};
 
 use async_graphql::parser::types::ConstDirective;
 
 use crate::blueprint::*;
-use crate::config::{Config, Field};
+use crate::config::{Config, Field, Type};
 use crate::directive::DirectiveCodec;
 use crate::valid::{Valid, ValidationError};
 
@@ -14,14 +14,34 @@ fn validate_query(config: &Config) -> Valid<(), String> {
         return Valid::fail("Query type is not defined".to_owned()).trace(query_type_name);
       };
 
-      Valid::from_iter(query.fields.iter(), validate_field_has_resolver).trace(query_type_name)
+      Valid::from_iter(query.fields.iter(), |i| validate_field_has_resolver(i, &config.types)).trace(query_type_name)
     })
     .unit()
 }
 
-fn validate_field_has_resolver((name, field): (&String, &Field)) -> Valid<(), String> {
+// Return is this type has resolver
+// Will recursive check all fields
+fn validate_field_of_type_has_resolver(name: &str, ty: &Type, types: &BTreeMap<String, Type>) -> Valid<(), String> {
+  Valid::from_iter(ty.fields.iter(), |i| validate_field_has_resolver(i, &types))
+    .trace(name)
+    .unit()
+}
+fn validate_field_has_resolver((name, field): (&String, &Field), types: &BTreeMap<String, Type>) -> Valid<(), String> {
   Valid::<(), String>::fail("No resolver has been found in the schema".to_owned())
-    .when(|| !field.has_resolver())
+    .when(|| {
+      if !field.has_resolver() {
+        let f_type = &field.type_of;
+        if let Some(ty2) = types.get(f_type) {
+          let res = validate_field_of_type_has_resolver(f_type, ty2, types);
+          return !res.is_succeed();
+        } else {
+          return true;
+        }
+      }
+      {
+        return false;
+      }
+    })
     .trace(name)
 }
 
@@ -50,9 +70,11 @@ fn validate_mutation(config: &Config) -> Valid<(), String> {
       return Valid::fail("Mutation type is not defined".to_owned()).trace(mutation_type_name);
     };
 
-    Valid::from_iter(mutation.fields.iter(), validate_field_has_resolver)
-      .trace(mutation_type_name)
-      .unit()
+    Valid::from_iter(mutation.fields.iter(), |i| {
+      validate_field_has_resolver(i, &config.types)
+    })
+    .trace(mutation_type_name)
+    .unit()
   } else {
     Valid::succeed(())
   }
