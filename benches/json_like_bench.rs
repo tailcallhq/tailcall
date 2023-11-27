e4a620bb35f312d2ca13fe5b03a365868bb84bd2
alankritdabral_2 <149957793+AlooDon@users.noreply.github.com>
2023-11-27 23:03:31 +0530

    Update json_like_bench.rs

--- benches/json_like_bench.rs
+++ benches/json_like_bench.rs
@@ -3,24 +3,24 @@ use serde_json::json;
 use tailcall::benchmark::gather_path_matches;
 
 fn benchmark_batched_body(c: &mut Criterion) {
-  c.bench_function("test_batched_body", |b| {
-    b.iter(|| {
-      let input = json!({
-          "data": [
-              {"user": {"id": "1"}},
-              {"user": {"id": "2"}},
-              {"user": {"id": "3"}},
-              {"user": [
-                  {"id": "4"},
-                  {"id": "5"}
-              ]}
-          ]
-      });
+    c.bench_function("test_batched_body", |b| {
+        b.iter(|| {
+            let input = json!({
+                "data": [
+                    {"user": {"id": "1"}},
+                    {"user": {"id": "2"}},
+                    {"user": {"id": "3"}},
+                    {"user": [
+                        {"id": "4"},
+                        {"id": "5"}
+                    ]}
+                ]
+            });
 
-      // Use the gather_path_matches function
-      black_box(serde_json::to_value(gather_path_matches(&input, &["data", "user", "id"])).unwrap());
-    })
-  });
+            // Use the gather_path_matches function
+            black_box(serde_json::to_value(gather_path_matches(&input, &["data", "user", "id"])).unwrap());
+        })
+    });
 }
 
 criterion_group!(benches, benchmark_batched_body);
