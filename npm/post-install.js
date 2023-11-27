246e5ac57d4d43d1c070d9303326fd837030751f
alankritdabral <dabralalankrit@gmai.com>
2023-11-27 21:54:06 +0530

    Used prettier

--- npm/post-install.js
+++ npm/post-install.js
@@ -1,7 +1,7 @@
 // @ts-check
-import { familySync, GLIBC, MUSL } from "detect-libc"
-import { exec } from 'child_process'
-import util from 'util'
+import {familySync, GLIBC, MUSL} from "detect-libc"
+import {exec} from "child_process"
+import util from "util"
 
 const execa = util.promisify(exec)
 const platform = process.platform
@@ -19,7 +19,7 @@ const pkg = `@tailcallhq/core-${platform}-${arch}${libc}`
 
 try {
   // @ts-ignore
-  const { stdout, stderr } = await execa(`npm install ${pkg}@${version} --no-save`)
+  const {stdout, stderr} = await execa(`npm install ${pkg}@${version} --no-save`)
   stderr ? console.log(stderr) : console.log(`Successfully installed optional dependency: ${pkg}`, stdout)
 } catch (error) {
   console.error(`Failed to install optional dependency: ${pkg}`, error.stderr)
