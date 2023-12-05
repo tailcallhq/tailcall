262c47d0a08de48fee679dac237c6827eba996cb
Sujeet Sreenivasan <sujeetsr@gmail.com>
2023-12-04 21:00:46 -0800

    create field with type in resolver for interface

--- src/blueprint/into_schema.rs
+++ src/blueprint/into_schema.rs
@@ -1,14 +1,16 @@
 use std::borrow::Cow;
 use std::sync::Arc;
 
+use anyhow::Result;
 use async_graphql::dynamic::{
-  FieldFuture, FieldValue, SchemaBuilder, {self},
+  FieldFuture, FieldValue, ResolverContext, SchemaBuilder, {self},
 };
 use async_graphql_value::ConstValue;
 
 use crate::blueprint::{Blueprint, Definition, Type};
 use crate::http::RequestContext;
-use crate::lambda::EvaluationContext;
+use crate::json::JsonLike;
+use crate::lambda::{EvaluationContext, Expression};
 
 fn to_type_ref(type_of: &Type) -> dynamic::TypeRef {
   match type_of {
@@ -30,7 +32,39 @@ fn to_type_ref(type_of: &Type) -> dynamic::TypeRef {
   }
 }
 
-fn to_type(def: &Definition) -> dynamic::Type {
+fn get_type_from_value(value: &ConstValue) -> String {
+  let path = vec!["__typename".to_string()];
+  match value.get_path(&path).unwrap_or(&async_graphql::Value::Null) {
+    ConstValue::String(s) => s.clone(),
+    _ => "".to_string(),
+  }
+}
+
+fn create_field_value_with_type(value: ConstValue) -> FieldValue<'static> {
+  let typename = get_type_from_value(&value);
+  let field_value = FieldValue::from(value);
+  FieldValue::with_type(field_value, typename)
+}
+
+async fn get_evaled_value<'a>(
+  req_ctx: &'a Arc<RequestContext>,
+  ctx: &'a ResolverContext<'_>,
+  expr: Expression,
+) -> Result<async_graphql::Value> {
+  let ctx = EvaluationContext::new(req_ctx, ctx);
+  expr.eval(&ctx).await
+}
+
+fn get_field_value_from_ctx<'a>(
+  req_ctx: &'a Arc<RequestContext>,
+  ctx: &'a ResolverContext<'_>,
+  field_name: String,
+) -> Option<FieldValue<'static>> {
+  let ctx = EvaluationContext::new(req_ctx, ctx);
+  ctx.path_value(&[field_name]).map(|a| FieldValue::from(a.to_owned()))
+}
+
+fn to_type(def: &Definition, definitions: &Vec<Definition>) -> dynamic::Type {
   match def {
     Definition::ObjectTypeDefinition(def) => {
       let mut object = dynamic::Object::new(def.name.clone());
@@ -38,28 +72,51 @@ fn to_type(def: &Definition) -> dynamic::Type {
         let field = field.clone();
         let type_ref = to_type_ref(&field.of_type);
         let field_name = &field.name.clone();
-        let mut dyn_schema_field = dynamic::Field::new(field_name, type_ref, move |ctx| {
-          let req_ctx = ctx.ctx.data::<Arc<RequestContext>>().unwrap();
-          let field_name = &field.name;
-          match &field.resolver {
-            None => {
-              let ctx = EvaluationContext::new(req_ctx, &ctx);
-              FieldFuture::from_value(ctx.path_value(&[field_name]).map(|a| a.to_owned()))
-            }
-            Some(expr) => {
-              let expr = expr.to_owned();
-              FieldFuture::new(async move {
-                let ctx = EvaluationContext::new(req_ctx, &ctx);
-                let const_value = expr.eval(&ctx).await?;
-                let p = match const_value {
-                  ConstValue::List(a) => FieldValue::list(a),
-                  a => FieldValue::from(a),
-                };
-                Ok(Some(p))
-              })
-            }
-          }
-        });
+        let is_interface_type = is_interface_type(field.of_type.name(), definitions);
+
+        let mut dyn_schema_field = if !is_interface_type {
+          dynamic::Field::new(field_name, type_ref, move |ctx| {
+            let req_ctx = ctx.ctx.data::<Arc<RequestContext>>().unwrap();
+            let field_name = field.name.clone();
+            let resolver = field.resolver.clone();
+            FieldFuture::new(async move {
+              match resolver {
+                None => Ok(get_field_value_from_ctx(req_ctx, &ctx, field_name)),
+                Some(expr) => {
+                  let const_value = get_evaled_value(req_ctx, &ctx, expr).await?;
+                  let p = match const_value {
+                    ConstValue::List(a) => FieldValue::list(a),
+                    a => FieldValue::from(a),
+                  };
+                  Ok(Some(p))
+                }
+              }
+            })
+          })
+        } else {
+          dynamic::Field::new(field_name, type_ref, move |ctx| {
+            let req_ctx = ctx.ctx.data::<Arc<RequestContext>>().unwrap();
+            let field_name = field.name.clone();
+            let resolver = field.resolver.clone();
+            FieldFuture::new(async move {
+              match resolver {
+                None => Ok(get_field_value_from_ctx(req_ctx, &ctx, field_name)),
+                Some(expr) => {
+                  let const_value = get_evaled_value(req_ctx, &ctx, expr).await?;
+                  let p = match const_value {
+                    ConstValue::List(a) => FieldValue::list(
+                      a.iter()
+                        .map(|item| create_field_value_with_type(item.clone()))
+                        .collect::<Vec<_>>(),
+                    ),
+                    a => create_field_value_with_type(a),
+                  };
+                  Ok(Some(p))
+                }
+              }
+            })
+          })
+        };
         if let Some(description) = &field.description {
           dyn_schema_field = dyn_schema_field.description(description);
         }
@@ -127,10 +184,21 @@ fn create(blueprint: &Blueprint) -> SchemaBuilder {
   let mut schema = dynamic::Schema::build(query.as_str(), mutation.as_deref(), None);
 
   for def in blueprint.definitions.iter() {
-    schema = schema.register(to_type(def));
+    schema = schema.register(to_type(def, &blueprint.definitions));
   }
 
   schema
+}
+
+fn is_interface_type(name: &str, definitions: &Vec<Definition>) -> bool {
+  for def in definitions {
+    if def.name() == name {
+      if let Definition::InterfaceTypeDefinition(_) = def {
+        return true;
+      }
+    }
+  }
+  false
 }
 
 impl From<&Blueprint> for SchemaBuilder {
