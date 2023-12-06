4d065b42ecf04cdc32776730c2757dba7cbf4ea6
meskill <8974488+meskill@users.noreply.github.com>
2023-12-06 10:34:05 +0000

    refactor server tests

--- tests/server_spec.rs
+++ tests/server_spec.rs
@@ -1,77 +1,15 @@
-use futures::future;
+use reqwest;
+use reqwest::Client;
+use serde_json::json;
+
 use tailcall::config::Config;
 use tailcall::http::{start_server, ServerControl, ServerMessage};
 
-#[tokio::test]
-async fn server_start() {
-  use reqwest;
-  use reqwest::Client;
-  use serde_json::json;
-
+async fn test_server(configs: &[&str], url: &str) {
   let (server_control, server_up_sender, shutdown_sender) = ServerControl::new();
-
-  let file_paths = vec!["tests/server/config/server-start.graphql".to_string()];
-  let config = Config::from_file_or_url(file_paths.iter()).await.unwrap();
-  tokio::spawn(async {
-    start_server(config, server_up_sender, server_control.shutdown.receiver)
-      .await
-      .unwrap();
-  });
-
-  match server_control.server_up.receiver.await {
-    Ok(ServerMessage::ServerUp) => {
-      println!("Server started");
-    }
-    _ => panic!("Server did not start up correctly"),
-  }
-
-  let client = Client::new();
-  let query = json!({
-      "query": "{ greet }"
-  });
-
-  let mut tasks = vec![];
-  for _ in 0..100 {
-    let client = client.clone();
-    let query = query.clone();
-    let task = tokio::spawn(async move {
-      let send_request = || async { client.post("http://localhost:8000/graphql").json(&query).send().await };
-      let response = send_request().await.expect("Failed to send request");
-      let response_body: serde_json::Value = response.json().await.expect("Failed to parse response body");
-      response_body
-    });
-    tasks.push(task);
-  }
-
-  let tasks = future::join_all(tasks).await;
-
-  for task in tasks {
-    let response_body = match task {
-      Ok(body) => body,
-      Err(_) => panic!("Task panicked"),
-    };
-    let expected_response = json!({
-        "data": {
-            "greet": "Hello World!"
-        }
-    });
-    assert_eq!(response_body, expected_response, "Unexpected response from server");
-  }
-
-  shutdown_sender.send(ServerMessage::Shutdown).ok();
-}
-
-#[tokio::test]
-async fn server_start_http2() {
-  use reqwest;
-  use reqwest::Client;
-  use serde_json::json;
-
-  let (server_control, server_up_sender, shutdown_sender) = ServerControl::new();
+  let config = Config::from_file_or_url(configs.iter()).await.unwrap();
 
   tokio::spawn(async {
-    let file_paths = vec!["tests/server/config/server-start-http2.graphql".to_string()];
-    let config = Config::from_file_or_url(file_paths.iter()).await.unwrap();
     start_server(config, server_up_sender, server_control.shutdown.receiver)
       .await
       .unwrap();
@@ -91,23 +29,22 @@ async fn server_start_http2() {
   let mut tasks = vec![];
   for _ in 0..100 {
     let client = client.clone();
+    let url = url.to_owned();
     let query = query.clone();
-    let task = tokio::spawn(async move {
-      let send_request = || async { client.post("https://0.0.0.0:8001/graphql").json(&query).send().await };
-      let response = send_request().await.expect("Failed to send request");
-      let response_body: serde_json::Value = response.json().await.expect("Failed to parse response body");
-      response_body
+
+    let task: tokio::task::JoinHandle<Result<serde_json::Value, anyhow::Error>> = tokio::spawn(async move {
+      let response = client.post(url).json(&query).send().await?;
+      let response_body: serde_json::Value = response.json().await?;
+      Ok(response_body)
     });
     tasks.push(task);
   }
 
-  let tasks = future::join_all(tasks).await;
-
   for task in tasks {
-    let response_body = match task {
-      Ok(body) => body,
-      Err(_) => panic!("Task panicked"),
-    };
+    let response_body = task
+      .await
+      .expect("Spawned task should success")
+      .expect("Request should success");
     let expected_response = json!({
         "data": {
             "greet": "Hello World!"
@@ -117,4 +54,22 @@ async fn server_start_http2() {
   }
 
   shutdown_sender.send(ServerMessage::Shutdown).ok();
+}
+
+#[tokio::test]
+async fn server_start() {
+  test_server(
+    &["tests/server/config/server-start.graphql"],
+    "http://localhost:8800/graphql",
+  )
+  .await
+}
+
+#[tokio::test]
+async fn server_start_http2() {
+  test_server(
+    &["tests/server/config/server-start-http2.graphql"],
+    "https://localhost:8801/graphql",
+  )
+  .await
 }
