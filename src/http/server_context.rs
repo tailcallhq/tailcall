048fa4f647dad8950877094045a2facf25a84088
Tushar Mathur <tusharmath@gmail.com>
2023-12-07 13:41:43 +0530

    drop partial server context

--- src/http/server_context.rs
+++ src/http/server_context.rs
@@ -17,49 +17,45 @@ pub struct ServerContext {
   pub gql_data_loaders: Arc<Vec<DataLoader<GraphqlDataLoader>>>,
 }
 
-struct PartialServerContext {
-  http_client: Arc<dyn HttpClient>,
-  blueprint: Blueprint,
-  http_data_loaders: Vec<DataLoader<HttpDataLoader>>,
-  gql_data_loaders: Vec<DataLoader<GraphqlDataLoader>>,
-}
+impl ServerContext {
+  pub fn new(mut blueprint: Blueprint, http_client: Arc<dyn HttpClient>) -> Self {
+    let mut http_data_loaders = vec![];
+    let mut gql_data_loaders = vec![];
 
-impl PartialServerContext {
-  fn create_server_ctx(mut self) -> ServerContext {
-    for def in self.blueprint.definitions.iter_mut() {
+    for def in blueprint.definitions.iter_mut() {
       if let Definition::ObjectTypeDefinition(def) = def {
         for field in &mut def.fields {
           if let Some(Expression::Unsafe(expr_unsafe)) = &mut field.resolver {
             match expr_unsafe {
               Unsafe::Http { req_template, group_by, .. } => {
                 let data_loader = HttpDataLoader::new(
-                  self.http_client.clone(),
+                  http_client.clone(),
                   group_by.clone(),
                   matches!(&field.of_type, ListType { .. }),
                 )
-                .to_data_loader(self.blueprint.upstream.batch.clone().unwrap_or_default());
+                .to_data_loader(blueprint.upstream.batch.clone().unwrap_or_default());
 
                 field.resolver = Some(Expression::Unsafe(Unsafe::Http {
                   req_template: req_template.clone(),
                   group_by: group_by.clone(),
-                  dl_id: Some(DataLoaderId(self.http_data_loaders.len())),
+                  dl_id: Some(DataLoaderId(http_data_loaders.len())),
                 }));
 
-                self.http_data_loaders.push(data_loader);
+                http_data_loaders.push(data_loader);
               }
 
               Unsafe::GraphQLEndpoint { req_template, field_name, batch, .. } => {
-                let graphql_data_loader = GraphqlDataLoader::new(self.http_client.clone(), *batch)
-                  .to_data_loader(self.blueprint.upstream.batch.clone().unwrap_or_default());
+                let graphql_data_loader = GraphqlDataLoader::new(http_client.clone(), *batch)
+                  .to_data_loader(blueprint.upstream.batch.clone().unwrap_or_default());
 
                 field.resolver = Some(Expression::Unsafe(Unsafe::GraphQLEndpoint {
                   req_template: req_template.clone(),
                   field_name: field_name.clone(),
                   batch: *batch,
-                  dl_id: Some(DataLoaderId(self.gql_data_loaders.len())),
+                  dl_id: Some(DataLoaderId(gql_data_loaders.len())),
                 }));
 
-                self.gql_data_loaders.push(graphql_data_loader);
+                gql_data_loaders.push(graphql_data_loader);
               }
               _ => {}
             }
@@ -68,23 +64,14 @@ impl PartialServerContext {
       }
     }
 
-    let schema = self.blueprint.to_schema();
+    let schema = blueprint.to_schema();
 
     ServerContext {
       schema,
-      http_client: self.http_client,
-      blueprint: self.blueprint,
-      http_data_loaders: Arc::new(self.http_data_loaders),
-      gql_data_loaders: Arc::new(self.gql_data_loaders),
+      http_client,
+      blueprint,
+      http_data_loaders: Arc::new(http_data_loaders),
+      gql_data_loaders: Arc::new(gql_data_loaders),
     }
-  }
-}
-
-impl ServerContext {
-  pub fn new(blueprint: Blueprint, http_client: Arc<dyn HttpClient>) -> Self {
-    let http_data_loaders = vec![];
-    let gql_data_loaders = vec![];
-
-    PartialServerContext { http_client, blueprint, http_data_loaders, gql_data_loaders }.create_server_ctx()
   }
 }
