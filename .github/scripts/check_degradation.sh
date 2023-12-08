377601adddbb3ac1958aa1060be1bd65ed408482
alankritdabral <95607957+alankritdabral@users.noreply.github.com>
2023-12-08 19:01:50 +0530

    Update check_degradation.sh

--- .github/scripts/check_degradation.sh
+++ .github/scripts/check_degradation.sh
@@ -4,10 +4,10 @@ fail_ci=0
 critcmp main_branch new_branch | awk 'NR>2 {
     item = $1
     before = $3
-    change = $5
+    change = $6
     after = $7
     
-    printf "| %-30s | %-20s | %-20s | %-10.2f |\n", item, before, after, change 
+    printf "| %-30s | %-20s | %-20s | %-10.2f |\n", item, before, after, change-1.00
 
     if (change > 1.1) {
          printf "Percentage change for %s exceeds 10%%.\n", item >> "benches/critcmp.txt"
