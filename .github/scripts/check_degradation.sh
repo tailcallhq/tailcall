a2287a4f44e6d61884e4c2208dc8ee16b37555e0
alankritdabral <95607957+alankritdabral@users.noreply.github.com>
2023-12-08 17:42:48 +0530

    Update check_degradation.sh

--- .github/scripts/check_degradation.sh
+++ .github/scripts/check_degradation.sh
@@ -1,22 +1,22 @@
-echo "| Test                          | Base         | PR               | % change   |\n" >> "benches/critcmp.txt"
-echo "|-------------------------------|--------------|------------------|------------|\n" >> "benches/critcmp.txt"
-critcmp new_branch main_branch | awk 'NR>2 {
+critcmp main_branch new_branch 
+echo "| Test                           | Base         | PR           | % change   |" 
+echo "|--------------------------------|--------------|--------------|------------|" 
+fail_ci=0
+critcmp main_branch new_branch | awk 'NR>2 {
     item = $1
+    change = $2
     before = $3
-    change = $5
     after = $7
     
-    printf "| %-30s | %-20s | %-20s | %-10.2f |\n", item, before, after, change >> "benches/critcmp.txt"
+    printf "| %-30s | %-20s | %-20s | %-10.2f |\n", item, before, after, change 
 
     if (change > 1.1) {
-         printf "Percentage change for %s exceeds 10%%.\n", item
-         flag=1
-    }
-}
-
-END {
-    if (flag!=0) {
-        print "CI failed due to exceeding percentage change."
-        exit 1
+         printf "Percentage change for %s exceeds 10%%.\n", item >> "benches/critcmp.txt"
+         fail_ci=1
     }
 }'
+
+if [ "$fail_ci" -eq 1 ]; then
+    echo "$(cat benches/critcmp.txt)"
+    exit 1
+fi
