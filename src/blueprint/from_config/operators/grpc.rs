b5df7ffe6503d48c1ba728297cc57e6a03f23854
amit <amitksingh1490@gmail.com>
2023-12-14 13:46:04 +0530

    refactor: drop unwraps

--- src/blueprint/from_config/operators/grpc.rs
+++ src/blueprint/from_config/operators/grpc.rs
@@ -21,8 +21,31 @@ pub fn update_grpc<'a>() -> TryFold<'a, (&'a Config, &'a Field, &'a config::Type
         grpc.base_url.as_ref().or(config.upstream.base_url.as_ref()),
         "No base URL defined".to_string(),
       )
+      .zip(
+        Valid::from_option(
+          Some(PathBuf::from(&grpc.proto_path)),
+          "No proto path defined".to_string(),
+        )
+        .and_then(|path| {
+          Valid::from(
+            crate::grpc::protobuf::ProtobufSet::from_proto_file(&path).map_err(|e| ValidationError::new(e.to_string())),
+          )
+        })
+        .and_then(|service| {
+          Valid::from(
+            crate::grpc::protobuf::ProtobufService::new(&service, grpc.service.as_str())
+              .map_err(|e| ValidationError::new(e.to_string())),
+          )
+        })
+        .and_then(|operation| {
+          Valid::from(
+            crate::grpc::protobuf::ProtobufOperation::new(&operation, grpc.method.as_str())
+              .map_err(|e| ValidationError::new(e.to_string())),
+          )
+        }),
+      )
       .zip(helpers::headers::to_headermap(&grpc.headers))
-      .and_then(|(base_url, mut header_map)| {
+      .and_then(|((base_url, operation), mut header_map)| {
         let mut base_url = base_url.trim_end_matches('/').to_owned();
         base_url.push('/');
         base_url.push_str(grpc.service.clone().as_str());
@@ -35,25 +58,17 @@ pub fn update_grpc<'a>() -> TryFold<'a, (&'a Config, &'a Field, &'a config::Type
 
         RequestTemplate::try_from(
           Endpoint::new(base_url.to_string())
-            .method(Method::GET)
+            .method(Method::POST)
             .output(output_schema)
             .input(input_schema)
             .body(grpc.body.clone())
             .headers(header_map),
         )
+        .map(|teml| teml.grpc(Some(operation)))
         .map_err(|e| ValidationError::new(e.to_string()))
         .into()
       })
-      .map(|req_template| {
-        let pathbuf = PathBuf::from(&grpc.proto_path);
-        let file = crate::grpc::protobuf::ProtobufSet::from_proto_file(&pathbuf).unwrap();
-        let service = crate::grpc::protobuf::ProtobufService::new(&file, grpc.service.as_str()).unwrap();
-        let operation = crate::grpc::protobuf::ProtobufOperation::new(&service, grpc.method.as_str()).unwrap();
-
-        b_field.resolver(Some(
-          Lambda::from_request_template(req_template.grpc(Some(operation))).expression,
-        ))
-      })
+      .map(|req_template| b_field.resolver(Some(Lambda::from_request_template(req_template).expression)))
     },
   )
 }
