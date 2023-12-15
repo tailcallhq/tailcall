8120d83527a07da6d38ab2872caccbcfcc9aa73a
Tushar Mathur <tusharmath@gmail.com>
2023-12-15 18:07:12 +0530

    inlining a variable

--- tests/http_spec.rs
+++ tests/http_spec.rs
@@ -336,12 +336,13 @@ async fn run(spec: HttpSpec, downstream_assertion: &&DownstreamAssertion) -> any
   let headers = downstream_assertion.request.0.headers.clone();
   let url = downstream_assertion.request.0.url.clone();
   let server_context = spec.server_context().await;
-  let req = headers.into_iter().fold(
-    Request::builder().method(method.to_hyper()).uri(url.as_str()),
-    |acc, (key, value)| acc.header(key, value),
-  );
-
-  let req = req.body(Body::from(query_string))?;
+  let req = headers
+    .into_iter()
+    .fold(
+      Request::builder().method(method.to_hyper()).uri(url.as_str()),
+      |acc, (key, value)| acc.header(key, value),
+    )
+    .body(Body::from(query_string))?;
 
   // TODO: reuse logic from server.rs to select the correct handler
   if server_context.blueprint.server.enable_batch_requests {
