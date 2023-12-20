28a2dc6cd2f1ccc099ac053070223edaa82ea715
Shashi Kant <shashitnak@gmail.com>
2023-12-20 22:19:25 +0530

    changing wrk.lua file

--- ci-benchmark/wrk.lua
+++ ci-benchmark/wrk.lua
@@ -1,4 +1,4 @@
 wrk.method = "POST"
-wrk.body = '{"operationName":null,"variables":{},"query":"{posts{title}}"}'
+wrk.body = '{"operationName":null,"variables":{},"query":"{posts{user{id}}}"}'
 wrk.headers["Connection"] = "keep-alive"
 wrk.headers["Content-Type"] = "application/json"
