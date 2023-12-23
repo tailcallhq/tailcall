b78a5311a1bec5ae80c2cc539d379504baaea4a7
Hans Tognon <git@tobihans.space>
2023-12-23 23:39:08 +0100

    wip: operation_spec

--- tests/operation_spec.rs
+++ tests/operation_spec.rs
@@ -1,6 +1,41 @@
+use std::fs;
+use std::path::PathBuf;
 
+#[derive(Debug)]
+struct OperationSpec {
+  test_queries: Vec<OperationQuerySpec>,
+}
+
+#[derive(Debug)]
+struct OperationQuerySpec {
+  query: String,
+  diagnostic_count: u8,
+}
+
+impl OperationSpec {
+  fn new(path: PathBuf, content: &str) -> OperationSpec {
+    // TODO: Fill this later
+    OperationSpec { test_queries: vec![] }
+  }
+
+  fn cargo_read(path: &str) -> std::io::Result<Vec<OperationSpec>> {
+    let mut dir_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    dir_path.push(path);
+
+    let entries = fs::read_dir(dir_path.clone())?;
+    let mut files: Vec<OperationSpec> = vec![];
+
+    for entry in entries {
+      let path = entry?.path();
+      if path.is_file() && path.extension().unwrap_or_default() == "graphql" {
+        let contents = fs::read_to_string(path.clone())?;
+
+        for component in contents.split("#>") {}
+      }
+    }
+    Ok(vec![])
+  }
+}
 
 #[test]
-fn test_operations_errors() {
-
-}
+fn test_schema_operations() {}
