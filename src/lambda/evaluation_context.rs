700de713d588de7e2a5e95d9d142d984b8505090
Wesley Matos <wm4tos.777@gmail.com>
2023-12-29 12:03:30 -0300

    refactor(evaluation_context.rs): return `&str` instead of `String`

--- src/lambda/evaluation_context.rs
+++ src/lambda/evaluation_context.rs
@@ -49,8 +49,8 @@ impl<'a, Ctx: ResolverContextLike<'a>> EvaluationContext<'a, Ctx> {
     value.to_str().ok()
   }
 
-  pub fn env_var(&self, key: &str) -> Option<String> {
-    self.req_ctx.env_vars.get(key).cloned()
+  pub fn env_var(&self, key: &str) -> Option<&str> {
+    self.req_ctx.env_vars.get(key).map(|v| v.as_str())
   }
 
   pub fn var(&self, key: &str) -> Option<&str> {
