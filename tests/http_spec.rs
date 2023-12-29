c398dac4ce8141e2b9d0decead01b7333dbaacae
Ezhil Shanmugham <ezhil930@gmail.com>
2023-12-29 15:33:38 +0530

    fix: show log

--- tests/http_spec.rs
+++ tests/http_spec.rs
@@ -354,7 +354,7 @@ async fn assert_downstream(spec: HttpSpec) {
           );
         }
         Err(_) => {
-          log::info!("{} {} ... ok", spec.name, spec.path.display());
+          log::info!("{} {} ... failed (expected)", spec.name, spec.path.display());
         }
       }
     } else {
