e1dd8c488b74b47a5264bb7b531e259ab1da08bc
Shylock Hg <tcath2s@gmail.com>
2023-12-29 17:26:17 +0800

    Remove unused function.

--- src/blueprint/mod.rs
+++ src/blueprint/mod.rs
@@ -1,11 +1,9 @@
 mod blueprint;
 mod compress;
-mod const_utils;
 mod from_config;
 mod into_schema;
 mod timeout;
 
 pub use blueprint::*;
-pub use const_utils::*;
 pub use from_config::*;
 pub use timeout::GlobalTimeout;
