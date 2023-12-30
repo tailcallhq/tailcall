62c725af0d7b7c4e6b19321ea241c617bbe207ef
Mayant Mukul <mayantmukul@outlook.in>
2023-12-30 13:42:42 -0800

    chore: rename ExprBody to ExprNode

--- src/blueprint/from_config/operators/expr.rs
+++ src/blueprint/from_config/operators/expr.rs
@@ -1,6 +1,6 @@
 use crate::blueprint::*;
 use crate::config;
-use crate::config::{Config, ExprBody, Field};
+use crate::config::{Config, ExprNode, Field};
 use crate::try_fold::TryFold;
 use crate::valid::Valid;
 
@@ -14,22 +14,22 @@ pub fn update_expr(
       };
 
       match &expr.body {
-        ExprBody::Http(http) => {
+        ExprNode::Http(http) => {
           let field_with_http = (*field).clone().http(http.clone());
           let http_field_def = update_http().try_fold(&(config, &field_with_http, ty, name), b_field);
           http_field_def
         }
-        ExprBody::Const(const_field) => {
+        ExprNode::Const(const_field) => {
           let field_with_const = (*field).clone().const_field(const_field.clone());
           let const_field_def = update_const_field().try_fold(&(config, &field_with_const, ty, name), b_field);
           const_field_def
         }
-        ExprBody::GraphQL(gql) => {
+        ExprNode::GraphQL(gql) => {
           let field_with_gql = (*field).clone().graphql(gql.clone());
           let gql_field_def = update_graphql(operation_type).try_fold(&(config, &field_with_gql, ty, name), b_field);
           gql_field_def
         }
-        ExprBody::Grpc(grpc) => {
+        ExprNode::Grpc(grpc) => {
           let field_with_grpc = (*field).clone().grpc(grpc.clone());
           let grpc_field_def = update_grpc(operation_type).try_fold(&(config, &field_with_grpc, ty, name), b_field);
           grpc_field_def
