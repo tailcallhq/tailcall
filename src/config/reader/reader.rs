a18333db5ee3f80616c146e631eb705456944d64
Sandipsinh Rathod <zotbysandip@gmail.com>
2024-01-04 15:58:55 +0530

    minute optimization

--- src/config/reader/reader.rs
+++ src/config/reader/reader.rs
@@ -3,6 +3,10 @@ pub use super::reader_cli::*;
 #[cfg(not(feature = "default"))]
 pub use super::reader_wasm::*;
 
+pub struct ConfigReader {
+  pub file_paths: Vec<String>,
+}
+
 impl ConfigReader {
   pub fn init<Iter>(file_paths: Iter) -> Self
   where
