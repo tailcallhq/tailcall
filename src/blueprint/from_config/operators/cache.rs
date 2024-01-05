c2e9e02d464744c540ce096ff2d4babcd0f730ff
Shylock Hg <tcath2s@icloud.com>
2024-01-05 17:40:15 +0800

    Add cache key test.

--- src/blueprint/from_config/operators/cache.rs
+++ src/blueprint/from_config/operators/cache.rs
@@ -9,20 +9,23 @@ use crate::try_fold::TryFold;
 use crate::valid::Valid;
 
 pub fn update_cache(object_name: &str) -> TryFold<'_, (&Config, &Field, &config::Type, &str), FieldDefinition, String> {
-  TryFold::<(&Config, &Field, &config::Type, &str), FieldDefinition, String>::new(|(_config, field, _, _), b_field| {
-    let mut updated_b_field = b_field;
-    match updated_b_field.resolver.as_ref() {
-      Some(source) => {
-        if let Some(cache) = &field.cache {
-          let mut hasher = DefaultHasher::new();
-          object_name.hash(&mut hasher);
-          field.name().hash(&mut hasher);
-          let cache = Expression::Cache(Cache::new(hasher, cache.max_age, Box::new(source.clone())));
-          updated_b_field.resolver = Some(cache);
+  TryFold::<(&Config, &Field, &config::Type, &str), FieldDefinition, String>::new(
+    |(_config, field, _, name), b_field| {
+      let mut updated_b_field = b_field;
+      match updated_b_field.resolver.as_ref() {
+        Some(source) => {
+          if let Some(cache) = &field.cache {
+            let mut hasher = DefaultHasher::new();
+            object_name.hash(&mut hasher);
+            name.hash(&mut hasher);
+
+            let cache = Expression::Cache(Cache::new(hasher, cache.max_age, Box::new(source.clone())));
+            updated_b_field.resolver = Some(cache);
+          }
+          Valid::succeed(updated_b_field)
         }
-        Valid::succeed(updated_b_field)
+        None => Valid::succeed(updated_b_field),
       }
-      None => Valid::succeed(updated_b_field),
-    }
-  })
+    },
+  )
 }
