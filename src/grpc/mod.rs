21d908a7f8e3f3ef7fae67a99fcda826425842ff
Tushar Mathur <tusharmath@gmail.com>
2024-01-08 19:56:28 +0530

    style: lint fixes

--- src/grpc/mod.rs
+++ src/grpc/mod.rs
@@ -8,7 +8,6 @@ pub use data_loader_request::DataLoaderRequest;
 pub use request::execute_grpc_request;
 pub use request_template::RequestTemplate;
 
-
 // TODO: request version isn't support in WASM builds
 #[cfg(feature = "default")]
 fn set_req_version(req: &mut reqwest::Request) {
