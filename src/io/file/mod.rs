4e7793492402e6306a94ccafe7ff3298821cc8c5
Tushar Mathur <tusharmath@gmail.com>
2024-01-09 12:25:24 +0530

    merge native implementation for file

--- src/io/file/mod.rs
+++ src/io/file/mod.rs
@@ -1,24 +1,18 @@
 #[cfg(feature = "default")]
-pub mod native;
+pub mod native_impl;
 #[cfg(not(feature = "default"))]
 pub mod wasm;
 
 #[cfg(feature = "default")]
-pub use native::*;
+pub use native_impl::*;
+
 #[cfg(not(feature = "default"))]
 pub use wasm::*;
 
-#[derive(Default)]
-pub struct FileIO {
-  files: Vec<String>,
-}
+pub struct FileIO {}
 
 impl FileIO {
-  pub fn init<Iter>(file_paths: Iter) -> Self
-  where
-    Iter: Iterator,
-    Iter::Item: AsRef<str>,
-  {
-    Self { files: file_paths.map(|path| path.as_ref().to_owned()).collect() }
+  pub fn init() -> Self {
+    FileIO {}
   }
 }
--- src/io/file/native_impl.rs
+++ src/io/file/native_impl.rs
@@ -1,11 +1,16 @@
 use anyhow::{anyhow, Result};
-use tokio::fs::File;
-use tokio::io::AsyncReadExt;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use url::Url;
 
 use crate::io::file::FileIO;
 
 impl FileIO {
+  pub async fn write(file: &str, content: &[u8]) -> Result<()> {
+    let mut file = tokio::fs::File::create(file).await?;
+    file.write_all(content).await?;
+    Ok(())
+  }
+
   pub async fn read_file(file_path: &str) -> Result<(String, String)> {
     if let Ok(url) = Url::parse(file_path) {
       let response = crate::io::http::get_string(url).await?;
@@ -16,15 +21,15 @@ impl FileIO {
       };
       return Ok((response.body, sdl));
     }
-    let mut f = File::open(file_path).await?;
+    let mut file = tokio::fs::File::open(file_path).await?;
     let mut buffer = Vec::new();
-    f.read_to_end(&mut buffer).await?;
+    file.read_to_end(&mut buffer).await?;
     Ok((String::from_utf8(buffer)?, file_path.to_string()))
   }
 
-  pub async fn read_files(&self) -> Result<Vec<(String, String)>> {
+  pub async fn read_files(&self, src_files: &Vec<String>) -> Result<Vec<(String, String)>> {
     let mut files = vec![];
-    for file in &self.files {
+    for file in src_files {
       let content = Self::read_file(file).await?;
       files.push(content);
     }
