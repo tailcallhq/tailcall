d3ada255067271e310e5c12ad762c1c023637a14
Tushar Mathur <tusharmath@gmail.com>
2024-01-09 13:10:27 +0530

    merge http implementations for native

--- src/io/http/native.rs
+++ src/io/http/native.rs
@@ -1,11 +1,12 @@
 use std::time::Duration;
 
+use anyhow::Result;
 use http_cache_reqwest::{Cache, CacheMode, HttpCache, HttpCacheOptions, MokaManager};
-use reqwest::Client;
+use reqwest::{Client, IntoUrl};
 use reqwest_middleware::{ClientBuilder, ClientWithMiddleware};
 
 use crate::config::Upstream;
-use crate::http::HttpClientOptions;
+use crate::http::{HttpClientOptions, Response};
 
 pub fn make_client(upstream: &Upstream, options: HttpClientOptions) -> ClientWithMiddleware {
   let mut builder = Client::builder()
@@ -34,4 +35,34 @@ pub fn make_client(upstream: &Upstream, options: HttpClientOptions) -> ClientWit
     }))
   }
   client.build()
+}
+
+pub async fn execute_raw(client: &ClientWithMiddleware, request: reqwest::Request) -> Result<Response<Vec<u8>>> {
+  log::info!("{} {} {:?}", request.method(), request.url(), request.version());
+  let response = client.execute(request).await?;
+  super::to_resp_raw(response).await
+}
+
+pub async fn execute(
+  client: &ClientWithMiddleware,
+  request: reqwest::Request,
+) -> Result<Response<async_graphql::Value>> {
+  log::info!("{} {} {:?}", request.method(), request.url(), request.version());
+  let response = client.execute(request).await?;
+  super::to_resp_value(response).await
+}
+
+pub async fn get_raw<T: IntoUrl>(url: T) -> Result<Response<Vec<u8>>> {
+  let response = reqwest::get(url).await?;
+  super::to_resp_raw(response).await
+}
+
+pub async fn get_string<T: IntoUrl>(url: T) -> Result<Response<String>> {
+  let response = reqwest::get(url).await?;
+  super::to_resp_string(response).await
+}
+
+pub async fn get_value<T: IntoUrl>(url: T) -> Result<Response<async_graphql::Value>> {
+  let response = reqwest::get(url).await?;
+  super::to_resp_value(response).await
 }
--- src/io/http/native/mod.rs
+++
@@ -1,7 +0,0 @@
-mod client;
-mod execute;
-mod get;
-
-pub use client::*;
-pub use execute::*;
-pub use get::*;
