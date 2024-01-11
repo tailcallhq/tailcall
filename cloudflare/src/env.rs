5bb104c053c55dd87b94d0c1d22fc90eb9672f1a
Sandipsinh Rathod <zotbysandip@gmail.com>
2024-01-11 23:35:35 +0530

    reverted changes

--- cloudflare/src/env.rs
+++ cloudflare/src/env.rs
@@ -1,10 +1,10 @@
-use std::sync::Arc;
+use std::rc::Rc;
 
 use tailcall::io::EnvIO;
 use worker::Env;
 
 pub struct EnvCloudflare {
-  env: Arc<Env>,
+  env: Rc<Env>,
 }
 
 unsafe impl Send for EnvCloudflare {}
@@ -17,7 +17,7 @@ impl EnvIO for EnvCloudflare {
 }
 
 impl EnvCloudflare {
-  pub fn init(env: Arc<Env>) -> Self {
+  pub fn init(env: Rc<Env>) -> Self {
     Self { env }
   }
 }
