a2725de45b13a840ad836f73b905b62d7a354c55
Sandipsinh Rathod <zotbysandip@gmail.com>
2024-01-12 09:36:16 +0530

    Revert "clean up handle"
    This reverts commit cffbc2edd9654b67be2623374f73c666db4f2136.

--- cloudflare/src/handle.rs
+++ cloudflare/src/handle.rs
@@ -113,3 +113,51 @@ pub async fn to_request(mut req: worker::Request) -> anyhow::Result<hyper::Reque
 fn to_anyhow<T: std::fmt::Display>(e: T) -> anyhow::Error {
   anyhow!("{}", e)
 }
+
+async fn to_response(response: hyper::Response<hyper::Body>) -> anyhow::Result<worker::Response> {
+  let status = response.status().as_u16();
+  let headers = response.headers().clone();
+  let bytes = hyper::body::to_bytes(response).await?;
+  let body = worker::ResponseBody::Body(bytes.to_vec());
+  let mut w_response = worker::Response::from_body(body).map_err(to_anyhow)?;
+  w_response = w_response.with_status(status);
+  let mut_headers = w_response.headers_mut();
+  for (name, value) in headers.iter() {
+    let value = String::from_utf8(value.as_bytes().to_vec())?;
+    mut_headers.append(name.as_str(), &value).map_err(to_anyhow)?;
+  }
+
+  Ok(w_response)
+}
+
+fn to_method(method: worker::Method) -> anyhow::Result<hyper::Method> {
+  let method = &*method.to_string().to_uppercase();
+  match method {
+    "GET" => Ok(hyper::Method::GET),
+    "POST" => Ok(hyper::Method::POST),
+    "PUT" => Ok(hyper::Method::PUT),
+    "DELETE" => Ok(hyper::Method::DELETE),
+    "HEAD" => Ok(hyper::Method::HEAD),
+    "OPTIONS" => Ok(hyper::Method::OPTIONS),
+    "PATCH" => Ok(hyper::Method::PATCH),
+    "CONNECT" => Ok(hyper::Method::CONNECT),
+    "TRACE" => Ok(hyper::Method::TRACE),
+    method => Err(anyhow!("Unsupported HTTP method: {}", method)),
+  }
+}
+
+async fn to_request(mut req: worker::Request) -> anyhow::Result<hyper::Request<hyper::Body>> {
+  let body = req.text().await.map_err(to_anyhow)?;
+  let method = req.method();
+  let uri = req.url().map_err(to_anyhow)?.as_str().to_string();
+  let headers = req.headers();
+  let mut builder = hyper::Request::builder().method(to_method(method)?).uri(uri);
+  for (k, v) in headers {
+    builder = builder.header(k, v);
+  }
+  Ok(builder.body(hyper::body::Body::from(body))?)
+}
+
+fn to_anyhow<T: std::fmt::Display>(e: T) -> anyhow::Error {
+  anyhow!("{}", e)
+}
