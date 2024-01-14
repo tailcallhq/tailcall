a5b5ecdc271a6e1f6c700631c3ba2509475a8217
Wesley <100464352+ologbonowiwi@users.noreply.github.com>
2024-01-14 07:40:53 -0300

    feat: add`@omit` operator (#935)
    Co-authored-by: Tushar Mathur <tusharmath@gmail.com>

--- src/config/into_document.rs
+++ src/config/into_document.rs
@@ -184,6 +184,7 @@ fn get_directives(field: &crate::config::Field) -> Vec<Positioned<ConstDirective
     field.unsafe_operation.as_ref().map(|d| pos(d.to_directive())),
     field.const_field.as_ref().map(|d| pos(d.to_directive())),
     field.modify.as_ref().map(|d| pos(d.to_directive())),
+    field.omit.as_ref().map(|d| pos(d.to_directive())),
     field.graphql.as_ref().map(|d| pos(d.to_directive())),
     field.grpc.as_ref().map(|d| pos(d.to_directive())),
     field.expr.as_ref().map(|d| pos(d.to_directive())),
