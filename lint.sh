6b4031f2d6a1d9c0b726f688f687754047668613
Sandipsinh Rathod <zotbysandip@gmail.com>
2024-01-17 18:39:29 +0530

    added json schema check to lint

--- lint.sh
+++ lint.sh
@@ -17,13 +17,8 @@ run_cargo_clippy() {
     MODE=$1
     CMD="cargo +nightly clippy --all-targets --all-features"
     if [ "$MODE" == "fix" ]; then
-        # if mode is fix first run clippy with --fix flag as usual
         $CMD --fix --allow-staged --allow-dirty
     fi
-    # call anyway check command without --fix despite actual $MODE
-    # this is to show error as warnings similar to check mode
-    # since clippy won't fail on warnings when --fix and -D warnings are used together
-    # see https://github.com/rust-lang/rust-clippy/issues/11241
     CMD="$CMD -- -D warnings"
     $CMD
     return $?
@@ -39,6 +34,12 @@ run_prettier() {
     return $?
 }
 
+run_autogen_schema() {
+    MODE=$1
+    cargo run --package autogen-schema --bin autogen-schema -- $MODE
+    return $?
+}
+
 # Extract the mode from the argument
 if [[ $1 == "--mode="* ]]; then
     MODE=${1#--mode=}
@@ -50,6 +51,8 @@ fi
 # Run commands based on mode
 case $MODE in
     check|fix)
+        run_autogen_schema $MODE
+        AUTOGEN_SCHEMA_EXIT_CODE=$?
         run_cargo_fmt $MODE
         FMT_EXIT_CODE=$?
         run_cargo_clippy $MODE
@@ -64,6 +67,6 @@ case $MODE in
 esac
 
 # If any command failed, exit with a non-zero status code
-if [ $FMT_EXIT_CODE -ne 0 ] || [ $CLIPPY_EXIT_CODE -ne 0 ] || [ $PRETTIER_EXIT_CODE -ne 0 ]; then
+if [ $FMT_EXIT_CODE -ne 0 ] || [ $CLIPPY_EXIT_CODE -ne 0 ] || [ $PRETTIER_EXIT_CODE -ne 0 ] || [ $AUTOGEN_SCHEMA_EXIT_CODE -ne 0 ]; then
     exit 1
 fi
