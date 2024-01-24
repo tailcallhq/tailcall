b7cbdc3b3b47f97f6af6f7256d81a52fcdadc840
Tushar Mathur <tusharmath@gmail.com>
2024-01-24 14:08:34 +0530

    add init function in script

--- src/cli/script.rs
+++ src/cli/script.rs
@@ -38,14 +38,24 @@ impl JSEngine {
       RUNTIME
         .spawn(async move {
           V8.with_borrow_mut(|v8| {
-            let closure: Value = v8.eval(create_closure(script.as_str())).unwrap();
-            CLOSURE.replace(closure);
+            let closure: mini_v8::Function = Self::init(v8, script).unwrap();
+            CLOSURE.replace(Value::Function(closure));
             Self {}
           })
         })
         .await
         .unwrap()
     })
+  }
+
+  fn init(v8: &MiniV8, script: String) -> anyhow::Result<mini_v8::Function> {
+    let value: mini_v8::Value = v8
+      .eval(create_closure(script.as_str()))
+      .map_err(|e| anyhow::anyhow!("failed to validate script: {}", e.to_string().replace("mini_v8::", "")))?;
+    let function = value
+      .as_function()
+      .ok_or_else(|| anyhow::anyhow!("expected an 'onEvent' function"))?;
+    Ok(function.clone())
   }
 }
 
@@ -58,7 +68,7 @@ impl<Event: JSValue, Command: JSValue> ScriptIO<Event, Command> for JSEngine {
           let command: Command = Command::from_value(
             closure
               .as_function()
-              .expect("expected function")
+              .unwrap() // Can not fail since we set it in the constructor
               .call(event.to_values())
               .expect("failed to call function"),
           );
