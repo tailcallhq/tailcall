05782284a67981f2d1f7309ee9bddfed82353d87
Sandipsinh Rathod <zotbysandip@gmail.com>
2024-01-24 17:16:53 +0530

    removed unwanted env in cffileio

--- cloudflare/src/file.rs
+++ cloudflare/src/file.rs
@@ -8,29 +8,20 @@ use crate::to_anyhow;
 
 #[derive(Clone)]
 pub struct CloudflareFileIO {
-  env: Rc<Env>,
-  bucket_id: String,
+  bucket: Rc<worker::Bucket>,
 }
 
 impl CloudflareFileIO {
   pub fn init(env: Rc<Env>, bucket_id: String) -> anyhow::Result<Self> {
-    // TODO create TestFileIO and pass it from handle_req
-    // let bucket = env.bucket(bucket_id.as_str()).map_err(|e| anyhow!(e.to_string()))?;
-    Ok(CloudflareFileIO { env, bucket_id })
+    let bucket = env.bucket(bucket_id.as_str()).map_err(|e| anyhow!(e.to_string()))?;
+    let bucket = Rc::new(bucket);
+    Ok(CloudflareFileIO { bucket })
   }
 }
 
 impl CloudflareFileIO {
-  fn bucket(&self) -> anyhow::Result<worker::Bucket> {
-    Ok(
-      self
-        .env
-        .bucket(self.bucket_id.as_str())
-        .map_err(|e| anyhow!(e.to_string()))?,
-    )
-  }
   async fn get(&self, path: String) -> anyhow::Result<String> {
-    let maybe_object = self.bucket()?.get(&path).execute().await.map_err(to_anyhow)?;
+    let maybe_object = self.bucket.get(&path).execute().await.map_err(to_anyhow)?;
     let object = maybe_object.ok_or(anyhow!("File '{}' was not found in bucket", path))?;
 
     let body = match object.body() {
@@ -41,7 +32,7 @@ impl CloudflareFileIO {
   }
 
   async fn put(&self, path: String, value: Vec<u8>) -> anyhow::Result<()> {
-    self.bucket()?.put(&path, value).execute().await.map_err(to_anyhow)?;
+    self.bucket.put(&path, value).execute().await.map_err(to_anyhow)?;
     Ok(())
   }
 }
