dad04ede1376f84030495c4f1e180c5f83e8bce9
Biswarghya Biswas <iambiswarghya@gmail.com>
2024-01-27 06:42:15 +0000

    chore: new upstream

--- src/blueprint/upstream.rs
+++ src/blueprint/upstream.rs
@@ -1,7 +1,28 @@
 use super::TryFoldConfig;
-use crate::config::{Config, Upstream};
+use crate::config::Batch;
+use crate::config::Config;
 use crate::try_fold::TryFold;
 use crate::valid::{Valid, ValidationError};
+use derive_setters::Setters;
+
+#[derive(Clone, Debug, Setters)]
+pub struct Upstream {
+    pub pool_idle_timeout: u64,
+    pub pool_max_idle_per_host: usize,
+    pub keep_alive_interval: u64,
+    pub keep_alive_timeout: u64,
+    pub keep_alive_while_idle: bool,
+    pub proxy: String,
+    pub connect_timeout: u64,
+    pub timeout: u64,
+    pub tcp_keep_alive: u64,
+    pub user_agent: String,
+    pub allowed_headers: String,
+    pub base_url: String,
+    pub http_cache: bool,
+    pub batch: Option<Batch>,
+    pub http2_only: bool,
+}
 
 pub fn to_upstream<'a>() -> TryFold<'a, Config, Upstream, String> {
     TryFoldConfig::<Upstream>::new(|config, up| {
