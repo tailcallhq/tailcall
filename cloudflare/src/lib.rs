e4cf0eb019285b91951729fc3b6752a3c8b74d21
Sandipsinh Rathod <zotbysandip@gmail.com>
2024-02-02 13:43:33 +0530

    remove unwanted public methods

--- cloudflare/src/lib.rs
+++ cloudflare/src/lib.rs
@@ -13,20 +13,20 @@ mod file;
 pub mod handle;
 mod http;
 
-pub fn init_env(env: Rc<worker::Env>) -> Arc<dyn EnvIO> {
+fn init_env(env: Rc<worker::Env>) -> Arc<dyn EnvIO> {
     Arc::new(env::CloudflareEnv::init(env))
 }
 
-pub fn init_file(env: Rc<worker::Env>, bucket_id: String) -> anyhow::Result<Arc<dyn FileIO>> {
+fn init_file(env: Rc<worker::Env>, bucket_id: String) -> anyhow::Result<Arc<dyn FileIO>> {
     #[allow(clippy::arc_with_non_send_sync)]
     Ok(Arc::new(file::CloudflareFileIO::init(env, bucket_id)?))
 }
 
-pub fn init_http() -> Arc<dyn HttpIO> {
+fn init_http() -> Arc<dyn HttpIO> {
     Arc::new(http::CloudflareHttp::init())
 }
 
-pub fn init_cache(env: Rc<worker::Env>) -> Arc<dyn tailcall::Cache<Key = u64, Value = ConstValue>> {
+fn init_cache(env: Rc<worker::Env>) -> Arc<dyn tailcall::Cache<Key = u64, Value = ConstValue>> {
     Arc::new(cache::CloudflareChronoCache::init(env))
 }
 
