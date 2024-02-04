9b771525f05561d6d4cd88e781dac8ef23198235
Gergő Móricz <mo.geryy@gmail.com>
2024-02-04 12:53:35 +0100

    chore(test): delete tests that have been converted to md (#1102)
    Co-authored-by: Tushar Mathur <tusharmath@gmail.com>

--- tests/graphql_spec.rs
+++ tests/graphql_spec.rs
@@ -197,6 +197,10 @@ impl GraphQLSpec {
         let mut dir_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
         dir_path.push(path);
 
+        if !dir_path.exists() {
+            return Ok(Vec::with_capacity(0));
+        }
+
         let entries = fs::read_dir(dir_path.clone())?;
         let mut files = Vec::new();
         let mut only_files = Vec::new();
@@ -217,12 +221,6 @@ impl GraphQLSpec {
                 }
             }
         }
-
-        assert!(
-            !files.is_empty() || !only_files.is_empty(),
-            "No files found in {}",
-            dir_path.to_str().unwrap_or_default()
-        );
 
         if !only_files.is_empty() {
             Ok(only_files)
