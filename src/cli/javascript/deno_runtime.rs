d4247e9241426e3ab7d1e57ec2862e1610f49a56
Tushar Mathur <tusharmath@gmail.com>
2024-02-05 13:12:23 +0530

    chore: implement on_event

--- src/cli/javascript/deno_runtime.rs
+++ src/cli/javascript/deno_runtime.rs
@@ -1,10 +1,12 @@
-use std::cell::{OnceCell, RefCell};
+use std::{
+    cell::{OnceCell, RefCell},
+    thread,
+};
 
 use deno_core::{v8, FastString, JsRuntime};
+use serde::{Deserialize, Serialize};
 
 use crate::{blueprint, WorkerIO};
-
-use super::channel::Message;
 
 struct LocalRuntime {
     value: v8::Global<v8::Value>,
@@ -12,15 +14,33 @@ struct LocalRuntime {
 }
 
 thread_local! {
-  static LOCAL_RUNTIME: OnceCell<LocalRuntime> = const { OnceCell::new() };
-  static JS_RUNTIME : RefCell<JsRuntime> = RefCell::new(JsRuntime::new(Default::default()));
+  static LOCAL_RUNTIME: RefCell<OnceCell<LocalRuntime>> = RefCell::new(OnceCell::new());
 }
+
+#[derive(Serialize, Deserialize)]
+struct Message {
+    pub message: MessageContent,
+    pub id: Option<u64>,
+}
+
+#[derive(Serialize, Deserialize)]
+enum MessageContent {
+    Request(JsRequest),
+    Response(JsResponse),
+    Empty,
+}
+
+#[derive(Serialize, Deserialize)]
+struct JsRequest {}
+#[derive(Serialize, Deserialize)]
+struct JsResponse {}
 
 impl LocalRuntime {
     fn try_new(script: blueprint::Script) -> anyhow::Result<Self> {
         let source = create_closure(script.source.as_str());
         let mut js_runtime = JsRuntime::new(Default::default());
         let value = js_runtime.execute_script("<anon>", FastString::from(source))?;
+        log::debug!("JS Runtime created: {:?}", thread::current().name());
         Ok(Self { value, js_runtime })
     }
 }
@@ -42,15 +62,29 @@ impl Runtime {
 #[async_trait::async_trait]
 impl WorkerIO<Message, Message> for Runtime {
     async fn dispatch(&self, event: Message) -> anyhow::Result<Message> {
-        LOCAL_RUNTIME.with(move |local_runtime_cell| {
+        LOCAL_RUNTIME.with(move |cell| {
             let script = self.script.clone();
-            let local_runtime =
-                local_runtime_cell.get_or_init(|| LocalRuntime::try_new(script).unwrap());
-            on_event_impl(local_runtime, event)
+            cell.borrow()
+                .get_or_init(|| LocalRuntime::try_new(script).unwrap());
+            on_event(event)
         })
     }
 }
 
-fn on_event_impl(rtm: &LocalRuntime, event: Message) -> anyhow::Result<Message> {
-    todo!()
+fn on_event(message: Message) -> anyhow::Result<Message> {
+    LOCAL_RUNTIME.with_borrow_mut(|cell| {
+        let local_runtime = cell.get_mut().unwrap();
+        let scope = &mut local_runtime.js_runtime.handle_scope();
+        let value = &local_runtime.value;
+        let local_value = v8::Local::new(scope, value);
+        let closure: v8::Local<v8::Function> = local_value.try_into()?;
+        let input = serde_v8::to_v8(scope, message)?;
+        let null_ctx = v8::null(scope);
+        let output = closure.call(scope, null_ctx.into(), &[input]);
+
+        match output {
+            None => Ok(Message { message: MessageContent::Empty, id: None }),
+            Some(output) => Ok(serde_v8::from_v8(scope, output)?),
+        }
+    })
 }
