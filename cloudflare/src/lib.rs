39f67c7ca7449c89aa6203e63248b422564c107a
mogery <mo.geryy@gmail.com>
2024-02-05 10:57:11 +0000

    fix(cloudflare): remove unnecessary traits from init_http/init_file

--- cloudflare/src/lib.rs
+++ cloudflare/src/lib.rs
@@ -20,11 +20,11 @@ pub fn init_env(env: Rc<worker::Env>) -> Arc<dyn EnvIO> {
 pub fn init_file(
     env: Rc<worker::Env>,
     bucket_id: String,
-) -> anyhow::Result<Arc<dyn FileIO + Send + Sync>> {
+) -> anyhow::Result<Arc<dyn FileIO>> {
     Ok(Arc::new(file::CloudflareFileIO::init(env, bucket_id)?))
 }
 
-pub fn init_http() -> Arc<dyn HttpIO + Send + Sync> {
+pub fn init_http() -> Arc<dyn HttpIO> {
     Arc::new(http::CloudflareHttp::init())
 }
 
