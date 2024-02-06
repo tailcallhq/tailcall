180c3c2e2c4dd61f4dc1a220cc105eaed4d86ee1
Tushar Mathur <tusharmath@gmail.com>
2024-02-06 20:56:31 +0530

    chore: simpler javascript runtime (#1088)
    Co-authored-by: amit <amitksingh1490@gmail.com>
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

--- src/cli/javascript/mod.rs
+++ src/cli/javascript/mod.rs
@@ -1,15 +1,35 @@
-mod http_filter;
-mod runtime;
-mod serde_v8;
-mod shim;
+use std::collections::BTreeMap;
 pub use std::sync::Arc;
 
+use hyper::header::{HeaderName, HeaderValue};
+
+mod channel;
+mod http_filter;
+mod js_request;
+mod js_response;
+mod runtime;
+
 pub use http_filter::HttpFilter;
+pub use js_request::JsRequest;
+pub use js_response::JsResponse;
 pub use runtime::Runtime;
 
 use crate::{blueprint, HttpIO};
 
 pub fn init_http(http: impl HttpIO, script: blueprint::Script) -> Arc<dyn HttpIO + Sync + Send> {
+    log::debug!("Initializing JavaScript HTTP filter: {}", script.source);
     let script_io = Runtime::new(script);
     Arc::new(HttpFilter::new(http, script_io))
+}
+
+fn create_header_map(
+    headers: BTreeMap<String, String>,
+) -> anyhow::Result<reqwest::header::HeaderMap> {
+    let mut header_map = reqwest::header::HeaderMap::new();
+    for (key, value) in headers.iter() {
+        let key = HeaderName::from_bytes(key.as_bytes())?;
+        let value = HeaderValue::from_str(value.as_str())?;
+        header_map.insert(key, value);
+    }
+    Ok(header_map)
 }
