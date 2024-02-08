8be53a85c9008168362a0a94167deb93f0684c5b
mogery <mo.geryy@gmail.com>
2024-02-08 17:46:49 +0000

    fix(aws-lambda): simplify app_ctx management

--- aws-lambda/src/main.rs
+++ aws-lambda/src/main.rs
@@ -1,31 +1,15 @@
 use std::sync::Arc;
 
 use http::{to_request, to_response};
-use lambda_http::{run, service_fn, Error, Request, Response};
-use lazy_static::lazy_static;
+use lambda_http::{run, service_fn, Error, Response};
 use runtime::init_runtime;
 use tailcall::async_graphql_hyper::GraphQLRequest;
 use tailcall::blueprint::Blueprint;
 use tailcall::config::reader::ConfigReader;
 use tailcall::http::{handle_request, AppContext};
-use tokio::sync::RwLock;
 
 mod http;
 mod runtime;
-
-lazy_static! {
-    static ref APP_CTX: RwLock<Option<Arc<AppContext>>> = RwLock::new(None);
-}
-
-async fn function_handler(event: Request) -> Result<Response<hyper::Body>, Error> {
-    let app_ctx = APP_CTX
-        .read()
-        .await
-        .clone()
-        .expect("AppContext not initialized yet, please wait");
-    let resp = handle_request::<GraphQLRequest>(to_request(event), app_ctx).await?;
-    Ok(to_response(resp)?)
-}
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
@@ -45,7 +29,9 @@ async fn main() -> Result<(), Error> {
 
     let app_ctx = Arc::new(AppContext::new(blueprint, runtime));
 
-    APP_CTX.write().await.replace(app_ctx);
-
-    run(service_fn(function_handler)).await
+    run(service_fn(|event| async {
+        let resp = handle_request::<GraphQLRequest>(to_request(event), app_ctx.clone()).await?;
+        Ok::<Response<hyper::Body>, Error>(to_response(resp)?)
+    }))
+    .await
 }
