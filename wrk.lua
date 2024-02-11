02a5ac5237ade6b4985fa46441d6b753f6a9bf12
Sandipsinh Rathod <zotbysandip@gmail.com>
2024-02-11 23:17:56 +0530

    remove accidental file push

--- wrk.lua
+++
@@ -1,5 +0,0 @@
-wrk.method = "POST"
-wrk.body = '{"operationName":null,"variables":{},"query":"{posts {id user { id }}}"}'
-wrk.headers["Connection"] = "keep-alive"
-wrk.headers["Content-Type"] = "application/json"
-wrk.timeout = 10
