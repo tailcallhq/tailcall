5093f0336a66e7194016f8814ef72d9fbce43828
Sandipsinh Rathod <zotbysandip@gmail.com>
2024-02-13 00:13:31 +0530

    revert testIO

--- benches/impl_path_string_for_evaluation_context.rs
+++ benches/impl_path_string_for_evaluation_context.rs
@@ -9,10 +9,139 @@ use hyper::header::HeaderValue;
 use hyper::HeaderMap;
 use indexmap::IndexMap;
 use once_cell::sync::Lazy;
-// use tailcall::blueprint::{Server, Upstream};
 use tailcall::http::RequestContext;
 use tailcall::lambda::{EvaluationContext, ResolverContextLike};
 use tailcall::path::PathString;
+
+#[cfg(test)]
+pub mod test {
+    use std::collections::HashMap;
+    use std::sync::Arc;
+    use std::time::Duration;
+
+    use anyhow::{anyhow, Result};
+    use http_cache_reqwest::{Cache, CacheMode, HttpCache, HttpCacheOptions, MokaManager};
+    use hyper::body::Bytes;
+    use reqwest::Client;
+    use reqwest_middleware::{ClientBuilder, ClientWithMiddleware};
+    use tailcall::cache::InMemoryCache;
+    use tailcall::http::Response;
+    use tailcall::runtime::TargetRuntime;
+    use tailcall::{EnvIO, FileIO, HttpIO};
+    use tokio::io::{AsyncReadExt, AsyncWriteExt};
+
+    #[derive(Clone)]
+    struct TestHttp {
+        client: ClientWithMiddleware,
+    }
+
+    impl Default for TestHttp {
+        fn default() -> Self {
+            Self { client: ClientBuilder::new(Client::new()).build() }
+        }
+    }
+
+    impl TestHttp {
+        fn init(h2only: bool) -> Self {
+            let mut builder = Client::builder()
+                .tcp_keepalive(Some(Duration::from_secs(5)))
+                .timeout(Duration::from_secs(60))
+                .connect_timeout(Duration::from_secs(60))
+                .http2_keep_alive_interval(Some(Duration::from_secs(60)))
+                .http2_keep_alive_timeout(Duration::from_secs(60))
+                .http2_keep_alive_while_idle(false)
+                .pool_idle_timeout(Some(Duration::from_secs(60)))
+                .pool_max_idle_per_host(60)
+                .user_agent("Tailcall/1.0".to_string());
+
+            // Add Http2 Prior Knowledge
+            if h2only {
+                builder = builder.http2_prior_knowledge();
+            }
+
+            let mut client = ClientBuilder::new(builder.build().expect("Failed to build client"));
+
+            client = client.with(Cache(HttpCache {
+                mode: CacheMode::Default,
+                manager: MokaManager::default(),
+                options: HttpCacheOptions::default(),
+            }));
+
+            Self { client: client.build() }
+        }
+    }
+
+    #[async_trait::async_trait]
+    impl HttpIO for TestHttp {
+        async fn execute(&self, request: reqwest::Request) -> Result<Response<Bytes>> {
+            let response = self.client.execute(request).await;
+            Response::from_reqwest(response?.error_for_status()?).await
+        }
+    }
+
+    #[derive(Clone)]
+    struct TestFileIO {}
+
+    impl TestFileIO {
+        fn init() -> Self {
+            TestFileIO {}
+        }
+    }
+
+    #[async_trait::async_trait]
+    impl FileIO for TestFileIO {
+        async fn write<'a>(&'a self, path: &'a str, content: &'a [u8]) -> anyhow::Result<()> {
+            let mut file = tokio::fs::File::create(path).await?;
+            file.write_all(content)
+                .await
+                .map_err(|e| anyhow!("{}", e))?;
+            Ok(())
+        }
+
+        async fn read<'a>(&'a self, path: &'a str) -> anyhow::Result<String> {
+            let mut file = tokio::fs::File::open(path).await?;
+            let mut buffer = Vec::new();
+            file.read_to_end(&mut buffer)
+                .await
+                .map_err(|e| anyhow!("{}", e))?;
+            Ok(String::from_utf8(buffer)?)
+        }
+    }
+
+    #[derive(Clone)]
+    struct TestEnvIO {
+        vars: HashMap<String, String>,
+    }
+
+    impl EnvIO for TestEnvIO {
+        fn get(&self, key: &str) -> Option<String> {
+            self.vars.get(key).cloned()
+        }
+    }
+
+    impl TestEnvIO {
+        pub fn init() -> Self {
+            Self { vars: std::env::vars().collect() }
+        }
+    }
+
+    pub fn init() -> TargetRuntime {
+        let http: Arc<dyn HttpIO + Sync + Send> = Arc::new(TestHttp::init(false));
+
+        let http2: Arc<dyn HttpIO + Sync + Send> = Arc::new(TestHttp::init(true));
+
+        let file = TestFileIO::init();
+        let env = TestEnvIO::init();
+
+        TargetRuntime {
+            http,
+            http2_only: http2,
+            env: Arc::new(env),
+            file: Arc::new(file),
+            cache: Arc::new(InMemoryCache::new()),
+        }
+    }
+}
 
 const INPUT_VALUE: &[&[&str]] = &[
     // existing values
@@ -146,7 +275,7 @@ fn assert_test(eval_ctx: &EvaluationContext<'_, MockGraphqlContext>) {
 }
 
 fn request_context() -> RequestContext {
-    let runtime = tailcall::cli::runtime::init(&Default::default(), None);
+    let runtime = test::init();
     RequestContext {
         req_headers: HeaderMap::new(),
         upstream: Default::default(),
--- tests/server_spec.rs
+++ tests/server_spec.rs
@@ -1,9 +1,139 @@
 #[cfg(test)]
+pub mod test {
+    use std::collections::HashMap;
+    use std::sync::Arc;
+    use std::time::Duration;
+
+    use anyhow::{anyhow, Result};
+    use http_cache_reqwest::{Cache, CacheMode, HttpCache, HttpCacheOptions, MokaManager};
+    use hyper::body::Bytes;
+    use reqwest::Client;
+    use reqwest_middleware::{ClientBuilder, ClientWithMiddleware};
+    use tailcall::cache::InMemoryCache;
+    use tailcall::http::Response;
+    use tailcall::runtime::TargetRuntime;
+    use tailcall::{EnvIO, FileIO, HttpIO};
+    use tokio::io::{AsyncReadExt, AsyncWriteExt};
+
+    #[derive(Clone)]
+    struct TestHttp {
+        client: ClientWithMiddleware,
+    }
+
+    impl Default for TestHttp {
+        fn default() -> Self {
+            Self { client: ClientBuilder::new(Client::new()).build() }
+        }
+    }
+
+    impl TestHttp {
+        fn init(h2only: bool) -> Self {
+            let mut builder = Client::builder()
+                .tcp_keepalive(Some(Duration::from_secs(5)))
+                .timeout(Duration::from_secs(60))
+                .connect_timeout(Duration::from_secs(60))
+                .http2_keep_alive_interval(Some(Duration::from_secs(60)))
+                .http2_keep_alive_timeout(Duration::from_secs(60))
+                .http2_keep_alive_while_idle(false)
+                .pool_idle_timeout(Some(Duration::from_secs(60)))
+                .pool_max_idle_per_host(60)
+                .user_agent("Tailcall/1.0".to_string());
+
+            // Add Http2 Prior Knowledge
+            if h2only {
+                builder = builder.http2_prior_knowledge();
+            }
+
+            let mut client = ClientBuilder::new(builder.build().expect("Failed to build client"));
+
+            client = client.with(Cache(HttpCache {
+                mode: CacheMode::Default,
+                manager: MokaManager::default(),
+                options: HttpCacheOptions::default(),
+            }));
+
+            Self { client: client.build() }
+        }
+    }
+
+    #[async_trait::async_trait]
+    impl HttpIO for TestHttp {
+        async fn execute(&self, request: reqwest::Request) -> Result<Response<Bytes>> {
+            let response = self.client.execute(request).await;
+            Response::from_reqwest(response?.error_for_status()?).await
+        }
+    }
+
+    #[derive(Clone)]
+    struct TestFileIO {}
+
+    impl TestFileIO {
+        fn init() -> Self {
+            TestFileIO {}
+        }
+    }
+
+    #[async_trait::async_trait]
+    impl FileIO for TestFileIO {
+        async fn write<'a>(&'a self, path: &'a str, content: &'a [u8]) -> anyhow::Result<()> {
+            let mut file = tokio::fs::File::create(path).await?;
+            file.write_all(content)
+                .await
+                .map_err(|e| anyhow!("{}", e))?;
+            Ok(())
+        }
+
+        async fn read<'a>(&'a self, path: &'a str) -> anyhow::Result<String> {
+            let mut file = tokio::fs::File::open(path).await?;
+            let mut buffer = Vec::new();
+            file.read_to_end(&mut buffer)
+                .await
+                .map_err(|e| anyhow!("{}", e))?;
+            Ok(String::from_utf8(buffer)?)
+        }
+    }
+
+    #[derive(Clone)]
+    struct TestEnvIO {
+        vars: HashMap<String, String>,
+    }
+
+    impl EnvIO for TestEnvIO {
+        fn get(&self, key: &str) -> Option<String> {
+            self.vars.get(key).cloned()
+        }
+    }
+
+    impl TestEnvIO {
+        pub fn init() -> Self {
+            Self { vars: std::env::vars().collect() }
+        }
+    }
+
+    pub fn init() -> TargetRuntime {
+        let http: Arc<dyn HttpIO + Sync + Send> = Arc::new(TestHttp::init(false));
+
+        let http2: Arc<dyn HttpIO + Sync + Send> = Arc::new(TestHttp::init(true));
+
+        let file = TestFileIO::init();
+        let env = TestEnvIO::init();
+
+        TargetRuntime {
+            http,
+            http2_only: http2,
+            env: Arc::new(env),
+            file: Arc::new(file),
+            cache: Arc::new(InMemoryCache::new()),
+        }
+    }
+}
+#[cfg(test)]
 mod server_spec {
     use reqwest::Client;
     use serde_json::json;
     use tailcall::builder::TailcallBuilder;
     use tailcall::cli::server::Server;
+    use crate::test;
 
     async fn test_server(configs: &[&str], url: &str) {
         let runtime = tailcall::cli::runtime::init(&Default::default(), None);
@@ -93,7 +223,7 @@ mod server_spec {
     #[tokio::test]
     async fn server_start_http2_nokey() {
         let configs = &["tests/server/config/server-start-http2-nokey.graphql"];
-        let runtime = tailcall::cli::runtime::init(&Default::default(), None);
+        let runtime = test::init();
         let tailcall_executor = TailcallBuilder::init(runtime).with_config_paths(configs);
         assert!(tailcall_executor.await.is_err())
     }
