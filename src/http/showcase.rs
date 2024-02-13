7e4bf631833178384ed65b2302a771973c8064f3
Sandipsinh Rathod <zotbysandip@gmail.com>
2024-02-13 20:09:35 +0530

    drop validationerr in showcase.rs

--- src/http/showcase.rs
+++ src/http/showcase.rs
@@ -8,13 +8,12 @@ use url::Url;
 use crate::async_graphql_hyper::GraphQLResponse;
 use crate::builder::{TailcallBuilder, TailcallExecutor};
 use crate::runtime::TargetRuntime;
-use crate::valid::ValidationError;
 
 pub async fn create_tailcall_executor(
     req: &Request<Body>,
     runtime: TargetRuntime,
     enable_fs: bool,
-) -> Result<Result<TailcallExecutor, Response<Body>>, ValidationError<String>> {
+) -> Result<Result<TailcallExecutor, Response<Body>>> {
     let config_url = req
         .uri()
         .query()
@@ -27,18 +26,14 @@ pub async fn create_tailcall_executor(
         let mut response = async_graphql::Response::default();
         let server_error = ServerError::new("No Config URL specified", None);
         response.errors = vec![server_error];
-        return Ok(Err(GraphQLResponse::from(response)
-            .into_response()
-            .map_err(|e| ValidationError::new(e.to_string()))?));
+        return Ok(Err(GraphQLResponse::from(response).into_response()?));
     };
 
     if !enable_fs && Url::parse(&config_url).is_err() {
         let mut response = async_graphql::Response::default();
         let server_error = ServerError::new("Invalid Config URL specified", None);
         response.errors = vec![server_error];
-        return Ok(Err(GraphQLResponse::from(response)
-            .into_response()
-            .map_err(|e| ValidationError::new(e.to_string()))?));
+        return Ok(Err(GraphQLResponse::from(response).into_response()?));
     }
 
     let tc_builder = TailcallBuilder::init(runtime.clone());
@@ -48,9 +43,7 @@ pub async fn create_tailcall_executor(
             let mut response = async_graphql::Response::default();
             let server_error = ServerError::new(format!("Failed to read config: {}", e), None);
             response.errors = vec![server_error];
-            return Ok(Err(GraphQLResponse::from(response)
-                .into_response()
-                .map_err(|e| ValidationError::new(e.to_string()))?));
+            return Ok(Err(GraphQLResponse::from(response).into_response()?));
         }
     };
 
