d2757e802e491f7ec83ab9ae1622c0893138c97b
Sandipsinh Rathod <zotbysandip@gmail.com>
2024-02-17 12:22:08 +0530

    add wait loop

---
+++ ci-benchmark/cf_start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+url="http://localhost:19194/\?config\=https://raw.githubusercontent.com/tailcallhq/tailcall/main/ci-benchmark/benchmark.graphql"
+while true; do
+    response_code=$(curl -s -o /dev/null -w "%{http_code}" "$url")
+
+    if [ "$response_code" -eq 200 ]; then
+        break
+    else
+        echo "Waiting for Cloudflare Worker to be ready..."
+        sleep 5
+    fi
+done
