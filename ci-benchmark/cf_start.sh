96a1ebb06622161df0e96490c5f5e3e36007b4e6
Sandipsinh Rathod <zotbysandip@gmail.com>
2024-02-17 13:36:02 +0530

    add upper bound to worker start loop

--- ci-benchmark/cf_start.sh
+++ ci-benchmark/cf_start.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 url="http://localhost:19194/"
+counter=0
+
 while true; do
     response_code=$(curl -s -o /dev/null -w "%{http_code}" "$url")
 
@@ -9,5 +11,11 @@ while true; do
     else
         echo "Waiting for Cloudflare Worker to be ready..."
         sleep 10
+        ((counter++))
+
+        if [ "$counter" -gt 60 ]; then
+            echo "Unable to start worker, exiting to prevent infinite loop"
+            exit 1
+        fi
     fi
 done
