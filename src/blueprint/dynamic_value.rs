13517c4b53ddd37d12026ec3d7c778af2358aa7d
amit <amitksingh1490@gmail.com>
2024-02-22 14:00:39 +0530

    refactor: simplify validation

--- src/blueprint/dynamic_value.rs
+++ src/blueprint/dynamic_value.rs
@@ -1,4 +1,4 @@
-use async_graphql_value::Name;
+use async_graphql_value::{ConstValue, Name};
 use indexmap::IndexMap;
 use serde_json::Value;
 
@@ -6,13 +6,52 @@ use crate::mustache::Mustache;
 
 #[derive(Debug, Clone)]
 pub enum DynamicValue {
-    Value(serde_json::Value),
+    Value(Value),
     Mustache(Mustache),
-    MustacheObject(IndexMap<Name, MustacheOrValue>),
-    MustacheArray(Vec<MustacheOrValue>),
+    Object(IndexMap<Name, DynamicValue>),
+    Array(Vec<DynamicValue>),
 }
 
-impl TryFrom<&serde_json::Value> for DynamicValue {
+impl TryFrom<&DynamicValue> for ConstValue {
+    type Error = anyhow::Error;
+
+    fn try_from(value: &DynamicValue) -> Result<Self, Self::Error> {
+        match value {
+            DynamicValue::Value(v) => {
+                ConstValue::from_json(v.to_owned()).map_err(anyhow::Error::new)
+            }
+            DynamicValue::Mustache(_) => Err(anyhow::anyhow!(
+                "mustache cannot be converted to const value at compiletime"
+            )),
+            DynamicValue::Object(obj) => {
+                let out: Result<IndexMap<Name, ConstValue>, anyhow::Error> = obj
+                    .into_iter()
+                    .map(|(k, v)| Ok((k.to_owned(), ConstValue::try_from(v)?.to_owned())))
+                    .collect();
+                Ok(ConstValue::Object(out?))
+            }
+            DynamicValue::Array(arr) => {
+                let out: Result<Vec<ConstValue>, anyhow::Error> =
+                    arr.iter().map(ConstValue::try_from).collect();
+                Ok(ConstValue::List(out?))
+            }
+        }
+    }
+}
+
+impl DynamicValue {
+    // Helper method to determine if the value is constant (non-mustache).
+    pub fn is_const(&self) -> bool {
+        match self {
+            DynamicValue::Mustache(m) => m.is_const(),
+            DynamicValue::Object(obj) => obj.values().all(|v| v.is_const()),
+            DynamicValue::Array(arr) => arr.iter().all(|v| v.is_const()),
+            _ => true,
+        }
+    }
+}
+
+impl TryFrom<&Value> for DynamicValue {
     type Error = anyhow::Error;
 
     fn try_from(value: &Value) -> Result<Self, Self::Error> {
@@ -20,26 +59,15 @@ impl TryFrom<&serde_json::Value> for DynamicValue {
             Value::Object(obj) => {
                 let mut out = IndexMap::new();
                 for (k, v) in obj {
-                    let m = Mustache::parse(v.to_string().as_str())?;
-                    if m.is_const() {
-                        out.insert(Name::new(k), MustacheOrValue::Value(v.clone()));
-                    } else {
-                        out.insert(Name::new(k), MustacheOrValue::Mustache(m));
-                    }
+                    let dynamic_value = DynamicValue::try_from(v)?;
+                    out.insert(Name::new(k), dynamic_value);
                 }
-                Ok(DynamicValue::MustacheObject(out))
+                Ok(DynamicValue::Object(out))
             }
             Value::Array(arr) => {
-                let mut out = Vec::new();
-                for v in arr {
-                    let m = Mustache::parse(v.to_string().as_str())?;
-                    if m.is_const() {
-                        out.push(MustacheOrValue::Value(v.clone()));
-                    } else {
-                        out.push(MustacheOrValue::Mustache(m));
-                    }
-                }
-                Ok(DynamicValue::MustacheArray(out))
+                let out: Result<Vec<DynamicValue>, Self::Error> =
+                    arr.iter().map(DynamicValue::try_from).collect();
+                Ok(DynamicValue::Array(out?))
             }
             Value::String(s) => {
                 let m = Mustache::parse(s.as_str())?;
@@ -50,21 +78,6 @@ impl TryFrom<&serde_json::Value> for DynamicValue {
                 }
             }
             _ => Ok(DynamicValue::Value(value.clone())),
-        }
-    }
-}
-
-#[derive(Debug, Clone)]
-pub enum MustacheOrValue {
-    Mustache(Mustache),
-    Value(serde_json::Value),
-}
-
-impl MustacheOrValue {
-    pub fn is_const(&self) -> bool {
-        match self {
-            MustacheOrValue::Mustache(m) => m.is_const(),
-            _ => true,
         }
     }
 }
