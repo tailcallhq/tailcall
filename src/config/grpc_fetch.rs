9071ff4b4b12f8e8759afe85a575ec23cd1e7739
Sandipsinh Rathod <zotbysandip@gmail.com>
2024-02-23 20:04:50 +0530

    switch to structs to handle response

--- src/config/grpc_fetch.rs
+++ src/config/grpc_fetch.rs
@@ -27,7 +27,8 @@ fn get_protobuf_set() -> Result<ProtobufSet> {
 #[derive(Debug, Serialize, Deserialize)]
 #[serde(deny_unknown_fields, rename_all = "camelCase")]
 struct OriginalRequest {
-    list_services: String,
+    list_services: Option<String>,
+    file_containing_symbol: Option<String>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -42,9 +43,27 @@ struct ListServicesResponse {
 
 #[derive(Debug, Serialize, Deserialize)]
 #[serde(deny_unknown_fields, rename_all = "camelCase")]
+struct FileDescriptorProtoResponse {
+    file_descriptor_proto: Vec<String>,
+}
+
+impl FileDescriptorProtoResponse {
+    fn get(self) -> Result<Vec<u8>> {
+        let file_descriptor_proto = self
+            .file_descriptor_proto
+            .first()
+            .context("Received empty fileDescriptorProto")?;
+
+        Ok(BASE64_STANDARD.decode(file_descriptor_proto)?)
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(deny_unknown_fields, rename_all = "camelCase")]
 struct CustomResponse {
     original_request: OriginalRequest,
-    list_services_response: ListServicesResponse,
+    list_services_response: Option<ListServicesResponse>,
+    file_descriptor_response: Option<FileDescriptorProtoResponse>,
 }
 
 /// Makes `ListService` request to the grpc reflection server
@@ -66,12 +85,12 @@ pub async fn list_all_files(url: &str, target_runtime: &TargetRuntime) -> Result
     let operation = reflection_service.find_operation("ServerReflectionInfo")?;
 
     let response: Value = serde_json::from_value(operation.convert_output(body)?.into_json()?)?;
-    println!("{}",response);
     let response: CustomResponse = serde_json::from_value(response)?;
 
     // Extracting names from services
     let methods: Vec<String> = response
         .list_services_response
+        .context("Expected listServicesResponse but found none")?
         .service
         .iter()
         .map(|s| s.name.clone())
@@ -81,54 +100,17 @@ pub async fn list_all_files(url: &str, target_runtime: &TargetRuntime) -> Result
 }
 
 /// Makes `Get Service` request to the grpc reflection server
-
 pub async fn get_by_service(
     url: &str,
     target_runtime: &TargetRuntime,
     service: &str,
 ) -> Result<FileDescriptorProto> {
-    let protobuf_set = get_protobuf_set()?;
-
     let mut url: url::Url = url.parse()?;
     url.set_path("grpc.reflection.v1alpha.ServerReflection/ServerReflectionInfo");
 
     let mut req = reqwest::Request::new(Method::POST, url);
     *req.body_mut() = Some(reqwest::Body::from(format!("\0\0\0\0\x12\"\x10{service}")));
-
-    let resp = target_runtime.http.execute(req).await?;
-    let body = resp.body.as_bytes();
-
-    let reflection_service =
-        protobuf_set.find_service("grpc.reflection.v1alpha.ServerReflection")?;
-    let operation = reflection_service.find_operation("ServerReflectionInfo")?;
-
-    let response: Value = serde_json::from_value(operation.convert_output(body)?.into_json()?)?;
-    let object = response
-        .as_object()
-        .context("Invalid response, expected object.")?;
-    let file_descriptor_response = object
-        .get("fileDescriptorResponse")
-        .context("Expected key fileDescriptorResponse but found none")?;
-    let file_descriptor_response_object = file_descriptor_response
-        .as_object()
-        .context("Expected fileDescriptorResponse to be an object")?;
-    let file_descriptor_proto = file_descriptor_response_object
-        .get("fileDescriptorProto")
-        .context("expected fileDescriptorProto as object")?;
-    let file_descriptor_proto_arr = file_descriptor_proto
-        .as_array()
-        .context("Expected fileDescriptorProto to be an array")?;
-    let file_descriptor_proto = file_descriptor_proto_arr
-        .first()
-        .context("Received empty fileDescriptorProto")?;
-
-    let file_descriptor_proto = file_descriptor_proto
-        .as_str()
-        .context("Expected content of fileDescriptorProto as a string")?;
-    let file_descriptor_proto =
-        FileDescriptorProto::decode(BASE64_STANDARD.decode(file_descriptor_proto)?.as_bytes())?;
-
-    Ok(file_descriptor_proto)
+    make_req(req, target_runtime).await
 }
 
 /// Makes `Get Proto/Symbol Name` request to the grpc reflection server
@@ -137,8 +119,6 @@ pub async fn get_by_proto_name(
     target_runtime: &TargetRuntime,
     proto_name: &str,
 ) -> Result<FileDescriptorProto> {
-    let protobuf_set = get_protobuf_set()?;
-
     let mut url: url::Url = url.parse()?;
     url.set_path("grpc.reflection.v1alpha.ServerReflection/ServerReflectionInfo");
 
@@ -148,6 +128,15 @@ pub async fn get_by_proto_name(
         proto_name
     )));
 
+    make_req(req, target_runtime).await
+}
+
+async fn make_req(
+    req: reqwest::Request,
+    target_runtime: &TargetRuntime,
+) -> Result<FileDescriptorProto> {
+    let protobuf_set = get_protobuf_set()?;
+
     let resp = target_runtime.http.execute(req).await?;
     let body = resp.body.as_bytes();
 
@@ -155,31 +144,123 @@ pub async fn get_by_proto_name(
         protobuf_set.find_service("grpc.reflection.v1alpha.ServerReflection")?;
     let operation = reflection_service.find_operation("ServerReflectionInfo")?;
 
-    let response: Value = serde_json::from_value(operation.convert_output(body)?.into_json()?)?;
-    let object = response
-        .as_object()
-        .context("Invalid response, expected object.")?;
-    let file_descriptor_response = object
-        .get("fileDescriptorResponse")
-        .context("Expected key fileDescriptorResponse but found none")?;
-    let file_descriptor_response_object = file_descriptor_response
-        .as_object()
-        .context("Expected fileDescriptorResponse to be an object")?;
-    let file_descriptor_proto = file_descriptor_response_object
-        .get("fileDescriptorProto")
-        .context("expected fileDescriptorProto as object")?;
-    let file_descriptor_proto_arr = file_descriptor_proto
-        .as_array()
-        .context("Expected fileDescriptorProto to be an array")?;
-    let file_descriptor_proto = file_descriptor_proto_arr
-        .first()
-        .context("Received empty fileDescriptorProto")?;
-
-    let file_descriptor_proto = file_descriptor_proto
-        .as_str()
-        .context("Expected content of fileDescriptorProto as a string")?;
+    let response: CustomResponse =
+        serde_json::from_value(operation.convert_output(body)?.into_json()?)?;
+    let file_descriptor_resp = response
+        .file_descriptor_response
+        .context("Expected fileDescriptorResponse but found none")?;
     let file_descriptor_proto =
-        FileDescriptorProto::decode(BASE64_STANDARD.decode(file_descriptor_proto)?.as_bytes())?;
+        FileDescriptorProto::decode(file_descriptor_resp.get()?.as_bytes())?;
 
     Ok(file_descriptor_proto)
+}
+
+#[cfg(test)]
+mod grpc_fetch {
+    use std::path::PathBuf;
+
+    use anyhow::Result;
+
+    use crate::config::grpc_fetch::{get_by_proto_name, get_by_service, list_all_files};
+
+    const NEWS_PROTO: &[u8] = include_bytes!(concat!(
+        env!("CARGO_MANIFEST_DIR"),
+        "/tests/fake_descriptor.bin"
+    ));
+
+    const REFLECTION_LIST_ALL: &[u8] = &[
+        0, 0, 0, 0, 70, 18, 2, 58, 0, 50, 64, 10, 18, 10, 16, 110, 101, 119, 115, 46, 78, 101, 119,
+        115, 83, 101, 114, 118, 105, 99, 101, 10, 42, 10, 40, 103, 114, 112, 99, 46, 114, 101, 102,
+        108, 101, 99, 116, 105, 111, 110, 46, 118, 49, 97, 108, 112, 104, 97, 46, 83, 101, 114,
+        118, 101, 114, 82, 101, 102, 108, 101, 99, 116, 105, 111, 110,
+    ];
+
+    fn start_mock_server() -> httpmock::MockServer {
+        httpmock::MockServer::start()
+    }
+    #[tokio::test]
+    async fn test_resp_file_name() -> Result<()> {
+        let server = start_mock_server();
+
+        let http_reflection_file_mock = server.mock(|when, then| {
+            when.method(httpmock::Method::POST)
+                .path("/grpc.reflection.v1alpha.ServerReflection/ServerReflectionInfo")
+                .body("\0\0\0\0\x0c\x1a\nnews.proto");
+            then.status(200).body(NEWS_PROTO);
+        });
+
+        let runtime = crate::runtime::test::init(None);
+        let resp = get_by_proto_name(
+            &format!("http://localhost:{}", server.port()),
+            &runtime,
+            "news.proto",
+        )
+        .await?;
+        let mut news_proto = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        news_proto.push("src/grpc/tests/news.proto");
+        let content = runtime.file.read(news_proto.to_str().unwrap()).await?;
+        let expected = protox_parse::parse("news.proto", &content)?;
+
+        assert_eq!(expected.name(), resp.name());
+
+        http_reflection_file_mock.assert();
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_resp_service() -> Result<()> {
+        let server = start_mock_server();
+
+        let http_reflection_file_mock = server.mock(|when, then| {
+            when.method(httpmock::Method::POST)
+                .path("/grpc.reflection.v1alpha.ServerReflection/ServerReflectionInfo")
+                .body("\0\0\0\0\x12\"\x10news.NewsService");
+            then.status(200).body(NEWS_PROTO);
+        });
+
+        let runtime = crate::runtime::test::init(None);
+        let resp = get_by_service(
+            &format!("http://localhost:{}", server.port()),
+            &runtime,
+            "news.NewsService",
+        )
+        .await?;
+        let mut news_proto = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        news_proto.push("src/grpc/tests/news.proto");
+        let content = runtime.file.read(news_proto.to_str().unwrap()).await?;
+        let expected = protox_parse::parse("news.proto", &content)?;
+
+        assert_eq!(expected.name(), resp.name());
+
+        http_reflection_file_mock.assert();
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn foo() -> Result<()> {
+        let server = start_mock_server();
+
+        let http_reflection_list_all = server.mock(|when, then| {
+            when.method(httpmock::Method::POST)
+                .path("/grpc.reflection.v1alpha.ServerReflection/ServerReflectionInfo")
+                .body("\0\0\0\0\x02:\0");
+            then.status(200).body(REFLECTION_LIST_ALL);
+        });
+
+        let runtime = crate::runtime::test::init(None);
+        let resp = list_all_files(&format!("http://localhost:{}", server.port()), &runtime).await?;
+
+        assert_eq!(
+            [
+                "news.NewsService".to_string(),
+                "grpc.reflection.v1alpha.ServerReflection".to_string()
+            ]
+            .to_vec(),
+            resp
+        );
+
+        http_reflection_list_all.assert();
+
+        Ok(())
+    }
 }
