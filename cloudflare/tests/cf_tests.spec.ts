ba3226b30344f2f108802f9ac3f354713a1495b2
Sandipsinh Rathod <62684960+ssddOnTop@users.noreply.github.com>
2024-02-23 15:43:11 +0530

    chore(cf): add tests for worker (#1232)

--- cloudflare/tests/cf_tests.spec.ts
+++ cloudflare/tests/cf_tests.spec.ts
@@ -6,11 +6,17 @@ describe("fetch", () => {
   test("loadfiles", async () => {
     let placeholder = (await readFile("../examples/jsonplaceholder.graphql")).toString()
     let placeholder_batch = (await readFile("../examples/jsonplaceholder_batch.graphql")).toString()
+    let grpc = (await readFile("../examples/grpc.graphql")).toString()
+    let news_proto = (await readFile("../src/grpc/tests/news.proto")).toString()
 
     let bucket = await mf.getR2Bucket("MY_R2")
+    await bucket.put("examples/grpc.graphql", grpc)
+    await bucket.put("examples/../src/grpc/tests/news.proto", news_proto)
+    await bucket.put("src/grpc/tests/news.proto", grpc)
     await bucket.put("examples/jsonplaceholder.graphql", placeholder)
     await bucket.put("examples/jsonplaceholder_batch.graphql", placeholder_batch)
   })
+
   test("ide", async () => {
     let resp = await mf.dispatchFetch("https://fake.host/", {
       method: "GET",
@@ -38,6 +44,17 @@ describe("fetch", () => {
     })
     let body = await resp.json()
     let expected = {data: {posts: [{id: 1}]}}
+    expect(body).toEqual(expected)
+    expect(resp.status).toBe(200)
+  })
+
+  test("test_grpc", async () => {
+    let resp = await mf.dispatchFetch("https://fake.host/graphql?config=examples/grpc.graphql", {
+      method: "POST",
+      body: '{"query":"{ news { news { id } } }"}',
+    })
+    let body = await resp.json()
+    let expected = {data: {news: {news: [{id: 1}, {id: 2}, {id: 3}, {id: 4}, {id: 5}]}}}
     expect(body).toEqual(expected)
     expect(resp.status).toBe(200)
   })
