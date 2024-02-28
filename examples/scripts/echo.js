4c04664a73a44b0ec736bcffa7a06afb01604939
Tushar Mathur <tusharmath@gmail.com>
2024-02-28 15:43:14 -0500

    lint fixes

--- examples/scripts/echo.js
+++ examples/scripts/echo.js
@@ -1,11 +1,11 @@
 function onRequest(request) {
-    return {
-        response: {
-            status: 200,
-            headers: {
-                "Content-Type": "text/plain"
-            },
-            body: JSON.stringify([{ title: request.url }])
-        }
-    };
+  return {
+    response: {
+      status: 200,
+      headers: {
+        "Content-Type": "text/plain",
+      },
+      body: JSON.stringify([{title: request.url}]),
+    },
+  }
 }
