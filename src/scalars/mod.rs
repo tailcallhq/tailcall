cb2d142c9db2a67613535c80711e6128c6549347
Sandipsinh Rathod <zotbysandip@gmail.com>
2024-02-29 21:36:00 +0530

    fix `to_scalar_type_definition`

--- src/scalars/mod.rs
+++ src/scalars/mod.rs
@@ -1,3 +1,5 @@
+use async_graphql_value::ConstValue;
+
 pub use crate::scalars::email::Email;
 
 mod email;
@@ -6,4 +8,11 @@ mod email;
 /// A wrapper to store all custom scalar types
 pub struct Scalars {
     pub email: Email,
+}
+
+pub fn get_scalar(name: &str) -> fn(&ConstValue) -> bool {
+    match name {
+        "Email" | "email" => Email::validate,
+        &_ => |_| true,
+    }
 }
