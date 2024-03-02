62354afb5aff00c43bbefbddbce85b68000f0bdc
mehulmathur16 <mathur.mehul01@gmail.com>
2024-03-02 16:17:24 +0530

    fix: colors

--- src/cli/tc.rs
+++ src/cli/tc.rs
@@ -1,5 +1,7 @@
 use std::path::Path;
 use std::{env, fs};
+use std::io::Write;
+use log::{Level};
 
 use anyhow::Result;
 use clap::Parser;
@@ -174,6 +176,16 @@ fn display_config(config: &Config, n_plus_one_queries: bool) {
     Fmt::display(Fmt::table(seq));
 }
 
+fn color_for_level(level: Level) -> &'static str {
+    match level {
+        Level::Error => "\x1b[31m", // Red color for Error
+        Level::Warn => "\x1b[33m",  // Yellow color for Warn
+        Level::Info => "\x1b[32m",  // Green color for Info
+        Level::Debug => "\x1b[34m", // Blue color for Debug
+        Level::Trace => "\x1b[35m", // Magenta color for Trace
+    }
+}
+
 // initialize logger
 fn logger_init() {
     // set the log level
@@ -188,5 +200,11 @@ fn logger_init() {
     // use the log level from the env if there is one, otherwise use the default.
     let env = Env::new().filter_or(filter_env_name, "info");
 
-    env_logger::Builder::from_env(env).format_timestamp(None).format_target(false).init();
+    
+    env_logger::Builder::from_env(env).format(|buf, record| {
+        let level = record.level();
+        let color = color_for_level(level);
+        writeln!(buf, "{}[{}]\x1b[0m {}", color, record.level(), record.args())?;
+        Ok(())
+    }).init();
 }
