acbd40158b50ed38af6d62bbbe665f659630f69f
Tushar Mathur <tusharmath@gmail.com>
2024-03-02 13:56:59 +0530

    simplify echo

--- examples/scripts/echo.js
+++ examples/scripts/echo.js
@@ -1,11 +1,3 @@
 function onRequest(request) {
-  return {
-    response: {
-      status: 200,
-      headers: {
-        "Content-Type": "text/plain",
-      },
-      body: JSON.stringify([{title: request.url}]),
-    },
-  }
+  return {request}
 }
