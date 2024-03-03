0746a394f5b33255cf18fb4ca9668d8600bcabc2
mehulmathur16 <mathur.mehul01@gmail.com>
2024-03-03 22:16:36 +0530

    perf: buffer implementation

--- src/cli/tc.rs
+++ src/cli/tc.rs
@@ -193,15 +193,18 @@ fn logger_init() {
         .format(|buf, record| {
             let level = record.level();
             let color_styles = buf.default_level_style(level);
+            let mut lines_buffer = Vec::new();
 
             for line in record.args().to_string().lines() {
                 writeln!(
-                    buf,
+                    &mut lines_buffer,
                     "{color_styles}[{}]{color_styles:#} {}",
                     record.level(),
                     line,
                 )?;
             }
+
+            buf.write_all(&lines_buffer)?;
 
             Ok(())
         })
