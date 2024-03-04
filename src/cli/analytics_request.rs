ee161cad032b6531e0a7bce88df57f573df2249e
Biswarghya Biswas <iambiswarghya@gmail.com>
2024-03-04 23:58:27 +0530

    fix

--- src/cli/analytics_request.rs
+++ src/cli/analytics_request.rs
@@ -2,7 +2,7 @@ use anyhow::{Context, Result};
 use reqwest::Client;
 use serde::{Deserialize, Serialize};
 
-// secrets & constant
+// secrets & constants
 const API_SECRET: &str = "secret";
 const MEASUREMENT_ID: &str = "G-NCQKBRNVDW";
 const BASE_URL: &str = "https://www.google-analytics.com";
