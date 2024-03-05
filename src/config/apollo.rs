d2b2e30e073def2ecd4c0f978953473f16340adc
Shashi Kant <shashitnak@gmail.com>
2024-03-05 09:21:24 +0530

    lint fix

--- src/config/apollo.rs
+++ src/config/apollo.rs
@@ -4,29 +4,23 @@ use serde::{Deserialize, Serialize};
 pub struct Apollo {
     ///
     /// Setting `api_key` for Apollo.
-    ///
     pub api_key: String,
     ///
     /// Setting `graph_id` for Apollo.
-    ///
     pub graph_id: String,
     ///
     /// Setting `variant` for Apollo.
-    ///
     pub variant: String,
     ///
     /// Setting `userVersion` for Apollo.
-    ///
     #[serde(default = "default_user_version")]
     pub user_version: String,
     ///
     /// Setting `platform` for Apollo.
-    ///
     #[serde(default = "default_platform")]
     pub platform: String,
     ///
     /// Setting `version` for Apollo.
-    ///
     #[serde(default = "default_version")]
     pub version: String,
 }
