e3bf36b66d362fece145bcf27b323bbd028ba624
Sandipsinh Rathod <zotbysandip@gmail.com>
2024-03-06 18:27:36 +0530

    add phone number scalar

--- src/scalars/mod.rs
+++ src/scalars/mod.rs
@@ -5,13 +5,16 @@ use async_graphql_value::ConstValue;
 use lazy_static::lazy_static;
 
 pub use crate::scalars::email::Email;
+pub use crate::scalars::phone::PhoneNumber;
 
 mod email;
+mod phone;
 
 lazy_static! {
     pub static ref CUSTOM_SCALARS: HashMap<String, Arc<dyn Scalar + Send + Sync>> = {
         let mut hm: HashMap<String, Arc<dyn Scalar + Send + Sync>> = HashMap::new();
         hm.insert("Email".to_string(), Arc::new(Email::default()));
+        hm.insert("PhoneNumber".to_string(), Arc::new(PhoneNumber::default()));
         hm
     };
 }
@@ -19,6 +22,7 @@ lazy_static! {
 #[derive(schemars::JsonSchema)]
 pub enum CustomScalar {
     Email(Email),
+    PhoneNumber(PhoneNumber),
 }
 
 pub trait Scalar {
