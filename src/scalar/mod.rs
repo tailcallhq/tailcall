4a9b5a5bfb90a0950790bec776410ed7fc3c742c
Sandipsinh Rathod <zotbysandip@gmail.com>
2024-03-08 16:16:54 +0530

    drop CustomScalars enum

--- src/scalar/mod.rs
+++ src/scalar/mod.rs
@@ -13,14 +13,45 @@ use std::sync::Arc;
 
 use async_graphql_value::ConstValue;
 use lazy_static::lazy_static;
+use schemars::schema::Schema;
+use schemars::schema_for;
+
+pub struct ScalarMetadata {
+    pub instance: Arc<dyn Scalar + Send + Sync>,
+    pub schema: Schema,
+}
 
 lazy_static! {
-    static ref CUSTOM_SCALARS: HashMap<String, Arc<dyn Scalar + Send + Sync>> = {
-        let mut hm: HashMap<String, Arc<dyn Scalar + Send + Sync>> = HashMap::new();
-        hm.insert("Email".to_string(), Arc::new(Email::default()));
-        hm.insert("PhoneNumber".to_string(), Arc::new(PhoneNumber::default()));
-        hm.insert("Date".to_string(), Arc::new(Date::default()));
-        hm.insert("Url".to_string(), Arc::new(Url::default()));
+    pub static ref CUSTOM_SCALARS: HashMap<String, ScalarMetadata> = {
+        let mut hm = HashMap::new();
+        hm.insert(
+            "Email".to_string(),
+            ScalarMetadata {
+                instance: Arc::new(Email::default()),
+                schema: Schema::Object(schema_for!(Email).schema),
+            },
+        );
+        hm.insert(
+            "PhoneNumber".to_string(),
+            ScalarMetadata {
+                instance: Arc::new(PhoneNumber::default()),
+                schema: Schema::Object(schema_for!(PhoneNumber).schema),
+            },
+        );
+        hm.insert(
+            "Date".to_string(),
+            ScalarMetadata {
+                instance: Arc::new(Date::default()),
+                schema: Schema::Object(schema_for!(Date).schema),
+            },
+        );
+        hm.insert(
+            "Url".to_string(),
+            ScalarMetadata {
+                instance: Arc::new(Url::default()),
+                schema: Schema::Object(schema_for!(Url).schema),
+            },
+        );
         hm
     };
 }
@@ -37,14 +68,6 @@ pub fn is_scalar(type_name: &str) -> bool {
     SCALAR_TYPES.contains(type_name)
 }
 
-#[derive(schemars::JsonSchema)]
-pub enum CustomScalar {
-    Email(Email),
-    PhoneNumber(PhoneNumber),
-    Date(Date),
-    Url(Url),
-}
-
 pub trait Scalar {
     fn validate(&self) -> fn(&ConstValue) -> bool;
 }
@@ -52,6 +75,6 @@ pub trait Scalar {
 pub fn get_scalar(name: &str) -> fn(&ConstValue) -> bool {
     CUSTOM_SCALARS
         .get(name)
-        .map(|v| v.validate())
+        .map(|v| v.instance.validate())
         .unwrap_or(|_| true)
 }
