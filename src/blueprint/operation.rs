b42800e9737568fec614416b28274368965866e3
Sandipsinh Rathod <zotbysandip@gmail.com>
2024-03-11 12:52:25 +0530

    init

--- src/blueprint/operation.rs
+++ src/blueprint/operation.rs
@@ -3,45 +3,47 @@ use std::fmt::Write;
 use async_graphql::dynamic::Schema;
 
 use super::{Blueprint, SchemaModifiers};
+use crate::async_graphql_hyper::GraphQLRequest;
 use crate::valid::{Cause, Valid, Validator};
 
 #[derive(Debug)]
 pub struct OperationQuery {
-    query: String,
+    query: GraphQLRequest,
     file: String,
 }
 
 impl OperationQuery {
-    pub fn new(query: String, trace: String) -> Self {
-        Self { query, file: trace }
+    pub fn new(query: String, trace: String) -> anyhow::Result<Self> {
+        let query = serde_json::from_str(query.as_str())?;
+        Ok(Self { query, file: trace })
     }
 
-    fn to_cause(&self, err: &async_graphql::ServerError) -> Cause<String> {
-        let mut trace = Vec::new();
-        let file = self.file.as_str();
-
-        for loc in err.locations.iter() {
-            let mut message = String::new();
-            message.write_str(file).unwrap();
-            message
-                .write_str(format!(":{}:{}", loc.line, loc.column).as_str())
-                .unwrap();
-
-            trace.push(message);
-        }
-
-        Cause::new(err.message.clone()).trace(trace)
-    }
-
-    async fn validate(&self, schema: &Schema) -> Vec<Cause<String>> {
+    async fn validate(self, schema: &Schema) -> Vec<Cause<String>> {
+        let file = self.file.clone();
         schema
-            .execute(&self.query)
+            .execute(self.query.0)
             .await
             .errors
             .iter()
-            .map(|e| self.to_cause(e))
+            .map(|e| to_cause(file.as_str(), e))
             .collect()
     }
+}
+
+fn to_cause(file: &str, err: &async_graphql::ServerError) -> Cause<String> {
+    let mut trace = Vec::new();
+
+    for loc in err.locations.iter() {
+        let mut message = String::new();
+        message.write_str(file).unwrap();
+        message
+            .write_str(format!(":{}:{}", loc.line, loc.column).as_str())
+            .unwrap();
+
+        trace.push(message);
+    }
+
+    Cause::new(err.message.clone()).trace(trace)
 }
 
 pub async fn validate_operations(
@@ -50,7 +52,7 @@ pub async fn validate_operations(
 ) -> Valid<(), String> {
     let schema = blueprint.to_schema_with(SchemaModifiers::default().with_no_resolver());
     Valid::from_iter(
-        futures_util::future::join_all(operations.iter().map(|op| op.validate(&schema)))
+        futures_util::future::join_all(operations.into_iter().map(|op| op.validate(&schema)))
             .await
             .iter(),
         |errors| {
