b192cade169b6b78a19244c63977ded680e3f2e5
shashitnak <shashitnak@gmail.com>
2024-03-14 13:05:51 +0530

    feat(apollo): Apollo studio integration (#1380)

--- src/cli/telemetry.rs
+++ src/cli/telemetry.rs
@@ -90,6 +90,7 @@ fn set_trace_provider(
             ))?,
         // Prometheus works only with metrics
         TelemetryExporter::Prometheus(_) => return Ok(None),
+        TelemetryExporter::Apollo(_) => return Ok(None),
     };
     let tracer = provider.tracer("tracing");
     let telemetry = tracing_opentelemetry::layer().with_tracer(tracer);
@@ -129,6 +130,7 @@ fn set_logger_provider(
         ,
         // Prometheus works only with metrics
         TelemetryExporter::Prometheus(_) => return Ok(None),
+        TelemetryExporter::Apollo(_) => return Ok(None),
     };
 
     let otel_tracing_appender = OpenTelemetryTracingBridge::new(&provider);
@@ -172,6 +174,7 @@ fn set_meter_provider(exporter: &TelemetryExporter) -> MetricsResult<()> {
                 .with_reader(exporter)
                 .build()
         }
+        _ => return Ok(()),
     };
 
     global::set_meter_provider(provider);
