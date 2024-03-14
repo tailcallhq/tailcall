b9845c7387855540b4001550d320e63a09edaa3e
shashitnak <shashitnak@gmail.com>
2024-03-14 13:05:51 +0530

    feat(apollo): Apollo studio integration (#1380)

--- tests/execution_spec.rs
+++ tests/execution_spec.rs
@@ -184,6 +184,7 @@ pub mod test {
             env: Arc::new(env),
             file: Arc::new(file),
             cache: Arc::new(InMemoryCache::new()),
+            extensions: Arc::new(vec![]),
         }
     }
 }
@@ -634,6 +635,7 @@ impl ExecutionSpec {
             file: Arc::new(MockFileSystem::new(self.clone())),
             env: Arc::new(Env::init(env)),
             cache: Arc::new(InMemoryCache::new()),
+            extensions: Arc::new(vec![]),
         };
 
         // TODO: move inside tailcall core if possible
