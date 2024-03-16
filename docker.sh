797f2df3a7e90a5dfa09c1cb33c29181b23d661e
Dhanus <dhanus3133@gmail.com>
2024-03-16 11:10:19 +0530

    chore: delete docker.sh

--- docker.sh
+++
@@ -1,8 +0,0 @@
-# Convert Cargo.toml to YAML
-yj -t < Cargo.toml > Cargo.yaml
-
-# Modify Cargo.yaml
-yq e 'del(.workspace.members[] | select(. == "cloudflare" or . == "autogen" or . == "testconv" or . == "aws-lambda"))' -i Cargo.yaml
-
-# Convert Cargo.yaml back to TOML
-yj -yt < Cargo.yaml > Cargo.toml
