7b4c4671da5ae04f8e150dd14cb63a22a8a7b42b
Shashi Kant <shashitnak@gmail.com>
2024-03-16 10:44:47 +0530

    Revert "fixing deadlock"
    This reverts commit a0193f71014770c4c6d0c882320fcd80a4541a41.

--- src/http/mod.rs
+++ src/http/mod.rs
@@ -13,7 +13,7 @@ pub use cache::*;
 pub use data_loader::*;
 pub use data_loader_request::*;
 pub use method::Method;
-pub use request_context::RequestContext;
+pub use request_context::{CacheValue, RequestContext};
 pub use request_handler::{graphiql, handle_request};
 pub use request_template::RequestTemplate;
 pub use response::*;
