bb6d2318cb3ba2fe135a2c73df19e2a0d582040e
meskill <8974488+meskill@users.noreply.github.com>
2024-03-17 11:21:38 +0000

    feat: add /api prefix for rest routes

--- src/http/request_handler.rs
+++ src/http/request_handler.rs
@@ -181,13 +181,14 @@ async fn handle_rest_apis(
     *request.uri_mut() = request.uri().path().replace(API_URL_PREFIX, "").parse()?;
     let req_ctx = Arc::new(create_request_context(&request, app_ctx.as_ref()));
     if let Some(p_request) = app_ctx.endpoints.matches(&request) {
-        req_counter.set_http_route(p_request.path.as_str());
+        let http_route = format!("{API_URL_PREFIX}{}", p_request.path.as_str());
+        req_counter.set_http_route(&http_route);
         let span = tracing::info_span!(
             "REST",
             otel.name = format!("REST {} {}", request.method(), p_request.path.as_str()),
             otel.kind = ?SpanKind::Server,
             { HTTP_REQUEST_METHOD } = %request.method(),
-            { HTTP_ROUTE } = p_request.path.as_str()
+            { HTTP_ROUTE } = http_route
         );
         return async {
             let graphql_request = p_request.into_request(request).await?;
