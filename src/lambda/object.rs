8ffb289c4e06eec69e9090a21f4e1d4874433d4d
Sandipsinh Rathod <zotbysandip@gmail.com>
2024-03-17 13:40:53 +0530

    switch to `strum_macros::Display`

--- src/lambda/object.rs
+++ src/lambda/object.rs
@@ -1,13 +1,4 @@
-use std::fmt::{Display, Formatter};
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, strum_macros::Display)]
 pub enum Object {
     Path(Vec<String>),
-}
-
-impl Display for Object {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Object::Path(s) => f.write_str(format!("{:?}", s).as_str()), // TODO remove debug
-        }
-    }
 }
