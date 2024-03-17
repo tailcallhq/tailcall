c3b3922912be45e6d7b056b2b46020db48f33ac4
Shashi Kant <shashitnak@gmail.com>
2024-03-17 15:46:30 +0530

    uncommenting previously failing test

--- src/async_cache.rs
+++ src/async_cache.rs
@@ -126,17 +126,15 @@ mod tests {
         assert!(actual.is_err());
     }
 
-    // FIXME: this test is failing
-    // #[tokio::test]
-    // async fn test_with_multi_get_failure() {
-    //     let cache = AsyncCache::<i32, i32>::new();
-    //     let _ = cache
-    //         .get_or_eval(1, || Box::pin(async { Err(anyhow::anyhow!("error"))
-    // }))         .await;
+    #[tokio::test]
+    async fn test_with_multi_get_failure() {
+        let cache = AsyncCache::<i32, i32>::new();
+        let _ = cache
+            .get_or_eval(1, || Box::pin(async { Err(anyhow::anyhow!("error")) }))
+            .await;
 
-    //     let actual = cache.get_or_eval(1, || Box::pin(async { Ok(2)
-    // })).await;
+        let actual = cache.get_or_eval(1, || Box::pin(async { Ok(2) })).await;
 
-    //     assert!(actual.is_err());
-    // }
+        assert!(actual.is_err());
+    }
 }
