ab2d6882f1a9d2433b43d27fcab047797a195d9b
Tushar Mathur <tusharmath@gmail.com>
2024-03-21 18:08:24 +0530

    dropping map implementation for now

--- src/lens.rs
+++ src/lens.rs
@@ -1,34 +1,7 @@
 use std::borrow::Cow;
 
-pub trait LensPath<A> {
+pub trait LensPath<A>: Sized {
     fn get_path<'a>(&'a self, a: &[String]) -> Option<Cow<'a, A>>
     where
         A: Clone;
-}
-
-pub trait LensPathOperation<A, B> {
-    fn map(&self, f: impl FnOnce(Cow<A>) -> Cow<B>) -> LensPathMap<A, B>
-    where
-        A: Clone,
-        B: Clone;
-}
-
-impl<A: Clone, B: Clone, X: LensPath<A> + Clone> LensPathOperation<A, B> for X {
-    fn map(&self, f: impl FnOnce(Cow<A>) -> Cow<B>) -> LensPathMap<A, B> {
-        LensPathMap { lens: Box::new(self.clone()), f: Box::new(f) }
-    }
-}
-
-pub struct LensPathMap<A: Clone, B: Clone> {
-    lens: Box<dyn LensPath<A>>,
-    f: Box<dyn FnOnce(Cow<A>) -> Cow<B>>,
-}
-
-impl<A: Clone, B: Clone> LensPath<B> for LensPathMap<A, B> {
-    fn get_path<'a>(&'a self, a: &[String]) -> Option<Cow<'a, B>>
-    where
-        B: Clone,
-    {
-        self.lens.get_path(a).map(|b| (self.f)(b))
-    }
 }
