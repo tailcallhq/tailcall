ebbb293b95b06e2a614282f91e895ea70a9d2600
Shashi Kant <shashitnak@gmail.com>
2024-03-23 19:21:08 +0530

    encoding message from struct

--- benches/protobuf_convert_output.rs
+++ benches/protobuf_convert_output.rs
@@ -1,9 +1,16 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use prost::Message;
 use tailcall::blueprint::GrpcMethod;
 use tailcall::grpc::protobuf::ProtobufSet;
 
 const PROTO_FILE_PATH: &str = "src/grpc/tests/proto/greetings.proto";
 const SERVICE_NAME: &str = "greetings.Greeter.SayHello";
+
+#[derive(Message)]
+struct HelloReply {
+    #[prost(string, tag = "1")]
+    message: String,
+}
 
 fn benchmark_convert_output(c: &mut Criterion) {
     let file_descriptor_set = protox::compile([PROTO_FILE_PATH], ["."]).unwrap();
@@ -11,11 +18,14 @@ fn benchmark_convert_output(c: &mut Criterion) {
     let method = GrpcMethod::try_from(SERVICE_NAME).unwrap();
     let service = protobuf_set.find_service(&method).unwrap();
     let protobuf_operation = service.find_operation(&method).unwrap();
-    let msg = b"\0\0\0\0\x0e\n\x0ctest message";
+    let mut msg: Vec<u8> = vec![0, 0, 0, 0, 14];
+    HelloReply { message: "test message".to_string() }
+        .encode(&mut msg)
+        .unwrap();
 
     c.bench_function("test_batched_body", |b| {
         b.iter(|| {
-            black_box(protobuf_operation.convert_output(msg).unwrap());
+            black_box(protobuf_operation.convert_output(&msg).unwrap());
         })
     });
 }
