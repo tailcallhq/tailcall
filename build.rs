117870746fa7b0cc7e1d521334eb6d570082a04e
Shashi Kant <shashitnak@gmail.com>
2024-03-25 14:45:07 +0530

    adding protoc in Cargo.toml

--- build.rs
+++ build.rs
@@ -5,6 +5,9 @@ const PROTO_FILE: &str = "dummy.proto";
 
 fn main() {
     std::env::set_var("OUT_DIR", OUT_DIR);
+    let path = protoc_bin_vendored::protoc_bin_path()
+        .expect("Failed to find protoc binary");
+    std::env::set_var("PROTOC", format!("{}", path.display()));
     let proto_file_path = Path::new(OUT_DIR).join(PROTO_FILE);
     tonic_build::configure()
         .compile(&[proto_file_path], &["proto"])
