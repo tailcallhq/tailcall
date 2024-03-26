215f2ed8dfd71ef02b848bccec93fbfb0df861f0
Kunam Balaram Reddy <webbdays@gmail.com>
2024-03-26 23:02:49 +0530

    remove changes for examples

--- examples/scripts/echo.js
+++ examples/scripts/echo.js
@@ -1,12 +1,3 @@
 function onRequest({request}) {
   return {request}
 }
-function bar({request}) {
-  console.log("its a bar")
-  return {request}
-}
-
-function foo({request}) {
-  console.log("its a foo")
-  return {request}
-}
