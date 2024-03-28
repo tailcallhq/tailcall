19aeb5849f0f672f0195c4217ac4e415e7af5cac
meskill <8974488+meskill@users.noreply.github.com>
2024-03-28 10:44:10 +0000

    fix lint

--- cloudflare/src/env.rs
+++ cloudflare/src/env.rs
@@ -13,7 +13,7 @@ unsafe impl Sync for CloudflareEnv {}
 
 impl EnvIO for CloudflareEnv {
     fn get(&self, key: &str) -> Option<Cow<'_, str>> {
-        self.env.var(key).ok().map(Cow::from)
+        self.env.var(key).ok().map(|v| Cow::from(v.to_string()))
     }
 }
 
