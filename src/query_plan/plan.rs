e7e79b18d0eaddb51740c7f3f5f598cb1a43c09d
meskill <8974488+meskill@users.noreply.github.com>
2024-04-01 20:51:52 +0000

    calculate selection set for resolvers tree

--- src/query_plan/plan.rs
+++ src/query_plan/plan.rs
@@ -1,7 +1,10 @@
+use std::fmt::{Display, Write};
+
 use async_graphql::{
-    parser::types::{Selection, SelectionSet},
-    Name,
+    parser::types::{Field, Selection, SelectionSet},
+    Name, Pos, Positioned,
 };
+use indenter::indented;
 use indexmap::IndexMap;
 
 use crate::{
@@ -10,57 +13,67 @@ use crate::{
     scalar::is_scalar,
 };
 
-use super::resolver::PlanResolver;
-
-#[derive(Default, Clone)]
-pub struct FieldPlan {
-    resolver: Option<Expression>,
-}
-
-impl std::fmt::Debug for FieldPlan {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.debug_struct("FieldPlan")
-            .field(
-                "resolver",
-                &if let Some(resolver) = &self.resolver {
-                    Some(resolver.to_string())
-                } else {
-                    None
-                },
-            )
-            .finish()
-    }
-}
+use super::resolver::{FieldPlan, FieldPlanSelection, Id};
 
 #[derive(Debug)]
-enum Plan {
-    Scalar(FieldPlan),
+enum Fields {
+    Scalar(Option<Id>),
     Complex {
-        field_plan: FieldPlan,
-        children: IndexMap<Name, Plan>,
+        field_plan_id: Option<Id>,
+        children: IndexMap<Name, Fields>,
     },
 }
 
-impl Plan {
-    fn field_plan(&self) -> &FieldPlan {
+impl Display for Fields {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Plan::Scalar(plan) => plan,
-            Plan::Complex { field_plan, children } => field_plan,
-        }
-    }
+            Fields::Scalar(id) => write!(f, "Scalar"),
+            Fields::Complex { children, .. } => {
+                let mut f = indented(f);
 
-    fn with_resolver(self, resolver: Option<Expression>) -> Self {
-        match self {
-            Plan::Scalar(_) => Plan::Scalar(FieldPlan { resolver }),
-            Plan::Complex { children, .. } => {
-                Plan::Complex { field_plan: FieldPlan { resolver }, children: children }
+                for (name, fields) in children.iter() {
+                    writeln!(f, "{}({:?}):", name, fields.field_plan_id())?;
+                    writeln!(indented(&mut f), "{}", fields)?;
+                }
+
+                Ok(())
             }
         }
     }
 }
 
-impl Plan {
-    pub fn from_operation(definitions: &Vec<Definition>, name: &str) -> Self {
+#[derive(Debug)]
+struct GeneralPlan {
+    fields: Fields,
+    field_plans: Vec<FieldPlan>,
+}
+
+#[derive(Debug)]
+struct ExecutionPlan {
+    fields: Fields,
+    selections: IndexMap<Id, FieldPlanSelection>,
+}
+
+impl Fields {
+    fn field_plan_id(&self) -> &Option<Id> {
+        match self {
+            Fields::Scalar(id) => id,
+            Fields::Complex { field_plan_id, .. } => field_plan_id,
+        }
+    }
+
+    fn with_field_plan_id(self, id: Option<Id>) -> Self {
+        match self {
+            Fields::Scalar(_) => Fields::Scalar(id),
+            Fields::Complex { children, .. } => Fields::Complex { field_plan_id: id, children },
+        }
+    }
+
+    fn from_operation(
+        field_plans: &mut Vec<FieldPlan>,
+        definitions: &Vec<Definition>,
+        name: &str,
+    ) -> Self {
         let definition = definitions.iter().find(|def| def.name() == name);
         let mut children = IndexMap::new();
 
@@ -69,40 +82,137 @@ impl Plan {
                 let type_name = field.of_type.name();
                 let resolver = field.resolver.clone();
 
-                let plan = if is_scalar(type_name) {
-                    Plan::Scalar(FieldPlan::default())
+                let id = if let Some(resolver) = resolver {
+                    // TODO: figure out dependencies
+                    let depends_on: Vec<Id> = vec![];
+                    let id = field_plans.len().into();
+                    let field_plan = FieldPlan { id, resolver, depends_on };
+                    field_plans.push(field_plan);
+                    Some(id)
                 } else {
-                    Self::from_operation(definitions, type_name)
+                    None
                 };
-                children.insert(Name::new(&field.name), plan.with_resolver(resolver));
+
+                let plan = if is_scalar(type_name) {
+                    Self::Scalar(id)
+                } else {
+                    Self::from_operation(field_plans, definitions, type_name)
+                };
+                children.insert(Name::new(&field.name), plan.with_field_plan_id(id));
             }
         }
 
-        Self::Complex { field_plan: FieldPlan::default(), children }
+        Self::Complex { field_plan_id: None, children }
     }
 
-    pub fn from_request(&self, selection_set: &SelectionSet) -> Self {
-        let Self::Complex { field_plan, children } = self else {
-            return Self::Scalar(self.field_plan().clone());
+    pub fn prepare_for_request(
+        &self,
+        result_selection: &mut FieldPlanSelection,
+        selections: &mut IndexMap<Id, FieldPlanSelection>,
+        input_selection_set: &SelectionSet,
+    ) -> Self {
+        let (field_plan_id, children) = match self {
+            Fields::Scalar(id) => {
+                assert!(input_selection_set.items.is_empty());
+
+                return Self::Scalar(*id);
+            }
+            Fields::Complex { field_plan_id, children } => (field_plan_id, children),
         };
 
         let mut req_children = IndexMap::new();
-        for selection in &selection_set.items {
+        for selection in &input_selection_set.items {
+            let mut current_selection_set = FieldPlanSelection::default();
+
             match &selection.node {
                 Selection::Field(field) => {
                     let name = &field.node.name.node;
-                    let plan = children
-                        .get(name)
-                        .unwrap()
-                        .from_request(&field.node.selection_set.node);
-                    req_children.insert(name.clone(), plan);
+                    let fields = children.get(name).unwrap();
+                    let fields = fields.prepare_for_request(
+                        &mut current_selection_set,
+                        selections,
+                        &field.node.selection_set.node,
+                    );
+
+                    if let Some(field_plan_id) = fields.field_plan_id() {
+                        let field_selection = selections.entry(*field_plan_id);
+
+                        match field_selection {
+                            indexmap::map::Entry::Occupied(mut entry) => {
+                                entry.get_mut().extend(current_selection_set)
+                            }
+                            indexmap::map::Entry::Vacant(slot) => {
+                                slot.insert(current_selection_set);
+                            }
+                        }
+                    } else {
+                        result_selection.add(selection, current_selection_set);
+                    }
+
+                    req_children.insert(name.clone(), fields);
                 }
                 Selection::FragmentSpread(_) => todo!(),
                 Selection::InlineFragment(_) => todo!(),
             }
         }
 
-        Self::Complex { field_plan: field_plan.clone(), children: req_children }
+        Self::Complex { field_plan_id: *field_plan_id, children: req_children }
+    }
+}
+
+impl GeneralPlan {
+    pub fn from_operation(definitions: &Vec<Definition>, name: &str) -> Self {
+        let mut field_plans = Vec::new();
+        let fields = Fields::from_operation(&mut field_plans, definitions, name);
+
+        Self { fields, field_plans }
+    }
+}
+
+impl Display for GeneralPlan {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        writeln!(f, "GeneralPlan")?;
+        let mut f = indented(f);
+
+        writeln!(f, "fields:")?;
+        writeln!(f, "{}", &self.fields)?;
+        writeln!(f, "field_plans:")?;
+
+        for plan in self.field_plans.iter() {
+            writeln!(f, "{}", plan)?;
+        }
+
+        Ok(())
+    }
+}
+
+impl ExecutionPlan {
+    pub fn from_request(plan: &GeneralPlan, selection_set: &SelectionSet) -> Self {
+        let mut selections = IndexMap::new();
+        let mut result_selection = FieldPlanSelection::default();
+        let fields =
+            plan.fields
+                .prepare_for_request(&mut result_selection, &mut selections, selection_set);
+
+        Self { fields, selections }
+    }
+}
+
+impl Display for ExecutionPlan {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        writeln!(f, "ExecutionPlan")?;
+        writeln!(f, "fields:")?;
+        writeln!(indented(f), "{}", &self.fields)?;
+        writeln!(f, "selections:")?;
+
+        let mut f = indented(f);
+
+        for (id, selection) in &self.selections {
+            writeln!(f, "Resolver({}):", id)?;
+            writeln!(indented(&mut f), "{}", selection)?;
+        }
+
+        Ok(())
     }
 }
 
@@ -116,7 +226,7 @@ mod tests {
         use crate::{
             blueprint::Blueprint,
             config::{Config, ConfigModule},
-            query_plan::plan::Plan,
+            query_plan::plan::{ExecutionPlan, GeneralPlan},
             valid::Validator,
         };
 
@@ -128,22 +238,32 @@ mod tests {
             let config = ConfigModule::from(config);
             let blueprint = Blueprint::try_from(&config).unwrap();
 
-            let plan = Plan::from_operation(&blueprint.definitions, &blueprint.query());
+            let general_plan =
+                GeneralPlan::from_operation(&blueprint.definitions, &blueprint.query());
 
-            insta::assert_debug_snapshot!(plan);
+            insta::assert_snapshot!(general_plan);
 
             let document = parse_query(
                 r#"
-                query PostsSimple { posts { title } }
-                query PostsComplex { posts {title body user {name username}} }
+                query PostsSimple {
+                    posts { title }
+                }
+                query PostsComplex {
+                    posts {title body user {name username website}}
+                }
+                query PostAndUser {
+                    posts { title user { name } }
+                    user { username email }
+                }
             "#,
             )
             .unwrap();
 
             for (name, operation) in document.operations.iter() {
-                let req_plan = plan.from_request(&operation.node.selection_set.node);
+                let execution_plan =
+                    ExecutionPlan::from_request(&general_plan, &operation.node.selection_set.node);
 
-                insta::assert_debug_snapshot!(name.unwrap().to_string(), req_plan);
+                insta::assert_snapshot!(name.unwrap().to_string(), execution_plan);
             }
         }
     }
--- src/query_plan/resolver.rs
+++ src/query_plan/resolver.rs
@@ -1,19 +1,102 @@
+use std::fmt::{Display, Write};
+
 use crate::lambda::Expression;
 
-use async_graphql::parser::types::SelectionSet;
+use async_graphql::{
+    parser::types::{Field, Selection, SelectionSet},
+    Positioned,
+};
+use indenter::indented;
 
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
 pub struct Id(usize);
 
-pub struct PlanResolver {
-  id: Id,
-  expression: Expression,
-  selection_set: SelectionSet,
-  depends_on: Vec<Id>
+impl Display for Id {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", &self.0)
+    }
 }
 
+impl From<usize> for Id {
+    fn from(value: usize) -> Self {
+        Self(value)
+    }
+}
 
-impl PlanResolver {
-  pub fn new() -> Self {
-    todo!()
-  }
+#[derive(Clone)]
+pub struct FieldPlan {
+    pub(super) id: Id,
+    pub(super) resolver: Expression,
+    pub(super) depends_on: Vec<Id>,
+}
+
+impl Display for FieldPlan {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "FieldPlan[{}] ({})", &self.id, &self.resolver)
+    }
+}
+
+impl std::fmt::Debug for FieldPlan {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("FieldPlan")
+            .field("id", &self.id)
+            .field("resolver", &self.resolver.to_string())
+            .field("depends_on", &self.depends_on)
+            .finish()
+    }
+}
+
+#[derive(Debug, Default)]
+pub struct FieldPlanSelection(SelectionSet);
+
+impl FieldPlanSelection {
+    pub fn add(&mut self, selection: &Positioned<Selection>, plan_selection: FieldPlanSelection) {
+        match &selection.node {
+            Selection::Field(field) => self.0.items.push(Positioned::new(
+                Selection::Field(Positioned::new(
+                    Field {
+                        selection_set: Positioned::new(
+                            plan_selection.0,
+                            field.node.selection_set.pos,
+                        ),
+                        ..field.node.clone()
+                    },
+                    field.pos,
+                )),
+                selection.pos,
+            )),
+            Selection::FragmentSpread(_) => todo!(),
+            Selection::InlineFragment(_) => todo!(),
+        }
+    }
+
+    pub fn extend(&mut self, other: FieldPlanSelection) {
+        self.0.items.extend(other.0.items);
+    }
+}
+
+impl Display for FieldPlanSelection {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        for selection in &self.0.items {
+            match &selection.node {
+                Selection::Field(field) => {
+                    let name = field.node.name.node.as_str();
+                    let mut f = indented(f);
+
+                    let selection_set = &field.node.selection_set.node;
+
+                    if selection_set.items.is_empty() {
+                        writeln!(f, "{name}")?;
+                    } else {
+                        writeln!(f, "{name}:")?;
+                        writeln!(f, "{}", FieldPlanSelection(selection_set.clone()))?;
+                    }
+                }
+                Selection::FragmentSpread(_) => todo!(),
+                Selection::InlineFragment(_) => todo!(),
+            }
+        }
+
+        Ok(())
+    }
 }
