0627703ef7538bebbb35d90f07d42e9a064f0306
ologbonowiwi <100464352+ologbonowiwi@users.noreply.github.com>
2024-04-02 04:47:57 -0300

    fix: fail when input type has `@protected` directive (#1597)
    Co-authored-by: Tushar Mathur <tusharmath@gmail.com>

--- src/blueprint/definitions.rs
+++ src/blueprint/definitions.rs
@@ -401,7 +401,7 @@ fn to_fields(
             .and(update_call(&operation_type).trace(config::Call::trace_name().as_str()))
             .and(fix_dangling_resolvers())
             .and(update_cache_resolvers())
-            .and(update_protected().trace(Protected::trace_name().as_str()))
+            .and(update_protected(object_name).trace(Protected::trace_name().as_str()))
             .try_fold(
                 &(config_module, field, type_of, name),
                 FieldDefinition::default(),
