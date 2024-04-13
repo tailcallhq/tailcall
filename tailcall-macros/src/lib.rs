1f1287582da15b336ba2d09714003ee4a34d8c22
Shylock Hg <tcath2s@icloud.com>
2024-04-13 22:12:01 +0800

    Add MergeRight derive macro.

---
+++ tailcall-macros/src/lib.rs
@@ -0,0 +1,52 @@
+extern crate proc_macro;
+
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, Data, DeriveInput, Fields};
+
+#[proc_macro_derive(MergeRight)]
+pub fn merge_right_derive(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as DeriveInput);
+
+    let name = input.ident;
+    let gen = match input.data {
+        // Implement for structs
+        Data::Struct(data) => {
+            let fields = if let Fields::Named(fields) = data.fields {
+                fields.named
+            } else {
+                // Adjust this match arm to handle other kinds of struct fields (unnamed/tuple structs, unit structs)
+                unimplemented!()
+            };
+
+            let merge_logic = fields.iter().map(|f| {
+                let name = &f.ident;
+                quote! {
+                    #name: self.#name.merge_right(other.#name),
+                }
+            });
+
+            quote! {
+                impl MergeRight for #name {
+                    fn merge_right(self, other: Self) -> Self {
+                        Self {
+                            #(#merge_logic)*
+                        }
+                    }
+                }
+            }
+        },
+        // Implement for enums
+        Data::Enum(_) => quote! {
+            impl MergeRight for #name {
+                fn merge_right(self, other: Self) -> Self {
+                    other
+                }
+            }
+        },
+        // Optionally handle or disallow unions
+        Data::Union(_) => unimplemented!(),
+    };
+
+    gen.into()
+}
