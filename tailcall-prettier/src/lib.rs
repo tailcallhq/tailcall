59e90a9d0deaecf5b6a0cc245f6ebb2824045bd2
Sandipsinh Rathod <zotbysandip@gmail.com>
2024-04-15 15:51:11 +0530

    drop `FileTypes`

--- tailcall-prettier/src/lib.rs
+++ tailcall-prettier/src/lib.rs
@@ -1,56 +1,13 @@
-use std::fmt::{Display, Formatter};
+
 use std::io::Write;
 use std::process::{Command, Stdio};
 
 use anyhow::{anyhow, Result};
 
-enum FileTypes {
-    Gql,
-    Yml,
-    Json,
-    Md,
-    Ts,
-    Js,
-}
-
-impl Display for FileTypes {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        match self {
-            FileTypes::Gql => write!(f, "graphql"),
-            FileTypes::Yml => write!(f, "yml"),
-            FileTypes::Json => write!(f, "json"),
-            FileTypes::Md => write!(f, "md"),
-            FileTypes::Ts => write!(f, "ts"),
-            FileTypes::Js => write!(f, "js"),
-        }
-    }
-}
-
-impl FileTypes {
-    fn detect(path: &str) -> Result<Self> {
-        let ext = path
-            .split('.')
-            .last()
-            .ok_or(anyhow!("No file extension found"))?
-            .to_lowercase();
-        match ext.as_str() {
-            "gql" | "graphql" => Ok(FileTypes::Gql),
-            "yml" | "yaml" => Ok(FileTypes::Yml),
-            "json" => Ok(FileTypes::Json),
-            "md" => Ok(FileTypes::Md),
-            "ts" => Ok(FileTypes::Ts),
-            "js" => Ok(FileTypes::Js),
-            _ => Err(anyhow!("Unsupported file type")),
-        }
-    }
-}
-
 pub fn format_with_prettier<T: AsRef<str>>(code: T, file_ty: T) -> Result<String> {
-    let file_ty = FileTypes::detect(file_ty.as_ref())?;
-
     let mut child = Command::new("prettier")
         .arg("--stdin-filepath")
-        .arg(format!("file.{}", file_ty))
+        .arg(file_ty.as_ref())
         .stdin(Stdio::piped())
         .stdout(Stdio::piped())
         .spawn()?;
