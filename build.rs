c2812143133a6d1a422e549eca9c3dc5a5ebb250
meskill <8974488+meskill@users.noreply.github.com>
2024-04-19 20:59:43 +0000

    remove unused files

--- build.rs
+++
@@ -1,7 +0,0 @@
-const PROTO_PATH: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/benches/grpc/dummy.proto");
-
-fn main() {
-    let path = protoc_bin_vendored::protoc_bin_path().expect("Failed to find protoc binary");
-    std::env::set_var("PROTOC", format!("{}", path.display()));
-    tonic_build::compile_protos(PROTO_PATH).unwrap();
-}
