00ede27c3e32f88963e5813c21ec2d116c35455e
meskill <8974488+meskill@users.noreply.github.com>
2024-04-23 11:30:36 +0000

    fix upstream grpc compilation

--- tailcall-upstream-grpc/build.rs
+++ tailcall-upstream-grpc/build.rs
@@ -1,9 +1,12 @@
-use std::env::var;
+use std::env::{set_var, var};
 use std::path::PathBuf;
 
 use tailcall_fixtures::get_fixture_path;
 
 fn main() {
+    let path = protoc_bin_vendored::protoc_bin_path().expect("Failed to find protoc binary");
+    set_var("PROTOC", format!("{}", path.display()));
+
     let news = get_fixture_path("grpc/proto/news.proto");
 
     let out_dir = PathBuf::from(var("OUT_DIR").unwrap());
