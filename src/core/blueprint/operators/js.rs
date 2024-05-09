f5fcf0c983741048ba742431b256f94e01ae0594
Sandipsinh Rathod <zotbysandip@gmail.com>
2024-05-09 19:10:01 +0530

    drop feature flags in js.rs

--- src/core/blueprint/operators/js.rs
+++ src/core/blueprint/operators/js.rs
@@ -4,7 +4,7 @@ use crate::core::config::Field;
 use crate::core::lambda::IO;
 use crate::core::try_fold::TryFold;
 use crate::core::valid::Valid;
-use crate::{ConfigModule, Expression, ValidationError, Validator};
+use crate::{ConfigModule, Expression, Validator};
 
 pub struct CompileJs<'a> {
     pub name: &'a str,
@@ -12,36 +12,9 @@ pub struct CompileJs<'a> {
 }
 
 pub fn compile_js(inputs: CompileJs) -> Valid<Expression, String> {
-    #[cfg(not(feature = "js"))]
-    return js_disabled(inputs);
-
-    #[cfg(feature = "js")]
-    js_enabled(inputs)
-}
-
-#[cfg(not(feature = "js"))]
-fn js_disabled(_: CompileJs) -> Valid<Expression, String> {
-    Valid::fail("js is disabled or unsupported on this platform".to_string())
-}
-
-#[cfg(feature = "js")]
-fn js_enabled(inputs: CompileJs) -> Valid<Expression, String> {
     let name = inputs.name;
-    let quickjs = rquickjs::Runtime::new().unwrap();
-    let ctx = rquickjs::Context::full(&quickjs).unwrap();
-
-    Valid::from_option(inputs.script.as_ref(), "script is required".to_string()).and_then(
-        |script| {
-            Valid::from(
-                ctx.with(|ctx| {
-                    ctx.eval::<(), &str>(script)?;
-                    Ok::<_, anyhow::Error>(())
-                })
-                .map_err(|e| ValidationError::new(e.to_string())),
-            )
-            .map(|_| Expression::IO(IO::Js { name: name.to_string() }))
-        },
-    )
+    Valid::from_option(inputs.script.as_ref(), "script is required".to_string())
+        .map(|_| Expression::IO(IO::Js { name: name.to_string() }))
 }
 
 pub fn update_js_field<'a>(
