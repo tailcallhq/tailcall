db28db510197a79b8412649eb11e5708a46a8b6d
mobley-trent <oyieko99@gmail.com>
2024-05-14 18:17:42 +0300

    Update validate.rs

--- src/blueprint/operators/validate.rs
+++ src/blueprint/operators/validate.rs
@@ -1,12 +1,69 @@
 use crate::blueprint::*;
 use crate::config::{self, ConfigModule, Field};
-use core::time::Duration;
-use crate::lambda::{Expression, IO};
 use crate::try_fold::TryFold;
 use crate::valid::{Valid, ValidationError, Validator};
 use rquickjs::{Runtime, Context, Function, FromJs};
+use serde_json;
 use crate::cli::javascript::setup_builtins;
 
+
+fn json_to_js<'js>(value: serde_json::Value, ctx: &rquickjs::Ctx<'js>) -> rquickjs::Result<(rquickjs::Value<'js>,)> {
+    let object = rquickjs::Object::new(ctx.clone())?;
+
+    match value {
+        serde_json::Value::Number(num) => {
+            if num.is_i64() {
+                object.set("value", num.as_i64().unwrap())?;
+            } else if num.is_f64() {
+                object.set("value", num.as_f64().unwrap())?;
+            }
+        }
+        serde_json::Value::String(string) => {
+            object.set("value", string)?;
+        }
+        serde_json::Value::Bool(boolean) => {
+            object.set("value", boolean)?;
+        }
+        serde_json::Value::Object(obj) => {
+            for (key, value) in obj {
+                let (value,) = json_to_js(value, ctx)?;
+                object.set(key, value)?;
+            }
+        }
+        _ => {}
+    }
+
+    Ok((object.into_value(),))
+}
+
+
+fn call(func: String, val: serde_json::Value, src: String) -> () {
+    let js_runtime = Runtime::new().ok().unwrap();
+    let context = Context::full(&js_runtime).ok().unwrap();
+
+    // context.with(|ctx| {
+    //     setup_builtins(&ctx).ok().unwrap();
+    //     ctx.eval(src)
+    // });
+
+    context.with(|ctx| {
+        let fn_as_value = ctx
+            .globals()
+            .get::<&str, Function>(func.as_str())
+            .map_err(|_| anyhow::anyhow!("globalThis not initialized"))
+            .ok().unwrap();
+
+        let function = fn_as_value
+            .as_function()
+            .ok_or(anyhow::anyhow!("`{func}` is not a function"))
+            .ok().unwrap();
+
+        let custom_scalar = json_to_js(val.clone(), &ctx).ok().unwrap();
+        let command: Option<rquickjs::Value> = function.call(custom_scalar).ok();
+
+        ()
+    });
+}
 
 pub fn update_validate<'a>(
     type_name: &'a str,
@@ -32,23 +89,6 @@ pub fn update_validate<'a>(
             let script = config_module.extensions.script.clone().expect("script is required");
             let js_runtime = Runtime::new().ok().unwrap();
             let context = Context::full(&js_runtime).ok().unwrap();
-
-            // context.with(|ctx| {
-            //     setup_builtins(&ctx).ok().unwrap();
-            //     ctx.eval(script)
-            // });
-
-            context.with(|ctx| {
-                let fn_as_value = ctx
-                    .globals()
-                    .get::<&str, Function>(js_func.as_str())
-                    .map_err(|_| anyhow::anyhow!("globalThis not initialized"));
-
-                let function = fn_as_value.ok().unwrap()
-                    .as_function()
-                    .ok_or(anyhow::anyhow!("`{js_func}` is not a function"));
-
-            });
 
             todo!();
 
