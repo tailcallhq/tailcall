7ebc8ad93a1a6e1ec806b877f0674e972d4a98d6
amit <amitksingh1490@gmail.com>
2024-05-14 13:52:26 +0530

    fix: http1 tuning

--- src/cli/server/http_1.rs
+++ src/cli/server/http_1.rs
@@ -33,9 +33,7 @@ pub async fn start_http_1(
     let builder = hyper::Server::try_bind(&addr)
         .map_err(CLIError::from)?
         .http1_only(true)
-        .http1_half_close(true)
         .http1_keepalive(true)
-        .http1_max_buf_size(32 * 1024)
         .tcp_keepalive(Some(std::time::Duration::from_secs(60)))
         .tcp_nodelay(true)
         .http1_pipeline_flush(sc.app_ctx.blueprint.server.pipeline_flush);
