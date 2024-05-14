e3194ea6030c5df5f5dd9c2b6dcc5332ca9fdb77
Ranjit Mahadik <43403528+ranjitmahadik@users.noreply.github.com>
2024-05-14 17:10:27 +0530

    - moved NativeHttp to separate workspace.

---
+++ tailcall-http-client/src/http.rs
@@ -0,0 +1,67 @@
+use std::time::Duration;
+
+use http_cache_reqwest::{Cache, CacheMode, HttpCache, HttpCacheOptions, MokaManager};
+use reqwest::Client;
+use reqwest_middleware::{ClientBuilder, ClientWithMiddleware};
+
+use tailcall::{Upstream};
+
+#[derive(Clone)]
+pub struct NativeHttp {
+    client: ClientWithMiddleware,
+    http2_only: bool,
+    enable_telemetry: bool,
+}
+
+impl Default for NativeHttp {
+    fn default() -> Self {
+        Self {
+            client: ClientBuilder::new(Client::new()).build(),
+            http2_only: false,
+            enable_telemetry: false,
+        }
+    }
+}
+
+impl NativeHttp {
+    pub fn init(upstream: &Upstream) -> Self {
+        let mut builder = Client::builder()
+            .tcp_keepalive(Some(Duration::from_secs(upstream.tcp_keep_alive)))
+            .timeout(Duration::from_secs(upstream.timeout))
+            .connect_timeout(Duration::from_secs(upstream.connect_timeout))
+            .http2_keep_alive_interval(Some(Duration::from_secs(upstream.keep_alive_interval)))
+            .http2_keep_alive_timeout(Duration::from_secs(upstream.keep_alive_timeout))
+            .http2_keep_alive_while_idle(upstream.keep_alive_while_idle)
+            .pool_idle_timeout(Some(Duration::from_secs(upstream.pool_idle_timeout)))
+            .pool_max_idle_per_host(upstream.pool_max_idle_per_host)
+            .user_agent(upstream.user_agent.clone());
+
+        // Add Http2 Prior Knowledge
+        if upstream.http2_only {
+            builder = builder.http2_prior_knowledge();
+        }
+
+        // Add Http Proxy
+        if let Some(ref proxy) = upstream.proxy {
+            builder = builder.proxy(
+                reqwest::Proxy::http(proxy.url.clone())
+                    .expect("Failed to set proxy in http client"),
+            );
+        }
+
+        let mut client = ClientBuilder::new(builder.build().expect("Failed to build client"));
+
+        if upstream.http_cache {
+            client = client.with(Cache(HttpCache {
+                mode: CacheMode::Default,
+                manager: MokaManager::default(),
+                options: HttpCacheOptions::default(),
+            }))
+        }
+        Self {
+            client: client.build(),
+            http2_only: upstream.http2_only,
+            enable_telemetry: false
+        }
+    }
+}
