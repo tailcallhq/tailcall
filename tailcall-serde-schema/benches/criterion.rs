6c10ac93cc4b737370b6aa9a076d0ba5f5e5dfe7
Tushar Mathur <tusharmath@gmail.com>
2024-05-19 19:58:54 +0530

    adding benches

---
+++ tailcall-serde-schema/benches/criterion.rs
@@ -0,0 +1,51 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+
+use serde::Deserialize;
+
+#[derive(Clone, Debug, Deserialize)]
+pub struct PostRef<'a> {
+    pub user_id: i64,
+    pub id: i64,
+    #[serde(borrow)]
+    pub title: &'a str,
+    #[serde(borrow)]
+    pub body: &'a str,
+}
+
+#[derive(Clone, Debug, Deserialize)]
+pub struct Post {
+    pub user_id: i64,
+    pub id: i64,
+    pub title: String,
+    pub body: String,
+}
+
+const JSON: &str = include_str!("../data/posts.json");
+
+fn bench_typed_ref() -> Vec<PostRef<'static>> {
+    serde_json::from_str(JSON).unwrap()
+}
+
+fn bench_typed() -> Vec<Post> {
+    serde_json::from_str(JSON).unwrap()
+}
+
+fn bench_untyped_ref() -> serde_json_borrow::Value<'static> {
+    serde_json::from_str(JSON).unwrap()
+}
+
+fn bench_untyped() -> serde_json::Value {
+    serde_json::from_str(JSON).unwrap()
+}
+
+fn bench_post_deserializer(c: &mut Criterion) {
+    let mut group = c.benchmark_group("PostDeserialize");
+    group.bench_function("typed", |b| b.iter(|| black_box(bench_typed())));
+    group.bench_function("typed_ref", |b| b.iter(|| black_box(bench_typed_ref())));
+    group.bench_function("untyped_ref", |b| b.iter(|| black_box(bench_untyped_ref())));
+    group.bench_function("untyped", |b| b.iter(|| black_box(bench_untyped())));
+    group.finish();
+}
+
+criterion_group!(benches, bench_post_deserializer);
+criterion_main!(benches);
