f7f04e96913564c90d02c23c7c27a6fbeaa0e425
Tushar Mathur <tusharmath@gmail.com>
2024-05-19 20:12:24 +0530

    add helpers

--- tailcall-serde-schema/src/schema.rs
+++ tailcall-serde-schema/src/schema.rs
@@ -19,12 +19,28 @@ pub enum N {
 }
 
 impl Schema {
-    pub fn deserialize(&self, input: &str) -> serde_json::Result<serde_json::Value> {
+    pub fn from_str(&self, input: &str) -> serde_json::Result<serde_json::Value> {
         let mut deserializer = serde_json::Deserializer::new(StrRead::new(input));
         Deserialize::new(self).deserialize(&mut deserializer)
     }
 
     pub fn array(item: Schema) -> Schema {
         Schema::Array(Box::new(item))
+    }
+
+    pub fn i64() -> Schema {
+        Schema::Number(N::I64)
+    }
+
+    pub fn u64() -> Schema {
+        Schema::Number(N::U64)
+    }
+
+    pub fn f64() -> Schema {
+        Schema::Number(N::F64)
+    }
+
+    pub fn object(map: HashMap<String, Schema>) -> Schema {
+        Schema::Object(map.into_iter().map(|(k, v)| (k, Box::new(v))).collect())
     }
 }
