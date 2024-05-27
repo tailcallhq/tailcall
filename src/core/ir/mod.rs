4a6398500ca58f5d5a99f4fb221dd2601ddc68e3
Tushar Mathur <tusharmath@gmail.com>
2024-05-27 13:24:58 +0530

    add error

--- src/core/ir/mod.rs
+++ src/core/ir/mod.rs
@@ -1,4 +1,5 @@
 mod cache;
+mod error;
 mod eval;
 mod evaluation_context;
 mod graphql_operation_context;
@@ -6,23 +7,19 @@ mod io;
 mod modify;
 mod resolver_context_like;
 
+use core::future::Future;
+use std::fmt::{Debug, Display};
+use std::pin::Pin;
+
+use async_graphql_value::ConstValue;
 pub use cache::*;
+pub use error::*;
 pub use eval::*;
 pub use evaluation_context::EvaluationContext;
 pub use graphql_operation_context::GraphQLOperationContext;
 pub use io::*;
 pub use resolver_context_like::{EmptyResolverContext, ResolverContext, ResolverContextLike};
 
-use core::future::Future;
-use std::fmt::{Debug, Display};
-use std::pin::Pin;
-use std::sync::Arc;
-
-use async_graphql::ErrorExtensions;
-use async_graphql_value::ConstValue;
-use thiserror::Error;
-
-use crate::core::auth;
 use crate::core::blueprint::DynamicValue;
 use crate::core::json::JsonLike;
 use crate::core::serde_value_ext::ValueExt;
@@ -56,90 +53,6 @@ pub enum Context {
     Path(Vec<String>),
     PushArgs { expr: Box<IR>, and_then: Box<IR> },
     PushValue { expr: Box<IR>, and_then: Box<IR> },
-}
-
-#[derive(Debug, Error, Clone)]
-pub enum EvaluationError {
-    #[error("IOException: {0}")]
-    IOException(String),
-
-    #[error("gRPC Error: status: {grpc_code}, description: `{grpc_description}`, message: `{grpc_status_message}`")]
-    GRPCError {
-        grpc_code: i32,
-        grpc_description: String,
-        grpc_status_message: String,
-        grpc_status_details: ConstValue,
-    },
-
-    #[error("APIValidationError: {0:?}")]
-    APIValidationError(Vec<String>),
-
-    #[error("ExprEvalError: {0}")]
-    ExprEvalError(String),
-
-    #[error("DeserializeError: {0}")]
-    DeserializeError(String),
-
-    #[error("Authentication Failure: {0}")]
-    AuthError(auth::error::Error),
-}
-
-// TODO: remove conversion from anyhow and don't use anyhow to pass errors
-// since it loses potentially valuable information that could be later provided
-// in the error extensions
-impl From<anyhow::Error> for EvaluationError {
-    fn from(value: anyhow::Error) -> Self {
-        match value.downcast::<EvaluationError>() {
-            Ok(err) => err,
-            Err(err) => EvaluationError::IOException(err.to_string()),
-        }
-    }
-}
-
-impl From<Arc<anyhow::Error>> for EvaluationError {
-    fn from(error: Arc<anyhow::Error>) -> Self {
-        match error.downcast_ref::<EvaluationError>() {
-            Some(err) => err.clone(),
-            None => EvaluationError::IOException(error.to_string()),
-        }
-    }
-}
-
-impl ErrorExtensions for EvaluationError {
-    fn extend(&self) -> async_graphql::Error {
-        async_graphql::Error::new(format!("{}", self)).extend_with(|_err, e| {
-            if let EvaluationError::GRPCError {
-                grpc_code,
-                grpc_description,
-                grpc_status_message,
-                grpc_status_details,
-            } = self
-            {
-                e.set("grpcCode", *grpc_code);
-                e.set("grpcDescription", grpc_description);
-                e.set("grpcStatusMessage", grpc_status_message);
-                e.set("grpcStatusDetails", grpc_status_details.clone());
-            }
-        })
-    }
-}
-
-impl<'a> From<crate::core::valid::ValidationError<&'a str>> for EvaluationError {
-    fn from(value: crate::core::valid::ValidationError<&'a str>) -> Self {
-        EvaluationError::APIValidationError(
-            value
-                .as_vec()
-                .iter()
-                .map(|e| e.message.to_owned())
-                .collect(),
-        )
-    }
-}
-
-impl From<auth::error::Error> for EvaluationError {
-    fn from(value: auth::error::Error) -> Self {
-        EvaluationError::AuthError(value)
-    }
 }
 
 impl IR {
