442450ce81889c830133438a5337744585affd94
Ranjit Mahadik <43403528+ranjitmahadik@users.noreply.github.com>
2024-05-30 16:24:50 +0530

    - added test cases for extract_base_url.

--- src/core/generator/json/url_utils.rs
+++ src/core/generator/json/url_utils.rs
@@ -6,6 +6,39 @@ pub fn extract_base_url(url: &Url) -> Option<String> {
             Some(port) => Some(format!("{}://{}:{}", url.scheme(), host, port)),
             None => Some(format!("{}://{}", url.scheme(), host)),
         },
-        None => None
+        None => None,
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use url::Url;
+
+    #[test]
+    fn test_extract_base_url_with_port() {
+        let url = Url::parse("http://example.com:8080/path/to/resource").unwrap();
+        assert_eq!(
+            extract_base_url(&url),
+            Some("http://example.com:8080".to_string())
+        );
+    }
+
+    #[test]
+    fn test_extract_base_url_without_port() {
+        let url = Url::parse("https://subdomain.example.org").unwrap();
+        assert_eq!(
+            extract_base_url(&url),
+            Some("https://subdomain.example.org".to_string())
+        );
+    }
+
+    #[test]
+    fn test_extract_base_url_with_ip_address() {
+        let url = Url::parse("http://192.168.1.1:8080/path/to/resource").unwrap();
+        assert_eq!(
+            extract_base_url(&url),
+            Some("http://192.168.1.1:8080".to_string())
+        );
     }
 }
