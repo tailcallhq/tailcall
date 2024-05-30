8c4e406245441fdf4fcd05c54f78464e2d46194d
Ranjit Mahadik <43403528+ranjitmahadik@users.noreply.github.com>
2024-05-30 11:42:29 +0530

    - field base url generator created.

---
+++ src/core/generator/json/field_base_url_generator.rs
@@ -0,0 +1,44 @@
+use url::Url;
+
+use crate::core::config::Config;
+
+use super::ConfigGenerator;
+
+pub struct FieldBaseUrlGenerator<'a> {
+    url: &'a Url,
+    query: &'a str,
+}
+
+impl<'a> FieldBaseUrlGenerator<'a> {
+    pub fn new(url: &'a Url, query: &'a str) -> Self {
+        Self { url, query }
+    }
+}
+
+impl ConfigGenerator for FieldBaseUrlGenerator<'_> {
+    fn apply(&mut self, mut config: Config) -> Config {
+        let base_url = match self.url.host_str() {
+            Some(host) => match self.url.port() {
+                Some(port) => format!("{}://{}:{}", self.url.scheme(), host, port),
+                None => format!("{}://{}", self.url.scheme(), host),
+            },
+            None => return config,
+        };
+
+        if let Some(query_type) = config.types.get_mut(self.query) {
+            for field in query_type.fields.values_mut() {
+                field.http = match field.http.clone() {
+                    Some(mut http) => {
+                        if http.base_url.is_none() {
+                            http.base_url = Some(base_url.clone());
+                        }
+                        Some(http)
+                    }
+                    None => None,
+                }
+            }
+        }
+
+        config
+    }
+}
