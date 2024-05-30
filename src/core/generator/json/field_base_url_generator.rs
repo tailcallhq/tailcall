3f210dc087f24331381f814fdd0cf0b9e4fc9f8b
Ranjit Mahadik <43403528+ranjitmahadik@users.noreply.github.com>
2024-05-30 13:18:02 +0530

    - added test cases and updated snaps.

--- src/core/generator/json/field_base_url_generator.rs
+++ src/core/generator/json/field_base_url_generator.rs
@@ -41,3 +41,105 @@ impl ConfigGenerator for FieldBaseUrlGenerator<'_> {
         config
     }
 }
+
+#[cfg(test)]
+mod test {
+    use url::Url;
+
+    use super::FieldBaseUrlGenerator;
+    use crate::core::config::{Config, Field, Http, Type};
+    use crate::core::generator::json::ConfigGenerator;
+
+    #[test]
+    fn should_add_base_url_for_http_fields() {
+        let url = Url::parse("https://example.com").unwrap();
+        let query = "Query";
+        let mut field_base_url_gen = FieldBaseUrlGenerator::new(&url, query);
+
+        let mut config = Config::default();
+        let mut query_type = Type::default();
+        query_type.fields.insert(
+            "f1".to_string(),
+            Field {
+                type_of: "Int".to_string(),
+                http: Some(Http { path: "/day".to_string(), ..Default::default() }),
+                ..Default::default()
+            },
+        );
+        query_type.fields.insert(
+            "f2".to_string(),
+            Field {
+                type_of: "String".to_string(),
+                http: Some(Http { path: "/month".to_string(), ..Default::default() }),
+                ..Default::default()
+            },
+        );
+        query_type.fields.insert(
+            "f3".to_string(),
+            Field {
+                type_of: "String".to_string(),
+                http: Some(Http { path: "/status".to_string(), ..Default::default() }),
+                ..Default::default()
+            },
+        );
+        config.types.insert("Query".to_string(), query_type);
+
+        config = field_base_url_gen.apply(config);
+
+        insta::assert_snapshot!(config.to_sdl());
+    }
+
+    #[test]
+    fn should_add_base_url_if_not_present() {
+        let url = Url::parse("https://example.com").unwrap();
+        let query = "Query";
+        let mut field_base_url_gen = FieldBaseUrlGenerator::new(&url, query);
+
+        let mut config = Config::default();
+        let mut query_type = Type::default();
+        query_type.fields.insert(
+            "f1".to_string(),
+            Field {
+                type_of: "Int".to_string(),
+                http: Some(Http {
+                    base_url: Some("https://calender.com/api/v1/".to_string()),
+                    path: "/day".to_string(),
+                    ..Default::default()
+                }),
+                ..Default::default()
+            },
+        );
+        query_type.fields.insert(
+            "f2".to_string(),
+            Field {
+                type_of: "String".to_string(),
+                http: Some(Http { path: "/month".to_string(), ..Default::default() }),
+                ..Default::default()
+            },
+        );
+        query_type.fields.insert(
+            "f3".to_string(),
+            Field {
+                type_of: "String".to_string(),
+                http: Some(Http { path: "/status".to_string(), ..Default::default() }),
+                ..Default::default()
+            },
+        );
+        config.types.insert("Query".to_string(), query_type);
+
+        config = field_base_url_gen.apply(config);
+
+        insta::assert_snapshot!(config.to_sdl());
+    }
+
+    #[test]
+    fn should_not_add_base_url_when_query_not_present() {
+        let url = Url::parse("https://example.com").unwrap();
+        let query = "Query";
+        let mut field_base_url_gen = FieldBaseUrlGenerator::new(&url, query);
+        assert!(field_base_url_gen
+            .apply(Default::default())
+            .to_sdl()
+            .is_empty());
+    }
+}
