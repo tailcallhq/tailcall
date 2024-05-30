b62f4dca14fa903f4a0d308c843a43aae9a8ff5a
Ranjit Mahadik <43403528+ranjitmahadik@users.noreply.github.com>
2024-05-30 11:42:57 +0530

    - insert baseUrl only when API's don't have same domain.

--- src/core/generator/from_json.rs
+++ src/core/generator/from_json.rs
@@ -1,10 +1,9 @@
 use serde_json::Value;
 use url::Url;
 
-use super::json::{QueryGenerator, SchemaGenerator, TypesGenerator};
+use super::json::{FieldBaseUrlGenerator, QueryGenerator, SchemaGenerator, TypesGenerator};
 use crate::core::config::Config;
 use crate::core::generator::json::StepConfigGenerator;
-use crate::core::merge_right::MergeRight;
 
 pub struct ConfigGenerationRequest {
     url: Url,
@@ -17,12 +16,34 @@ impl ConfigGenerationRequest {
     }
 }
 
+fn has_same_domain(config_gen_req: &[ConfigGenerationRequest]) -> bool {
+    let mut has_same_domain = true;
+    let mut url_domain = "";
+    for cfg_gen_req in config_gen_req.iter() {
+        let domain = cfg_gen_req.url.host_str().unwrap_or("");
+        if domain.is_empty() || !url_domain.is_empty() && url_domain != domain {
+            // if we find different domains are present then add baseUrl in http directive else in upstream.
+            has_same_domain = false;
+            break;
+        }
+        url_domain = domain;
+    }
+    has_same_domain
+}
+
 pub fn from_json(
     config_gen_req: &[ConfigGenerationRequest],
     query: &str,
 ) -> anyhow::Result<Config> {
-    let mut config = Config::default();
     let mut type_counter = 1;
+
+    let url_for_schema = match has_same_domain(config_gen_req) {
+        true => Some(config_gen_req[0].url.clone()),
+        false => None,
+    };
+
+    let mut step_config_gen = StepConfigGenerator::default();
+
     for (i, request) in config_gen_req.iter().enumerate() {
         let operation_field_name = format!("f{}", i + 1);
         let query_operation_gen = QueryGenerator::new(
@@ -31,17 +52,24 @@ pub fn from_json(
             query,
             &operation_field_name,
         );
-        let generated_config = StepConfigGenerator::default()
-            .pipe(TypesGenerator::new(
-                &request.resp,
-                &mut type_counter,
-                query_operation_gen,
-            ))
-            .pipe(SchemaGenerator::new(Some(query.to_string()), None))
-            .get();
+        step_config_gen = step_config_gen.pipe(TypesGenerator::new(
+            &request.resp,
+            &mut type_counter,
+            query_operation_gen,
+        ));
 
-        config = config.merge_right(generated_config);
+        if url_for_schema.is_none() {
+            // if all API's are of not same domain, then add base url in each field of query opeartion.
+            step_config_gen = step_config_gen.pipe(FieldBaseUrlGenerator::new(&request.url, &query))
+        }
     }
+
+    step_config_gen = step_config_gen.pipe(SchemaGenerator::new(
+        Some(query.to_string()),
+        url_for_schema,
+    ));
+
+    let mut config = step_config_gen.get();
 
     let unused_types = config.unused_types();
     config = config.remove_types(unused_types);
