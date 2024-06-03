f24d3360c68c2c27771ff490caa48a92cdda4b22
Sandipsinh Rathod <zotbysandip@gmail.com>
2024-06-03 12:47:14 +0530

    fix example

--- tailcall-wasm/example/node/index.js
+++ tailcall-wasm/example/node/index.js
@@ -4,7 +4,7 @@ async function run() {
   try {
     let schema = "https://raw.githubusercontent.com/tailcallhq/tailcall/main/examples/jsonplaceholder.graphql"
     let builder = new tc.TailcallBuilder()
-    builder = await builder.with_config("jsonplaceholder.graphql", schema)
+    builder = await builder.with_config(schema)
     let executor = await builder.build()
     let result = await executor.execute("{posts { id }}")
     console.log("result: " + result)
