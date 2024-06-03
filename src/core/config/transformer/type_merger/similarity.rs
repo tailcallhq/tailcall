7404ab6583b1329a5e27882f356501ec581e4838
Ranjit Mahadik <43403528+ranjitmahadik@users.noreply.github.com>
2024-06-03 21:44:14 +0530

    - updated logic of similarity_inner to return bool value of given two types. - added tests.

--- src/core/config/transformer/type_merger/similarity.rs
+++ src/core/config/transformer/type_merger/similarity.rs
@@ -18,29 +18,26 @@ impl SimilarityStat {
 
 pub struct Similarity<'a> {
     config: &'a Config,
+    thresh: f32,
 }
 
-impl Similarity<'_> {
-    pub fn new(config: &Config) -> Similarity {
-        Similarity { config }
+impl<'a> Similarity<'a> {
+    pub fn new(config: &'a Config, thresh: f32) -> Similarity {
+        Similarity { config, thresh }
     }
 
-    pub fn similarity(&self, type_1: &Type, type_2: &Type) -> f32 {
+    pub fn similarity(&self, type_1: &Type, type_2: &Type) -> bool {
         self.similarity_inner(type_1, type_2, &mut PairSet::default())
-            .as_f32()
     }
 
-    /// calculate_distance returns pair of u32 ints -> (count of similar fields,
-    /// total count of fields)
-    /// TODO: optimize this recursive function.
     fn similarity_inner(
         &self,
         type_1: &Type,
         type_2: &Type,
         visited_type: &mut PairSet<String>,
-    ) -> SimilarityStat {
+    ) -> bool {
         let config = &self.config;
-        let mut distance = SimilarityStat::default();
+        let mut similarity_stats = SimilarityStat::default();
 
         for (field_name_1, field_1) in type_1.fields.iter() {
             if let Some(field_2) = type_2.fields.get(field_name_1) {
@@ -48,31 +45,253 @@ impl Similarity<'_> {
                 let field_2_type_of = field_2.type_of.to_owned();
 
                 if field_1_type_of == field_2_type_of {
-                    distance.same_field_count += 2; // 1 from field_1 + 1 from
+                    similarity_stats.same_field_count += 1; // 1 from field_1 + 1 from
                                                     // field_2
                 } else if let Some(type_1) = config.types.get(field_1_type_of.as_str()) {
                     if let Some(type_2) = config.types.get(field_2_type_of.as_str()) {
                         if visited_type.contains(&field_1_type_of, &field_2_type_of) {
-                            distance.same_field_count += 2;
-                            continue;
+                            // it's cyclic type, return true as they're the same.
+                            return true;
                         }
                         visited_type.insert(field_1_type_of, field_2_type_of);
 
-                        let type_similarity_metric =
+                        let is_nested_type_similar =
                             self.similarity_inner(type_1, type_2, visited_type);
 
-                        distance.total_field_count -= 2; // don't count the non-comparable field, it'll get counted by recursive
-                                                         // call.
-
-                        distance.same_field_count += type_similarity_metric.same_field_count;
-                        distance.total_field_count += type_similarity_metric.total_field_count;
+                        similarity_stats.same_field_count += if is_nested_type_similar { 1 } else { 0 };
                     }
                 }
             }
         }
 
-        distance.total_field_count += (type_1.fields.len() + type_2.fields.len()) as u32;
+        let union_field_len =
+            (type_1.fields.len() + type_2.fields.len()) as u32 - similarity_stats.same_field_count;
+        similarity_stats.total_field_count = union_field_len;
 
-        distance
+        similarity_stats.as_f32() >= self.thresh
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::core::config::{Config, Field, Type};
+
+    use super::Similarity;
+
+    #[test]
+    fn should_return_false_when_thresh_is_not_met() {
+        let mut foo1 = Type::default();
+        foo1.fields.insert(
+            "a".to_owned(),
+            Field { type_of: "Int".to_owned(), ..Default::default() },
+        );
+        foo1.fields.insert(
+            "b".to_owned(),
+            Field { type_of: "String".to_owned(), ..Default::default() },
+        );
+        foo1.fields.insert(
+            "c".to_owned(),
+            Field { type_of: "Bar1".to_owned(), ..Default::default() },
+        );
+
+        let mut foo2 = Type::default();
+        foo2.fields.insert(
+            "a".to_owned(),
+            Field { type_of: "Int".to_owned(), ..Default::default() },
+        );
+        foo2.fields.insert(
+            "b".to_owned(),
+            Field { type_of: "Float".to_owned(), ..Default::default() },
+        );
+        foo2.fields.insert(
+            "c".to_owned(),
+            Field { type_of: "Bar2".to_owned(), ..Default::default() },
+        );
+
+        let mut bar1 = Type::default();
+        bar1.fields.insert(
+            "a".to_owned(),
+            Field { type_of: "Int".to_owned(), ..Default::default() },
+        );
+        bar1.fields.insert(
+            "c".to_owned(),
+            Field { type_of: "Float".to_owned(), ..Default::default() },
+        );
+
+        let mut bar2 = Type::default();
+        bar2.fields.insert(
+            "a".to_owned(),
+            Field { type_of: "Int".to_owned(), ..Default::default() },
+        );
+        bar2.fields.insert(
+            "c".to_owned(),
+            Field { type_of: "String".to_owned(), ..Default::default() },
+        );
+
+        let mut cfg: Config = Config::default();
+        cfg.types.insert("Foo1".to_owned(), foo1.to_owned());
+        cfg.types.insert("Foo2".to_owned(), foo2.to_owned());
+        cfg.types.insert("Bar1".to_owned(), bar1);
+        cfg.types.insert("Bar2".to_owned(), bar2);
+
+        let gen = Similarity::new(&cfg, 0.5);
+        let is_similar = gen.similarity(&foo1, &foo2);
+
+        assert!(!is_similar)
+    }
+
+    #[test]
+    fn should_return_true_when_thresh_is_met() {
+        let mut foo1 = Type::default();
+        foo1.fields.insert(
+            "a".to_owned(),
+            Field { type_of: "Int".to_owned(), ..Default::default() },
+        );
+        foo1.fields.insert(
+            "b".to_owned(),
+            Field { type_of: "String".to_owned(), ..Default::default() },
+        );
+        foo1.fields.insert(
+            "c".to_owned(),
+            Field { type_of: "Bar1".to_owned(), ..Default::default() },
+        );
+
+        let mut foo2 = Type::default();
+        foo2.fields.insert(
+            "a".to_owned(),
+            Field { type_of: "Int".to_owned(), ..Default::default() },
+        );
+        foo2.fields.insert(
+            "b".to_owned(),
+            Field { type_of: "Float".to_owned(), ..Default::default() },
+        );
+        foo2.fields.insert(
+            "c".to_owned(),
+            Field { type_of: "Bar2".to_owned(), ..Default::default() },
+        );
+
+        let mut bar1 = Type::default();
+        bar1.fields.insert(
+            "a".to_owned(),
+            Field { type_of: "Int".to_owned(), ..Default::default() },
+        );
+        bar1.fields.insert(
+            "c".to_owned(),
+            Field { type_of: "Float".to_owned(), ..Default::default() },
+        );
+
+        let mut bar2 = Type::default();
+        bar2.fields.insert(
+            "a".to_owned(),
+            Field { type_of: "Int".to_owned(), ..Default::default() },
+        );
+        bar2.fields.insert(
+            "c".to_owned(),
+            Field { type_of: "Float".to_owned(), ..Default::default() },
+        );
+        bar2.fields.insert(
+            "k".to_owned(),
+            Field { type_of: "Int".to_owned(), ..Default::default() },
+        );
+
+        let mut cfg: Config = Config::default();
+        cfg.types.insert("Foo1".to_owned(), foo1.to_owned());
+        cfg.types.insert("Foo2".to_owned(), foo2.to_owned());
+        cfg.types.insert("Bar1".to_owned(), bar1);
+        cfg.types.insert("Bar2".to_owned(), bar2);
+
+        let gen = Similarity::new(&cfg, 0.5);
+        let is_similar = gen.similarity(&foo1, &foo2);
+
+        assert!(is_similar)
+    }
+
+    #[test]
+    fn test_cyclic_type() {
+        let mut foo1 = Type::default();
+        foo1.fields.insert(
+            "a".to_owned(),
+            Field { type_of: "Bar1".to_owned(), ..Default::default() },
+        );
+
+        let mut foo2 = Type::default();
+        foo2.fields.insert(
+            "a".to_owned(),
+            Field { type_of: "Bar2".to_owned(), ..Default::default() },
+        );
+
+        let mut bar1 = Type::default();
+        bar1.fields.insert(
+            "a".to_owned(),
+            Field { type_of: "Foo1".to_owned(), ..Default::default() },
+        );
+
+        let mut bar2 = Type::default();
+        bar2.fields.insert(
+            "a".to_owned(),
+            Field { type_of: "Foo2".to_owned(), ..Default::default() },
+        );
+
+        let mut cfg: Config = Config::default();
+        cfg.types.insert("Foo1".to_owned(), foo1.to_owned());
+        cfg.types.insert("Foo2".to_owned(), foo2.to_owned());
+        cfg.types.insert("Bar1".to_owned(), bar1);
+        cfg.types.insert("Bar2".to_owned(), bar2);
+
+        let gen = Similarity::new(&cfg, 0.8);
+        let is_similar = gen.similarity(&foo1, &foo2);
+
+        assert!(is_similar)
+    }
+
+    #[test]
+    fn test_nested_types() {
+        let mut foo1 = Type::default();
+        foo1.fields.insert(
+            "a".to_owned(),
+            Field { type_of: "Bar1".to_owned(), ..Default::default() },
+        );
+
+        let mut foo2 = Type::default();
+        foo2.fields.insert(
+            "a".to_owned(),
+            Field { type_of: "Bar2".to_owned(), ..Default::default() },
+        );
+
+        let mut bar1 = Type::default();
+        bar1.fields.insert(
+            "a".to_owned(),
+            Field { type_of: "Far1".to_owned(), ..Default::default() },
+        );
+
+        let mut bar2 = Type::default();
+        bar2.fields.insert(
+            "a".to_owned(),
+            Field { type_of: "Far2".to_owned(), ..Default::default() },
+        );
+
+        let mut far1 = Type::default();
+        far1.fields.insert(
+            "a".to_owned(),
+            Field { type_of: "Int".to_owned(), ..Default::default() },
+        );
+        let mut far2 = Type::default();
+        far2.fields.insert(
+            "a".to_owned(),
+            Field { type_of: "Int".to_owned(), ..Default::default() },
+        );
+
+        let mut cfg: Config = Config::default();
+        cfg.types.insert("Foo1".to_owned(), foo1.to_owned());
+        cfg.types.insert("Foo2".to_owned(), foo2.to_owned());
+        cfg.types.insert("Bar1".to_owned(), bar1);
+        cfg.types.insert("Bar2".to_owned(), bar2);
+        cfg.types.insert("Far1".to_owned(), far1);
+        cfg.types.insert("Far2".to_owned(), far2);
+
+        let gen = Similarity::new(&cfg, 0.8);
+        let is_similar = gen.similarity(&foo1, &foo2);
+
+        assert!(is_similar)
     }
 }
