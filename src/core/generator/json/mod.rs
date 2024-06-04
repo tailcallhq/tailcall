38b9085f3376e7df01ec36c1d20787e7a9cf394e
Ranjit Mahadik <43403528+ranjitmahadik@users.noreply.github.com>
2024-06-04 13:35:20 +0530

    - export FieldBaseUrlGenerator.

--- src/core/generator/json/mod.rs
+++ src/core/generator/json/mod.rs
@@ -4,6 +4,7 @@ mod schema_generator;
 mod types_generator;
 mod url_utils;
 
+pub use field_base_url_generator::FieldBaseUrlGenerator;
 pub use query_generator::QueryGenerator;
 pub use schema_generator::SchemaGenerator;
 pub use types_generator::TypesGenerator;
