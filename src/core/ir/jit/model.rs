e23473e130d1e73f2759fe0b3e8184bcc1417b6d
Tushar Mathur <tusharmath@gmail.com>
2024-06-04 18:00:40 +0530

    move tests into model

--- src/core/ir/jit/model.rs
+++ src/core/ir/jit/model.rs
@@ -315,3 +315,165 @@ impl ExecutionPlanBuilder {
         Ok(fields)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::core::blueprint::Blueprint;
+    use crate::core::config::Config;
+    use crate::core::ir::jit::model::ExecutionPlanBuilder;
+    use crate::core::valid::Validator;
+
+    const CONFIG: &str = include_str!("./fixtures/jsonplaceholder-mutation.graphql");
+
+    fn create_query_plan(query: impl AsRef<str>) -> ExecutionPlan {
+        let config = Config::from_sdl(CONFIG).to_result().unwrap();
+        let blueprint = Blueprint::try_from(&config.into()).unwrap();
+        let document = async_graphql::parser::parse_query(query).unwrap();
+
+        ExecutionPlanBuilder::new(blueprint)
+            .build(document)
+            .unwrap()
+    }
+
+    #[tokio::test]
+    async fn test_from_document() {
+        let plan = create_query_plan(
+            r#"
+            query {
+                posts { user { id name } }
+            }
+        "#,
+        );
+        insta::assert_debug_snapshot!(plan);
+    }
+
+    #[test]
+    fn test_simple_query() {
+        let plan = create_query_plan(
+            r#"
+            query {
+                posts { user { id } }
+            }
+        "#,
+        );
+        insta::assert_debug_snapshot!(plan);
+    }
+
+    #[test]
+    fn test_simple_mutation() {
+        let plan = create_query_plan(
+            r#"
+            mutation {
+              createUser(user: {
+                id: 101,
+                name: "Tailcall",
+                email: "tailcall@tailcall.run",
+                phone: "2345234234",
+                username: "tailcall",
+                website: "tailcall.run"
+              }) {
+                id
+                name
+                email
+                phone
+                website
+                username
+              }
+            }
+        "#,
+        );
+        insta::assert_debug_snapshot!(plan);
+    }
+
+    #[test]
+    fn test_fragments() {
+        let plan = create_query_plan(
+            r#"
+            fragment UserPII on User {
+              name
+              email
+              phone
+            }
+
+            query {
+              user(id:1) {
+                ...UserPII
+              }
+            }
+        "#,
+        );
+        insta::assert_debug_snapshot!(plan);
+    }
+
+    #[test]
+    fn test_multiple_operations() {
+        let plan = create_query_plan(
+            r#"
+            query {
+              user(id:1) {
+                id
+                username
+              }
+              posts {
+                id
+                title
+              }
+            }
+        "#,
+        );
+        insta::assert_debug_snapshot!(plan);
+    }
+
+    #[test]
+    fn test_variables() {
+        let plan = create_query_plan(
+            r#"
+            query user($id: Int!) {
+              user(id: $id) {
+                id
+                name
+              }
+            }
+        "#,
+        );
+        insta::assert_debug_snapshot!(plan);
+    }
+
+    #[test]
+    fn test_unions() {
+        let plan = create_query_plan(
+            r#"
+            query {
+              getUserIdOrEmail(id:1) {
+                ...on UserId {
+                  id
+                }
+                ...on UserEmail {
+                  email
+                }
+              }
+            }
+        "#,
+        );
+        insta::assert_debug_snapshot!(plan);
+    }
+
+    #[test]
+    fn test_default_value() {
+        let plan = create_query_plan(
+            r#"
+            mutation {
+              createPost(post:{
+                userId:123,
+                title:"tailcall",
+                body:"tailcall test"
+              }) {
+                id
+              }
+            }
+        "#,
+        );
+        insta::assert_debug_snapshot!(plan);
+    }
+}
