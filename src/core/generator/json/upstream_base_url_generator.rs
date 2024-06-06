2dd151c1ba744e7b8cd8c296dec2f557fb6ff634
Ranjit Mahadik <43403528+ranjitmahadik@users.noreply.github.com>
2024-06-07 00:55:27 +0530

    - use get_operation_type_names from config.

--- src/core/generator/json/upstream_base_url_generator.rs
+++ src/core/generator/json/upstream_base_url_generator.rs
@@ -8,22 +8,18 @@ pub struct UpstreamBaseUrlGenerator;
 
 impl UpstreamBaseUrlGenerator {
     fn generate_base_url(&self, mut config: Config) -> Config {
-        let operation_types = [
-            &config.schema.query,
-            &config.schema.mutation,
-            &config.schema.subscription,
-        ];
+        let operation_types = config.get_operation_type_names();
 
         let mut base_url_set = HashSet::new();
-        let mut types_to_clean = vec![];
+        let mut types_to_clean = HashSet::new();
 
-        for operation_type in operation_types.iter().filter_map(|op| op.as_deref()) {
-            if let Some(type_) = config.types.get(operation_type) {
+        for operation_type in operation_types {
+            if let Some(type_) = config.types.get(&operation_type) {
                 for field in type_.fields.values() {
                     if let Some(http_directive) = &field.http {
                         if let Some(base_url) = &http_directive.base_url {
                             base_url_set.insert(base_url.to_owned());
-                            types_to_clean.push(operation_type);
+                            types_to_clean.insert(operation_type.to_owned());
 
                             if base_url_set.len() > 1 {
                                 return config;
@@ -38,7 +34,7 @@ impl UpstreamBaseUrlGenerator {
             config.upstream.base_url = Some(base_url.to_owned());
 
             for operation_type in types_to_clean {
-                if let Some(type_) = config.types.get_mut(operation_type) {
+                if let Some(type_) = config.types.get_mut(&operation_type) {
                     for field in type_.fields.values_mut() {
                         if let Some(http_directive) = &mut field.http {
                             http_directive.base_url = None;
