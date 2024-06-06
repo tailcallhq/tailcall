634e7adfdfcc11ccd579f6590e0d6c42e3cf4d72
Ranjit Mahadik <43403528+ranjitmahadik@users.noreply.github.com>
2024-06-06 22:49:35 +0530

    - store currently generate name.

--- src/core/generator/json/mod.rs
+++ src/core/generator/json/mod.rs
@@ -6,6 +6,8 @@ mod type_name_generator;
 mod types_generator;
 mod url_utils;
 
+use std::cell::RefCell;
+
 pub use field_base_url_generator::FieldBaseUrlGenerator;
 pub use query_generator::QueryGenerator;
 pub use schema_generator::SchemaGenerator;
@@ -17,15 +19,26 @@ use crate::core::counter::Counter;
 pub struct NameGenerator {
     counter: Counter,
     prefix: String,
+    current_name: RefCell<String>,
 }
 
 impl NameGenerator {
     pub fn new(prefix: &str) -> Self {
-        Self { counter: Counter::new(1), prefix: prefix.to_string() }
+        Self {
+            counter: Counter::new(1),
+            prefix: prefix.to_string(),
+            current_name: RefCell::new(format!("{}{}", prefix, 1)),
+        }
+    }
+
+    pub fn get_name(&self) -> String {
+        self.current_name.borrow().to_owned()
     }
 
     pub fn generate_name(&self) -> String {
         let id = self.counter.next();
-        format!("{}{}", self.prefix, id)
+        let generated_name = format!("{}{}", self.prefix, id);
+        self.current_name.replace(generated_name.to_owned());
+        generated_name
     }
 }
