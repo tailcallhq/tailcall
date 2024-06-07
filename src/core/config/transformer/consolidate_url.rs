65855e2cc93e65c6c96253627fd6e313c9426d42
Ranjit Mahadik <43403528+ranjitmahadik@users.noreply.github.com>
2024-06-07 23:56:14 +0530

    - take thresh from user to consolidate the base url.

--- src/core/config/transformer/consolidate_url.rs
+++ src/core/config/transformer/consolidate_url.rs
@@ -4,43 +4,68 @@ use crate::core::config::transformer::Transform;
 use crate::core::config::Config;
 use crate::core::valid::Valid;
 
-pub struct ConsolidateURL;
+use super::MaxValueMap;
+
+pub struct ConsolidateURL {
+    threshold: f32,
+}
 
 impl ConsolidateURL {
+    pub fn new(threshold: f32) -> Self {
+        let mut validated_thresh = threshold;
+        if !(0.0..=1.0).contains(&threshold) {
+            validated_thresh = 1.0;
+            tracing::warn!(
+                "Invalid threshold value ({:.2}), reverting to default threshold ({:.2}). allowed range is [0.0 - 1.0] inclusive",
+                threshold,
+                validated_thresh
+            );
+        }
+        Self { threshold: validated_thresh }
+    }
+
     fn generate_base_url(&self, mut config: Config) -> Config {
-        let operation_types = config.get_operation_type_names();
+        // maintain the base_url frequency map to determine the most common base url.
+        let mut url_frequency_map = MaxValueMap::new();
+        let mut type_with_base_url_fields = HashSet::new();
 
-        let mut base_url_set = HashSet::new();
-        let mut types_to_clean = HashSet::new();
-
-        for operation_type in operation_types {
-            if let Some(type_) = config.types.get(&operation_type) {
-                for field in type_.fields.values() {
-                    if let Some(http_directive) = &field.http {
-                        if let Some(base_url) = &http_directive.base_url {
-                            base_url_set.insert(base_url.to_owned());
-                            types_to_clean.insert(operation_type.to_owned());
-
-                            if base_url_set.len() > 1 {
-                                tracing::warn!(
-                                    "Multiple base URLs found, transformation cannot be performed."
-                                );
-                                return config;
-                            }
-                        }
+        for (type_name, type_) in config.types.iter() {
+            for field_ in type_.fields.values() {
+                if let Some(http_directive) = &field_.http {
+                    if let Some(base_url) = &http_directive.base_url {
+                        url_frequency_map.increment(base_url.to_owned(), 1);
+                        type_with_base_url_fields.insert(type_name.to_owned());
                     }
                 }
             }
         }
 
-        if let Some(base_url) = base_url_set.iter().next() {
-            config.upstream.base_url = Some(base_url.to_owned());
+        // If there is a most common base URL, update the config.
+        if let Some((most_common_base_url, most_common_base_url_frequency)) =
+            url_frequency_map.get_max_pair()
+        {
+            let should_perform_updation = most_common_base_url_frequency.to_owned() as f32
+                >= (url_frequency_map.len() as f32 * self.threshold);
 
-            for operation_type in types_to_clean {
-                if let Some(type_) = config.types.get_mut(&operation_type) {
-                    for field in type_.fields.values_mut() {
-                        if let Some(http_directive) = &mut field.http {
-                            http_directive.base_url = None;
+            if !should_perform_updation {
+                tracing::warn!(
+                    "Threshold matching base url not found, transformation cannot be performed."
+                );
+                return config;
+            }
+
+            config.upstream.base_url = Some(most_common_base_url.to_owned());
+
+            // Remove the base URL from the HTTP directives in the relevant types.
+            for type_name in type_with_base_url_fields {
+                if let Some(type_) = config.types.get_mut(&type_name) {
+                    for field_ in type_.fields.values_mut() {
+                        if let Some(http_directive) = &mut field_.http {
+                            if let Some(base_url) = http_directive.base_url.clone() {
+                                if &base_url == most_common_base_url {
+                                    http_directive.base_url = None;
+                                }
+                            }
                         }
                     }
                 }
@@ -86,7 +111,7 @@ mod test {
         )
         .to_result()?;
 
-        let transformed_config = ConsolidateURL.transform(config).to_result()?;
+        let transformed_config = ConsolidateURL::new(0.5).transform(config).to_result()?;
         insta::assert_snapshot!(transformed_config.to_sdl());
 
         Ok(())
@@ -110,7 +135,7 @@ mod test {
         )
         .to_result()?;
 
-        let transformed_config = ConsolidateURL.transform(config).to_result()?;
+        let transformed_config = ConsolidateURL::new(0.5).transform(config).to_result()?;
         insta::assert_snapshot!(transformed_config.to_sdl());
 
         Ok(())
