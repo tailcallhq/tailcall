6a54ce935fc163be036f1a1c2536297b47dace89
meskill <8974488+meskill@users.noreply.github.com>
2024-06-15 19:59:51 +0000

    fix lint

--- src/core/blueprint/into_schema.rs
+++ src/core/blueprint/into_schema.rs
@@ -123,10 +123,8 @@ fn to_type(def: &Definition) -> dynamic::Type {
                                         let ctx: ResolverContext = ctx.into();
                                         let ctx = &mut EvaluationContext::new(req_ctx, &ctx);
 
-                                        let value = expr
-                                            .eval(ctx)
-                                            .await
-                                            .map_err(|err| err.extend())?;
+                                        let value =
+                                            expr.eval(ctx).await.map_err(|err| err.extend())?;
 
                                         if let ConstValue::Null = value {
                                             Ok(FieldValue::NONE)
