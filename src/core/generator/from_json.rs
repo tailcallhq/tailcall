61d6b229f2fe1c8876100fd521113f9eadbacbd8
Ranjit Mahadik <43403528+ranjitmahadik@users.noreply.github.com>
2024-06-17 13:31:26 +0530

    - removed old from_json method.

--- src/core/generator/from_json.rs
+++ src/core/generator/from_json.rs
@@ -123,35 +123,3 @@ impl Generate for FromJsonGenerator<'_> {
         Valid::fail("config generation pipeline failed".to_string())
     }
 }
-
-pub fn from_json(
-    config_gen_req: &[RequestSample],
-    query: &str,
-    field_name_gen: &NameGenerator,
-    type_name_gen: &NameGenerator,
-) -> anyhow::Result<Config> {
-    let mut config = Config::default();
-
-    for request in config_gen_req.iter() {
-        let field_name = field_name_gen.generate_name();
-        let query_generator = QueryGenerator::new(
-            request.response.is_array(),
-            &request.url,
-            query,
-            &field_name,
-        );
-
-        config = TypesGenerator::new(&request.response, query_generator, type_name_gen)
-            .pipe(SchemaGenerator::new(query.to_owned()))
-            .pipe(FieldBaseUrlGenerator::new(&request.url, query))
-            .pipe(RemoveUnused)
-            .pipe(TypeMerger::new(0.8)) //TODO: take threshold value from user
-            .pipe(TypeNameGenerator)
-            .transform(config)
-            .to_result()?;
-    }
-
-    let config = ConsolidateURL::new(0.5).transform(config).to_result()?;
-
-    Ok(config)
-}
