4cdbbf46ffd0d9b4bb00aed6a499223177f9c093
Ranjit Mahadik <43403528+ranjitmahadik@users.noreply.github.com>
2024-06-18 00:25:03 +0530

    - added conditional transforms.

--- src/core/config/transformer/preset.rs
+++ src/core/config/transformer/preset.rs
@@ -1,11 +1,16 @@
+use derive_setters::Setters;
+
 use crate::core::config::Config;
 use crate::core::transform::{self, Transform, TransformerOps};
 
 /// Defines a set of default transformers that can be applied to any
 /// configuration to make it more maintainable.
+#[derive(Setters)]
 pub struct Preset {
     merge_type: f32,
     consolidate_url: f32,
+    tree_shake: bool,
+    use_better_names: bool,
 }
 
 impl Transform for Preset {
@@ -17,9 +22,9 @@ impl Transform for Preset {
         config: Self::Value,
     ) -> crate::core::valid::Valid<Self::Value, Self::Error> {
         transform::default()
-            .pipe(super::RemoveUnused)
+            .when(super::RemoveUnused, || self.tree_shake)
             .pipe(super::TypeMerger::new(self.merge_type))
-            .pipe(super::TypeNameGenerator)
+            .when(super::TypeNameGenerator, || self.use_better_names)
             .pipe(super::ConsolidateURL::new(self.consolidate_url))
             .transform(config)
     }
@@ -27,6 +32,11 @@ impl Transform for Preset {
 
 impl Default for Preset {
     fn default() -> Self {
-        Self { merge_type: 1.0, consolidate_url: 0.5 }
+        Self {
+            merge_type: 1.0,
+            consolidate_url: 0.5,
+            use_better_names: true,
+            tree_shake: true,
+        }
     }
 }
