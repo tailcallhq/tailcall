b22ebe5072888af4d41e3f154eae2de7fb174c69
Ranjit Mahadik <43403528+ranjitmahadik@users.noreply.github.com>
2024-06-21 19:42:39 +0530

    - for now mutation operation isn't supported.

--- src/cli/generator/config.rs
+++ src/cli/generator/config.rs
@@ -109,7 +109,6 @@ pub struct Output<Status = UnResolved> {
 pub enum Operation {
     #[default]
     Query,
-    Mutation,
 }
 
 #[derive(Debug)]
@@ -123,8 +122,6 @@ pub struct UnResolved {}
 pub struct Schema {
     #[serde(skip_serializing_if = "Option::is_none")]
     pub query: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub mutation: Option<String>,
 }
 
 impl Output<UnResolved> {
