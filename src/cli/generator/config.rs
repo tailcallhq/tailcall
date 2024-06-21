36d54e7bf2898cf68fbbdca7e2f67607df243c01
Ranjit Mahadik <43403528+ranjitmahadik@users.noreply.github.com>
2024-06-21 17:19:11 +0530

    - remove un-supported ops.

--- src/cli/generator/config.rs
+++ src/cli/generator/config.rs
@@ -119,7 +119,6 @@ pub struct Output<Status = UnResolved> {
 pub enum Operation {
     #[default]
     Query,
-    Mutation,
 }
 
 #[derive(Debug)]
@@ -133,8 +132,6 @@ pub struct UnResolved {}
 pub struct Schema {
     #[serde(skip_serializing_if = "Option::is_none")]
     pub query: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub mutation: Option<String>,
 }
 
 impl Output<UnResolved> {
