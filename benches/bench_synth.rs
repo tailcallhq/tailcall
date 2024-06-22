d147b67bcf4fa9ac305107767a75495241e3abc6
Tushar Mathur <tusharmath@gmail.com>
2024-06-22 18:59:13 +0530

    update snapshots

--- benches/bench_synth.rs
+++ benches/bench_synth.rs
@@ -4,7 +4,6 @@ use tailcall::core::ir::common::JsonPlaceholder;
 pub fn bench_synth_nested(c: &mut Criterion) {
     c.bench_function("synth_nested", |b| {
         let synth = JsonPlaceholder::init("{ posts { id title user { id name } } }");
-        insta::assert_snapshot!(synth.synthesize());
         b.iter(|| {
             let a = synth.synthesize();
             drop(a);
