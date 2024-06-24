6f1cd26ea1767d06ccaa6b29ec0a9b4945626e68
laststylebender <ran.mahadik14@gmail.com>
2024-06-25 01:11:58 +0530

    - utilise cache.

--- tailcall-http-cache/src/fs_cache.rs
+++ tailcall-http-cache/src/fs_cache.rs
@@ -1,17 +1,15 @@
 use std::path::{Path, PathBuf};
 
 use async_trait::async_trait;
-use crypto_hash::{hex_digest, Algorithm};
 use http_cache_reqwest::{CacheManager, HttpResponse};
 use http_cache_semantics::CachePolicy;
 use serde::{Deserialize, Serialize};
-use tokio::fs;
 
 pub type BoxError = Box<dyn std::error::Error + Send + Sync>;
 pub type Result<T> = std::result::Result<T, BoxError>;
 
 pub struct FsCacheManager {
-    cache_dir: PathBuf,
+    path: PathBuf,
 }
 
 #[derive(Clone, Deserialize, Serialize)]
@@ -22,27 +20,18 @@ pub struct Store {
 
 impl Default for FsCacheManager {
     fn default() -> Self {
-        Self { cache_dir: PathBuf::from("./.cache") }
+        Self { path: PathBuf::from("./.cache") }
     }
 }
 
 impl FsCacheManager {
     pub fn new(cache_dir: impl AsRef<Path>) -> Self {
         let cache_dir = PathBuf::from(cache_dir.as_ref());
-        Self { cache_dir }
-    }
-
-    fn get_cache_file_path(&self, cache_key: &str) -> PathBuf {
-        self.cache_dir.join(cache_key)
-    }
-
-    fn encode(&self, key: &str) -> String {
-        hex_digest(Algorithm::SHA256, key.as_bytes())
+        Self { path: cache_dir }
     }
 
     pub async fn clear(&self) -> Result<()> {
-        fs::remove_dir_all(&self.cache_dir).await?;
-        fs::create_dir_all(&self.cache_dir).await?;
+        cacache::clear(&self.path).await?;
         Ok(())
     }
 }
@@ -50,14 +39,13 @@ impl FsCacheManager {
 #[async_trait]
 impl CacheManager for FsCacheManager {
     async fn get(&self, cache_key: &str) -> Result<Option<(HttpResponse, CachePolicy)>> {
-        let cache_file_path = self.get_cache_file_path(&self.encode(cache_key));
-        if cache_file_path.exists() {
-            let file_content = fs::read(&cache_file_path).await?;
-            let store: Store = serde_json::from_slice(&file_content)?;
-            Ok(Some((store.response, store.policy)))
-        } else {
-            Ok(None)
-        }
+        let store: Store = match cacache::read(&self.path, cache_key).await {
+            Ok(d) => serde_json::from_slice(&d)?,
+            Err(_e) => {
+                return Ok(None);
+            }
+        };
+        Ok(Some((store.response, store.policy)))
     }
 
     async fn put(
@@ -66,23 +54,13 @@ impl CacheManager for FsCacheManager {
         response: HttpResponse,
         policy: CachePolicy,
     ) -> Result<HttpResponse> {
-        let store = Store { response: response.clone(), policy };
-        let cache_file_path = self.get_cache_file_path(&self.encode(&cache_key));
-        let file_content = serde_json::to_vec(&store)?;
-        if !self.cache_dir.exists() {
-            // if cache_dir not exists, then create it.
-            fs::create_dir(&self.cache_dir).await?;
-        }
-
-        fs::write(cache_file_path, file_content).await?;
+        let data = Store { response: response.clone(), policy };
+        let bytes = serde_json::to_vec(&data)?;
+        cacache::write(&self.path, cache_key, bytes).await?;
         Ok(response)
     }
 
     async fn delete(&self, cache_key: &str) -> Result<()> {
-        let cache_file_path = self.get_cache_file_path(cache_key);
-        if cache_file_path.exists() {
-            fs::remove_file(cache_file_path).await?;
-        }
-        Ok(())
+        Ok(cacache::remove(&self.path, cache_key).await?)
     }
 }
