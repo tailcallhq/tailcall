521aa8a51b39fee78aa3e5f406640a7fad6818cc
Shashi Kant <shashitnak@gmail.com>
2024-06-26 23:59:24 +0530

    fixing tests

--- src/core/generator/from_openapi.rs
+++ src/core/generator/from_openapi.rs
@@ -23,3 +23,41 @@ impl OpenApiToConfigConverter {
 pub fn from_openapi_spec(spec: OpenApiV3Spec) -> anyhow::Result<Config> {
     OpenApiToConfigConverter::new(spec).map(|converter| converter.convert())
 }
+
+#[cfg(test)]
+mod tests {
+    use std::path::Path;
+
+    use super::*;
+
+    #[test]
+    fn test_openapi_apis_guru() {
+        let apis_guru = config_from_openapi_spec("apis-guru.yml").unwrap();
+        insta::assert_snapshot!(apis_guru);
+    }
+
+    #[test]
+    fn test_openapi_jsonplaceholder() {
+        let jsonplaceholder = config_from_openapi_spec("jsonplaceholder.yml").unwrap();
+        insta::assert_snapshot!(jsonplaceholder);
+    }
+
+    #[test]
+    fn test_openapi_spotify() {
+        let spotify = config_from_openapi_spec("spotify.yml").unwrap();
+        insta::assert_snapshot!(spotify);
+    }
+
+    fn config_from_openapi_spec(filename: &str) -> Option<String> {
+        let spec_path = Path::new("src")
+            .join("core")
+            .join("generator")
+            .join("tests")
+            .join("fixtures")
+            .join("openapi")
+            .join(filename);
+
+        let spec = oas3::from_path(spec_path).unwrap();
+        from_openapi_spec(spec).ok().map(|config| config.to_sdl())
+    }
+}
