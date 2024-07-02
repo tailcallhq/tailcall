119e597b5fd7b497933e2ef9d9a228599c9e2689
Sandipsinh Rathod <zotbysandip@gmail.com>
2024-07-02 16:42:34 +0530

    avoid wrapping data with `data` key in synth

--- src/core/jit/error.rs
+++ src/core/jit/error.rs
@@ -1,4 +1,5 @@
 use derive_more::From;
+use serde::Serialize;
 
 #[derive(From, Debug, Clone)]
 pub enum Error {
@@ -9,3 +10,17 @@ pub enum Error {
 }
 
 pub type Result<A> = std::result::Result<A, Error>;
+
+impl Serialize for Error {
+    // TODO: this needs a review
+    fn serialize<S: serde::Serializer>(
+        &self,
+        serializer: S,
+    ) -> std::result::Result<S::Ok, S::Error> {
+        match self {
+            Error::BuildError(msg) => serializer.serialize_str(msg),
+            Error::ParseError(err) => serializer.serialize_str(&err.to_string()),
+            Error::IR(err) => serializer.serialize_str(&err.to_string()),
+        }
+    }
+}
