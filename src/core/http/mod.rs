399873145fca038fa06ee0cf8a1611081e28829b
Sandipsinh Dilipsinh Rathod <62684960+ssddOnTop@users.noreply.github.com>
2024-07-03 19:37:20 +0530

    chore: remove `pub use AppCtx` from `http` (#2339)
    Co-authored-by: Tushar Mathur <tusharmath@gmail.com>

--- src/core/http/mod.rs
+++ src/core/http/mod.rs
@@ -8,8 +8,6 @@ pub use request_handler::{handle_request, API_URL_PREFIX};
 pub use request_template::RequestTemplate;
 pub use response::*;
 
-pub use crate::core::app_context::AppContext;
-
 mod cache;
 mod data_loader;
 mod data_loader_request;
