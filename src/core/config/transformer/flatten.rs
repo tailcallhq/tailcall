ab733c4b1c4a310902c141c642695a08a43ab0d8
Sandipsinh Rathod <zotbysandip@gmail.com>
2024-07-05 10:51:24 +0530

    cleanup iter function

--- src/core/config/transformer/flatten.rs
+++ src/core/config/transformer/flatten.rs
@@ -1,3 +1,5 @@
+use derive_setters::Setters;
+
 use crate::core::config::Config;
 use crate::core::scalar::SCALAR_TYPES;
 use crate::core::valid::Valid;
@@ -6,14 +8,26 @@ use crate::core::Transform;
 #[derive(Default)]
 pub struct Flatten;
 
+#[derive(Setters)]
+struct IterHelper<'a> {
+    config: &'a Config,
+    ty: &'a str,
+    root_field: Option<String>,
+}
+
+impl<'a> IterHelper<'a> {
+    fn new(config: &'a Config, ty: &'a str) -> Self {
+        Self { config, ty, root_field: None }
+    }
+}
+
 impl Transform for Flatten {
     type Value = Config;
     type Error = String;
 
     fn transform(&self, mut config: Self::Value) -> Valid<Self::Value, Self::Error> {
         for root in roots(&config) {
-            println!("{}", root);
-            if let Some((field, ty_of)) = iter(&config, &root, None) {
+            if let Some((field, ty_of)) = iter(IterHelper::new(&config, &root)) {
                 let ty = config.types.get_mut(&root).unwrap();
                 let fields = &mut ty.fields;
                 fields.get_mut(field.as_str()).unwrap().type_of = ty_of;
@@ -48,22 +62,28 @@ fn roots(config: &Config) -> Vec<String> {
 }
 
 #[inline(always)]
-fn iter(config: &Config, ty: &str, root_field: Option<String>) -> Option<(String, String)> {
+fn iter(iter_helper: IterHelper) -> Option<(String, String)> {
+    let config = iter_helper.config;
+    let ty = iter_helper.ty;
     let ty = config.types.get(ty).unwrap();
 
     if ty.fields.len() == 1 {
         if let Some((field_name, field)) = ty.fields.iter().next() {
             if SCALAR_TYPES.contains(field.type_of.as_str()) {
-                return if let Some(field_name) = root_field {
+                return if let Some(field_name) = iter_helper.root_field {
                     Some((field_name, field.type_of.clone()))
                 } else {
                     Some((field_name.clone(), field.type_of.clone()))
                 };
             }
-            return if let Some(field_name) = root_field {
-                iter(config, &field.type_of, Some(field_name))
+            return if iter_helper.root_field.is_some() {
+                iter(iter_helper.ty(field.type_of.as_str()))
             } else {
-                iter(config, &field.type_of, Some(field_name.clone()))
+                iter(
+                    iter_helper
+                        .ty(field.type_of.as_str())
+                        .root_field(Some(field_name.clone())),
+                )
             };
         }
     }
@@ -79,9 +99,13 @@ mod test {
 
     #[test]
     fn test_foo() {
-        let config = Config::from_sdl(tailcall_fixtures::generator::FLATTEN)
-            .to_result()
-            .unwrap();
+        let config = Config::from_sdl(
+            std::fs::read_to_string(tailcall_fixtures::generator::FLATTEN)
+                .unwrap()
+                .as_str(),
+        )
+        .to_result()
+        .unwrap();
         let transformed = Flatten.transform(config).to_result().unwrap();
         println!("{}", transformed.to_sdl());
     }
