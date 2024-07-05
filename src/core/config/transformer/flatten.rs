4c097e8729564b8dca2cb17bc6d395a3c9d49ab1
Sandipsinh Rathod <zotbysandip@gmail.com>
2024-07-05 11:43:16 +0530

    switch to valid

--- src/core/config/transformer/flatten.rs
+++ src/core/config/transformer/flatten.rs
@@ -2,7 +2,7 @@ use derive_setters::Setters;
 
 use crate::core::config::Config;
 use crate::core::scalar::SCALAR_TYPES;
-use crate::core::valid::Valid;
+use crate::core::valid::{Valid, Validator};
 use crate::core::Transform;
 
 #[derive(Default)]
@@ -12,12 +12,11 @@ pub struct Flatten;
 struct IterHelper<'a> {
     config: &'a Config,
     ty: &'a str,
-    root_field: Option<String>,
 }
 
 impl<'a> IterHelper<'a> {
     fn new(config: &'a Config, ty: &'a str) -> Self {
-        Self { config, ty, root_field: None }
+        Self { config, ty }
     }
 }
 
@@ -26,27 +25,34 @@ impl Transform for Flatten {
     type Error = String;
 
     fn transform(&self, mut config: Self::Value) -> Valid<Self::Value, Self::Error> {
-        for root in roots(&config) {
-            let mut unused = vec![];
-            if let Some((field, ty_of)) = iter(IterHelper::new(&config, &root), &mut unused) {
-                let ty = config.types.get_mut(&root).unwrap();
-                let fields = &mut ty.fields;
-                fields.get_mut(field.as_str()).unwrap().type_of = ty_of;
-                for unused in unused {
-                    config.types.remove(&unused);
-                }
-            }
-        }
-        /*Valid::from_iter(roots(&config), |root| {
-            Valid::from_option(config.types.get(&root), "Root type not found")
-        }).and_then(|root_types| {
-            Valid::from_iter(root_types, |ty| {
-                if let Some((field, ty_of)) = iter(&config, ty, None) {
-
-                }
+        Valid::from_iter(roots(&config), |root| {
+            Valid::from_option(
+                config.types.get(&root).cloned(),
+                "Root schema not found".to_string(),
+            )
+            .map(|v| (v, root))
+        })
+        .and_then(|tys| {
+            Valid::from_iter(tys, |(ty, root)| {
+                Valid::from_iter(ty.fields, |(name, field)| {
+                    let mut unused = vec![];
+                    let ty = &field.type_of;
+                    if !SCALAR_TYPES.contains(ty.as_str()) {
+                        unused.push(ty.as_str().to_string());
+                    }
+                    if let Some(ty_of) = iter(IterHelper::new(&config, ty), &mut unused) {
+                        let ty = config.types.get_mut(&root).unwrap();
+                        let fields = &mut ty.fields;
+                        fields.get_mut(name.as_str()).unwrap().type_of = ty_of;
+                        for unused in unused {
+                            config.types.remove(&unused);
+                        }
+                    }
+                    Valid::succeed(())
+                })
             })
-        })*/
-        Valid::succeed(config)
+        })
+        .and_then(|_| Valid::succeed(config))
     }
 }
 
@@ -66,33 +72,20 @@ fn roots(config: &Config) -> Vec<String> {
 }
 
 #[inline(always)]
-fn iter(iter_helper: IterHelper, unused: &mut Vec<String>) -> Option<(String, String)> {
+fn iter(iter_helper: IterHelper, unused: &mut Vec<String>) -> Option<String> {
     let config = iter_helper.config;
     let ty = iter_helper.ty;
-    let ty = config.types.get(ty).unwrap();
+    let ty = config.types.get(ty)?;
 
     if ty.fields.len() == 1 {
-        if let Some((field_name, field)) = ty.fields.iter().next() {
+        if let Some((_, field)) = ty.fields.iter().next() {
             let ty_of = field.type_of.clone();
 
             if SCALAR_TYPES.contains(ty_of.as_str()) {
-                return if let Some(field_name) = iter_helper.root_field {
-                    Some((field_name, ty_of))
-                } else {
-                    Some((field_name.clone(), ty_of))
-                };
+                return Some(ty_of);
             }
             unused.push(ty_of.as_str().to_string());
-            return if iter_helper.root_field.is_some() {
-                iter(iter_helper.ty(ty_of.as_str()), unused)
-            } else {
-                iter(
-                    iter_helper
-                        .ty(ty_of.as_str())
-                        .root_field(Some(field_name.clone())),
-                    unused,
-                )
-            };
+            return iter(iter_helper.ty(ty_of.as_str()), unused);
         }
     }
     None
@@ -115,6 +108,6 @@ mod test {
         .to_result()
         .unwrap();
         let transformed = Flatten.transform(config).to_result().unwrap();
-        insta::assert_snapshot!(transformed.to_sdl())
+        insta::assert_snapshot!(transformed.to_sdl());
     }
 }
