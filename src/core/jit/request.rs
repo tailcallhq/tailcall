22d45981ec74b56025722a5a1d88b706b1ccd473
Vamshi Maskuri <117595548+varshith257@users.noreply.github.com>
2024-07-05 17:03:02 +0530

    remove data

--- src/core/jit/request.rs
+++ src/core/jit/request.rs
@@ -18,9 +18,6 @@ pub struct Request<Value> {
     pub variables: HashMap<String, Value>,
     #[serde(default)]
     pub extensions: HashMap<String, Value>,
-
-    #[serde(skip)]
-    pub data: Extras,
 }
 
 impl Hash for Request<async_graphql::Value> {
@@ -31,23 +28,6 @@ impl Hash for Request<async_graphql::Value> {
             name.hash(state);
             value.to_string().hash(state);
         }
-    }
-}
-
-// we already have a struct named Data in store
-// anyways I don't think we need this struct
-#[derive(Default, Debug)]
-pub struct Extras(pub HashMap<TypeId, Box<dyn Any + Sync + Send>>);
-
-impl Extras {
-    pub fn insert<T: Any + Sync + Send>(&mut self, value: T) {
-        self.0.insert(TypeId::of::<T>(), Box::new(value));
-    }
-
-    pub fn get<T: Any + Sync + Send>(&self) -> Option<&T> {
-        self.0
-            .get(&TypeId::of::<T>())
-            .and_then(|value| value.downcast_ref::<T>())
     }
 }
 
