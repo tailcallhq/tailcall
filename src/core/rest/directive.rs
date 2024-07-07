2aaf3e344ea0f17b63de802cad1c0ea59bf9cfd9
Mehul Mathur <mathur.mehul01@gmail.com>
2024-07-07 18:10:06 +0000

    fix: failing tests

--- src/core/rest/directive.rs
+++ src/core/rest/directive.rs
@@ -215,7 +215,10 @@ mod tests {
         let query = generate_query_with_directive(&query, DEFAULT_QUERY_PARAM);
         let directive = query_to_directive(&query);
         // Will panic
-        Rest::try_from(&directive).unwrap();
+        match Rest::try_from(&directive) {
+            Ok(_) => panic!("Test should have failed but it succeeded"),
+            Err(e) => panic!("{}", e),
+        }
     }
 
     #[test]
@@ -226,6 +229,9 @@ mod tests {
         let query = generate_query_with_directive(&query, DEFAULT_QUERY_PARAM);
         let directive = query_to_directive(&query);
         // Will panic
-        Rest::try_from(&directive).unwrap();
+        match Rest::try_from(&directive) {
+            Ok(_) => panic!("Test should have failed but it succeeded"),
+            Err(e) => panic!("{}", e),
+        }
     }
 }
