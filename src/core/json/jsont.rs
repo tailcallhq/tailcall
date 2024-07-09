4b032b34be747427452d5c072e196447150b2af4
Sandipsinh Rathod <zotbysandip@gmail.com>
2024-07-09 19:18:23 +0530

    drop Input and Output

--- src/core/json/jsont.rs
+++ src/core/json/jsont.rs
@@ -3,63 +3,59 @@
 use std::collections::HashMap;
 
 pub trait JsonT {
-    type Output;
-    type Input;
-
-    fn array_ok(value: &Self::Input) -> Option<&Vec<Self::Output>>;
-    fn str_ok(value: &Self::Input) -> Option<&str>;
-    fn i64_ok(value: &Self::Input) -> Option<i64>;
-    fn u64_ok(value: &Self::Input) -> Option<u64>;
-    fn f64_ok(value: &Self::Input) -> Option<f64>;
-    fn bool_ok(value: &Self::Input) -> Option<bool>;
-    fn null_ok(value: &Self::Input) -> Option<()>;
-    fn option_ok(value: &Self::Input) -> Option<Option<&Self::Output>>;
-    fn get_path<'a, T: AsRef<str>>(value: &'a Self::Input, path: &'a [T]) -> Option<&'a Self::Output>;
-    fn get_key<'a>(value: &'a Self::Input, path: &'a str) -> Option<&'a Self::Output>;
-    fn new(value: &Self::Output) -> &Self;
-    fn group_by<'a>(value: &'a Self::Input, path: &'a [String]) -> HashMap<String, Vec<&'a Self::Output>>;
+    fn array_ok(value: &Self) -> Option<&Vec<Self>>
+    where
+        Self: Sized;
+    fn str_ok(value: &Self) -> Option<&str>;
+    fn i64_ok(value: &Self) -> Option<i64>;
+    fn u64_ok(value: &Self) -> Option<u64>;
+    fn f64_ok(value: &Self) -> Option<f64>;
+    fn bool_ok(value: &Self) -> Option<bool>;
+    fn null_ok(value: &Self) -> Option<()>;
+    fn option_ok(value: &Self) -> Option<Option<&Self>>;
+    fn get_path<'a, T: AsRef<str>>(value: &'a Self, path: &'a [T]) -> Option<&'a Self>;
+    fn get_key<'a>(value: &'a Self, path: &'a str) -> Option<&'a Self>;
+    fn new(value: &Self) -> &Self;
+    fn group_by<'a>(value: &'a Self, path: &'a [String]) -> HashMap<String, Vec<&'a Self>>;
 }
 
 impl JsonT for serde_json::Value {
-    type Output = serde_json::Value;
-    type Input = serde_json::Value;
-
-    fn array_ok(value: &Self::Input) -> Option<&Vec<Self::Output>> {
+    fn array_ok(value: &Self) -> Option<&Vec<Self>> {
         value.as_array()
     }
 
-    fn str_ok(value: &Self::Input) -> Option<&str> {
+    fn str_ok(value: &Self) -> Option<&str> {
         value.as_str()
     }
 
-    fn i64_ok(value: &Self::Input) -> Option<i64> {
+    fn i64_ok(value: &Self) -> Option<i64> {
         value.as_i64()
     }
 
-    fn u64_ok(value: &Self::Input) -> Option<u64> {
+    fn u64_ok(value: &Self) -> Option<u64> {
         value.as_u64()
     }
 
-    fn f64_ok(value: &Self::Input) -> Option<f64> {
+    fn f64_ok(value: &Self) -> Option<f64> {
         value.as_f64()
     }
 
-    fn bool_ok(value: &Self::Input) -> Option<bool> {
+    fn bool_ok(value: &Self) -> Option<bool> {
         value.as_bool()
     }
 
-    fn null_ok(value: &Self::Input) -> Option<()> {
+    fn null_ok(value: &Self) -> Option<()> {
         value.as_null()
     }
 
-    fn option_ok(value: &Self::Input) -> Option<Option<&Self::Output>> {
+    fn option_ok(value: &Self) -> Option<Option<&Self>> {
         match value {
             serde_json::Value::Null => Some(None),
             value => Some(Some(value)),
         }
     }
 
-    fn get_path<'a, T: AsRef<str>>(value: &'a Self::Input, path: &'a [T]) -> Option<&'a Self::Output> {
+    fn get_path<'a, T: AsRef<str>>(value: &'a Self, path: &'a [T]) -> Option<&'a Self> {
         let mut val = value;
         for token in path {
             val = match val {
@@ -74,23 +70,24 @@ impl JsonT for serde_json::Value {
         Some(val)
     }
 
-    fn get_key<'a>(value: &'a Self::Input, path: &'a str) -> Option<&'a Self::Output> {
+    fn get_key<'a>(value: &'a Self, path: &'a str) -> Option<&'a Self> {
         value.get(path)
     }
 
-    fn new(value: &Self::Output) -> &Self {
+    fn new(value: &Self) -> &Self {
         value
     }
 
-    fn group_by<'a>(_value: &'a Self::Input, _path: &'a [String]) -> HashMap<String, Vec<&'a Self::Output>> {
+    fn group_by<'a>(_value: &'a Self, _path: &'a [String]) -> HashMap<String, Vec<&'a Self>> {
         todo!()
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::JsonT;
     use serde_json::Value;
+
+    use super::JsonT;
 
     #[test]
     fn test_array_ok() {
