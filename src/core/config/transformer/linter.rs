5d70944a8a0414f7a84cedcc6cec7a125d64f117
Sandipsinh Rathod <zotbysandip@gmail.com>
2024-07-10 13:59:07 +0530

    remove commented code

--- src/core/config/transformer/linter.rs
+++ src/core/config/transformer/linter.rs
@@ -60,23 +60,6 @@ fn resolve_enum(mut config: Config) -> Valid<Config, String> {
         config.enums = resolved_enums;
         Valid::succeed(config)
     })
-
-    /*    // Handle Enums and Enum Values
-    for (mut enum_name, mut enum_) in config.enums {
-        let mut resolved_vals = BTreeSet::new();
-
-        for mut enum_val in enum_.variants {
-            enum_val.name = enum_val.name.to_uppercase();
-            resolved_vals.insert(enum_val);
-        }
-        enum_.variants = resolved_vals;
-
-        enum_name = enum_name.to_pascal_case();
-        resolved_enums.insert(enum_name, enum_);
-    }
-    config.enums = resolved_enums;
-
-    Valid::succeed(config)*/
 }
 
 fn resolve_types_and_fields(mut config: Config) -> Valid<Config, String> {
@@ -105,34 +88,6 @@ fn resolve_types_and_fields(mut config: Config) -> Valid<Config, String> {
         config.types = resolved_types;
         Valid::succeed(config)
     })
-
-    /*    // Handle Types
-    let mut resolved_types = BTreeMap::new();
-    for (mut type_name, mut type_) in config.types {
-        // Handle Fields
-        let mut resolved_fields = BTreeMap::new();
-        for (mut field_name, mut field) in type_.fields {
-            field.type_of = field.type_of.to_pascal_case();
-
-            // Update type names in arg
-            for (_, arg) in field.args.iter_mut() {
-                arg.type_of = arg.type_of.to_pascal_case();
-            }
-            field_name = field_name.to_camel_case();
-            resolved_fields.insert(field_name, field);
-        }
-
-        // Insert resolved fields
-        type_.fields = resolved_fields;
-
-        // Insert resolved types
-        type_name = type_name.to_pascal_case();
-        resolved_types.insert(type_name, type_);
-    }
-    // Insert resolved types
-    config.types = resolved_types;
-
-    Valid::succeed(config)*/
 }
 
 #[cfg(test)]
