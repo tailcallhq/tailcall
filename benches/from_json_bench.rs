1a9e9bead0b86c41be22b3e75f8afdabf25dc025
Bnchi <bnchifahmi@gmail.com>
2024-07-10 23:12:15 +0300

    fix failing test

--- benches/from_json_bench.rs
+++ benches/from_json_bench.rs
@@ -23,7 +23,7 @@ pub fn benchmark_from_json_method(c: &mut Criterion) {
         url: request_url.parse().unwrap(),
         response: reqs[0].clone(),
         field_name: "f1".to_string(),
-        route: Some("/users"),
+        route: Some("/users".to_string()),
     }];
 
     let config_generator = Generator::default().inputs(cfg_gen_reqs);
