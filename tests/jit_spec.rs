e4f0f3fe68b61ddfa253b28c5345865ce0c996df
meskill <8974488+meskill@users.noreply.github.com>
2024-07-11 12:13:28 +0000

    fix(jit): update response headers

--- tests/jit_spec.rs
+++ tests/jit_spec.rs
@@ -6,27 +6,46 @@ mod tests {
     use tailcall::core::app_context::AppContext;
     use tailcall::core::blueprint::Blueprint;
     use tailcall::core::config::{Config, ConfigModule};
+    use tailcall::core::http::RequestContext;
     use tailcall::core::jit::variables::Variables;
-    use tailcall::core::jit::{ConstValueExecutor, Request};
+    use tailcall::core::jit::{ConstValueExecutor, Error, Request, Response};
     use tailcall::core::rest::EndpointSet;
     use tailcall::core::valid::Validator;
 
-    async fn new_executor(request: &Request<ConstValue>) -> anyhow::Result<ConstValueExecutor> {
-        let sdl = tokio::fs::read_to_string(tailcall_fixtures::configs::JSONPLACEHOLDER).await?;
-        let config = Config::from_sdl(&sdl).to_result()?;
-        let blueprint = Blueprint::try_from(&ConfigModule::from(config))?;
-        let runtime = tailcall::cli::runtime::init(&blueprint);
-        let app_ctx = Arc::new(AppContext::new(blueprint, runtime, EndpointSet::default()));
+    struct TestExecutor {
+        app_ctx: Arc<AppContext>,
+        req_ctx: Arc<RequestContext>,
+    }
 
-        Ok(ConstValueExecutor::new(request, app_ctx)?)
+    impl TestExecutor {
+        async fn try_new() -> anyhow::Result<Self> {
+            let sdl =
+                tokio::fs::read_to_string(tailcall_fixtures::configs::JSONPLACEHOLDER).await?;
+            let config = Config::from_sdl(&sdl).to_result()?;
+            let blueprint = Blueprint::try_from(&ConfigModule::from(config))?;
+            let runtime = tailcall::cli::runtime::init(&blueprint);
+            let app_ctx = Arc::new(AppContext::new(blueprint, runtime, EndpointSet::default()));
+            let req_ctx = Arc::new(RequestContext::from(app_ctx.as_ref()));
+
+            Ok(Self { app_ctx, req_ctx })
+        }
+
+        async fn run(
+            &self,
+            request: Request<ConstValue>,
+        ) -> anyhow::Result<Response<ConstValue, Error>> {
+            let executor = ConstValueExecutor::new(&request, self.app_ctx.clone())?;
+
+            Ok(executor.execute(&self.req_ctx, request).await)
+        }
     }
 
     #[tokio::test]
     async fn test_executor() {
         //  NOTE: This test makes a real HTTP call
         let request = Request::new("query {posts {id title}}");
-        let executor = new_executor(&request).await.unwrap();
-        let response = executor.execute(request).await;
+        let executor = TestExecutor::try_new().await.unwrap();
+        let response = executor.run(request).await.unwrap();
         let data = response.data;
 
         insta::assert_json_snapshot!(data);
@@ -36,8 +55,8 @@ mod tests {
     async fn test_executor_nested() {
         //  NOTE: This test makes a real HTTP call
         let request = Request::new("query {posts {title userId user {id name blog} }}");
-        let executor = new_executor(&request).await.unwrap();
-        let response = executor.execute(request).await;
+        let executor = TestExecutor::try_new().await.unwrap();
+        let response = executor.run(request).await.unwrap();
         let data = response.data;
 
         insta::assert_json_snapshot!(data);
@@ -49,8 +68,8 @@ mod tests {
         let request = Request::new(
             "query {posts { id user { id albums { id photos { id title combinedId } } } }}",
         );
-        let executor = new_executor(&request).await.unwrap();
-        let response = executor.execute(request).await;
+        let executor = TestExecutor::try_new().await.unwrap();
+        let response = executor.run(request).await.unwrap();
         let data = response.data;
 
         insta::assert_json_snapshot!(data);
@@ -76,8 +95,8 @@ mod tests {
             }
         "#,
         );
-        let executor = new_executor(&request).await.unwrap();
-        let response = executor.execute(request).await;
+        let executor = TestExecutor::try_new().await.unwrap();
+        let response = executor.run(request).await.unwrap();
         let data = response.data;
 
         insta::assert_json_snapshot!(data);
@@ -106,8 +125,8 @@ mod tests {
             }
         "#,
         );
-        let executor = new_executor(&request).await.unwrap();
-        let response = executor.execute(request).await;
+        let executor = TestExecutor::try_new().await.unwrap();
+        let response = executor.run(request).await.unwrap();
         let data = response.data;
 
         insta::assert_json_snapshot!(data);
@@ -117,8 +136,8 @@ mod tests {
     async fn test_executor_arguments() {
         //  NOTE: This test makes a real HTTP call
         let request = Request::new("query {user(id: 1) {id}}");
-        let executor = new_executor(&request).await.unwrap();
-        let response = executor.execute(request).await;
+        let executor = TestExecutor::try_new().await.unwrap();
+        let response = executor.run(request).await.unwrap();
         let data = response.data;
 
         insta::assert_json_snapshot!(data);
@@ -128,8 +147,8 @@ mod tests {
     async fn test_executor_arguments_default_value() {
         //  NOTE: This test makes a real HTTP call
         let request = Request::new("query {post {id title}}");
-        let executor = new_executor(&request).await.unwrap();
-        let response = executor.execute(request).await;
+        let executor = TestExecutor::try_new().await.unwrap();
+        let response = executor.run(request).await.unwrap();
         let data = response.data;
 
         insta::assert_json_snapshot!(data);
@@ -147,8 +166,9 @@ mod tests {
             }
         "#;
         let request = Request::new(query);
+        let executor = TestExecutor::try_new().await.unwrap();
 
-        match new_executor(&request).await {
+        match executor.run(request).await {
             Ok(_) => panic!("Should fail with unresolved variable"),
             Err(err) => assert_eq!(
                 err.to_string(),
@@ -156,9 +176,9 @@ mod tests {
             ),
         };
 
+        let request = Request::new(query);
         let request = request.variables(Variables::from_iter([("id".into(), ConstValue::from(1))]));
-        let executor = new_executor(&request).await.unwrap();
-        let response = executor.execute(request).await;
+        let response = executor.run(request).await.unwrap();
         let data = response.data;
 
         insta::assert_json_snapshot!(data);
@@ -169,8 +189,8 @@ mod tests {
         //  NOTE: This test makes a real HTTP call
         let request =
             Request::new("query {user1: user(id: 1) {id name} user2: user(id: 2) {id name}}");
-        let executor = new_executor(&request).await.unwrap();
-        let response = executor.execute(request).await;
+        let executor = TestExecutor::try_new().await.unwrap();
+        let response = executor.run(request).await.unwrap();
         let data = response.data;
 
         insta::assert_json_snapshot!(data);
