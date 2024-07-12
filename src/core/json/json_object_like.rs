e81820a7646bf68370cf411536a73243bcbf96f8
Tushar Mathur <tusharmath@gmail.com>
2024-07-12 19:20:31 +0530

    refactor: rename keys

--- src/core/json/json_object_like.rs
+++ src/core/json/json_object_like.rs
@@ -3,24 +3,25 @@ use indexmap::IndexMap;
 use crate::core::json::JsonLike;
 
 pub trait JsonObjectLike {
-    type Value<'a>: JsonLike
+    type Value<'a>
     where
         Self: 'a;
-    fn get<'a>(&'a self, key: &'a str) -> Option<&Self::Value<'a>>;
+    fn get_key<'a>(&'a self, key: &str) -> Option<&Self::Value<'a>>;
 }
 
+// SerdeValue
 impl JsonObjectLike for serde_json::Map<String, serde_json::Value> {
     type Value<'a> = serde_json::Value;
-
-    fn get<'a>(&'a self, key: &'a str) -> Option<&Self::Value<'a>> {
+    fn get_key<'a>(&'a self, key: &str) -> Option<&Self::Value<'a>> {
         self.get(key)
     }
 }
 
+// ConstValue
 impl<V: JsonLike + Clone> JsonObjectLike for IndexMap<async_graphql_value::Name, V> {
     type Value<'a> = V where V: 'a;
-
-    fn get<'a>(&'a self, key: &'a str) -> Option<&Self::Value<'a>> {
+    fn get_key<'a>(&'a self, key: &str) -> Option<&Self::Value<'a>> {
         self.get(&async_graphql_value::Name::new(key))
     }
 }
+
