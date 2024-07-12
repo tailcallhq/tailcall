6bb13d2b57b3d7ece2670a8c173a9a051dd1cb72
Sandipsinh Rathod <zotbysandip@gmail.com>
2024-07-12 16:45:57 +0530

    fix errs

--- src/core/jit/common/json_placeholder.rs
+++ src/core/jit/common/json_placeholder.rs
@@ -2,17 +2,17 @@ use std::collections::HashMap;
 
 use async_graphql::Value;
 use async_graphql_value::ConstValue;
-use serde_json_borrow::Value as BorrowedValue;
 use serde::Deserialize;
+use serde_json_borrow::Value as BorrowedValue;
 
 use crate::core::blueprint::Blueprint;
 use crate::core::config::{Config, ConfigModule};
 use crate::core::jit;
 use crate::core::jit::builder::Builder;
-use crate::core::jit::model::{ExecutionPlan, FieldId};
+use crate::core::jit::model::{ExecutionPlan, FieldId, Variables};
 use crate::core::jit::store::{Data, Store};
 use crate::core::jit::synth::{Synth, SynthBorrow};
-use crate::core::json::{JsonLike, JsonT, JsonObjectLike};
+use crate::core::json::{JsonLike, JsonObjectLike, JsonT};
 use crate::core::valid::Validator;
 
 /// NOTE: This is a bit of a boilerplate reducing module that is used in tests
@@ -20,12 +20,20 @@ use crate::core::valid::Validator;
 pub struct JsonPlaceholder;
 
 pub trait SynthExt<Value: JsonT> {
-    fn init(plan: ExecutionPlan, data: Vec<(FieldId, Data<Value>)>) -> Self;
+    fn init(
+        plan: ExecutionPlan,
+        data: Vec<(FieldId, Data<Value>)>,
+        vars: Variables<ConstValue>,
+    ) -> Self;
     fn synthesize(&'static self) -> jit::Result<Value>;
 }
 
 impl SynthExt<ConstValue> for Synth {
-    fn init(plan: ExecutionPlan, data: Vec<(FieldId, Data<Value>)>) -> Self {
+    fn init(
+        plan: ExecutionPlan,
+        data: Vec<(FieldId, Data<Value>)>,
+        vars: Variables<Value>,
+    ) -> Self {
         let store = data
             .into_iter()
             .fold(Store::new(), |mut store, (id, data)| {
@@ -33,7 +41,7 @@ impl SynthExt<ConstValue> for Synth {
                 store
             });
 
-        Synth::new(plan, store)
+        Synth::new(plan, store, vars)
     }
 
     fn synthesize(&'static self) -> jit::Result<Value> {
@@ -42,7 +50,11 @@ impl SynthExt<ConstValue> for Synth {
 }
 
 impl SynthExt<BorrowedValue<'static>> for SynthBorrow<'static> {
-    fn init(plan: ExecutionPlan, data: Vec<(FieldId, Data<BorrowedValue<'static>>)>) -> Self {
+    fn init(
+        plan: ExecutionPlan,
+        data: Vec<(FieldId, Data<BorrowedValue<'static>>)>,
+        _: Variables<Value>,
+    ) -> Self {
         let store = data
             .into_iter()
             .fold(Store::new(), |mut store, (id, data)| {
@@ -72,7 +84,8 @@ impl JsonPlaceholder {
         let posts = serde_json::from_str::<Vec<Value>>(Self::POSTS).unwrap();
         let users = serde_json::from_str::<Vec<Value>>(Self::USERS).unwrap();
         let user_map = users.iter().fold(HashMap::new(), |mut map, user| {
-            let id = Value::object_ok(user).and_then(|v| v.get("id").and_then(|u| u.as_u64_ok().ok()));
+            let id =
+                Value::object_ok(user).and_then(|v| v.get("id").and_then(|u| u.as_u64_ok().ok()));
 
             if let Some(id) = id {
                 map.insert(id, user);
@@ -101,7 +114,6 @@ impl JsonPlaceholder {
         TestData { posts, users }
     }
 
-
     fn plan(query: &str) -> ExecutionPlan {
         let config = ConfigModule::from(Config::from_sdl(Self::CONFIG).to_result().unwrap());
         let builder = Builder::new(
@@ -111,9 +123,7 @@ impl JsonPlaceholder {
         builder.build().unwrap()
     }
 
-    fn data<
-        Value: JsonT + Clone + serde::Deserialize<'static>
-    >(
+    fn data<Value: JsonT + Clone + serde::Deserialize<'static>>(
         plan: &ExecutionPlan,
     ) -> Vec<(FieldId, Data<Value>)> {
         let TestData { posts, users } = Self::value::<Value>();
@@ -125,23 +135,19 @@ impl JsonPlaceholder {
             .to_owned();
 
         let store = [
-            (
-                posts_id,
-                Data::Single(<Value as JsonT>::new_array(posts)),
-            ),
+            (posts_id, Data::Single(<Value as JsonT>::new_array(posts))),
             (users_id, Data::Multiple(users)),
         ];
 
         store.to_vec()
     }
 
-    pub fn init<
-        Value: JsonT + Deserialize<'static> + Clone,
-        T: SynthExt<Value>
-    >(query: &str) -> Box<T> {
+    pub fn init<Value: JsonT + Deserialize<'static> + Clone, T: SynthExt<Value>>(
+        query: &str,
+    ) -> Box<T> {
         let plan = Self::plan(query);
         let data = Self::data::<Value>(&plan);
-        Box::new(T::init(plan, data))
+        Box::new(T::init(plan, data, Default::default()))
     }
 }
 
@@ -151,8 +157,9 @@ mod tests {
 
     #[test]
     fn test_user() {
-        let synth: Box<SynthBorrow> = JsonPlaceholder::init("{ posts { id title userId user { id name } } }");
+        let synth: Box<SynthBorrow> =
+            JsonPlaceholder::init("{ posts { id title userId user { id name } } }");
         let val = synth.synthesize();
-        println!("{}",val);
+        println!("{}", val);
     }
 }
