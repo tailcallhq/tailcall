718441fc30a25da2704aac1a64408bd9d7111a22
Mehul Mathur <mathur.mehul01@gmail.com>
2024-07-18 22:41:35 +0000

    fix: error display

--- tailcall-fixtures/error.rs
+++ tailcall-fixtures/error.rs
@@ -4,10 +4,10 @@ use derive_more::{DebugCustom, From};
 
 #[derive(From, DebugCustom)]
 pub enum Error {
-    #[debug(fmt = "Std Fmt Error")]
+    #[debug(fmt = "Std Fmt Error: {}", _0)]
     StdFmt(std::fmt::Error),
 
-    #[debug(fmt = "Std IO Error")]
+    #[debug(fmt = "Std IO Error: {}", _0)]
     IO(std::io::Error),
 
     #[debug(fmt = "Failed to resolve filename: {}", _0)]
@@ -17,8 +17,8 @@ pub enum Error {
 impl Display for Error {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Error::StdFmt(_) => write!(f, "Std Fmt Error"),
-            Error::IO(_) => write!(f, "Std IO Error"),
+            Error::StdFmt(error) => write!(f, "Std Fmt Error: {}", error),
+            Error::IO(error) => write!(f, "Std IO Error: {}", error),
             Error::FilenameNotResolved(file_name) => {
                 write!(f, "Failed to resolve filename: {}", file_name)
             }
