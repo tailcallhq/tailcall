f7f6665fc376e36b474514e27046888fc80e6f71
LastStyleBender <ran.mahadik14@gmail.com>
2024-07-18 18:30:49 +0530

    - maitain args

--- src/core/jit/context.rs
+++ src/core/jit/context.rs
@@ -10,12 +10,12 @@ pub struct Context<'a, Input, Output> {
     request: &'a Request<Input>,
     value: Option<&'a Output>,
     args: Option<indexmap::IndexMap<Name, Input>>,
-    // TODO: remove the args, since they're already present inside the fields and support for
+    // TODO: remove the args, since they're already present inside the fields and add support for
     // default values.
     field: &'a Field<Nested<Input>, Input>,
     is_query: bool,
 }
-impl<'a, Input, Output> Context<'a, Input, Output> {
+impl<'a, Input: Clone, Output> Context<'a, Input, Output> {
     pub fn new(
         request: &'a Request<Input>,
         is_query: bool,
@@ -39,12 +39,14 @@ impl<'a, Input, Output> Context<'a, Input, Output> {
     }
 
     pub fn with_field(&self, field: &'a Field<Nested<Input>, Input>) -> Self {
+        let args = self.args.clone();
+
         Self {
             request: self.request,
-            args: None,
             is_query: self.is_query,
             value: self.value,
             field,
+            args,
         }
     }
 
