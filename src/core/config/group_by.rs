b9486200fb1d292b041b5195da09d37d0b7dd7d3
dekkku <abhigaikwad147@gmail.com>
2024-07-22 23:54:26 +0530

    update docs

--- src/core/config/group_by.rs
+++ src/core/config/group_by.rs
@@ -2,11 +2,14 @@ use serde::{Deserialize, Serialize};
 
 use crate::core::is_default;
 #[derive(Clone, Debug, Eq, Serialize, Deserialize, PartialEq, schemars::JsonSchema)]
-/// The `groupBy` parameter groups multiple data requests into a single call. For more details please refer out [n + 1 guide](https://tailcall.run/docs/guides/n+1#solving-using-batching).
+/// The `groupBy` parameter allows you to groups multiple data requests into a single call. For more details please refer out [n + 1 guide](https://tailcall.run/docs/guides/n+1#solving-using-batching).
 #[serde(rename_all = "camelCase")]
 pub struct GroupBy {
+    // batch_key is used to form the batched endpoint and is equivalent to a query parameter.
     #[serde(default, skip_serializing_if = "is_default")]
     batch_key: String,
+    // extraction_path is the path to the JSON object in the batched API response.
+    // It helps in extracting the required data from the nested structure of the response.
     #[serde(default, skip_serializing_if = "is_default")]
     extraction_path: Vec<String>,
 }
