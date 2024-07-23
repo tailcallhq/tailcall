8c0a826f95df26790bc2d3df6fc0bbd840cdbf78
Sandipsinh Rathod <zotbysandip@gmail.com>
2024-07-23 23:09:12 +0530

    bench synth with borrowed value

--- benches/bench_synth.rs
+++ benches/bench_synth.rs
@@ -11,3 +11,13 @@ pub fn bench_synth_nested(c: &mut Criterion) {
         })
     });
 }
+pub fn bench_synth_nested_borrow(c: &mut Criterion) {
+    c.bench_function("synth_nested_borrow", |b| {
+        let placeholder = JsonPlaceholder::init("{ posts { id title user { id name } } }");
+        let synth = placeholder.synth();
+        b.iter(|| {
+            let a: serde_json_borrow::Value = synth.synthesize().unwrap();
+            drop(a);
+        })
+    });
+}
