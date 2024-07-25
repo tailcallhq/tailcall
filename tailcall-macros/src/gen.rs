59e03a3a5f9d9acc712478b72e403863d03c0b2d
Sandipsinh Rathod <zotbysandip@gmail.com>
2024-07-25 11:52:52 +0530

    lint fixes

--- tailcall-macros/src/gen.rs
+++ tailcall-macros/src/gen.rs
@@ -1,42 +1,7 @@
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, Data, DeriveInput, Expr, Meta, Attribute};
+use syn::{parse_macro_input, Data, DeriveInput, Expr, Meta};
 
-/*fn extract_doc_type(attrs: &[Attribute]) -> String {
-    attrs.iter().filter_map(|attr| {
-        if attr.path().is_ident("gen_doc") {
-            if let Meta::List(meta_list) = &attr.meta {
-                let tokens = meta_list.tokens.clone();
-                println!("{:?}", tokens);
-                let parsed: Expr = syn::parse2(tokens).unwrap();
-                println!("{:?}", parsed);
-                if let Expr::Assign(assign) = parsed {
-                    if let Expr::Lit(lit) = assign.left.as_ref() {
-                        if let syn::Lit::Str(lit_str) = &lit.lit {
-                            if lit_str.value().eq("ty") {
-                                if let Expr::Lit(lit) = assign.right.as_ref() {
-                                    if let syn::Lit::Str(lit_str) = &lit.lit {
-                                        return Some(lit_str.value().trim().to_string());
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-                /*parsed.iter().find_map(|expr| {
-                    if let Expr::Lit(lit) = expr {
-                        if let syn::Lit::Str(lit_str) = &lit.lit {
-                            return Some(lit_str.value().trim().to_string());
-                        }
-                    }
-                    None::<String>
-                });*/
-            }
-        }
-        None::<String>
-    }).collect::<Vec<_>>().join(" ")
-}
-*/
 pub fn doc(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
 
@@ -54,7 +19,7 @@ pub fn doc(input: TokenStream) -> TokenStream {
             .attrs
             .iter()
             .filter_map(|attr| {
-                if attr.path().is_ident("gen_doc") {
+                if attr.path().is_ident("doc") {
                     if let Meta::NameValue(value) = &attr.meta {
                         if let Expr::Lit(lit) = &value.value {
                             if let syn::Lit::Str(lit_str) = &lit.lit {
@@ -72,21 +37,6 @@ pub fn doc(input: TokenStream) -> TokenStream {
             #name::#variant_name => #docs.to_string(),
         }
     });
-
-/*    let match_arms_ty = variants.iter().map(|variant| {
-        let variant_name = &variant.ident;
-        let ty = extract_doc_type(&variant.attrs).to_lowercase();
-
-        let instance_type = match ty.as_str() {
-            "integer" => quote! { InstanceType::Integer },
-            "string" => quote! { InstanceType::String },
-            _ => quote! { InstanceType::Null },
-        };
-
-        quote! {
-            #name::#variant_name => #instance_type,
-        }
-    });*/
 
     let expanded = quote! {
         impl #name {
