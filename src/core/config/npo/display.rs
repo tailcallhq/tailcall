8034154992090c8b71e19bc075d300d9e680ee78
Sandipsinh Rathod <zotbysandip@gmail.com>
2024-07-28 15:49:41 +0530

    move `as_vec` to yield.rs

--- src/core/config/npo/display.rs
+++ src/core/config/npo/display.rs
@@ -1,48 +1,6 @@
-use std::collections::{HashMap, HashSet};
 use std::fmt::{Display, Formatter};
 
-use super::n_plus_one::*;
 use crate::core::config::npo::Yield;
-
-impl<'a> Yield<'a> {
-    fn as_vec(&self) -> Vec<Vec<(&'a str, (&'a str, &'a str))>> {
-        let mut result = Vec::new();
-        let mut visited = HashSet::new();
-
-        #[allow(clippy::too_many_arguments)]
-        fn dfs<'a>(
-            map: &HashMap<TypeName<'a>, HashSet<(FieldName<'a>, TypeName<'a>)>>,
-            ty: TypeName<'a>,
-            path: Vec<(&'a str, (&'a str, &'a str))>,
-            result: &mut Vec<Vec<(&'a str, (&'a str, &'a str))>>,
-            visited: &mut HashSet<(TypeName<'a>, FieldName<'a>)>,
-        ) {
-            if let Some(fields) = map.get(&ty) {
-                for (field_name, ty_of) in fields {
-                    let mut new_path = path.clone();
-                    new_path.push((ty.0, (field_name.0, ty_of.0)));
-                    if !visited.contains(&(ty, *field_name)) {
-                        visited.insert((ty, *field_name));
-                        dfs(map, *ty_of, new_path, result, visited);
-                        visited.remove(&(ty, *field_name));
-                    }
-                }
-            } else {
-                result.push(path);
-            }
-        }
-
-        dfs(
-            &self.map,
-            TypeName(self.root),
-            Vec::new(),
-            &mut result,
-            &mut visited,
-        );
-
-        result
-    }
-}
 
 impl<'a> Display for Yield<'a> {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
--- src/core/config/npo/yield.rs
+++ src/core/config/npo/yield.rs
@@ -18,3 +18,43 @@ pub struct Yield<'a> {
     pub map: HashMap<TypeName<'a>, HashSet<(FieldName<'a>, TypeName<'a>)>>,
     pub root: &'a str,
 }
+
+impl<'a> Yield<'a> {
+    pub fn as_vec(&self) -> Vec<Vec<(&'a str, (&'a str, &'a str))>> {
+        let mut result = Vec::new();
+        let mut visited = HashSet::new();
+
+        #[allow(clippy::too_many_arguments)]
+        fn dfs<'a>(
+            map: &HashMap<TypeName<'a>, HashSet<(FieldName<'a>, TypeName<'a>)>>,
+            ty: TypeName<'a>,
+            path: Vec<(&'a str, (&'a str, &'a str))>,
+            result: &mut Vec<Vec<(&'a str, (&'a str, &'a str))>>,
+            visited: &mut HashSet<(TypeName<'a>, FieldName<'a>)>,
+        ) {
+            if let Some(fields) = map.get(&ty) {
+                for (field_name, ty_of) in fields {
+                    let mut new_path = path.clone();
+                    new_path.push((ty.0, (field_name.0, ty_of.0)));
+                    if !visited.contains(&(ty, *field_name)) {
+                        visited.insert((ty, *field_name));
+                        dfs(map, *ty_of, new_path, result, visited);
+                        visited.remove(&(ty, *field_name));
+                    }
+                }
+            } else {
+                result.push(path);
+            }
+        }
+
+        dfs(
+            &self.map,
+            TypeName(self.root),
+            Vec::new(),
+            &mut result,
+            &mut visited,
+        );
+
+        result
+    }
+}
