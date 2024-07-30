2af15513c7a760fd83d4f9ba6fc7d213618c6393
amit <amitksingh1490@gmail.com>
2024-07-30 11:51:38 +0530

    fix(deps): update rust version

--- src/core/data_loader/mod.rs
+++ src/core/data_loader/mod.rs
@@ -5,6 +5,8 @@ mod factory;
 mod loader;
 mod storage;
 
+// Making public as they are used in the public API.
+pub use cache::LruCache;
 pub use data_loader::DataLoader;
 pub use dedupe::DedupeResult;
 pub use loader::Loader;
