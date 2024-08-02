7fdb6be66ceafe7f5c0518d86c657d218696f624
Sahil Yeole <sahilyeole93@gmail.com>
2024-08-02 14:16:50 +0530

    fix: runtime and tests
    Signed-off-by: Sahil Yeole <sahilyeole93@gmail.com>

--- src/core/config/transformer/improve_type_names/improve_type_names_llm.rs
+++ src/core/config/transformer/improve_type_names/improve_type_names_llm.rs
@@ -2,6 +2,7 @@ use std::collections::HashMap;
 
 use reqwest;
 use reqwest::Error;
+use tokio::runtime::Runtime;
 use tokio::task::JoinSet;
 
 use crate::core::config::Config;
@@ -62,34 +63,34 @@ impl ImproveTypeNamesLLM {
     fn make_llm_requests(
         llm_requests: Vec<LLMRequest>,
     ) -> Result<HashMap<OriginalTypeName, Vec<AIGeneratedTypeName>>, String> {
-        let run_time = tokio::runtime::Builder::new_current_thread()
-            .enable_all()
-            .build()
-            .unwrap();
+        tokio::task::block_in_place(move || {
+            let runtime = Runtime::new().unwrap();
+            runtime.block_on(async {
+                let client = reqwest::Client::new();
+                let mut tasks_set: JoinSet<Result<LLMResponse, Error>> = JoinSet::new();
 
-        run_time.block_on(async {
-            let client = reqwest::Client::new();
-            let mut tasks_set: JoinSet<Result<LLMResponse, Error>> = JoinSet::new();
+                for llm_request in llm_requests.into_iter() {
+                    let client = client.clone();
+                    tasks_set
+                        .spawn(async move { Self::get_llm_response(client, llm_request).await });
+                }
 
-            for llm_request in llm_requests.into_iter() {
-                let client = client.clone();
-                tasks_set.spawn(async move { Self::get_llm_response(client, llm_request).await });
-            }
-
-            let mut results: HashMap<OriginalTypeName, Vec<AIGeneratedTypeName>> = HashMap::new();
-            while let Some(task) = tasks_set.join_next().await {
-                match task {
-                    Ok(response) => {
-                        match response {
-                            Ok(result) => results
-                                .insert(result.original_type_name, result.suggested_type_names),
+                let mut results: HashMap<OriginalTypeName, Vec<AIGeneratedTypeName>> =
+                    HashMap::new();
+                while let Some(task) = tasks_set.join_next().await {
+                    match task {
+                        Ok(response) => match response {
+                            Ok(result) => {
+                                results
+                                    .insert(result.original_type_name, result.suggested_type_names);
+                            }
                             Err(e) => return Err(e.to_string()),
-                        };
-                    }
-                    Err(err) => return Err(err.to_string()),
-                };
-            }
-            Ok(results)
+                        },
+                        Err(err) => return Err(err.to_string()),
+                    };
+                }
+                Ok(results)
+            })
         })
     }
 
@@ -98,7 +99,7 @@ impl ImproveTypeNamesLLM {
         llm_request: LLMRequest,
     ) -> Result<LLMResponse, Error> {
         let response = client
-            .post("https://e8b2-2405-201-101e-60f3-e984-6b14-8703-e5ed.ngrok-free.app/type_name")
+            .post("https://630c-2405-201-101e-60f3-fc34-439-491b-fe01.ngrok-free.app/type_name")
             .header("Content-Type", "application/json")
             .header(reqwest::header::TRANSFER_ENCODING, "chunked")
             .body(llm_request.prompt)
@@ -112,56 +113,5 @@ impl ImproveTypeNamesLLM {
             original_type_name: llm_request.original_type_name.to_string(),
             suggested_type_names: result,
         })
-    }
-}
-
-#[cfg(test)]
-mod test {
-    use std::fs;
-
-    use tailcall_fixtures::configs;
-
-    use crate::core::config::Config;
-    use crate::core::valid::Validator;
-
-    fn read_fixture(path: &str) -> String {
-        fs::read_to_string(path).unwrap()
-    }
-
-    #[test]
-    fn test_llm_type_name_generator_transform() {
-        let config = Config::from_sdl(read_fixture(configs::AUTO_GENERATE_CONFIG).as_str())
-            .to_result()
-            .unwrap();
-
-        let llm_response =
-            super::ImproveTypeNamesLLM::generate_llm_type_names(config.clone()).unwrap();
-        insta::assert_snapshot!(format!("{:?}", llm_response));
-    }
-
-    #[test]
-    fn test_llm_type_name_generator_with_cyclic_types() -> anyhow::Result<()> {
-        let config = Config::from_sdl(read_fixture(configs::CYCLIC_CONFIG).as_str())
-            .to_result()
-            .unwrap();
-
-        let llm_response =
-            super::ImproveTypeNamesLLM::generate_llm_type_names(config.clone()).unwrap();
-        insta::assert_snapshot!(format!("{:?}", llm_response));
-
-        Ok(())
-    }
-
-    #[test]
-    fn test_llm_type_name_generator() -> anyhow::Result<()> {
-        let config = Config::from_sdl(read_fixture(configs::NAME_GENERATION).as_str())
-            .to_result()
-            .unwrap();
-
-        let llm_response =
-            super::ImproveTypeNamesLLM::generate_llm_type_names(config.clone()).unwrap();
-        insta::assert_snapshot!(format!("{:?}", llm_response));
-
-        Ok(())
     }
 }
