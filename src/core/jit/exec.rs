2ae75dcf3cdaabd87106a0a51308bb730e8ffaff
meskill <8974488+meskill@users.noreply.github.com>
2024-08-07 21:46:34 +0000

    refactor: store __typename in value instead of separate entity

--- src/core/jit/exec.rs
+++ src/core/jit/exec.rs
@@ -9,12 +9,12 @@ use futures_util::future::join_all;
 use super::context::Context;
 use super::{DataPath, OperationPlan, Request, Response, Store};
 use crate::core::ir::model::IR;
-use crate::core::ir::TypeName;
+use crate::core::ir::TypedValue;
 use crate::core::jit;
 use crate::core::jit::synth::Synth;
 use crate::core::json::{JsonLike, JsonObjectLike};
 
-type SharedStore<Output, Error> = Arc<Mutex<Store<Result<TypedValue<Output>, Positioned<Error>>>>>;
+type SharedStore<Output, Error> = Arc<Mutex<Store<Result<Output, Positioned<Error>>>>>;
 
 ///
 /// Default GraphQL executor that takes in a GraphQL Request and produces a
@@ -38,7 +38,7 @@ where
     pub async fn store(
         &self,
         request: Request<Input>,
-    ) -> Store<Result<TypedValue<Output>, Positioned<jit::Error>>> {
+    ) -> Store<Result<Output, Positioned<jit::Error>>> {
         let store = Arc::new(Mutex::new(Store::new()));
         let mut ctx = ExecutorInner::new(request, store.clone(), self.plan.to_owned(), &self.exec);
         ctx.init().await;
@@ -62,7 +62,7 @@ struct ExecutorInner<'a, Input, Output, Error, Exec> {
 
 impl<'a, Input, Output, Error, Exec> ExecutorInner<'a, Input, Output, Error, Exec>
 where
-    Output: for<'i> JsonLike<'i> + Debug,
+    for<'i> Output: JsonLike<'i> + TypedValue<'i> + Debug,
     Input: Clone + Debug,
     Exec: IRExecutor<Input = Input, Output = Output, Error = Error>,
 {
@@ -105,22 +105,17 @@ where
         &'b self,
         ctx: &'b Context<'b, Input, Output>,
         data_path: &DataPath,
-        result: TypedValueRef<'b, Output>,
+        value: &'b Output,
     ) -> Result<(), Error> {
         let field = ctx.field();
-        let TypedValueRef { value, type_name } = result;
         // Array
         // Check if the field expects a list
         if field.type_of.is_list() {
             // Check if the value is an array
             if let Some(array) = value.as_array() {
                 join_all(array.iter().enumerate().map(|(index, value)| {
-                    let type_name = match &type_name {
-                        Some(TypeName::Single(type_name)) => type_name, /* TODO: should throw */
-                        // ValidationError
-                        Some(TypeName::Vec(v)) => &v[index],
-                        None => field.type_of.name(),
-                    };
+                    let type_name = value.get_type_name().unwrap_or(field.type_of.name());
+
                     join_all(field.nested_iter(type_name).map(|field| {
                         let ctx = ctx.with_value_and_field(value, field);
                         let data_path = data_path.clone().with_index(index);
@@ -136,11 +131,7 @@ where
         // TODO: Validate if the value is an Object
         // Has to be an Object, we don't do anything while executing if its a Scalar
         else {
-            let type_name = match &type_name {
-                Some(TypeName::Single(type_name)) => type_name,
-                Some(TypeName::Vec(_)) => panic!("TypeName type mismatch"), /* TODO: should throw ValidationError */
-                None => field.type_of.name(),
-            };
+            let type_name = value.get_type_name().unwrap_or(field.type_of.name());
 
             join_all(field.nested_iter(type_name).map(|child| {
                 let ctx = ctx.with_value_and_field(value, child);
@@ -163,8 +154,8 @@ where
         if let Some(ir) = &field.ir {
             let result = self.ir_exec.execute(ir, ctx).await;
 
-            if let Ok(ref result) = result {
-                self.iter_field(ctx, &data_path, result.as_ref()).await?;
+            if let Ok(value) = &result {
+                self.iter_field(ctx, &data_path, value).await?;
             }
 
             let mut store = self.store.lock().unwrap();
@@ -189,46 +180,10 @@ where
                 // here without doing the "fix"
                 .unwrap_or(&default_obj);
 
-            let result = TypedValueRef { value, type_name: None };
-
-            self.iter_field(ctx, &data_path, result).await?;
+            self.iter_field(ctx, &data_path, value).await?;
         }
 
         Ok(())
-    }
-}
-
-#[derive(Clone)]
-pub struct TypedValue<V> {
-    pub value: V,
-    pub type_name: Option<TypeName>,
-}
-
-pub struct TypedValueRef<'a, V> {
-    pub value: &'a V,
-    pub type_name: Option<&'a TypeName>,
-}
-
-impl<V> TypedValue<V> {
-    pub fn new(value: V) -> Self {
-        Self { value, type_name: None }
-    }
-
-    pub fn as_ref(&self) -> TypedValueRef<'_, V> {
-        TypedValueRef { value: &self.value, type_name: self.type_name.as_ref() }
-    }
-}
-
-impl<'a, V> TypedValueRef<'a, V> {
-    pub fn new(value: &'a V) -> Self {
-        Self { value, type_name: None }
-    }
-
-    pub fn map<'out, U>(&self, map: impl FnOnce(&V) -> &'out U) -> TypedValueRef<'out, U>
-    where
-        'a: 'out,
-    {
-        TypedValueRef { value: map(self.value), type_name: self.type_name }
     }
 }
 
@@ -241,5 +196,5 @@ pub trait IRExecutor {
         &'a self,
         ir: &'a IR,
         ctx: &'a Context<'a, Self::Input, Self::Output>,
-    ) -> Result<TypedValue<Self::Output>, Self::Error>;
+    ) -> Result<Self::Output, Self::Error>;
 }
