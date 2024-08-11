5148082c51ea836c18a508e3bf790e6e9245886d
dekkku <abhigaikwad147@gmail.com>
2024-08-11 16:55:18 +0530

    lint fixes

--- src/cli/llm/adapter.rs
+++ src/cli/llm/adapter.rs
@@ -67,7 +67,7 @@ pub enum OllamaModel {
 
 impl Display for Adapter {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.to_string())
+        write!(f, "{}", self)
     }
 }
 
