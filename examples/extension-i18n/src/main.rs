85825a5a1eaa579d9a775773f3d2d437079dca4a
Panagiotis <panagiotiskaratakis@gmail.com>
2024-08-12 17:01:20 +0300

    Attempt to make tests

--- examples/extension-i18n/src/main.rs
+++ examples/extension-i18n/src/main.rs
@@ -15,8 +15,6 @@ use tailcall::core::valid::Validator;
 pub struct TranslateExtension;
 
 impl ExtensionLoader for TranslateExtension {
-    fn load(&self) {
-        println!("TranslateExtension...loaded!")
     }
 
     fn prepare(
@@ -24,8 +22,6 @@ impl ExtensionLoader for TranslateExtension {
         ir: Box<tailcall::core::ir::model::IR>,
         params: ConstValue,
     ) -> Box<tailcall::core::ir::model::IR> {
-        println!("params: {:?}", params);
-        println!("ir: {:?}", ir);
         ir
     }
 
@@ -34,8 +30,6 @@ impl ExtensionLoader for TranslateExtension {
         params: ConstValue,
         value: ConstValue,
     ) -> Result<ConstValue, tailcall::core::ir::Error> {
-        println!("params: {:?}", params);
-        println!("value: {:?}", value);
         if let ConstValue::String(value) = value {
             let new_value = block_on(translate(&value));
             Ok(ConstValue::String(new_value))
@@ -50,7 +44,6 @@ pub struct ModifyIrExtension;
 
 impl ExtensionLoader for ModifyIrExtension {
     fn load(&self) {
-        println!("ModifyIrExtension...loaded!")
     }
 
     fn prepare(
@@ -58,9 +51,6 @@ impl ExtensionLoader for ModifyIrExtension {
         ir: Box<tailcall::core::ir::model::IR>,
         params: ConstValue,
     ) -> Box<tailcall::core::ir::model::IR> {
-        println!("params: {:?}", params);
-        println!("ir: {:?}", ir);
-
         if let tailcall::core::ir::model::IR::IO(tailcall::core::ir::model::IO::Http {
             req_template,
             group_by,
@@ -98,8 +88,6 @@ impl ExtensionLoader for ModifyIrExtension {
         params: ConstValue,
         value: ConstValue,
     ) -> Result<ConstValue, tailcall::core::ir::Error> {
-        println!("params: {:?}", params);
-        println!("value: {:?}", value);
         Ok(value)
     }
 }
@@ -116,7 +104,6 @@ async fn translate(value: &str) -> String {
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    println!("Extensions Example");
     let translate_ext = Arc::new(TranslateExtension {});
     let modify_ir_ext = Arc::new(ModifyIrExtension {});
     if let Ok(path) = dotenv() {
@@ -137,4 +124,122 @@ async fn main() -> anyhow::Result<()> {
     let server = Server::new(config_module);
     server.fork_start().await?;
     Ok(())
+}
+
+
+#[cfg(test)]
+mod tests {
+    use reqwest::Client;
+    use serde_json::json;
+    use tailcall::core::{http::Response, HttpIO};
+
+    use super::*;
+
+    struct MockHttp;
+
+    #[async_trait::async_trait]
+    impl HttpIO for MockHttp {
+        async fn execute(&self, _request: reqwest::Request) -> anyhow::Result<Response<hyper::body::Bytes>> {
+            let data = json!({
+                "id": 1,
+                "name": "Leanne Graham",
+                "username": "Bret",
+                "email": "Sincere@april.biz",
+                "address": {
+                    "street": "Kulas Light",
+                    "suite": "Apt. 556",
+                    "city": "Gwenborough",
+                    "zipcode": "92998-3874",
+                    "geo": {
+                            "lat": "-37.3159",
+                            "lng": "81.1496"
+                    }
+                },
+                "phone": "1-770-736-8031 x56442",
+                "website": "hildegard.org",
+                "company": {
+                    "name": "Romaguera-Crona",
+                    "catchPhrase": "Multi-layered client-server neural-net",
+                    "bs": "harness real-time e-markets"
+                }
+            }).to_string();
+            Ok(Response {
+                body: data.into(),
+                status: reqwest::StatusCode::OK,
+                headers: reqwest::header::HeaderMap::new(),
+            })
+        }
+    }
+
+    #[tokio::test]
+    async fn test_tailcall_extensions() {
+        let translate_ext = Arc::new(TranslateExtension {});
+        let modify_ir_ext = Arc::new(ModifyIrExtension {});
+        if let Ok(path) = dotenv() {
+            tracing::info!("Env file: {:?} loaded", path);
+        }
+        let mut runtime = runtime::init(&Blueprint::default());
+        runtime.http = Arc::new(MockHttp {});
+        runtime.http2_only = Arc::new(MockHttp {});
+        let config_reader = ConfigReader::init(runtime.clone());
+        let file_paths = ["./main.graphql"];
+        let config_module = config_reader.read_all(file_paths.as_ref()).await.unwrap();
+        let mut extensions = config_module.extensions().clone();
+        extensions
+            .plugin_extensions
+            .insert("translate".to_string(), translate_ext);
+        extensions
+            .plugin_extensions
+            .insert("modify_ir".to_string(), modify_ir_ext);
+        let config_module = config_module.merge_extensions(extensions);
+        let mut server = Server::new(config_module);
+        let url = "http://127.0.0.1:8800/graphql";
+        let server_up_receiver = server.server_up_receiver();
+
+        tokio::spawn(async move {
+            server.start().await.unwrap();
+        });
+
+        server_up_receiver
+            .await
+            .expect("Server did not start up correctly");
+
+        // required since our cert is self signed
+        let client = Client::builder()
+            .use_rustls_tls()
+            .danger_accept_invalid_certs(true)
+            .build()
+            .unwrap();
+        let query = json!({
+            "query": "{ user(id: 1) { id name company { catchPhrase } } }"
+        });
+
+        let client = client.clone();
+        let url = url.to_owned();
+        let query = query.clone();
+
+        let response = tokio::spawn(async move {
+            let response = client.post(url).json(&query).send().await;
+            let response = response.unwrap();
+            let response_body: serde_json::Value = response.json().await.expect("Request should success");
+            response_body
+        }).await
+        .expect("Spawned task should success");
+
+        let expected_response = json!({
+            "data": {
+                "user": {
+                    "id": 1,
+                    "name": "Leona Grahm",
+                    "company": {
+                        "catchPhrase": "Red neuronal cliente-servidor multicapa"
+                    }
+                }
+            }
+        });
+        assert_eq!(
+            response, expected_response,
+            "Unexpected response from server"
+        );
+    }
 }
