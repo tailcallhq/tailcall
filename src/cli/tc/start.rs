15c40b9c58a6a9d29ece66e07c1ff44b9e638577
Sahil Yeole <sahilyeole93@gmail.com>
2024-08-17 13:35:11 +0530

    optimize server in watch mode
    Signed-off-by: Sahil Yeole <sahilyeole93@gmail.com>

--- src/cli/tc/start.rs
+++ src/cli/tc/start.rs
@@ -33,42 +33,46 @@ pub(super) async fn start_command(
     Ok(())
 }
 
+/// Starts the server in watch mode
 async fn start_watch_server(
     file_paths: Vec<String>,
     config_reader: Arc<ConfigReader>,
 ) -> Result<()> {
-    let (tx, mut rx) = broadcast::channel(16);
+    let (tx, rx) = broadcast::channel(16);
 
     let watch_handler = task::spawn({
-        let file_paths = file_paths.clone();
+        let config_reader = Arc::clone(&config_reader);
         async move {
-            if let Err(err) = watch_files(&file_paths, tx).await {
+            if let Err(err) = watch_files(&file_paths, tx, rx, config_reader).await {
                 tracing::error!("Watch handler encountered an error: {}", err);
             }
         }
     });
 
-    let server_handler = task::spawn({
-        let config_reader = Arc::clone(&config_reader);
-        async move {
-            if let Err(err) = handle_server(&mut rx, &file_paths, config_reader).await {
-                tracing::error!("Server handler encountered an error: {}", err);
-            }
-        }
-    });
-
     if let Err(err) = watch_handler.await {
-        tracing::debug!("Error in watch handler: {}", err);
-    }
-    if let Err(err) = server_handler.await {
-        tracing::debug!("Error in server handler: {}", err);
+        tracing::error!("Error in watch handler: {}", err);
     }
 
     Ok(())
 }
 
-async fn watch_files(file_paths: &[String], tx: broadcast::Sender<()>) -> Result<()> {
+/// Watches the file paths for changes
+async fn watch_files(
+    file_paths: &[String],
+    tx: broadcast::Sender<()>,
+    mut rx: broadcast::Receiver<()>,
+    config_reader: Arc<ConfigReader>,
+) -> Result<()> {
     let (watch_tx, watch_rx) = std::sync::mpsc::channel();
+    // fake event to trigger the first server start
+    watch_tx
+        .clone()
+        .send(Ok(notify::event::Event::new(
+            notify::event::EventKind::Modify(notify::event::ModifyKind::Data(
+                notify::event::DataChange::Content,
+            )),
+        )))
+        .unwrap();
     let mut watcher = match RecommendedWatcher::new(watch_tx, Config::default()) {
         Ok(watcher) => watcher,
         Err(err) => {
@@ -92,10 +96,10 @@ async fn watch_files(file_paths: &[String], tx: broadcast::Sender<()>) -> Result
                         notify::event::DataChange::Content,
                     )) = event.kind
                     {
-                        tracing::info!("File change detected");
                         if let Err(err) = tx.send(()) {
                             tracing::error!("Failed to send the signal: {}", err);
                         }
+                        let _ = handle_server(&mut rx, file_paths, config_reader.clone()).await;
                     }
                 }
             }
@@ -104,35 +108,26 @@ async fn watch_files(file_paths: &[String], tx: broadcast::Sender<()>) -> Result
     }
 }
 
+/// Handles the server (in watch mode)
+/// Prevents server crashes if config reader fails
 async fn handle_server(
     rx: &mut broadcast::Receiver<()>,
     file_paths: &[String],
     config_reader: Arc<ConfigReader>,
 ) -> Result<()> {
-    let mut config_error = false;
+    match config_reader.read_all(file_paths).await {
+        Ok(config_module) => {
+            log_endpoint_set(&config_module.extensions().endpoint_set);
+            Fmt::log_n_plus_one(false, config_module.config());
 
-    loop {
-        match config_reader.read_all(file_paths).await {
-            Ok(config_module) => {
-                log_endpoint_set(&config_module.extensions().endpoint_set);
-                Fmt::log_n_plus_one(false, config_module.config());
-
-                let server = Server::new(config_module.clone());
-                if let Err(err) = server.fork_start(Some(rx)).await {
-                    tracing::error!("Failed to start server: {}", err);
-                }
-
-                config_error = false;
-                tracing::info!("Server restarted");
-            }
-            Err(err) => {
-                if !config_error {
-                    tracing::error!("Failed to read config files: {}", err);
-                    tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
-                    config_error = true;
-                }
+            let server = Server::new(config_module.clone());
+            if let Err(err) = server.fork_start(Some(rx)).await {
+                tracing::error!("Failed to start server: {}", err);
             }
         }
-        tokio::time::sleep(tokio::time::Duration::from_millis(100)).await;
+        Err(err) => {
+            tracing::error!("Failed to read config files: {}", err);
+        }
     }
+    Ok(())
 }
