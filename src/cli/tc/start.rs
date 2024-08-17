02f0b23f7d7fd3e397e1c78a2552cbc94214e858
Sahil Yeole <sahilyeole93@gmail.com>
2024-08-17 14:43:47 +0530

    watch for linked files also and add debounce delay
    Signed-off-by: Sahil Yeole <sahilyeole93@gmail.com>

--- src/cli/tc/start.rs
+++ src/cli/tc/start.rs
@@ -1,9 +1,10 @@
 use std::sync::Arc;
+use std::time::Duration;
 
 use anyhow::{Context, Result};
-use notify::{Config, RecommendedWatcher, RecursiveMode, Watcher};
+use notify::{Config, EventKind, FsEventWatcher, RecommendedWatcher, RecursiveMode, Watcher};
 use tokio::sync::broadcast;
-use tokio::task;
+use tokio::time::Instant;
 
 use super::helpers::log_endpoint_set;
 use crate::cli::fmt::Fmt;
@@ -71,23 +72,42 @@ async fn watch_files(
     };
 
     for path in file_paths {
-        if let Err(err) = watcher.watch(path.as_ref(), RecursiveMode::Recursive) {
+        if let Err(err) = watcher.watch(path.as_ref(), RecursiveMode::NonRecursive) {
             tracing::error!("Failed to watch path {:?}: {}", path, err);
         }
     }
 
+    // Debounce delay to prevent multiple server restarts on a single file change
+    // https://users.rust-lang.org/t/problem-with-notify-crate-v6-1/99877
+    let debounce_duration = Duration::from_secs(1);
+    let mut last_event_time = Instant::now() - debounce_duration;
     loop {
         match watch_rx.recv() {
             Ok(event) => {
                 if let Ok(event) = event {
-                    if let notify::EventKind::Modify(notify::event::ModifyKind::Data(
+                    if let EventKind::Modify(notify::event::ModifyKind::Data(
                         notify::event::DataChange::Content,
                     )) = event.kind
                     {
-                        if let Err(err) = tx.send(()) {
-                            tracing::error!("Failed to send the signal: {}", err);
+                        let now = Instant::now();
+                        if now.duration_since(last_event_time) >= debounce_duration {
+                            last_event_time = now;
+
+                            if let Err(err) = tx.send(()) {
+                                tracing::error!("Failed to send the signal: {}", err);
+                            }
+
+                            if let Err(e) = handle_server(
+                                &mut rx,
+                                file_paths,
+                                config_reader.clone(),
+                                &mut watcher,
+                            )
+                            .await
+                            {
+                                tracing::error!("Failed to handle server: {}", e);
+                            }
                         }
-                        let _ = handle_server(&mut rx, file_paths, config_reader.clone()).await;
                     }
                 }
             }
@@ -102,9 +122,21 @@ async fn handle_server(
     rx: &mut broadcast::Receiver<()>,
     file_paths: &[String],
     config_reader: Arc<ConfigReader>,
+    watcher: &mut FsEventWatcher,
 ) -> Result<()> {
     match config_reader.read_all(file_paths).await {
         Ok(config_module) => {
+            let links = config_module
+                .clone()
+                .links
+                .iter()
+                .map(|link| link.src.clone())
+                .collect::<Vec<_>>();
+            for link in links {
+                if let Err(err) = watcher.watch(link.as_ref(), RecursiveMode::NonRecursive) {
+                    tracing::error!("Failed to watch path {:?}: {}", link, err);
+                }
+            }
             log_endpoint_set(&config_module.extensions().endpoint_set);
             Fmt::log_n_plus_one(false, config_module.config());
 
