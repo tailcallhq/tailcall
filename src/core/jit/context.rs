9a50f131b72d677ecfa557c900d04578ef48dc91
laststylebender <43403528+laststylebender14@users.noreply.github.com>
2024-08-26 14:11:02 +0530

    fix(jit): add field aliases and args support (#2709)
    Co-authored-by: Panagiotis Karatakis <karatakis@users.noreply.github.com>
    Co-authored-by: Panagiotis <panagiotiskaratakis@gmail.com>
    Co-authored-by: Kiryl Mialeshka <8974488+meskill@users.noreply.github.com>

--- src/core/jit/context.rs
+++ src/core/jit/context.rs
@@ -2,6 +2,7 @@ use std::sync::{Arc, Mutex, MutexGuard};
 
 use async_graphql::{Name, ServerError};
 use async_graphql_value::ConstValue;
+use indexmap::IndexMap;
 
 use super::error::*;
 use super::{Field, Nested, OperationPlan, Positioned};
@@ -39,8 +40,8 @@ pub struct Context<'a, Input, Output> {
     request: &'a RequestContext<Input>,
 }
 impl<'a, Input: Clone, Output> Context<'a, Input, Output> {
-    pub fn new(field: &'a Field<Nested<Input>, Input>, env: &'a RequestContext<Input>) -> Self {
-        Self { value: None, args: None, field, request: env }
+    pub fn new(field: &'a Field<Nested<Input>, Input>, request: &'a RequestContext<Input>) -> Self {
+        Self { request, value: None, args: Self::build_args(field), field }
     }
 
     pub fn with_value_and_field(
@@ -48,19 +49,11 @@ impl<'a, Input: Clone, Output> Context<'a, Input, Output> {
         value: &'a Output,
         field: &'a Field<Nested<Input>, Input>,
     ) -> Self {
-        Self { args: None, value: Some(value), field, request: self.request }
-    }
-
-    pub fn with_args(&self, args: indexmap::IndexMap<&str, Input>) -> Self {
-        let mut map = indexmap::IndexMap::new();
-        for (key, value) in args {
-            map.insert(Name::new(key), value);
-        }
         Self {
-            value: self.value,
-            args: Some(map),
-            field: self.field,
             request: self.request,
+            args: Self::build_args(field),
+            value: Some(value),
+            field,
         }
     }
 
@@ -70,6 +63,26 @@ impl<'a, Input: Clone, Output> Context<'a, Input, Output> {
 
     pub fn field(&self) -> &Field<Nested<Input>, Input> {
         self.field
+    }
+
+    fn build_args(field: &Field<Nested<Input>, Input>) -> Option<IndexMap<Name, Input>> {
+        let mut arg_map = IndexMap::new();
+
+        for arg in field.args.iter() {
+            let name = arg.name.as_str();
+            let value = arg
+                .value
+                .clone()
+                // TODO: default value resolution should happen in the InputResolver
+                .or_else(|| arg.default_value.clone());
+            if let Some(value) = value {
+                arg_map.insert(Name::new(name), value);
+            } else if !arg.type_of.is_nullable() {
+                // TODO: throw error here
+                todo!()
+            }
+        }
+        Some(arg_map)
     }
 }
 
