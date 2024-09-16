6a550b34af30de6d6525ee112df1e418c333ce3d
laststylebender <ran.mahadik14@gmail.com>
2024-09-16 14:05:54 +0530

    - impl rules

--- src/core/jit/rules/query_complexity.rs
+++ src/core/jit/rules/query_complexity.rs
@@ -2,18 +2,21 @@ use crate::core::{jit::OperationPlan, valid::Valid};
 
 use super::Rule;
 
-pub struct QueryComplexity {
-    depth: usize,
-}
+pub struct QueryComplexity(usize);
 
 impl QueryComplexity {
     pub fn new(depth: usize) -> Self {
-        Self { depth }
+        Self(depth)
     }
 }
 
 impl Rule for QueryComplexity {
     fn validate(&self, plan: &OperationPlan<async_graphql_value::Value>) -> Valid<(), String> {
-        todo!()
+        let complexity = plan.calculate_complexity();
+        if complexity > self.0 {
+            Valid::fail("Query Complexity validation failed.".into())
+        } else {
+            Valid::succeed(())
+        }
     }
 }
--- src/core/jit/rules/query_depth.rs
+++ src/core/jit/rules/query_depth.rs
@@ -2,18 +2,21 @@ use crate::core::{jit::OperationPlan, valid::Valid};
 
 use super::Rule;
 
-pub struct QueryDepth {
-    depth: usize,
-}
+pub struct QueryDepth(usize);
 
 impl QueryDepth {
     pub fn new(depth: usize) -> Self {
-        Self { depth }
+        Self(depth)
     }
 }
 
 impl Rule for QueryDepth {
     fn validate(&self, plan: &OperationPlan<async_graphql_value::Value>) -> Valid<(), String> {
-        todo!()
+        let depth = plan.calculate_depth();
+        if depth > self.0 {
+            Valid::fail("Query Depth validation failed.".into())
+        } else {
+            Valid::succeed(())
+        }
     }
 }
