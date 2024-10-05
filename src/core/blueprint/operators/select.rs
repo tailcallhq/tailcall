44777afd9347012b6b441f897b3fd396cb74fcbf
Panagiotis Karatakis <karatakis@users.noreply.github.com>
2024-10-05 07:27:23 +0300

    feat: add a select field on HTTP and gRPC (#2962)
    Co-authored-by: Tushar Mathur <tusharmath@gmail.com>

---
+++ src/core/blueprint/operators/select.rs
@@ -0,0 +1,26 @@
+use serde_json::Value;
+
+use crate::core::blueprint::DynamicValue;
+use crate::core::ir::model::IR;
+use crate::core::valid::Valid;
+
+pub fn apply_select(input: (IR, &Option<Value>)) -> Valid<IR, String> {
+    let (mut ir, select) = input;
+
+    if let Some(select_value) = select {
+        let dynamic_value = match DynamicValue::try_from(select_value) {
+            Ok(dynamic_value) => dynamic_value.prepend("args"),
+            Err(e) => {
+                return Valid::fail_with(
+                    format!("syntax error when parsing `{:?}`", select),
+                    e.to_string(),
+                )
+            }
+        };
+
+        ir = ir.pipe(IR::Dynamic(dynamic_value));
+        Valid::succeed(ir)
+    } else {
+        Valid::succeed(ir)
+    }
+}
