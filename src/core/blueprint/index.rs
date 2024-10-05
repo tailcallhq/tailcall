ff1eb2420a5fbfd37883980f3f5e6e23e959685f
Sandipsinh Rathod <zotbysandip@gmail.com>
2024-10-05 01:54:14 -0400

    lint fixes

--- src/core/blueprint/index.rs
+++ src/core/blueprint/index.rs
@@ -48,7 +48,10 @@ impl Index {
         let def = self.map.get(type_name).map(|(def, _)| def);
 
         if let Some(Definition::Enum(enum_)) = def {
-            enum_.enum_values.iter().any(|v| v.name == value || v.alias.contains(value))
+            enum_
+                .enum_values
+                .iter()
+                .any(|v| v.name == value || v.alias.contains(value))
         } else {
             false
         }
