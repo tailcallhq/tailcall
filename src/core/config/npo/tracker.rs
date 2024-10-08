e7dbb9182f6345ed7be190eefcc695d8162bc4c7
meskill <8974488+meskill@users.noreply.github.com>
2024-10-08 09:03:36 +0000

    feat(npo): implement n+1 checks for entity resolvers

--- src/core/config/npo/tracker.rs
+++ src/core/config/npo/tracker.rs
@@ -7,16 +7,16 @@ use crate::core::config::Config;
 ///
 /// Represents a list of query paths that can issue a N + 1 query
 #[derive(Default, Debug, PartialEq)]
-pub struct QueryPath<'a>(Vec<Vec<&'a str>>);
+pub struct QueryPath(Vec<Vec<String>>);
 
-impl QueryPath<'_> {
+impl QueryPath {
     pub fn size(&self) -> usize {
         self.0.len()
     }
 }
 
-impl<'a> From<Chunk<Chunk<FieldName<'a>>>> for QueryPath<'a> {
-    fn from(chunk: Chunk<Chunk<FieldName<'a>>>) -> Self {
+impl<'a> From<Chunk<Chunk<ChunkName<'a>>>> for QueryPath {
+    fn from(chunk: Chunk<Chunk<ChunkName<'a>>>) -> Self {
         QueryPath(
             chunk
                 .as_vec()
@@ -25,7 +25,7 @@ impl<'a> From<Chunk<Chunk<FieldName<'a>>>> for QueryPath<'a> {
                     chunk
                         .as_vec()
                         .iter()
-                        .map(|field_name| field_name.as_str())
+                        .map(|chunk_name| chunk_name.to_string())
                         .collect()
                 })
                 .collect(),
@@ -33,7 +33,7 @@ impl<'a> From<Chunk<Chunk<FieldName<'a>>>> for QueryPath<'a> {
     }
 }
 
-impl<'a> Display for QueryPath<'a> {
+impl Display for QueryPath {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         let query_data: Vec<String> = self
             .0
@@ -91,9 +91,6 @@ impl<'a> FieldName<'a> {
     fn new(name: &'a str) -> Self {
         Self(name)
     }
-    fn as_str(self) -> &'a str {
-        self.0
-    }
 }
 impl Display for FieldName<'_> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
@@ -101,11 +98,26 @@ impl Display for FieldName<'_> {
     }
 }
 
+#[derive(Clone, Copy, Debug, Hash, PartialEq, Eq)]
+enum ChunkName<'a> {
+    Field(FieldName<'a>),
+    Entity(TypeName<'a>),
+}
+
+impl Display for ChunkName<'_> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            ChunkName::Field(field_name) => write!(f, "{}", field_name),
+            ChunkName::Entity(type_name) => write!(f, "__entity({})", type_name),
+        }
+    }
+}
+
 /// A module that tracks the query paths that can issue a N + 1 calls to
 /// upstream.
 pub struct PathTracker<'a> {
     config: &'a Config,
-    cache: HashMap<(TypeName<'a>, bool), Chunk<Chunk<FieldName<'a>>>>,
+    cache: HashMap<(TypeName<'a>, bool), Chunk<Chunk<ChunkName<'a>>>>,
 }
 
 impl<'a> PathTracker<'a> {
@@ -116,11 +128,11 @@ impl<'a> PathTracker<'a> {
     #[allow(clippy::too_many_arguments)]
     fn iter(
         &mut self,
-        path: Chunk<FieldName<'a>>,
+        path: Chunk<ChunkName<'a>>,
         type_name: TypeName<'a>,
         is_list: bool,
-        visited: HashSet<(TypeName<'a>, FieldName<'a>)>,
-    ) -> Chunk<Chunk<FieldName<'a>>> {
+        visited: HashSet<(TypeName<'a>, ChunkName<'a>)>,
+    ) -> Chunk<Chunk<ChunkName<'a>>> {
         if let Some(chunks) = self.cache.get(&(type_name, is_list)) {
             return chunks.clone();
         }
@@ -128,14 +140,15 @@ impl<'a> PathTracker<'a> {
         let mut chunks = Chunk::new();
         if let Some(type_of) = self.config.find_type(type_name.as_str()) {
             for (name, field) in type_of.fields.iter() {
-                let field_name = FieldName::new(name);
-                let path = path.clone().append(field_name);
-                if !visited.contains(&(type_name, field_name)) {
+                let chunk_name = ChunkName::Field(FieldName::new(name));
+                let path = path.clone().append(chunk_name);
+                if !visited.contains(&(type_name, chunk_name)) {
                     if is_list && field.has_resolver() && !field.has_batched_resolver() {
-                        chunks = chunks.append(path.clone());
+                        chunks = chunks.append(path);
                     } else {
                         let mut visited = visited.clone();
-                        visited.insert((type_name, field_name));
+
+                        visited.insert((type_name, chunk_name));
                         let is_list = is_list | field.type_of.is_list();
                         chunks = chunks.concat(self.iter(
                             path,
@@ -148,12 +161,14 @@ impl<'a> PathTracker<'a> {
             }
         }
 
-        self.cache.insert((type_name, is_list), chunks.clone());
+        // self.cache.insert((type_name, is_list), chunks.clone());
         chunks
     }
 
-    fn find_chunks(&mut self) -> Chunk<Chunk<FieldName<'a>>> {
-        match &self.config.schema.query {
+    fn find_chunks(&mut self) -> Chunk<Chunk<ChunkName<'a>>> {
+        // let mut visited = HashSet::new();
+
+        let mut chunks = match &self.config.schema.query {
             None => Chunk::new(),
             Some(query) => self.iter(
                 Chunk::new(),
@@ -161,10 +176,33 @@ impl<'a> PathTracker<'a> {
                 false,
                 HashSet::new(),
             ),
+        };
+
+        for (type_name, type_of) in &self.config.types {
+            if type_of.has_resolver() {
+                let chunk_name = ChunkName::Entity(TypeName(type_name.as_str()));
+                let chunk = Chunk::new().append(chunk_name);
+                // entity resolver are used to fetch multiple instances at once
+                // and therefore the resolver itself should be batched to avoid n + 1
+                if type_of.has_batched_resolver() {
+                    // if batched resolver is present traverse inner fields
+                    chunks = chunks.concat(self.iter(
+                        chunk,
+                        TypeName::new(type_name.as_str()),
+                        // entities are basically returning list of data
+                        true,
+                        HashSet::new(),
+                    ));
+                } else {
+                    chunks = chunks.append(chunk);
+                }
+            }
         }
+
+        chunks
     }
 
-    pub fn find(mut self) -> QueryPath<'a> {
+    pub fn find(mut self) -> QueryPath {
         QueryPath::from(self.find_chunks())
     }
 }
@@ -240,8 +278,14 @@ mod tests {
     #[test]
     fn test_multiple_keys() {
         let config = include_config!("fixtures/multiple-keys.graphql").unwrap();
-        let actual = config.n_plus_one();
 
-        insta::assert_snapshot!(actual);
+        assert_n_plus_one!(config);
+    }
+
+    #[test]
+    fn test_entity_resolver() {
+        let config = include_config!("fixtures/entity-resolver.graphql").unwrap();
+
+        assert_n_plus_one!(config);
     }
 }
