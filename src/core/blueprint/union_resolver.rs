6e20d0c2e7afe0aa18795ae21cc8748470680605
Panagiotis <panagiotiskaratakis@gmail.com>
2024-10-12 11:02:00 +0300

    feat(discriminator): Add Keyed and TypeField discriminators * prepare code

--- src/core/blueprint/union_resolver.rs
+++ src/core/blueprint/union_resolver.rs
@@ -22,7 +22,7 @@ fn compile_union_resolver(
     .and_then(|types| {
         let types: Vec<_> = types.into_iter().collect();
 
-        Discriminator::new(union_name, &types)
+        Discriminator::new(union_name, &types, crate::core::ir::DiscriminatorMode::Probability)
     })
 }
 
