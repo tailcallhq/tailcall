6f1901525c53f6becc719c179b39ced43a4c90d2
Tushar Mathur <tusharmath@gmail.com>
2024-10-20 09:50:20 +0530

    refactor: simplify transformation

--- src/core/jit/transform/skip.rs
+++ src/core/jit/transform/skip.rs
@@ -25,30 +25,18 @@ where
     type Error = Error;
 
     fn transform(&self, plan: Self::Value) -> Valid<Self::Value, Self::Error> {
-        // Drop all the fields that are not needed
-        let plan = filter_skipped(plan, self.variables);
+        let mut plan = plan;
+        let variables: &Variables<Var> = self.variables;
+        skip(&mut plan.selection, variables);
 
         Valid::succeed(plan)
     }
 }
 
-/// Remove fields which are skipped
-pub fn filter_skipped<Input, Var: for<'b> JsonLike<'b>>(
-    mut op: OperationPlan<Input>,
-    variables: &Variables<Var>,
-) -> OperationPlan<Input> {
-    filter_skipped_fields(&mut op.selection, variables);
-
-    op
-}
-
-// TODO: review and rename
-fn filter_skipped_fields<Input, Var: for<'b> JsonLike<'b>>(
-    fields: &mut Vec<Field<Input>>,
-    vars: &Variables<Var>,
-) {
+/// Drops all the fields that are not needed based on the set variables
+fn skip<Input, Var: for<'b> JsonLike<'b>>(fields: &mut Vec<Field<Input>>, vars: &Variables<Var>) {
     fields.retain(|f| !f.skip(vars));
     for field in fields {
-        filter_skipped_fields(&mut field.selection, vars);
+        skip(&mut field.selection, vars);
     }
 }
