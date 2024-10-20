ffa049c6759e406e89e32fb279c9e2c39cb70f23
Tushar Mathur <tusharmath@gmail.com>
2024-10-20 09:19:03 +0530

    refactor: move check_const logic into transform

--- src/core/jit/transform/check_const.rs
+++ src/core/jit/transform/check_const.rs
@@ -1,5 +1,8 @@
 use std::marker::PhantomData;
 
+use async_graphql::dynamic;
+
+use crate::core::ir::model::IR;
 use crate::core::jit::OperationPlan;
 use crate::core::valid::Valid;
 use crate::core::Transform;
@@ -11,13 +14,30 @@ impl<A> CheckConst<A> {
     }
 }
 
+/// Checks if the IR will always evaluate to a Constant Value
+pub fn is_const(ir: &IR) -> bool {
+    match ir {
+        IR::Dynamic(dynamic_value) => dynamic_value.is_const(),
+        IR::IO(_) => false,
+        IR::Cache(_) => false,
+        IR::Path(ir, _) => is_const(ir),
+        IR::ContextPath(_) => false,
+        IR::Protect(ir) => is_const(ir),
+        IR::Map(map) => is_const(&*map.input),
+        IR::Pipe(ir, ir1) => is_const(ir) && is_const(ir1),
+        IR::Discriminate(_, ir) => is_const(ir),
+        IR::Entity(hash_map) => hash_map.values().all(|ir| is_const(ir)),
+        IR::Service(_) => true,
+    }
+}
+
 impl<A> Transform for CheckConst<A> {
     type Value = OperationPlan<A>;
     type Error = ();
 
     fn transform(&self, mut plan: Self::Value) -> Valid<Self::Value, Self::Error> {
         let is_const = plan.as_flat().all(|field| match field.ir {
-            Some(ref ir) => ir.is_const(),
+            Some(ref ir) => is_const(ir),
             None => true,
         });
 
