fb80970464c3cf83d7b4e214c12555deb7def49e
Sandipsinh Rathod <zotbysandip@gmail.com>
2024-10-21 00:49:10 -0400

    replace `Base URL` with `URL` in errata

--- src/core/errata.rs
+++ src/core/errata.rs
@@ -306,7 +306,7 @@ mod tests {
     #[test]
     fn test_title_trace_caused_by() {
         let error = Errata::new("Configuration Error").caused_by(vec![Errata::new(
-            "Base URL needs to be specified",
+            "URL needs to be specified",
         )
         .trace(vec![
             "User".into(),
@@ -317,7 +317,7 @@ mod tests {
 
         let expected = r"|Configuration Error
                      |Caused by:
-                     |  • Base URL needs to be specified [at User.posts.@http.url]"
+                     |  • URL needs to be specified [at User.posts.@http.url]"
             .strip_margin();
 
         assert_eq!(error.to_string(), expected);
@@ -326,19 +326,19 @@ mod tests {
     #[test]
     fn test_title_trace_multiple_caused_by() {
         let error = Errata::new("Configuration Error").caused_by(vec![
-            Errata::new("Base URL needs to be specified").trace(vec![
+            Errata::new("URL needs to be specified").trace(vec![
                 "User".into(),
                 "posts".into(),
                 "@http".into(),
                 "url".into(),
             ]),
-            Errata::new("Base URL needs to be specified").trace(vec![
+            Errata::new("URL needs to be specified").trace(vec![
                 "Post".into(),
                 "users".into(),
                 "@http".into(),
                 "url".into(),
             ]),
-            Errata::new("Base URL needs to be specified")
+            Errata::new("URL needs to be specified")
                 .description("Set `url` in @http or @server directives".into())
                 .trace(vec![
                     "Query".into(),
@@ -346,7 +346,7 @@ mod tests {
                     "@http".into(),
                     "url".into(),
                 ]),
-            Errata::new("Base URL needs to be specified").trace(vec![
+            Errata::new("URL needs to be specified").trace(vec![
                 "Query".into(),
                 "posts".into(),
                 "@http".into(),
@@ -356,10 +356,10 @@ mod tests {
 
         let expected = r"|Configuration Error
                      |Caused by:
-                     |  • Base URL needs to be specified [at User.posts.@http.url]
-                     |  • Base URL needs to be specified [at Post.users.@http.url]
-                     |  • Base URL needs to be specified: Set `url` in @http or @server directives [at Query.users.@http.url]
-                     |  • Base URL needs to be specified [at Query.posts.@http.url]"
+                     |  • URL needs to be specified [at User.posts.@http.url]
+                     |  • URL needs to be specified [at Post.users.@http.url]
+                     |  • URL needs to be specified: Set `url` in @http or @server directives [at Query.users.@http.url]
+                     |  • URL needs to be specified [at Query.posts.@http.url]"
             .strip_margin();
 
         assert_eq!(error.to_string(), expected);
@@ -367,14 +367,14 @@ mod tests {
 
     #[test]
     fn test_from_validation() {
-        let cause = Cause::new("Base URL needs to be specified")
+        let cause = Cause::new("URL needs to be specified")
             .description("Set `url` in @http or @server directives")
             .trace(vec!["Query", "users", "@http", "url"]);
         let valid = ValidationError::from(cause);
         let error = Errata::from(valid);
         let expected = r"|Invalid Configuration
                      |Caused by:
-                     |  • Base URL needs to be specified: Set `url` in @http or @server directives [at Query.users.@http.url]"
+                     |  • URL needs to be specified: Set `url` in @http or @server directives [at Query.users.@http.url]"
             .strip_margin();
 
         assert_eq!(error.to_string(), expected);
