dca475ec94e877f19648d07ab0df71ea33d15e47
dekkku <abhigaikwad147@gmail.com>
2024-11-20 23:37:08 +0530

    remove unwraps

--- src/cli/update_checker.rs
+++ src/cli/update_checker.rs
@@ -3,7 +3,7 @@ use std::path::PathBuf;
 use chrono::Utc;
 use colored::Colorize;
 use ctrlc::set_handler;
-use dirs::cache_dir;
+use dirs::{cache_dir, config_dir};
 use tailcall_version::VERSION;
 use update_informer::{registry, Check};
 use which::which;
@@ -15,11 +15,10 @@ enum InstallationMethod {
     Direct,
 }
 
-fn get_state_file_path() -> Option<PathBuf> {
-    let mut config_path = cache_dir()?; // Get the appropriate config directory
-    config_path.push("tailcall"); // Your application name
-    config_path.push(".state");
-    Some(config_path)
+fn get_state_file_path() -> PathBuf {
+    let mut config_path = cache_dir().unwrap_or_else(|| config_dir().unwrap_or_default());
+    config_path.push(".tailcall");
+    config_path
 }
 
 fn get_installation_method() -> InstallationMethod {
@@ -46,6 +45,41 @@ fn get_installation_method() -> InstallationMethod {
     InstallationMethod::Direct
 }
 
+/// Checks if more than 24 hours have passed since the last update check.
+fn should_show_update_message() -> bool {
+    let state_file_path = get_state_file_path();
+    let epoch_time_now = Utc::now().timestamp();
+    let show_update_message = match std::fs::read_to_string(&state_file_path) {
+        Ok(data) => match data.trim().parse::<i64>() {
+            Ok(epoch_time) => (epoch_time_now - epoch_time) > 24 * 60 * 60,
+            Err(_) => true,
+        },
+        Err(_) => true,
+    };
+    show_update_message
+}
+
+/// Updates the state file with the current timestamp.
+fn update_version_check_time() {
+    let state_file_path = get_state_file_path();
+    let epoch_time_now = Utc::now().timestamp();
+    let path_exists = std::fs::metadata(state_file_path.clone()).is_ok();
+    if !path_exists {
+        if let Some(parent) = state_file_path.parent() {
+            // it's okay if it's fails to create the directory. We'll try again next time.
+            let _ = std::fs::create_dir_all(parent).is_ok();
+        } else {
+            let mut state_file_path = state_file_path.clone();
+            state_file_path.pop(); // remove the file name, so that we can create directories.
+                                   // it's okay if it's fails to create the directory. We'll try again next time.
+            let _ = std::fs::create_dir_all(state_file_path.clone()).is_ok();
+        }
+    }
+
+    // it's okay if it's fails to write. We'll try again next time.
+    let _ = std::fs::write(state_file_path, epoch_time_now.to_string()).is_ok();
+}
+
 pub async fn check_for_update() {
     tokio::task::spawn_blocking(move || {
         if VERSION.is_dev() {
@@ -53,18 +87,9 @@ pub async fn check_for_update() {
             return;
         }
 
-        let state_file_path = get_state_file_path().unwrap();
-        let epoch_time_now = Utc::now().timestamp();
-        let show_update_message = match std::fs::read_to_string(&state_file_path) {
-            Ok(data) => match data.trim().parse::<i64>() {
-                Ok(epoch_time) => (epoch_time_now - epoch_time) > 24 * 60 * 60,
-                Err(_) => true,
-            },
-            Err(_) => true,
-        };
-
-        if !show_update_message {
-            // if it's been less than 24 hours since the last check, don't show or look for the update.
+        if !should_show_update_message() {
+            // if it's been less than 24 hours since the last check, don't show or look for
+            // the update.
             return;
         }
 
@@ -73,50 +98,40 @@ pub async fn check_for_update() {
         let informer = update_informer::new(registry::GitHub, name, VERSION.as_str());
 
         if let Some(latest_version) = informer.check_version().ok().flatten() {
-            let github_release_url =
-                format!("https://github.com/{name}/releases/tag/{latest_version}",);
-            let installation_method = get_installation_method();
-
-            let path_exists = std::fs::metadata(state_file_path.clone()).is_ok();
-            if !path_exists {
-                std::fs::create_dir_all(state_file_path.parent().unwrap()).unwrap();
-            }
-
-            if let Ok(_) = std::fs::write(state_file_path, epoch_time_now.to_string()) {}
-
-            if true {
-                set_handler(move || {
-                    tracing::warn!(
-                        "{}",
-                        format!(
-                            "A new release of tailcall is available: {} {} {}",
-                            VERSION.as_str().cyan(),
-                            "➜".white(),
-                            latest_version.to_string().cyan()
+            update_version_check_time();
+            let _ = set_handler(move || {
+                let github_release_url =
+                    format!("https://github.com/{name}/releases/tag/{latest_version}",);
+                let installation_method = get_installation_method();
+                tracing::warn!(
+                    "{}",
+                    format!(
+                        "A new release of tailcall is available: {} {} {}",
+                        VERSION.as_str().cyan(),
+                        "➜".white(),
+                        latest_version.to_string().cyan()
+                    )
+                    .yellow()
+                );
+                match installation_method {
+                    InstallationMethod::Npx => tracing::warn!(
+                        "You're running an outdated version, run: npx @tailcallhq/tailcall@latest"
+                    ),
+                    InstallationMethod::Npm => {
+                        tracing::warn!("To upgrade, run: npm update -g @tailcallhq/tailcall")
+                    }
+                    InstallationMethod::Brew => {
+                        tracing::warn!("To upgrade, run: brew upgrade tailcall")
+                    }
+                    InstallationMethod::Direct => {
+                        tracing::warn!(
+                            "Please update by downloading the latest release from GitHub"
                         )
-                        .yellow()
-                    );
-                    std::process::exit(exitcode::CONFIG);
-                })
-                .expect("Error setting Ctrl-C handler");
-                return;
-            }
-
-            match installation_method {
-                InstallationMethod::Npx => tracing::warn!(
-                    "You're running an outdated version, run: npx @tailcallhq/tailcall@latest"
-                ),
-                InstallationMethod::Npm => {
-                    tracing::warn!("To upgrade, run: npm update -g @tailcallhq/tailcall")
+                    }
                 }
-                InstallationMethod::Brew => {
-                    tracing::warn!("To upgrade, run: brew upgrade tailcall")
-                }
-                InstallationMethod::Direct => {
-                    tracing::warn!("Please update by downloading the latest release from GitHub")
-                }
-            }
-            tracing::warn!("{}", github_release_url.yellow());
+                tracing::warn!("{}", github_release_url.yellow());
+                std::process::exit(exitcode::OK);
+            });
         }
     });
 }
