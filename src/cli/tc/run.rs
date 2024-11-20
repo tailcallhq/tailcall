d65a67635fe0793ff26cef5c311ef812c0215874
dekkku <abhigaikwad147@gmail.com>
2024-11-21 00:32:13 +0530

    check if RC files are valid or not.

--- src/cli/tc/run.rs
+++ src/cli/tc/run.rs
@@ -1,8 +1,10 @@
+use std::path::Path;
+
 use anyhow::Result;
 use clap::Parser;
 use dotenvy::dotenv;
 
-use super::helpers::TRACKER;
+use super::helpers::{TAILCALL_RC, TAILCALL_RC_SCHEMA, TRACKER};
 use super::{check, gen, init, start};
 use crate::cli::command::{Cli, Command};
 use crate::cli::{self, update_checker};
@@ -37,9 +39,11 @@ pub async fn run() -> Result<()> {
 async fn run_command(cli: Cli, config_reader: ConfigReader, runtime: TargetRuntime) -> Result<()> {
     match cli.command {
         Command::Start { file_paths } => {
+            validate_rc_config_files(runtime.clone(), &file_paths).await;
             start::start_command(file_paths, &config_reader).await?;
         }
         Command::Check { file_paths, n_plus_one_queries, schema, format } => {
+            validate_rc_config_files(runtime.clone(), &file_paths).await;
             check::check_command(
                 check::CheckParams { file_paths, n_plus_one_queries, schema, format, runtime },
                 &config_reader,
@@ -54,4 +58,58 @@ async fn run_command(cli: Cli, config_reader: ConfigReader, runtime: TargetRunti
         }
     }
     Ok(())
+}
+
+async fn validate_rc_config_files(runtime: TargetRuntime, file_paths: &[String]) {
+    // base config files.
+    let tailcallrc = include_str!("../../../generated/.tailcallrc.graphql");
+    let tailcallrc_json = include_str!("../../../generated/.tailcallrc.schema.json");
+
+    for path in file_paths {
+        let tailcall_rc_path = Path::new(path).join(TAILCALL_RC).display().to_string();
+        let tailcall_rc_schema_path = Path::new(path)
+            .join(TAILCALL_RC_SCHEMA)
+            .display()
+            .to_string();
+
+        // check if rc files already exist or not.
+        if std::fs::metadata(tailcall_rc_path.clone()).is_ok()
+            || std::fs::metadata(tailcall_rc_schema_path.clone()).is_ok()
+        {
+            let mut outdated_files = Vec::with_capacity(2);
+            if let Ok(content) = runtime.file.read(&tailcall_rc_path).await {
+                if content != tailcallrc {
+                    outdated_files.push(".tailcallrc.graphql");
+                }
+            } else {
+                // If unable to read the file, consider it outdated
+                outdated_files.push(".tailcallrc.graphql");
+            }
+            // Check .tailcallrc.schema.json
+            if let Ok(content) = runtime.file.read(&tailcall_rc_schema_path).await {
+                if content != tailcallrc_json {
+                    outdated_files.push(".tailcallrc.schema.json");
+                }
+            } else {
+                // If unable to read the file, consider it outdated
+                outdated_files.push(".tailcallrc.schema.json");
+            }
+
+            if !outdated_files.is_empty() {
+                let message = if outdated_files.len() == 2 {
+                    format!(
+                        "[{}, {}] is outdated, reinitialize using tailcall init.",
+                        outdated_files[0], outdated_files[1]
+                    )
+                } else {
+                    format!(
+                        "[{}] is outdated, reinitialize using tailcall init.",
+                        outdated_files[0]
+                    )
+                };
+                tracing::warn!(message);
+                return;
+            }
+        }
+    }
 }
