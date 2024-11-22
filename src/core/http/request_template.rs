a583192258af0fa85e2940c79cbe3f70475c0b13
laststylebender <ran.mahadik14@gmail.com>
2024-11-22 16:50:54 +0530

    - lint update and snap update

--- src/core/http/request_template.rs
+++ src/core/http/request_template.rs
@@ -143,15 +143,15 @@ impl RequestTemplate {
             let rendered_body = if batch_size > 0 {
                 // TODO: this is very expensive operation, think about better way to do
                 // this.
-                // 1. we convert the avail value into serde_json::Value and then expand it
-                //    with expander
+                // 1. we convert the avail value into serde_json::Value and then expand it with
+                //    expander
                 // 2. then we convert it back to string and then to mustache template
                 // 3. then we render it with context.
                 Expander::expand(body_path, batch_size)?.render(ctx)?
             } else {
                 body_path.render(ctx)?
             };
-            
+
             match &self.encoding {
                 Encoding::ApplicationJson => {
                     req.body_mut().replace(rendered_body.into());
@@ -159,7 +159,8 @@ impl RequestTemplate {
                 Encoding::ApplicationXWwwFormUrlencoded => {
                     // TODO: this is a performance bottleneck
                     // We first encode everything to string and then back to form-urlencoded
-                    let form_data = match serde_json::from_str::<serde_json::Value>(&rendered_body) {
+                    let form_data = match serde_json::from_str::<serde_json::Value>(&rendered_body)
+                    {
                         Ok(deserialized_data) => serde_urlencoded::to_string(deserialized_data)?,
                         Err(_) => rendered_body,
                     };
