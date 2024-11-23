12ad51a5e1aa71ecf1ed7123e1443afc370db3f9
Tushar Mathur <tusharmath@gmail.com>
2024-11-22 17:21:25 -0800

    simplify function implementations

--- src/core/jit/transform/auth_planer.rs
+++ src/core/jit/transform/auth_planer.rs
@@ -22,71 +22,65 @@ impl<A> Transform for AuthPlaner<A> {
     type Error = Infallible;
 
     fn transform(&self, mut plan: Self::Value) -> Valid<Self::Value, Self::Error> {
-        let mut before = plan.before;
+        let mut before = Vec::new();
 
         plan.selection = plan
             .selection
             .into_iter()
-            .map(|field| extract_ir_protect(&mut before, field))
+            .map(|field| drop_protect_field(&mut before, field))
             .collect();
+        let ir = before.into_iter();
 
-        Valid::succeed(OperationPlan { before, ..plan })
+        plan.before = match plan.before {
+            Some(before) => Some(ir.fold(before, |a, b| a.pipe(b))),
+            None => ir.reduce(|a, b| a.pipe(b)),
+        };
+        Valid::succeed(plan)
     }
 }
 
 /// Used to recursively update the field ands its selections to remove
 /// IR::Protected
-fn extract_ir_protect<A>(before: &mut Option<IR>, mut field: Field<A>) -> Field<A> {
-    if let Some(ir) = field.ir {
-        let (new_ir, is_protected) = detect_and_remove_ir_protect(ir);
+fn drop_protect_field<A>(before: &mut Vec<IR>, mut field: Field<A>) -> Field<A> {
+    if let Some(mut ir) = field.ir {
+        let is_protected = drop_protect_ir(&mut ir);
 
         field.selection = field
             .selection
             .into_iter()
-            .map(|selection_field| extract_ir_protect(before, selection_field))
+            .map(|field| drop_protect_field(before, field))
             .collect();
 
         if is_protected {
-            let ir = IR::Protect(Box::new(IR::Dynamic(DynamicValue::Value(Default::default()))));
-            *before = Some(ir);
+            let ir = IR::Protect(Box::new(IR::Dynamic(DynamicValue::Value(
+                Default::default(),
+            ))));
+            before.push(ir);
         }
 
-        field.ir = Some(new_ir);
+        field.ir = Some(ir);
     }
     field
 }
 
-/// This function modifies an IR pipe chain by detecting and removing any
+/// This function modifies an IR chain by detecting and removing any
 /// instances of IR::Protect from the chain. Returns `true` when it modifies the
 /// IR.
-pub fn detect_and_remove_ir_protect(ir: IR) -> (IR, bool) {
+pub fn drop_protect_ir(ir: &mut IR) -> bool {
     match ir {
-        IR::Dynamic(dynamic_value) => (IR::Dynamic(dynamic_value), false),
-        IR::IO(io) => (IR::IO(io), false),
-        IR::Cache(cache) => (IR::Cache(cache), false),
-        IR::Path(inner_ir, vec) => {
-            let (new_ir, removed) = detect_and_remove_ir_protect(*inner_ir);
-            (IR::Path(Box::new(new_ir), vec), removed)
-        }
-        IR::ContextPath(vec) => (IR::ContextPath(vec), false),
+        IR::Dynamic(_) => false,
+        IR::IO(_) => false,
+        IR::Cache(_) => false,
+        IR::Path(ir, _) => drop_protect_ir(ir),
+        IR::ContextPath(_) => false,
         IR::Protect(inner_ir) => {
-            let (new_ir, _) = detect_and_remove_ir_protect(*inner_ir);
-            (new_ir, true)
+            *ir = *inner_ir.clone();
+            true
         }
-        IR::Map(map) => (IR::Map(map), false),
-        IR::Pipe(ir1, ir2) => {
-            let (new_ir1, removed1) = detect_and_remove_ir_protect(*ir1);
-            let (new_ir2, removed2) = detect_and_remove_ir_protect(*ir2);
-            (
-                IR::Pipe(Box::new(new_ir1), Box::new(new_ir2)),
-                removed1 || removed2,
-            )
-        }
-        IR::Discriminate(discriminator, inner_ir) => {
-            let (new_ir, removed) = detect_and_remove_ir_protect(*inner_ir);
-            (IR::Discriminate(discriminator, Box::new(new_ir)), removed)
-        }
-        IR::Entity(hash_map) => (IR::Entity(hash_map), false),
-        IR::Service(service) => (IR::Service(service), false),
+        IR::Map(_) => false,
+        IR::Pipe(ir1, ir2) => drop_protect_ir(ir1) || drop_protect_ir(ir2),
+        IR::Discriminate(_, ir) => drop_protect_ir(ir),
+        IR::Entity(_) => false,
+        IR::Service(_) => false,
     }
 }
