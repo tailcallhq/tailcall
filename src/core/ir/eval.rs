7a53b7969490ba6787d7c9a4e6fc4845377d2dbd
meskill <8974488+meskill@users.noreply.github.com>
2024-11-25 15:48:14 +0000

    Revert "fix: move tests closer to implementation"
    This reverts commit b269c133be1d2a649fe29ad6ca1cf32fc98def7b.

--- src/core/ir/eval.rs
+++ src/core/ir/eval.rs
@@ -167,3 +167,72 @@ impl IR {
         })
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    mod merge {
+        use serde_json::json;
+
+        use super::*;
+        use crate::core::blueprint::{Blueprint, DynamicValue};
+        use crate::core::http::RequestContext;
+        use crate::core::ir::EmptyResolverContext;
+
+        #[tokio::test]
+        async fn test_const_values() {
+            let a = DynamicValue::Value(
+                ConstValue::from_json(json!({
+                    "a": 1,
+                    "c": {
+                        "ca": false
+                    }
+                }))
+                .unwrap(),
+            );
+
+            let b = DynamicValue::Value(
+                ConstValue::from_json(json!({
+                    "b": 2,
+                    "c": {
+                        "cb": 23
+                    }
+                }))
+                .unwrap(),
+            );
+
+            let c = DynamicValue::Value(
+                ConstValue::from_json(json!({
+                    "c" : {
+                        "ca": true,
+                        "cc": [1, 2]
+                    },
+                    "d": "additional"
+                }))
+                .unwrap(),
+            );
+
+            let ir = IR::Merge([a, b, c].into_iter().map(IR::Dynamic).collect());
+            let runtime = crate::cli::runtime::init(&Blueprint::default());
+            let req_ctx = RequestContext::new(runtime);
+            let res_ctx = EmptyResolverContext {};
+            let mut eval_ctx = EvalContext::new(&req_ctx, &res_ctx);
+
+            let actual = ir.eval(&mut eval_ctx).await.unwrap();
+            let expected = ConstValue::from_json(json!({
+                "a": 1,
+                "b": 2,
+                "c": {
+                    "ca": true,
+                    "cb": 23,
+                    "cc": [1, 2]
+                },
+                "d": "additional"
+            }))
+            .unwrap();
+
+            assert_eq!(actual, expected);
+        }
+    }
+}
