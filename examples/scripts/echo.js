1fc2d7258f5657d2b7bbe64573df28a581e4d760
laststylebender <ran.mahadik14@gmail.com>
2024-11-25 14:09:52 +0530

    - add example for onResponseBody.

--- examples/scripts/echo.js
+++ examples/scripts/echo.js
@@ -6,3 +6,9 @@ function hello(val) {
   let json = JSON.parse(val)
   return JSON.stringify(json.id)
 }
+
+function onResponse(response) {
+  const parsedValue = JSON.parse(response);
+  parsedValue.name += " - modified by JS";
+  return JSON.stringify(parsedValue);
+}
