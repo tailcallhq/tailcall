a34d149b9a2374b9797daf59625b5965b70e19a7
laststylebender <ran.mahadik14@gmail.com>
2024-11-26 18:20:35 +0530

    - fix bug

--- src/core/jit/transform/defer_planner.rs
+++ src/core/jit/transform/defer_planner.rs
@@ -1,4 +1,4 @@
-use std::{convert::Infallible, marker::PhantomData};
+use std::{convert::Infallible, fmt::Debug, marker::PhantomData};
 
 use tailcall_valid::Valid;
 
@@ -37,16 +37,32 @@ fn move_deferred_fields<A: Clone>(field: &mut Field<A>) -> Vec<Field<A>> {
     deferred_fields
 }
 
-impl<A: Clone> Transform for DeferPlanner<A> {
+impl<A: Clone + Debug> Transform for DeferPlanner<A> {
     type Value = OperationPlan<A>;
     type Error = Infallible;
 
     fn transform(&self, mut plan: Self::Value) -> Valid<Self::Value, Self::Error> {
         let mut deferred_fields = Vec::new();
         for field in plan.selection.iter_mut() {
+            match field.ir {
+                Some(IR::Deferred { .. }) => {
+                    deferred_fields.push(field.clone());
+                }
+                _ => {}
+            }
+
             deferred_fields.extend(move_deferred_fields(field));
         }
+
+        plan.selection.retain(|f| {
+            f.ir.as_ref()
+                .map_or(true, |ir| !matches!(ir, IR::Deferred { .. }))
+        });
+
         plan.deferred_fields = deferred_fields;
+
+        println!("{:?}", plan.deferred_fields);
+
         Valid::succeed(plan)
     }
 }
--- src/core/jit/transform/wrap_defer.rs
+++ src/core/jit/transform/wrap_defer.rs
@@ -3,24 +3,24 @@ use std::{convert::Infallible, marker::PhantomData};
 use tailcall_valid::Valid;
 
 use crate::core::{
-    counter::{Count, Counter}, ir::model::{IrId, IR}, jit::{Field, OperationPlan}, Transform
+    counter::{Count, Counter},
+    ir::model::{IrId, IR},
+    jit::{Field, OperationPlan},
+    Transform,
 };
 
-pub struct WrapDefer<A>{
+pub struct WrapDefer<A> {
     _marker: PhantomData<A>,
     defer_id: Counter<usize>,
 }
 
 impl<A> WrapDefer<A> {
     pub fn new() -> Self {
-        Self {
-            _marker: PhantomData,
-            defer_id: Counter::new(0),
-        }
+        Self { _marker: PhantomData, defer_id: Counter::new(0) }
     }
     /// goes through selection and finds out IR's that needs to be deferred.
     #[inline]
-    fn detect_and_wrap(&self,field: &mut Field<A>, path: &mut Vec<String>) {
+    fn detect_and_wrap(&self, field: &mut Field<A>, path: &mut Vec<String>) {
         path.push(field.output_name.clone());
         for selection in field.selection.iter_mut() {
             if let Some(ir) = std::mem::take(&mut selection.ir) {
@@ -29,9 +29,12 @@ impl<A> WrapDefer<A> {
                     .iter()
                     .find(|d| d.name == "defer")
                     .is_some()
-                    && field.ir.is_some()
                 {
-                    IR::Deferred { ir: Box::new(ir), path: vec![], id: IrId::new(self.defer_id.next()) }
+                    IR::Deferred {
+                        ir: Box::new(ir),
+                        path: vec![],
+                        id: IrId::new(self.defer_id.next()),
+                    }
                 } else {
                     ir
                 };
@@ -43,17 +46,27 @@ impl<A> WrapDefer<A> {
 
         path.pop();
     }
-
 }
-
 
 impl<A> Transform for WrapDefer<A> {
     type Value = OperationPlan<A>;
     type Error = Infallible;
     fn transform(&self, mut plan: Self::Value) -> Valid<Self::Value, Self::Error> {
-        plan.selection
-            .iter_mut()
-            .for_each(|f| self.detect_and_wrap(f, &mut vec![]));
+        plan.selection.iter_mut().for_each(|f| {
+            if let Some(ir) = std::mem::take(&mut f.ir) {
+                let ir = if f.directives.iter().find(|d| d.name == "defer").is_some() {
+                    IR::Deferred {
+                        ir: Box::new(ir),
+                        path: vec![],
+                        id: IrId::new(self.defer_id.next()),
+                    }
+                } else {
+                    ir
+                };
+                f.ir = Some(ir);
+            }
+            self.detect_and_wrap(f, &mut vec![])
+        });
         Valid::succeed(plan)
     }
 }
