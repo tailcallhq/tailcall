58d56b21bf714a2acba423765bb164519fc0b79c
laststylebender <ran.mahadik14@gmail.com>
2024-11-29 18:49:28 +0530

    - make checks compile time safe.

--- src/core/http/data_loader.rs
+++ src/core/http/data_loader.rs
@@ -121,87 +121,92 @@ impl Loader<DataLoaderRequest> for HttpDataLoader {
         if let Some(group_by) = &self.group_by {
             let query_name = group_by.key();
             let mut dl_requests = keys.to_vec();
-
             if cfg!(feature = "integration_test") || cfg!(test) {
-            // Sort keys to build consistent URLs only in Testing environment.
+                // Sort keys to build consistent URLs only in Testing environment.
                 dl_requests.sort_by(|a, b| a.to_request().url().cmp(b.to_request().url()));
             }
 
-            // Create base request
-            let mut base_request = batch_request_body(dl_requests[0].to_request(), &dl_requests);
+            if let Some(base_dl_request) = dl_requests.get(0).as_mut() {
+                // Create base request
+                let mut base_request =
+                    batch_request_body(base_dl_request.to_request(), &dl_requests);
 
-            // Merge query params in the request
-            for key in &dl_requests[1..] {
-                let request = key.to_request();
-                let url = request.url();
-                let pairs: Vec<_> = url
-                    .query_pairs()
-                    .filter(|(key, _)| group_by.key().eq(&key.to_string()))
-                    .collect();
-                if !pairs.is_empty() {
-                    // if pair's are empty then don't extend the query params else it ends
-                    // up appending '?' to the url.
-                    base_request.url_mut().query_pairs_mut().extend_pairs(pairs);
+                // Merge query params in the request
+                for key in dl_requests.iter().skip(1) {
+                    let request = key.to_request();
+                    let url = request.url();
+                    let pairs: Vec<_> = url
+                        .query_pairs()
+                        .filter(|(key, _)| group_by.key().eq(&key.to_string()))
+                        .collect();
+                    if !pairs.is_empty() {
+                        // if pair's are empty then don't extend the query params else it ends
+                        // up appending '?' to the url.
+                        base_request.url_mut().query_pairs_mut().extend_pairs(pairs);
+                    }
                 }
-            }
 
-            // Dispatch request
-            let res = self
-                .runtime
-                .http
-                .execute(base_request)
-                .await?
-                .to_json::<ConstValue>()?;
+                // Dispatch request
+                let res = self
+                    .runtime
+                    .http
+                    .execute(base_request)
+                    .await?
+                    .to_json::<ConstValue>()?;
 
-            // Create a response HashMap
-            #[allow(clippy::mutable_key_type)]
-            let mut hashmap = HashMap::with_capacity(dl_requests.len());
+                // Create a response HashMap
+                #[allow(clippy::mutable_key_type)]
+                let mut hashmap = HashMap::with_capacity(dl_requests.len());
 
-            // Parse the response body and group it by batchKey
-            let path = &group_by.path();
+                // Parse the response body and group it by batchKey
+                let path = &group_by.path();
 
-            // ResponseMap contains the response body grouped by the batchKey
-            let response_map = res.body.group_by(path);
+                // ResponseMap contains the response body grouped by the batchKey
+                let response_map = res.body.group_by(path);
 
-            // depending on graphql type, it will extract the data out of the response.
-            let data_extractor = if self.is_list {
-                get_body_value_list
+                // depending on graphql type, it will extract the data out of the response.
+                let data_extractor = if self.is_list {
+                    get_body_value_list
+                } else {
+                    get_body_value_single
+                };
+
+                // For each request and insert its corresponding value
+                if base_dl_request.method() == reqwest::Method::GET {
+                    for dl_req in dl_requests.iter() {
+                        let url = dl_req.url();
+                        let query_set: HashMap<_, _> = url.query_pairs().collect();
+                        let id = query_set.get(query_name).ok_or(anyhow::anyhow!(
+                            "Unable to find key {} in query params",
+                            query_name
+                        ))?;
+
+                        // Clone the response and set the body
+                        let body = data_extractor(&response_map, id);
+                        let res = res.clone().body(body);
+
+                        hashmap.insert(dl_req.clone(), res);
+                    }
+                } else {
+                    let path = group_by.body_path();
+                    for dl_req in dl_requests.into_iter() {
+                        // retrive the key from body
+                        let request_body = dl_req.body_value().ok_or(anyhow::anyhow!(
+                            "Unable to find body in request {}",
+                            dl_req.url().as_str()
+                        ))?;
+                        let extracted_value =
+                            data_extractor(&response_map, &get_key(request_body, path)?);
+                        let res = res.clone().body(extracted_value);
+                        hashmap.insert(dl_req.clone(), res);
+                    }
+                }
+
+                Ok(hashmap)
             } else {
-                get_body_value_single
-            };
-
-            // For each request and insert its corresponding value
-            if dl_requests[0].method() == reqwest::Method::GET {
-                for dl_req in dl_requests.iter() {
-                    let url = dl_req.url();
-                    let query_set: HashMap<_, _> = url.query_pairs().collect();
-                    let id = query_set.get(query_name).ok_or(anyhow::anyhow!(
-                        "Unable to find key {} in query params",
-                        query_name
-                    ))?;
-
-                    // Clone the response and set the body
-                    let body = data_extractor(&response_map, id);
-                    let res = res.clone().body(body);
-
-                    hashmap.insert(dl_req.clone(), res);
-                }
-            } else {
-                let path = group_by.body_path();
-                for dl_req in dl_requests.into_iter() {
-                    // retrive the key from body
-                    let request_body = dl_req.body_value().ok_or(anyhow::anyhow!(
-                        "Unable to find body in request {}",
-                        dl_req.url().as_str()
-                    ))?;
-                    let extracted_value =
-                        data_extractor(&response_map, &get_key(request_body, path)?);
-                    let res = res.clone().body(extracted_value);
-                    hashmap.insert(dl_req.clone(), res);
-                }
+                let error_message = "This is definitely a bug in http data loaders, please report it to the maintainers.";
+                Err(anyhow::anyhow!(error_message).into())
             }
-
-            Ok(hashmap)
         } else {
             let results = keys.iter().map(|key| async {
                 let result = self.runtime.http.execute(key.to_request()).await;
@@ -211,7 +216,7 @@ impl Loader<DataLoaderRequest> for HttpDataLoader {
             let results = join_all(results).await;
 
             #[allow(clippy::mutable_key_type)]
-            let mut hashmap = HashMap::new();
+            let mut hashmap = HashMap::with_capacity(results.len());
             for (key, value) in results {
                 hashmap.insert(key, value?.to_json()?);
             }
